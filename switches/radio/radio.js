b22ba51ed8ac153adb7a0b3d7f4459bcb4619c07
tdeekens <nerd@tdeekens.name>
2017-11-28 09:33:27 +0100

    feat(uikit/radio): glue together components

--- switches/radio/radio.js
+++ switches/radio/radio.js
@@ -5,50 +5,63 @@ import Spacings from '../../materials/spacings';
 import Text from '../../typography/text';
 import styles from './radio.mod.css';
 
-const Option = props => (
-  <div className={styles.optionsWrapper}>
-    <label>
-      <Spacings.Inline>
-        <input
-          className={classnames(styles.inputWrapper, {
-            [styles.inputDisabled]: props.isDisabled,
-            [styles.inputChecked]: props.isChecked,
-          })}
-          name={props.name}
-          onClick={props.onClick}
-          disabled={props.isDisabled}
-          checked={props.isChecked}
-          type="radio"
-        />
-        <Text.Detail>{props.children}</Text.Detail>
-      </Spacings.Inline>
-    </label>
-  </div>
-);
-Option.displayName = 'RadioOption';
-Option.propTypes = {
-  // Defaulted
-  isDisabled: PropTypes.booleean,
-  isChecked: PropTypes.booleean,
-  // Injected through as compond component
-  // not required as `createElement` is used.
-  name: PropTypes.string,
-  onClick: PropTypes.func,
-  children: PropTypes.node,
-};
-Option.defaultProps = {
-  isChecked: false,
-  isDisabled: false,
-};
+class Option extends React.PureComponent {
+  static displayName = 'RadioOption';
+  static propTypes = {
+    // Defaulted
+    isDisabled: PropTypes.booleean,
+    isChecked: PropTypes.booleean,
+    // Injected through as compond component
+    // not required as `createElement` is used.
+    name: PropTypes.string,
+    onClick: PropTypes.func,
+    value: PropTypes.oneOf(PropTypes.string, PropTypes.object).isRequired,
+    children: PropTypes.node,
+  };
+  static defaultProps = {
+    isChecked: false,
+    isDisabled: false,
+  };
+
+  handleChange = () => this.props.onClick(this.props.value);
+
+  render() {
+    return (
+      <div className={styles.optionsWrapper}>
+        <label>
+          <Spacings.Inline>
+            <input
+              className={classnames(styles.inputWrapper, {
+                [styles.inputDisabled]: this.props.isDisabled,
+                [styles.inputChecked]: this.props.isChecked,
+              })}
+              name={this.props.name}
+              value={this.props.value}
+              onClick={this.handleChange}
+              disabled={this.props.isDisabled}
+              checked={this.props.isChecked}
+              type="radio"
+            />
+            <Text.Detail>{this.props.children}</Text.Detail>
+          </Spacings.Inline>
+        </label>
+      </div>
+    );
+  }
+}
 
 class Group extends React.PureComponent {
   static displayName = 'RadioGroup';
   static propTypes = {
     wrapperClassname: PropTypes.string,
     selectedValue: PropTypes.string,
+    direction: PropTypes.oneOf(['stack', 'inline']),
     onChange: PropTypes.func.isRequired,
     name: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
+  };
+  static defaultProps = {
+    direction: 'stack',
   };
 
   state = {
@@ -57,16 +70,17 @@ class Group extends React.PureComponent {
 
   componentWillReceiveProps(nextProps) {
     if (this.props.selectedValue !== nextProps.selectedValue)
-      this.setState({
-        selectedValue: nextProps.selectedValue,
-      });
+      this.handleChange(nextProps.selectedValue);
   }
 
-  handleChange = event => {
-    console.log(event);
-  };
+  handleChange = nextValue =>
+    this.setState({
+      selectedValue: nextValue,
+    });
 
   render() {
+    const DirectionWrapper =
+      this.props.direction === 'stack' ? Spacings.Stack : Spacings.Inline;
     return (
       <div
         className={classnames(
@@ -74,18 +88,21 @@ class Group extends React.PureComponent {
           styles.wrapperClassname
         )}
       >
-        {React.Children.map(this.props.children, child => {
-          // NOTE:
-          //    Allowing to intersperse other elements
-          //    than `Option`.
-          if (child.type === Option)
-            return React.cloneElement(child, {
-              isActive: this.state.selectedValue === child.props.value,
-              name: this.props.name,
-              onClick: this.handleChange,
-            });
-          return child;
-        })}
+        <DirectionWrapper>
+          {React.Children.map(this.props.children, child => {
+            // NOTE:
+            //    Allowing to intersperse other elements
+            //    than `Option`.
+            if (child.type === Option)
+              return React.cloneElement(child, {
+                key: child.props.value,
+                isChecked: this.state.selectedValue === child.props.value,
+                name: this.props.name,
+                onClick: this.handleChange,
+              });
+            return child;
+          })}
+        </DirectionWrapper>
       </div>
     );
   }
--- switches/radio/radio.story.js
+++ switches/radio/radio.story.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
 import withReadme from 'storybook-readme/with-readme';
 import Section from '../../.storybook/decorators/section';
 import Readme from './README.md';
@@ -12,22 +12,28 @@ storiesOf('Switches', module)
   .addDecorator(withReadme(Readme))
   .add('Radio', () => (
     <Section>
-      <Radio.Group>
+      <Radio.Group
+        name="uikit-group"
+        direction={select('direction', ['stack', 'inline'], 'stack')}
+      >
         <Radio.Option
           isDisabled={boolean('isFirstOptionDisabled', false)}
           onClick={action('onClick first option')}
+          value="foo-value"
         >
           {text('Label of primary action', 'Primary option')}
         </Radio.Option>
         <Radio.Option
-          isDisabled={boolean('isSecondOptionDisabled', true)}
+          isDisabled={boolean('isSecondOptionDisabled', false)}
           onClick={action('onClick second option')}
+          value={{ value: 'bar-value', label: 'bar-label' }}
         >
           {text('Label of second action', 'Second option')}
         </Radio.Option>
         <Radio.Option
-          isDisabled={boolean('isThirdOptionDisabled', false)}
+          isDisabled={boolean('isThirdOptionDisabled', true)}
           onClick={action('onClick third option')}
+          value="baz-value"
         >
           {text('Label of third action', 'Third option')}
         </Radio.Option>
