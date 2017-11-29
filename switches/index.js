9556d26666d6b9de884adf6f394da8549315c32c
tdeekens <tobias.deekens@commercetools.de>
2017-11-29 15:39:07 +0100

    fix(uikit/checkbox): add export

--- switches/index.js
+++ switches/index.js
@@ -1,6 +1,2 @@
-/* eslint-disable import/prefer-default-export */
-
-// NOTE:
-//   This will start to contain more exports none of which is a default
-//   export (e.g. Checkbox, Radio and Slider)
 export { default as Radio } from './radio';
+export { default as Checkbox } from './checkbox';
