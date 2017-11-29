8fd0b10792854a42709c3f97c15b49c1e0ef2486
tdeekens <tobias.deekens@commercetools.de>
2017-11-29 15:29:34 +0100

    fix(uikit/switches): add export and fix webpack

---
+++ switches/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+
+// NOTE:
+//   This will start to contain more exports none of which is a default
+//   export (e.g. Checkbox, Radio and Slider)
+export { default as Radio } from './radio';
