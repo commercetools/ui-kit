96ddd82297f8c6e21edcffd4ff065ac8f60c3576
Dominik Ferber <dominik.ferber@gmail.com>
2017-12-04 16:49:29 +0400

    feat(ui-kit/text-input): initialize

---
+++ forms/text-input/text-input.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import styles from './text-input.mod.css';
+
+const TextInput = props => (
+  <input
+    name={props.name}
+    type="text"
+    value={props.value}
+    onChange={props.onChange}
+    onBlur={props.onBlur}
+    disabled={props.isDisabled}
+    placeholder={props.placeholder}
+    className={classnames(styles['input-secondary'], {
+      [styles.invalid]: props.hasError,
+      [styles.disabled]: props.isDisabled,
+    })}
+  />
+);
+
+TextInput.displayName = 'TextInput';
+
+TextInput.propTypes = {
+  name: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func,
+  isDisabled: PropTypes.bool,
+  placeholder: PropTypes.string,
+  hasError: PropTypes.bool,
+};
+
+export default TextInput;
