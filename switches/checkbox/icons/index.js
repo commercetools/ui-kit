56bf0895dd24fbb734f0a1d77bcd56900ee1758a
tdeekens <nerd@tdeekens.name>
2017-12-06 11:23:07 +0100

    refactor(uikit/switches): homogenize the icon names

--- switches/checkbox/icons/index.js
+++ switches/checkbox/icons/index.js
@@ -1,6 +1,6 @@
 // NOTE: explicit names for component names
 import CheckboxDefaultIcon from './checkbox-default.svg';
-import CheckboxHoverIcon from './checkbox-hover.svg';
+import CheckboxDefaultHoverIcon from './checkbox-hover.svg';
 import CheckboxCheckedIcon from './checkbox-checked.svg';
 import CheckboxCheckedHoverIcon from './checkbox-checked-hover.svg';
 import CheckboxDisabledIcon from './checkbox-disabled.svg';
@@ -8,7 +8,7 @@ import CheckboxCheckedDisabledIcon from './checkbox-checked-disabled.svg';
 
 export default {
   Default: CheckboxDefaultIcon,
-  Hover: CheckboxHoverIcon,
+  Hover: CheckboxDefaultHoverIcon,
   Checked: CheckboxCheckedIcon,
   CheckedHover: CheckboxCheckedHoverIcon,
   Disabled: CheckboxDisabledIcon,
