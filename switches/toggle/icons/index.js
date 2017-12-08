9812fbabf804ceed9d74684a231ce293bc640c86
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-12-08 11:12:47 +0100

    feat(toggle): add icons for Toggle button

---
+++ switches/toggle/icons/index.js
@@ -0,0 +1,27 @@
+import ToggleBackgroundDefaultIcon from './x32-toggle-normal-bkgr.svg';
+import ToggleBackgroundHoverIcon from './x32-toggle-active-hover-bkgr.svg';
+import ToggleBackgroundDisabledIcon from './x32-toggle-disable-bkgr.svg';
+import ToggleBackgroundCheckedIcon from './x32-toggled-bkgr.svg';
+import ToggleBackgroundCheckedHoverIcon from './x32-toggled-active-hover-bkgr.svg';
+import ToggleBackgroundCheckedDisabledIcon from './x32-toggled-disable-bkgr.svg';
+import ToggleButtonDefaultIcon from './x32-toggle-normal-bt.svg';
+import ToggleButtonHoverIcon from './x32-toggle-active-hover-bt.svg';
+import ToggleButtonDisabledIcon from './x32-toggle-disable-bt.svg';
+import ToggleButtonCheckedIcon from './x32-toggled-bt.svg';
+import ToggleButtonCheckedHoverIcon from './x32-toggled-active-hover-bt.svg';
+import ToggleButtonCheckedDisabledIcon from './x32-toggled-disable-bt.svg';
+
+export default {
+  BackgroundDefault: ToggleBackgroundDefaultIcon,
+  BackgroundHover: ToggleBackgroundHoverIcon,
+  BackgroundDisabled: ToggleBackgroundDisabledIcon,
+  BackgroundChecked: ToggleBackgroundCheckedIcon,
+  BackgroundCheckedHover: ToggleBackgroundCheckedHoverIcon,
+  BackgroundCheckedDisabled: ToggleBackgroundCheckedDisabledIcon,
+  ButtonDefault: ToggleButtonDefaultIcon,
+  ButtonHover: ToggleButtonHoverIcon,
+  ButtonDisabled: ToggleButtonDisabledIcon,
+  ButtonChecked: ToggleButtonCheckedIcon,
+  ButtonCheckedHover: ToggleButtonCheckedHoverIcon,
+  ButtonCheckedDisabled: ToggleButtonCheckedDisabledIcon,
+};
