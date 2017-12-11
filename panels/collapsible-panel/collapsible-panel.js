a1e4d21e0e4b7605dc4612911bcc61af0d799dc2
lufego <luis.gomes@commercetools.de>
2017-12-11 11:28:01 +0100

    fix(collpasible-panel): refactors controlled/uncontrolled logic

--- panels/collapsible-panel/collapsible-panel.js
+++ panels/collapsible-panel/collapsible-panel.js
@@ -5,123 +5,13 @@ import classnames from 'classnames';
 import { filterDataAttributes } from '@commercetools-local/utils/dataset';
 import Spacings from '../../materials/spacings';
 import Text from '../../typography/text';
-import Collapsible from '../../collapsible';
 import CollapsibleMotion from '../../collapsible-motion';
 import HeaderIcon from './header-icon';
 import styles from './collapsible-panel.mod.css';
 
-export class ControlledCollapsiblePanel extends React.PureComponent {
-  static displayName = 'ControlledCollapsiblePanel';
-
-  static propTypes = {
-    className: PropTypes.string,
-    label: PropTypes.node.isRequired,
-    description: PropTypes.string,
-    isClosed: PropTypes.bool.isRequired,
-    isSticky: PropTypes.bool.isRequired,
-    onToggle: PropTypes.func.isRequired,
-    isDisabled: PropTypes.bool,
-    children: PropTypes.node,
-    headerControls: PropTypes.node,
-    tone: PropTypes.oneOf(['urgent', 'primary']),
-    style: PropTypes.object,
-    registerContentNode: PropTypes.func,
-  };
-
-  static defaultProps = {
-    isClosed: false,
-    isSticky: false,
-    isDisabled: false,
-    tone: 'primary',
-  };
-
-  handleToggle = () => {
-    if (this.props.isDisabled) return;
-
-    this.props.onToggle();
-  };
-
-  render() {
-    // Pass only `data-*` props
-    const headerProps = filterDataAttributes(this.props);
-    return (
-      <div
-        className={classnames(this.props.className, styles.container, {
-          [styles['container-open']]: !this.props.isClosed,
-        })}
-      >
-        <div
-          className={classnames(styles['header-container'], {
-            [styles.sticky]: this.props.isSticky && !this.props.isClosed,
-          })}
-        >
-          <Spacings.InsetSquish>
-            <div
-              {...headerProps}
-              onClick={this.handleToggle}
-              className={styles.header}
-            >
-              {!this.props.isDisabled && (
-                <HeaderIcon
-                  isClosed={this.props.isClosed}
-                  tone={this.props.tone}
-                />
-              )}
-              <Text.Headline
-                elementType="h2"
-                data-track-component="CollapsiblePanel"
-                data-track-event="click"
-                data-track-label={this.props.isClosed ? 'open' : 'close'}
-              >
-                {this.props.label}
-              </Text.Headline>
-              {this.props.headerControls}
-            </div>
-          </Spacings.InsetSquish>
-        </div>
-
-        <div style={this.props.style}>
-          <div ref={this.props.registerContentNode}>
-            {this.props.description && (
-              <Spacings.Inset scale="m">
-                <Text.Detail>{this.props.description}</Text.Detail>
-              </Spacings.Inset>
-            )}
-            <Spacings.Inset scale="m">{this.props.children}</Spacings.Inset>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-export const UncontrolledCollapsiblePanel = ({
-  isDefaultClosed,
-  children,
-  ...remainingProps
-}) => (
-  <Collapsible isDefaultClosed={isDefaultClosed}>
-    {({ isOpen, toggle }) => (
-      <ControlledCollapsiblePanel
-        isClosed={!isOpen}
-        onToggle={toggle}
-        // We spread remaining props to get potential PropType
-        // warnings from `ControlledCollapsiblePanel`.
-        {...remainingProps}
-      >
-        {children}
-      </ControlledCollapsiblePanel>
-    )}
-  </Collapsible>
-);
-UncontrolledCollapsiblePanel.displayName = 'UncontrolledCollapsiblePanel';
-UncontrolledCollapsiblePanel.propTypes = {
-  isDefaultClosed: PropTypes.bool,
-  children: PropTypes.node,
-};
-
 // When `isClosed` is provided the component behaves as a controlled component,
 // otherwise it will behave like an uncontrolled component.
+
 export default class CollapsiblePanel extends React.PureComponent {
   static displayName = 'CollapsiblePanel';
   static propTypes = {
@@ -156,10 +46,9 @@ export default class CollapsiblePanel extends React.PureComponent {
       //  -> There may not be `onToggle`
       // when controlled (isClosed passed)
       //  -> `onToggle` is required
-
       const isControlledComponent = !isNil(props.isClosed);
 
-      // uncontrolled
+      //uncontrolled
       if (!isControlledComponent) {
         const hasOnToggle = !isNil(props.onToggle);
         if (hasOnToggle)
@@ -174,46 +63,76 @@ export default class CollapsiblePanel extends React.PureComponent {
         return null;
       }
 
-      return PropTypes.func.isRequired(props, propName, componentName, ...rest);
+      return PropTypes.func(props, propName, componentName, ...rest);
     },
   };
-  static defaultProps = {
-    tone: 'primary',
-  };
+
+  handleToggle(toggleFunc) {
+    if (this.props.isDisabled) return;
+    toggleFunc();
+  }
+
   render() {
-    return {}.hasOwnProperty.call(this.props, 'isClosed') ? (
-      <CollapsibleMotion>
+    const isControlledComponent = !isNil(this.props.isClosed);
+    // Pass only `data-*` props
+    const headerProps = filterDataAttributes(this.props);
+
+    return (
+      <CollapsibleMotion
+        isDefaultClosed={!isControlledComponent && this.props.isDefaultClosed}
+      >
         {({ isOpen, toggle, containerStyles, registerContentNode }) => (
-          <ControlledCollapsiblePanel
-            label={this.props.label}
-            description={this.props.description}
-            className={this.props.className}
-            isSticky={this.props.isSticky}
-            headerControls={this.props.headerControls}
-            isClosed={!isOpen}
-            onToggle={toggle}
-            isDisabled={this.props.isDisabled}
-            registerContentNode={registerContentNode}
-            style={containerStyles}
-            tone={this.props.tone}
+          <div
+            className={classnames(this.props.className, styles.container, {
+              [styles['container-open']]: isControlledComponent
+                ? !this.props.isClosed
+                : isOpen,
+            })}
           >
-            {this.props.children}
-          </ControlledCollapsiblePanel>
+            <div
+              className={classnames(styles['header-container'], {
+                [styles.sticky]: this.props.isSticky && !this.props.isClosed,
+              })}
+            >
+              <Spacings.InsetSquish>
+                <div
+                  {...headerProps}
+                  onClick={() =>
+                    this.handleToggle(
+                      isControlledComponent ? this.props.onToggle : toggle
+                    )
+                  }
+                  className={styles.header}
+                >
+                  {!this.props.isDisabled && (
+                    <HeaderIcon
+                      isClosed={
+                        isControlledComponent ? this.props.isClosed : !isOpen
+                      }
+                      tone={this.props.tone}
+                    />
+                  )}
+                  <Text.Headline elementType="h2">
+                    {this.props.label}
+                  </Text.Headline>
+                  {this.props.headerControls}
+                </div>
+              </Spacings.InsetSquish>
+            </div>
+
+            <div style={containerStyles}>
+              <div ref={registerContentNode}>
+                {this.props.description && (
+                  <Spacings.Inset scale="m">
+                    <Text.Detail>{this.props.description}</Text.Detail>
+                  </Spacings.Inset>
+                )}
+                <Spacings.Inset scale="m">{this.props.children}</Spacings.Inset>
+              </div>
+            </div>
+          </div>
         )}
       </CollapsibleMotion>
-    ) : (
-      <UncontrolledCollapsiblePanel
-        label={this.props.label}
-        description={this.props.description}
-        className={this.props.className}
-        isSticky={this.props.isSticky}
-        headerControls={this.props.headerControls}
-        isDefaultClosed={this.props.isDefaultClosed}
-        isDisabled={this.props.isDisabled}
-        tone={this.props.tone}
-      >
-        {this.props.children}
-      </UncontrolledCollapsiblePanel>
     );
   }
 }
