8147c925fb86a47cb0c52ae92b0371654f574220
lufego <luis.gomes@commercetools.de>
2017-12-11 11:22:49 +0100

    chore(collpasible-panel): improves story

--- panels/collapsible-panel/collapsible-panel.story.js
+++ panels/collapsible-panel/collapsible-panel.story.js
@@ -6,19 +6,32 @@ import Section from '../../.storybook/decorators/section';
 import CollapsiblePanel from './collapsible-panel';
 import Readme from './README.md';
 
-storiesOf('Panels', module)
+storiesOf('Panels/CollapsiblePanel', module)
   .addDecorator(withKnobs)
   .addDecorator(withReadme(Readme))
-  .add('Collapsible', () => (
+  .add('Uncontrolled', () => (
     <Section>
       <CollapsiblePanel
         label={text('Label', 'Title')}
-        description={text('Description', 'Description')}
+        description={text('Description', 'Uncontrolled component example')}
+        isSticky={boolean('isSticky', false)}
+        isDisabled={boolean('isDisabled', false)}
+        tone={select('tone', ['primary', 'urgent'], 'primary')}
+      >
+        <div>{text('Text', 'Sample text')}</div>
+      </CollapsiblePanel>
+    </Section>
+  ))
+  .add('Controlled', () => (
+    <Section>
+      <CollapsiblePanel
+        label={text('Label', 'Title')}
+        description={text('Description', 'Controlled component example')}
         isSticky={boolean('isSticky', false)}
         isDisabled={boolean('isDisabled', false)}
         tone={select('tone', ['primary', 'urgent'], 'primary')}
         isClosed={boolean('isClosed', false)}
-        // onToggle={'onToggle', () => alert('onToggle')}
+        onToggle={() => alert('togProps')}
       >
         <div>{text('Text', 'Sample text')}</div>
       </CollapsiblePanel>
