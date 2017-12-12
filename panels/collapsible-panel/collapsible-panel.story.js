b3c7ffcd713144b6ef6161097db1883de2794976
lufego <luis.gomes@commercetools.de>
2017-12-12 17:26:56 +0100

    chore(collapsible-panel): improves story wrapping controlled component with Collapsible

--- panels/collapsible-panel/collapsible-panel.story.js
+++ panels/collapsible-panel/collapsible-panel.story.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
 import withReadme from 'storybook-readme/with-readme';
 import Section from '../../.storybook/decorators/section';
+import Collapsible from '../../collapsible';
 import CollapsiblePanel from './collapsible-panel';
 import Readme from './README.md';
 
@@ -24,16 +25,20 @@ storiesOf('Panels/CollapsiblePanel', module)
   ))
   .add('Controlled', () => (
     <Section>
-      <CollapsiblePanel
-        label={text('Label', 'Title')}
-        description={text('Description', 'Controlled component example')}
-        isSticky={boolean('isSticky', false)}
-        isDisabled={boolean('isDisabled', false)}
-        tone={select('tone', ['primary', 'urgent'], 'primary')}
-        isClosed={boolean('isClosed', false)}
-        onToggle={() => alert('togProps')}
-      >
-        <div>{text('Text', 'Sample text')}</div>
-      </CollapsiblePanel>
+      <Collapsible>
+        {({ isOpen, toggle }) => (
+          <CollapsiblePanel
+            label={text('Label', 'Title')}
+            description={text('Description', 'Controlled component example')}
+            isSticky={boolean('isSticky', false)}
+            isDisabled={boolean('isDisabled', false)}
+            tone={select('tone', ['primary', 'urgent'], 'primary')}
+            isClosed={!isOpen}
+            onToggle={toggle}
+          >
+            <div>{text('Text', 'Sample text')}</div>
+          </CollapsiblePanel>
+        )}
+      </Collapsible>
     </Section>
   ));
