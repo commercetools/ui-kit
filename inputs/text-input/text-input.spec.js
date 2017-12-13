e9c5502faf7351b6e682253eb94e1aa91fd7deb4
lufego <luis.gomes@commercetools.de>
2017-12-13 16:30:47 +0100

    test(input-text): adds rendering and callbacks

---
+++ inputs/text-input/text-input.spec.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TextInput from './text-input';
+
+const createTestProps = customProps => ({
+  value: '',
+  onChange: () => jest.fn(),
+  ...customProps,
+});
+
+describe('rendering', () => {
+  describe('default', () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = createTestProps({
+        name: 'field1',
+        value: 'foo',
+      });
+      wrapper = shallow(<TextInput {...props} />);
+    });
+
+    it('should have class for a default tone', () => {
+      // console.log(wrapper.debug());
+      expect(wrapper).toHaveClassName('default');
+    });
+
+    it('input should have type text', () => {
+      expect(wrapper).toHaveProp('type', 'text');
+    });
+
+    it('input have name', () => {
+      expect(wrapper).toHaveProp('name', 'field1');
+    });
+
+    it('should have passed value', () => {
+      expect(wrapper).toHaveProp('value', 'foo');
+    });
+
+    describe('with tones', () => {
+      describe('warning', () => {
+        beforeEach(() => {
+          const props = createTestProps({
+            tone: 'warning',
+          });
+          wrapper = shallow(<TextInput {...props} />);
+        });
+
+        it('should have warning tone styles', () => {
+          expect(wrapper).toHaveClassName('warning');
+        });
+      });
+      describe('error', () => {
+        beforeEach(() => {
+          const props = createTestProps({
+            tone: 'error',
+          });
+          wrapper = shallow(<TextInput {...props} />);
+        });
+
+        it('should have error tone styles', () => {
+          expect(wrapper).toHaveClassName('error');
+        });
+      });
+      describe('info', () => {
+        beforeEach(() => {
+          const props = createTestProps({
+            tone: 'info',
+          });
+          wrapper = shallow(<TextInput {...props} />);
+        });
+
+        it('should have info tone styles', () => {
+          expect(wrapper).toHaveClassName('info');
+        });
+      });
+    });
+  });
+
+  describe('when disabled', () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = createTestProps({
+        isDisabled: true,
+      });
+      wrapper = shallow(<TextInput {...props} />);
+    });
+
+    it('should have class for the disabled state', () => {
+      expect(wrapper).toHaveClassName('disabled');
+    });
+  });
+
+  describe('when inactive', () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = createTestProps({
+        isInactive: true,
+      });
+      wrapper = shallow(<TextInput {...props} />);
+    });
+
+    it('should have class for the inactive state', () => {
+      console.log(wrapper.debug());
+      expect(wrapper).toHaveClassName('inactive');
+    });
+  });
+});
+
+describe('callbacks', () => {
+  describe('when changing value', () => {
+    let wrapper;
+    let onChangeFunc;
+    beforeEach(() => {
+      onChangeFunc = jest.fn();
+      const props = createTestProps({
+        value: 'foo',
+        onChange: onChangeFunc,
+      });
+      wrapper = shallow(<TextInput {...props} />);
+      wrapper.simulate('change', { target: { value: 'bar' } });
+    });
+
+    it('should call onChange', () => {
+      expect(onChangeFunc).toHaveBeenCalled();
+    });
+  });
+  describe('when clicking outside field', () => {
+    let wrapper;
+    let onBlurFun;
+    beforeEach(() => {
+      onBlurFun = jest.fn();
+      const props = createTestProps({
+        value: 'foo',
+        onBlur: onBlurFun,
+      });
+      wrapper = shallow(<TextInput {...props} />);
+      wrapper.simulate('blur');
+    });
+
+    it('should call onBlur', () => {
+      expect(onBlurFun).toHaveBeenCalled();
+    });
+  });
+});
