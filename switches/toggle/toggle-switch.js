e8b0baf16b31e8286d5993da8a9bd9a149cf3c7f
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-12-14 10:48:43 +0100

    fix(ui-kit/toggle): fix state handling for Toggle button

--- switches/toggle/toggle-switch.js
+++ switches/toggle/toggle-switch.js
@@ -4,55 +4,64 @@ import Icons from './icons';
 import styles from './toggle.mod.css';
 
 const ToggleButton = props => {
-  if (props.isChecked && props.isDisabled)
-    return (
-      <Icons.ButtonCheckedDisabled className={styles.toggleButtonChecked} />
-    );
-  else if (props.isChecked && props.isMouseOver)
-    return (
-      <Icons.ButtonCheckedHover className={styles.toggleButtonCheckedHover} />
-    );
-  else if (props.isChecked)
-    return <Icons.ButtonChecked className={styles.toggleButtonChecked} />;
-  else if (props.isDisabled)
-    return <Icons.ButtonDisabled className={styles.toggleButton} />;
-  else if (props.isMouseOver)
-    return <Icons.ButtonHover className={styles.toggleButtonHover} />;
+  if (props.isChecked) {
+    if (props.isDisabled) {
+      return <Icons.ButtonCheckedDisabled {...props} />;
+    } else if (props.isMouseOver) {
+      return <Icons.ButtonCheckedHover {...props} />;
+    }
+    return <Icons.ButtonChecked {...props} />;
+  }
 
-  return <Icons.ButtonDefault className={styles.toggleButton} />;
+  if (props.isDisabled) {
+    return <Icons.ButtonDisabled {...props} />;
+  } else if (props.isMouseOver) {
+    return <Icons.ButtonHover {...props} />;
+  }
+  return <Icons.ButtonDefault {...props} />;
 };
 
 ToggleButton.displayName = 'ToggleButton';
 ToggleButton.propTypes = {
+  size: PropTypes.string.isRequired,
   isChecked: PropTypes.bool.isRequired,
   isDisabled: PropTypes.bool.isRequired,
   isMouseOver: PropTypes.bool.isRequired,
 };
 
 const ToggleBar = props => {
-  if (props.isChecked)
-    if (props.isDisabled)
-      return <Icons.BackgroundCheckedDisabled className={styles.toggleBar} />;
-    else if (props.isMouseOver)
-      return <Icons.BackgroundCheckedHover className={styles.toggleBar} />;
-    else return <Icons.BackgroundChecked className={styles.toggleBar} />;
-  else if (props.isDisabled)
-    return <Icons.BackgroundDisabled className={styles.toggleBar} />;
-  else if (props.isMouseOver)
-    return <Icons.BackgroundHover className={styles.toggleBar} />;
-
-  return <Icons.BackgroundDefault className={styles.toggleBar} />;
+  if (props.isChecked) {
+    if (props.isDisabled) {
+      return (
+        <Icons.BackgroundCheckedDisabled
+          className={styles[`bar-${props.size}`]}
+        />
+      );
+    } else if (props.isMouseOver) {
+      return (
+        <Icons.BackgroundCheckedHover className={styles[`bar-${props.size}`]} />
+      );
+    }
+    return <Icons.BackgroundChecked className={styles[`bar-${props.size}`]} />;
+  }
+  if (props.isDisabled) {
+    return <Icons.BackgroundDisabled className={styles[`bar-${props.size}`]} />;
+  } else if (props.isMouseOver) {
+    return <Icons.BackgroundHover className={styles[`bar-${props.size}`]} />;
+  }
+  return <Icons.BackgroundDefault className={styles[`bar-${props.size}`]} />;
 };
 
 ToggleBar.displayName = 'ToggleBar';
 ToggleBar.propTypes = {
+  size: PropTypes.string.isRequired,
   isChecked: PropTypes.bool.isRequired,
   isDisabled: PropTypes.bool.isRequired,
   isMouseOver: PropTypes.bool.isRequired,
 };
 
 const ToggleSwitch = props => (
-  <div className={styles.toggleSwitch}>
+  <div className={styles[`container-${props.size}`]}>
     <ToggleButton {...props} />
     <ToggleBar {...props} />
   </div>
@@ -60,6 +69,7 @@ const ToggleSwitch = props => (
 
 ToggleSwitch.displayName = 'ToggleSwitch';
 ToggleSwitch.propTypes = {
+  size: PropTypes.string.isRequired,
   isChecked: PropTypes.bool.isRequired,
   isDisabled: PropTypes.bool.isRequired,
   isMouseOver: PropTypes.bool.isRequired,
