51a8810054896737c4398ea49a745015d74c9764
lufego <luis.gomes@commercetools.de>
2017-12-15 15:04:15 +0100

    fix(inputs-numeric): implements wrapper and improves isInactive styles

--- inputs/text-input.story.js
+++ inputs/text-input.story.js
@@ -4,6 +4,7 @@ import { action } from '@storybook/addon-actions';
 import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
 import withReadme from 'storybook-readme/with-readme';
 import Section from '../.storybook/decorators/section';
+import ControlledComponentWrapper from '../.storybook/wrappers/controlled-component-wrapper';
 import TextInputReadme from './text-input/README.md';
 import NumericInputReadme from './numeric-input/README.md';
 import TextInput from './text-input';
@@ -22,22 +23,29 @@ storiesOf('Forms/Inputs', module)
         isInactive={boolean('isInactive', false)}
         isReadOnly={boolean('isReadOnly', false)}
         placeholder={text('placeholder', 'Placeholder')}
-        tone={select('tone', ['none', 'warning', 'error', 'info'])}
+        tone={select('tone', ['plain', 'warning', 'error', 'info'])}
       />
     </Section>
   ))
   .addDecorator(withReadme(NumericInputReadme))
   .add('NumericInput', () => (
     <Section>
-      <NumericInput
-        name={text('name', '')}
-        value={text('value', '')}
-        onChange={action('onChange')}
-        isDisabled={boolean('isDisabled', false)}
-        isReadOnly={boolean('isReadOnly', false)}
-        isInactive={boolean('isInactive', false)}
-        placeholder={text('placeholder', 'Placeholder')}
-        tone={select('tone', ['none', 'warning', 'error', 'info'])}
-      />
+      <ControlledComponentWrapper>
+        {({ value, onChange }) => (
+          <NumericInput
+            name={text('name', '')}
+            value={value}
+            minValue={text('minValue', '')}
+            maxValue={text('maxValue', '')}
+            stepValue={text('stepValue', '')}
+            onChange={onChange}
+            isDisabled={boolean('isDisabled', false)}
+            isReadOnly={boolean('isReadOnly', false)}
+            isInactive={boolean('isInactive', false)}
+            placeholder={text('placeholder', 'Placeholder')}
+            tone={select('tone', ['plain', 'warning', 'error', 'info'])}
+          />
+        )}
+      </ControlledComponentWrapper>
     </Section>
   ));
