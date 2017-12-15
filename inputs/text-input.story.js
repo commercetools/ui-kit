862544a528a16f8baaa1652c7636962c177fbad2
lufego <luis.gomes@commercetools.de>
2017-12-15 17:38:30 +0100

    feat(inputs): uses react-value instead of controlled component

--- inputs/text-input.story.js
+++ inputs/text-input.story.js
@@ -3,8 +3,8 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
 import withReadme from 'storybook-readme/with-readme';
+import { Value } from 'react-value';
 import Section from '../.storybook/decorators/section';
-import ControlledComponentWrapper from '../.storybook/wrappers/controlled-component-wrapper';
 import TextInputReadme from './text-input/README.md';
 import NumericInputReadme from './numeric-input/README.md';
 import TextInput from './text-input';
@@ -29,23 +29,22 @@ storiesOf('Forms/Inputs', module)
   ))
   .addDecorator(withReadme(NumericInputReadme))
   .add('NumericInput', () => (
-    <Section>
-      <ControlledComponentWrapper>
-        {({ value, onChange }) => (
-          <NumericInput
-            name={text('name', '')}
-            value={value}
-            minValue={text('minValue', '')}
-            maxValue={text('maxValue', '')}
-            stepValue={text('stepValue', '')}
-            onChange={onChange}
-            isDisabled={boolean('isDisabled', false)}
-            isReadOnly={boolean('isReadOnly', false)}
-            isInactive={boolean('isInactive', false)}
-            placeholder={text('placeholder', 'Placeholder')}
-            tone={select('tone', ['plain', 'warning', 'error', 'info'])}
-          />
-        )}
-      </ControlledComponentWrapper>
-    </Section>
+    <Value
+      defaultValue={undefined}
+      render={(value, onChange) => (
+        <NumericInput
+          name={text('name', '')}
+          value={value}
+          onChange={input => onChange(input.target.value)}
+          minValue={text('minValue', '')}
+          maxValue={text('maxValue', '')}
+          stepValue={text('stepValue', '')}
+          isDisabled={boolean('isDisabled', false)}
+          isReadOnly={boolean('isReadOnly', false)}
+          isInactive={boolean('isInactive', false)}
+          placeholder={text('placeholder', 'Placeholder')}
+          tone={select('tone', ['plain', 'warning', 'error', 'info'])}
+        />
+      )}
+    />
   ));
