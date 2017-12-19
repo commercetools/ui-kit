66b76b74906ab1dd6dd712e6a391269ebab4a771
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2017-12-19 16:48:02 +0100

    feat(ui-kit/toggle): define base SVG with no style
    These icons are needed to implement the Toggle using a single SVG each
    for the bar and button. The SVG will be styled based on the different
    states of the Toggle.

--- switches/toggle/icons/index.js
+++ switches/toggle/icons/index.js
@@ -1,28 +1,7 @@
-import withSizeProp from '../with-size-prop';
-import ToggleBackgroundDefaultIcon from './toggle-normal-bkgr.svg';
-import ToggleBackgroundHoverIcon from './toggle-active-hover-bkgr.svg';
-import ToggleBackgroundDisabledIcon from './toggle-disable-bkgr.svg';
-import ToggleBackgroundCheckedIcon from './toggled-bkgr.svg';
-import ToggleBackgroundCheckedHoverIcon from './toggled-active-hover-bkgr.svg';
-import ToggleBackgroundCheckedDisabledIcon from './toggled-disable-bkgr.svg';
-import ToggleButtonDefaultIcon from './toggle-normal-bt.svg';
-import ToggleButtonHoverIcon from './toggle-active-hover-bt.svg';
-import ToggleButtonDisabledIcon from './toggle-disable-bt.svg';
-import ToggleButtonCheckedIcon from './toggled-bt.svg';
-import ToggleButtonCheckedHoverIcon from './toggled-active-hover-bt.svg';
-import ToggleButtonCheckedDisabledIcon from './toggled-disable-bt.svg';
+import ToggleButtonIcon from './toggle-button-default.svg';
+import ToggleBarIcon from './toggle-bar-default.svg';
 
 export default {
-  BackgroundDefault: ToggleBackgroundDefaultIcon,
-  BackgroundHover: ToggleBackgroundHoverIcon,
-  BackgroundDisabled: ToggleBackgroundDisabledIcon,
-  BackgroundChecked: ToggleBackgroundCheckedIcon,
-  BackgroundCheckedHover: ToggleBackgroundCheckedHoverIcon,
-  BackgroundCheckedDisabled: ToggleBackgroundCheckedDisabledIcon,
-  ButtonDefault: withSizeProp(ToggleButtonDefaultIcon),
-  ButtonHover: withSizeProp(ToggleButtonHoverIcon),
-  ButtonDisabled: withSizeProp(ToggleButtonDisabledIcon),
-  ButtonChecked: withSizeProp(ToggleButtonCheckedIcon),
-  ButtonCheckedHover: withSizeProp(ToggleButtonCheckedHoverIcon),
-  ButtonCheckedDisabled: withSizeProp(ToggleButtonCheckedDisabledIcon),
+  ButtonDefault: ToggleButtonIcon,
+  BarDefault: ToggleBarIcon,
 };
