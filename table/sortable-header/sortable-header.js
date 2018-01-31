1548f75e44cf944e9585c3c16b24752066077bbf
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-01-31 17:46:52 +0100

    feat(ppl/sorting): add correct arrow for sorting to table-header

--- table/sortable-header/sortable-header.js
+++ table/sortable-header/sortable-header.js
@@ -1,28 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import Text from '../../typography/text';
 import withMouseOverState from '../../hocs/with-mouse-over-state';
 import { AngleDownIcon, AngleUpIcon } from '../../icons';
 import styles from './sortable-header.mod.css';
 
+/*
+Logic of arrow indicating sort order is slightly complex:
+
++----------+-------------+---------------+------------+----------------+
+| isActive | isMouseOver | sortDirection | arrowColor | arrowDirection |
++----------+-------------+---------------+------------+----------------+
+|     0    |      0      |      n/a      |         *INVISIBLE*         |
++----------+-------------+---------------+-----------------------------+
+|     0    |      1      |      n/a      |    grey    |      down      |
++----------+-------------+---------------+------------+----------------+
+|     1    |      0      |      ASC      |    black   |      down      |
++----------+-------------+---------------+------------+----------------+
+|     1    |      0      |      DESC     |    black   |       up       |
++----------+-------------+---------------+------------+----------------+
+|     1    |      1      |      ASC      |    grey    |       up       |
++----------+-------------+---------------+------------+----------------+
+|     1    |      1      |      DESC     |    grey    |      down      |
++----------+-------------+---------------+------------+----------------+
+
+*/
+
 const SortableHeader = props => {
   const isActive = props.sortBy === props.columnKey;
-  const theme = !isActive && !props.isMouseOver ? 'grey' : 'black';
+  const theme = props.isMouseOver ? 'grey' : 'black';
+  const isArrowDown =
+    (!isActive && props.isMouseOver) ||
+    (isActive && !props.isMouseOver && props.sortDirection === 'ASC') ||
+    (isActive && props.isMouseOver && props.sortDirection === 'DESC');
 
   return (
     <div
       onMouseOver={props.handleMouseOver}
       onMouseOut={props.handleMouseOut}
-      className={styles.container}
+      className={classnames(styles.container, {
+        [styles.active]: isActive,
+      })}
     >
       <div className={styles.label}>
         <Text.Body isBold={isActive}>{props.children}</Text.Body>
       </div>
-      {props.sortDirection === 'ASC' || !isActive ? (
-        <AngleDownIcon size="small" theme={theme} />
-      ) : (
-        <AngleUpIcon size="small" theme={theme} />
-      )}
+      <span className={styles.arrow}>
+        {isArrowDown ? (
+          <AngleDownIcon size="small" theme={theme} />
+        ) : (
+          <AngleUpIcon className={styles.arrow} size="small" theme={theme} />
+        )}
+      </span>
     </div>
   );
 };
