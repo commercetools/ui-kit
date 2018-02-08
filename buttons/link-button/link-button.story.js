3fd62b8d967f7a729a28ea3991d6c7a64f093412
philippspo <philipp.sporrer@commercetools.de>
2018-02-08 11:28:50 +0100

    fix(ui-kit/link-button/story): fix story by manually rendering router

--- buttons/link-button/link-button.story.js
+++ buttons/link-button/link-button.story.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
-import StoryRouter from 'storybook-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 import withReadme from 'storybook-readme/with-readme';
 import Section from '../../.storybook/decorators/section';
 import * as icons from '../../icons';
@@ -13,16 +13,17 @@ const iconNames = Object.keys(icons);
 storiesOf('Buttons', module)
   .addDecorator(withKnobs)
   .addDecorator(withReadme(Readme))
-  .addDecorator(StoryRouter())
   .add('LinkButton', () => (
-    <Section>
-      <LinkButton
-        to={text('to', '/foo/bar')}
-        label={text('label', 'Accessibility text')}
-        iconLeft={React.createElement(
-          icons[select('icon', iconNames, iconNames[0])]
-        )}
-        isDisabled={boolean('isDisabled', false)}
-      />
-    </Section>
+    <Router>
+      <Section>
+        <LinkButton
+          to={text('to', '/foo/bar')}
+          label={text('label', 'Accessibility text')}
+          iconLeft={React.createElement(
+            icons[select('icon', iconNames, iconNames[0])]
+          )}
+          isDisabled={boolean('isDisabled', false)}
+        />
+      </Section>
+    </Router>
   ));
