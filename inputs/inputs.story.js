3c9a807517725b1df7129e43469b7cf77d9ca2ef
Dominik Ferber <dominik.ferber@gmail.com>
2018-03-20 11:20:26 +0100

    refactor(ui-kit/LocalizedTextInput): move story to folder

--- inputs/inputs.story.js
+++ inputs/inputs.story.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
 import withReadme from 'storybook-readme/with-readme';
 import { Value } from 'react-value';
 import Section from '../.storybook/decorators/section';
 import TextInputReadme from './text-input/README.md';
 import NumberInputReadme from './number-input/README.md';
-import LocalizedTextInputReadme from './localized-text-input/README.md';
 import TextInput from './text-input';
 import NumberInput from './number-input';
-import LocalizedTextInput from './localized-text-input';
 
 storiesOf('Forms/Inputs', module)
   .addDecorator(withKnobs)
@@ -53,52 +51,4 @@ storiesOf('Forms/Inputs', module)
         )}
       />
     </Section>
-  ))
-  .addDecorator(withReadme(LocalizedTextInputReadme))
-  .add('LocalizedTextInput', () => {
-    const isDefaultExpanded = boolean('isDefaultExpanded', false);
-    // We need to force the component to rerender in case a default value
-    // is changed. Otherwise the knob would have no effect.
-    // We do this by changing the key.
-    const key = isDefaultExpanded ? 'yes' : 'no';
-    return (
-      <Section>
-        <LocalizedTextInput
-          key={key}
-          id={text('id', undefined)}
-          name={text('name', undefined)}
-          value={{ en: text('value(en)', ''), de: text('value(de)', '') }}
-          onChange={action('onChange')}
-          selectedLanguage={select('selectedLanguage', ['en', 'de'], 'en')}
-          onBlur={action('onBlur')}
-          onFocus={action('onFocus')}
-          hideExpansionControls={boolean('hideExpansionControls', false)}
-          isDefaultExpanded={
-            isDefaultExpanded
-              ? true
-              : // we need to set undefined instead of false to avoid prop-type
-                // warnings in case hideExpansionControls is true
-                undefined
-          }
-          isAutofocussed={boolean('isAutofocussed', false)}
-          isDisabled={boolean('isDisabled', false)}
-          isReadOnly={boolean('isReadOnly', false)}
-          placeholder={{
-            en: text('placeholder(en)', ''),
-            de: text('placeholder(de)', ''),
-          }}
-          horizontalConstraint={select(
-            'horizontalConstraint',
-            ['xs', 's', 'm', 'l', 'xl', 'scale'],
-            'm'
-          )}
-          error={
-            boolean('has missing value error', false)
-              ? { missing: true }
-              : undefined
-          }
-          data-test="foo"
-        />
-      </Section>
-    );
-  });
+  ));
---
+++ inputs/localized-text-input/localized-text-input.story.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
+import withReadme from 'storybook-readme/with-readme';
+import Section from '../../.storybook/decorators/section';
+import LocalizedTextInputReadme from './README.md';
+import LocalizedTextInput from './localized-text-input';
+
+storiesOf('Forms/Inputs', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withReadme(LocalizedTextInputReadme))
+  .add('LocalizedTextInput', () => {
+    const isDefaultExpanded = boolean('isDefaultExpanded', false);
+    // We need to force the component to rerender in case a default value
+    // is changed. Otherwise the knob would have no effect.
+    // We do this by changing the key.
+    const key = isDefaultExpanded ? 'yes' : 'no';
+    return (
+      <Section>
+        <LocalizedTextInput
+          key={key}
+          id={text('id', undefined)}
+          name={text('name', undefined)}
+          value={{ en: text('value(en)', ''), de: text('value(de)', '') }}
+          onChange={action('onChange')}
+          selectedLanguage={select('selectedLanguage', ['en', 'de'], 'en')}
+          onBlur={action('onBlur')}
+          onFocus={action('onFocus')}
+          hideExpansionControls={boolean('hideExpansionControls', false)}
+          isDefaultExpanded={
+            isDefaultExpanded
+              ? true
+              : // we need to set undefined instead of false to avoid prop-type
+                // warnings in case hideExpansionControls is true
+                undefined
+          }
+          isAutofocussed={boolean('isAutofocussed', false)}
+          isDisabled={boolean('isDisabled', false)}
+          isReadOnly={boolean('isReadOnly', false)}
+          placeholder={{
+            en: text('placeholder(en)', ''),
+            de: text('placeholder(de)', ''),
+          }}
+          horizontalConstraint={select(
+            'horizontalConstraint',
+            ['xs', 's', 'm', 'l', 'xl', 'scale'],
+            'm'
+          )}
+          error={
+            boolean('has missing value error', false)
+              ? { missing: true }
+              : undefined
+          }
+          data-test="foo"
+        />
+      </Section>
+    );
+  });
