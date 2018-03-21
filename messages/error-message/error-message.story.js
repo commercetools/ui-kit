c47404cfeb7b30b40d98312b6333dcbcf4cfe8d0
Dominik Ferber <dominik.ferber@gmail.com>
2018-03-21 13:44:09 +0100

    feat(ui-kit/ErrorMessage): add story

---
+++ messages/error-message/error-message.story.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import withReadme from 'storybook-readme/with-readme';
+import Section from '../../.storybook/decorators/section';
+import Readme from './README.md';
+import ErrorMessage from './error-message';
+
+storiesOf('Messages', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withReadme(Readme))
+  .add('ErrorMessage', () => (
+    <Section>
+      <ErrorMessage>{text('children', 'Required text missing')}</ErrorMessage>
+    </Section>
+  ));
