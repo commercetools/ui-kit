c90af027fd57705ddab657f9e8f74c034ddbb53d
Dominik Ferber <dominik.ferber@gmail.com>
2018-03-21 19:56:19 +0100

    test(ui-kit/Text): add inline tests

--- typography/text/text.spec.js
+++ typography/text/text.spec.js
@@ -75,76 +75,177 @@ describe('<Subheadline>', () => {
 });
 
 describe('<Body>', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<Text.Body>{'Body'}</Text.Body>);
-  });
-  it('should render element tag p', () => {
-    expect(wrapper.type()).toBe('p');
-  });
-  it('should not have "bold" class', () => {
-    expect(wrapper).not.toContainClass('bold');
-  });
-  it('should render given text', () => {
-    expect(wrapper.text()).toMatch('Body');
-  });
-  describe('with bold text', () => {
+  describe('when used as block text', () => {
+    let wrapper;
     beforeEach(() => {
-      wrapper = shallow(<Text.Body isBold={true}>{'Body'}</Text.Body>);
+      wrapper = shallow(<Text.Body>{'Body'}</Text.Body>);
     });
     it('should render element tag p', () => {
       expect(wrapper.type()).toBe('p');
     });
-    it('should have "bold" class', () => {
-      expect(wrapper).toContainClass('bold');
+    it('should not have "bold" class', () => {
+      expect(wrapper).not.toContainClass('bold');
     });
     it('should render given text', () => {
       expect(wrapper.text()).toMatch('Body');
+    });
+    describe('with bold text', () => {
+      beforeEach(() => {
+        wrapper = shallow(<Text.Body isBold={true}>{'Body'}</Text.Body>);
+      });
+      it('should render element tag p', () => {
+        expect(wrapper.type()).toBe('p');
+      });
+      it('should have "bold" class', () => {
+        expect(wrapper).toContainClass('bold');
+      });
+      it('should render given text', () => {
+        expect(wrapper.text()).toMatch('Body');
+      });
+    });
+  });
+
+  describe('when used as inline text', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = shallow(<Text.Body isInline={true}>{'Body'}</Text.Body>);
+    });
+    it('should render element tag span', () => {
+      expect(wrapper.type()).toBe('span');
+    });
+    it('should not have "bold" class', () => {
+      expect(wrapper).not.toContainClass('bold');
+    });
+    it('should render given text', () => {
+      expect(wrapper.text()).toMatch('Body');
+    });
+    describe('with bold text', () => {
+      beforeEach(() => {
+        wrapper = shallow(
+          <Text.Body isInline={true} isBold={true}>
+            {'Body'}
+          </Text.Body>
+        );
+      });
+      it('should render element tag span', () => {
+        expect(wrapper.type()).toBe('span');
+      });
+      it('should have "bold" class', () => {
+        expect(wrapper).toContainClass('bold');
+      });
+      it('should render given text', () => {
+        expect(wrapper.text()).toMatch('Body');
+      });
     });
   });
 });
 
 describe('<Detail>', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<Text.Detail>{'Detail'}</Text.Detail>);
-  });
-  it('should render element tag small', () => {
-    expect(wrapper.type()).toBe('small');
-  });
-  it('should not have "bold" class', () => {
-    expect(wrapper).not.toContainClass('bold');
-  });
-  it('should render given text', () => {
-    expect(wrapper.text()).toMatch('Detail');
-  });
-  describe('with bold text', () => {
+  describe('when used as block text', () => {
+    let wrapper;
     beforeEach(() => {
-      wrapper = shallow(<Text.Detail isBold={true}>{'Detail'}</Text.Detail>);
+      wrapper = shallow(<Text.Detail>{'Detail'}</Text.Detail>);
     });
     it('should render element tag small', () => {
       expect(wrapper.type()).toBe('small');
     });
-    it('should have "bold" class', () => {
-      expect(wrapper).toContainClass('bold');
+    it('should not have "bold" class', () => {
+      expect(wrapper).not.toContainClass('bold');
     });
     it('should render given text', () => {
-      expect(wrapper).toHaveText('Detail');
+      expect(wrapper.text()).toMatch('Detail');
     });
-  });
+    describe('with bold text', () => {
+      beforeEach(() => {
+        wrapper = shallow(<Text.Detail isBold={true}>{'Detail'}</Text.Detail>);
+      });
+      it('should render element tag small', () => {
+        expect(wrapper.type()).toBe('small');
+      });
+      it('should have "bold" class', () => {
+        expect(wrapper).toContainClass('bold');
+      });
+      it('should render given text', () => {
+        expect(wrapper).toHaveText('Detail');
+      });
+    });
 
-  describe('with tone', () => {
+    describe('with tone', () => {
+      beforeEach(() => {
+        wrapper = shallow(
+          <Text.Detail tone="secondary">{'Detail'}</Text.Detail>
+        );
+      });
+      it('should render element tag small', () => {
+        expect(wrapper.type()).toBe('small');
+      });
+      it('should have "bold" class', () => {
+        expect(wrapper).toContainClass('secondary');
+      });
+      it('should render given text', () => {
+        expect(wrapper).toHaveText('Detail');
+      });
+    });
+  });
+  describe('when used as inline text', () => {
+    let wrapper;
     beforeEach(() => {
-      wrapper = shallow(<Text.Detail tone="secondary">{'Detail'}</Text.Detail>);
+      wrapper = shallow(<Text.Detail isInline={true}>{'Detail'}</Text.Detail>);
     });
     it('should render element tag small', () => {
       expect(wrapper.type()).toBe('small');
     });
-    it('should have "bold" class', () => {
-      expect(wrapper).toContainClass('secondary');
+    it('should not have "bold" class', () => {
+      expect(wrapper).not.toContainClass('bold');
+    });
+    it('should have "inline" class', () => {
+      expect(wrapper).toContainClass('inline');
     });
     it('should render given text', () => {
-      expect(wrapper).toHaveText('Detail');
+      expect(wrapper.text()).toMatch('Detail');
+    });
+    describe('with bold text', () => {
+      beforeEach(() => {
+        wrapper = shallow(
+          <Text.Detail isInline={true} isBold={true}>
+            {'Detail'}
+          </Text.Detail>
+        );
+      });
+      it('should render element tag small', () => {
+        expect(wrapper.type()).toBe('small');
+      });
+      it('should have "bold" class', () => {
+        expect(wrapper).toContainClass('bold');
+      });
+      it('should have "inline" class', () => {
+        expect(wrapper).toContainClass('inline');
+      });
+      it('should render given text', () => {
+        expect(wrapper).toHaveText('Detail');
+      });
+    });
+
+    describe('with tone', () => {
+      beforeEach(() => {
+        wrapper = shallow(
+          <Text.Detail isInline={true} tone="secondary">
+            {'Detail'}
+          </Text.Detail>
+        );
+      });
+      it('should render element tag small', () => {
+        expect(wrapper.type()).toBe('small');
+      });
+      it('should have "secondary" class', () => {
+        expect(wrapper).toContainClass('secondary');
+      });
+      it('should have "inline" class', () => {
+        expect(wrapper).toContainClass('inline');
+      });
+      it('should render given text', () => {
+        expect(wrapper).toHaveText('Detail');
+      });
     });
   });
 });
