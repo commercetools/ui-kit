1362e391df420843aa59b767d51d67e8cd68ef75
lufego <luis.gomes@commercetools.de>
2018-06-13 11:17:24 +0200

    fix(colors): changes dropdown and fixes icon button

--- buttons/icon-button/icon-button.js
+++ buttons/icon-button/icon-button.js
@@ -21,7 +21,7 @@ const getStateClassNames = (isDisabled, isToggled) =>
 // Gets the color which the icon should have based on context of button's state/cursor behavior
 const getIconThemeColor = props => {
   // if button has a theme, icon should be white when hovering/clicking
-  if ((props.theme !== 'default' && props.isToggled) || props.isMouseOver) {
+  if (props.theme !== 'default' && (props.isToggled || props.isMouseOver)) {
     if (props.isDisabled) {
       return 'grey';
     }
