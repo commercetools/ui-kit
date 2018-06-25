2e6a5fbeb6dd3eeaadf56e1f186592487ad42311
lufego <luis.gomes@commercetools.de>
2018-06-25 14:32:04 +0200

    fix(text-area): adds story suggestion

--- inputs/text-area/text-area.story.js
+++ inputs/text-area/text-area.story.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { IntlProvider } from 'react-intl';
+import { Value } from 'react-value';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
 import withReadme from 'storybook-readme/with-readme';
 import Section from '../../.storybook/decorators/section';
 import Readme from './README.md';
@@ -11,26 +12,49 @@ import TextArea from './text-area';
 storiesOf('Forms/Inputs', module)
   .addDecorator(withKnobs)
   .addDecorator(withReadme(Readme))
-  .add('TextArea', () => (
-    <Section>
-      <IntlProvider
-        locale="en"
-        messages={{
-          en: {
-            'UIKit.TextArea.expand': 'Expand',
-            'UIKit.TextArea.collapse': 'Collapse',
-          },
-        }}
-      >
-        <TextArea
-          isDisabled={boolean('isDisabled', false)}
-          isReadOnly={boolean('isReadOnly', false)}
-          hasError={boolean('hasError', false)}
-          isDefaultClosed={boolean('isDefaultClosed', false)}
-          hasWarning={boolean('hasWarning', false)}
-          value={text('value', '')}
-          onChange={action('onChange')}
-        />
-      </IntlProvider>
-    </Section>
-  ));
+  .add('TextArea', () => {
+    const defaultValue = text('value', '');
+    return (
+      <Section>
+        <IntlProvider
+          locale="en"
+          messages={{
+            en: {
+              'UIKit.TextArea.expand': 'Expand',
+              'UIKit.TextArea.collapse': 'Collapse',
+            },
+          }}
+        >
+          <Value
+            key={defaultValue}
+            defaultValue={defaultValue}
+            render={(value, onChange) => (
+              <TextArea
+                name={text('name', undefined)}
+                id={text('id', undefined)}
+                onBlur={action('onBlur')}
+                onFocus={action('onFocus')}
+                isAutofocussed={boolean('isAutofocussed', false)}
+                isDefaultClosed={boolean('isDefaultClosed', false)}
+                placeholder={text('placeholder')}
+                horizontalConstraint={select(
+                  'horizontalConstraint',
+                  ['m', 'l', 'xl', 'scale'],
+                  'scale'
+                )}
+                isDisabled={boolean('isDisabled', false)}
+                isReadOnly={boolean('isReadOnly', false)}
+                hasError={boolean('hasError', false)}
+                hasWarning={boolean('hasWarning', false)}
+                value={value}
+                onChange={event => {
+                  action('onChange')(event);
+                  onChange(event.target.value);
+                }}
+              />
+            )}
+          />
+        </IntlProvider>
+      </Section>
+    );
+  });
