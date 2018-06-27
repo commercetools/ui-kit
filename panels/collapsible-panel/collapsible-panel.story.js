0655efa4cd62db3172f085de186d4431ee5eedf0
qmateub <qmateub@yahoo.es>
2018-06-27 11:37:53 +0200

    refactor(uikit): improve story for collapsible panel

--- panels/collapsible-panel/collapsible-panel.story.js
+++ panels/collapsible-panel/collapsible-panel.story.js
@@ -8,98 +8,68 @@ import CollapsiblePanel from './collapsible-panel';
 import CollapsiblePanelHeader from './collapsible-panel-header';
 import Readme from './README.md';
 
-storiesOf('Panels/Condensed CollapsiblePanel', module)
-  .addDecorator(withKnobs)
-  .addDecorator(withReadme(Readme))
-  .add('Uncontrolled', () => (
-    <Section>
-      <CollapsiblePanel
-        header={text('header', 'Header')}
-        description={text('description', 'Uncontrolled component example')}
-        isSticky={boolean('isSticky', false)}
-        isDisabled={boolean('isDisabled', false)}
-        tone={select('tone', ['primary', 'urgent'], 'primary')}
-        headerControls={text('headerControls', 'headerControl')}
-        theme={select('theme', ['dark', 'light'])}
-        condensed={true}
-        secondaryHeader={text('secondaryHeader', 'Subtitle')}
-      >
-        {text('Text', 'Sample text')}
-      </CollapsiblePanel>
-    </Section>
-  ))
-  .add('Controlled', () => (
-    <Section>
-      <Collapsible>
-        {({ isOpen, toggle }) => (
-          <CollapsiblePanel
-            header={text('header', 'Header')}
-            description={text('description', 'Controlled component example')}
-            isSticky={boolean('isSticky', false)}
-            isDisabled={boolean('isDisabled', false)}
-            tone={select('tone', ['primary', 'urgent'], 'primary')}
-            isClosed={!isOpen}
-            onToggle={toggle}
-            headerControls={text('headerControls', 'headerControl')}
-            theme={select('theme', ['dark', 'light'])}
-            condensed={true}
-            secondaryHeader={text('secondaryHeader', 'Subtitle')}
-          >
-            {text('Text', 'Sample text')}
-          </CollapsiblePanel>
-        )}
-      </Collapsible>
-    </Section>
-  ));
-
 storiesOf('Panels/CollapsiblePanel', module)
   .addDecorator(withKnobs)
   .addDecorator(withReadme(Readme))
-  .add('Uncontrolled', () => (
-    <Section>
-      <CollapsiblePanel
-        header={
-          <CollapsiblePanelHeader>
-            {text('header', 'Header')}
-          </CollapsiblePanelHeader>
-        }
-        description={text('description', 'Uncontrolled component example')}
-        isSticky={boolean('isSticky', false)}
-        isDisabled={boolean('isDisabled', false)}
-        tone={select('tone', ['primary', 'urgent'], 'primary')}
-        headerControls={text('headerControls', 'headerControl')}
-        theme={select('theme', ['dark', 'light'])}
-        condensed={false}
-        secondaryHeader={text('secondaryHeader', 'Subtitle')}
-      >
-        {text('Text', 'Sample text')}
-      </CollapsiblePanel>
-    </Section>
-  ))
+  .add('Uncontrolled', () => {
+    const condensed = boolean('condensed', false);
+    return (
+      <Section>
+        <CollapsiblePanel
+          header={
+            condensed ? (
+              text('header', 'Header')
+            ) : (
+              <CollapsiblePanelHeader>
+                {text('header', 'Header')}
+              </CollapsiblePanelHeader>
+            )
+          }
+          description={text('description', 'Uncontrolled component example')}
+          isSticky={boolean('isSticky', false)}
+          isDisabled={boolean('isDisabled', false)}
+          tone={select('tone', ['primary', 'urgent'], 'primary')}
+          headerControls={text('headerControls', 'headerControl')}
+          theme={select('theme', ['dark', 'light'])}
+          condensed={condensed}
+          secondaryHeader={text('secondaryHeader', 'Subtitle')}
+        >
+          {text('Text', 'Sample text')}
+        </CollapsiblePanel>
+      </Section>
+    );
+  })
   .add('Controlled', () => (
     <Section>
       <Collapsible>
-        {({ isOpen, toggle }) => (
-          <CollapsiblePanel
-            header={
-              <CollapsiblePanelHeader>
-                {text('deader', 'Header')}
-              </CollapsiblePanelHeader>
-            }
-            description={text('description', 'Controlled component example')}
-            isSticky={boolean('isSticky', false)}
-            isDisabled={boolean('isDisabled', false)}
-            tone={select('tone', ['primary', 'urgent'], 'primary')}
-            isClosed={!isOpen}
-            onToggle={toggle}
-            headerControls={text('headerControls', 'headerControl')}
-            theme={select('theme', ['dark', 'light'])}
-            condensed={false}
-            secondaryHeader={text('secondaryHeader', 'Subtitle')}
-          >
-            {text('Text', 'Sample text')}
-          </CollapsiblePanel>
-        )}
+        {({ isOpen, toggle }) => {
+          const condensed = boolean('consensed', false);
+          return (
+            <CollapsiblePanel
+              header={
+                condensed ? (
+                  text('header', 'Header')
+                ) : (
+                  <CollapsiblePanelHeader>
+                    {text('header', 'Header')}
+                  </CollapsiblePanelHeader>
+                )
+              }
+              description={text('description', 'Controlled component example')}
+              isSticky={boolean('isSticky', false)}
+              isDisabled={boolean('isDisabled', false)}
+              tone={select('tone', ['primary', 'urgent'], 'primary')}
+              isClosed={!isOpen}
+              onToggle={toggle}
+              headerControls={text('headerControls', 'headerControl')}
+              theme={select('theme', ['dark', 'light'])}
+              condensed={condensed}
+              secondaryHeader={text('secondaryHeader', 'Subtitle')}
+            >
+              {text('Text', 'Sample text')}
+            </CollapsiblePanel>
+          );
+        }}
       </Collapsible>
     </Section>
   ));
