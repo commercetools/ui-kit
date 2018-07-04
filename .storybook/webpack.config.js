03313280db8ac8e49f696cf2511923716f3a34a6
Nicola Molinari <nicola.molinari@commercetools.de>
2018-07-04 14:48:24 +0200

    refactor(uikit/storybook): use full-control mode and reuse dev/prod webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,120 +1,51 @@
 const path = require('path');
-const postcssImport = require('postcss-import');
-const postcssCssNext = require('postcss-cssnext');
-const postcssReporter = require('postcss-reporter');
+const createWebpackConfigForDevelopment = require('@commercetools-frontend/mc-scripts/config/create-webpack-config-for-development');
+const createWebpackConfigForProduction = require('@commercetools-frontend/mc-scripts/config/create-webpack-config-for-production');
 
 const sourceFolders = [
   // This points to `packages-shared`
   path.resolve(__dirname, '../..'),
 ];
 
-module.exports = {
-  devtool: 'source-map',
+const mcWebpackConfigDev = createWebpackConfigForDevelopment({
+  distPath: '', // not important
+  entryPoint: '', // not important
+  sourceFolders,
+});
+const mcWebpackConfigProd = createWebpackConfigForProduction({
+  distPath: '', // not important
+  entryPoint: '', // not important
+  sourceFolders,
+});
+const uikitWebpackConfig = {
   module: {
-    // Makes missing exports an error instead of warning.
-    strictExportPresence: true,
-
     rules: [
-      // Disable require.ensure as it's not a standard language feature.
-      { parser: { requireEnsure: false } },
+      // Storybook uses a plugin to load and render markdown files.
       {
-        // For svg icons, we want to get them transformed into React components
-        // when we import them.
-        test: /\.svg$/,
-        exclude: /node_modules/,
+        test: /\.md$/,
         use: [
-          {
-            loader: require.resolve('babel-loader'),
-            options: {
-              babelrc: false,
-              presets: [
-                require.resolve('@commercetools-frontend/babel-preset-mc-app'),
-              ],
-              // This is a feature of `babel-loader` for webpack (not Babel itself).
-              // It enables caching results in ./node_modules/.cache/babel-loader/
-              // directory for faster rebuilds.
-              cacheDirectory: true,
-              highlightCode: true,
-            },
-          },
-          {
-            loader: require.resolve('@svgr/webpack'),
-            options: {
-              icon: true,
-            },
-          },
+          { loader: require.resolve('html-loader') },
+          { loader: require.resolve('markdown-loader') },
         ],
-      },
-      {
-        // Do not transform vendor's CSS with CSS-modules
-        // The point is that they remain in global scope.
-        // Since we require these CSS files in our JS or CSS files,
-        // they will be a part of our compilation either way.
-        // So, no need for ExtractTextPlugin here.
-        test: /\.css$/,
-        include: /node_modules/,
-        loaders: [
-          require.resolve('style-loader'),
-          require.resolve('css-loader'),
-        ],
-      },
-      {
-        test: function testForNormalCssFiles(fileName) {
-          return (
-            // Use this only for plain CSS.
-            // For css-modules, see loader below.
-            fileName.endsWith('.css') && !fileName.endsWith('.mod.css')
-          );
-        },
-        use: [
-          require.resolve('style-loader'),
-          require.resolve('css-loader'),
-          {
-            loader: require.resolve('postcss-loader'),
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                postcssImport(),
-                postcssCssNext({
-                  browsers: '> 1%',
-                  features: { autoprefixer: { grid: true } },
-                }),
-                postcssReporter(),
-              ],
-            },
-          },
-        ],
-        include: sourceFolders,
-      },
-      {
-        test: /\.mod\.css$/,
-        use: [
-          require.resolve('style-loader'),
-          {
-            loader: require.resolve('css-loader'),
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[name]__[local]___[hash:base64:5]',
-            },
-          },
-          {
-            loader: require.resolve('postcss-loader'),
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                postcssImport({ path: sourceFolders }),
-                postcssCssNext({
-                  browsers: '> 1%',
-                  features: { autoprefixer: { grid: true } },
-                }),
-                postcssReporter(),
-              ],
-            },
-          },
-        ],
-        include: sourceFolders,
       },
     ],
   },
+};
+
+const getRulesConfig = configType => {
+  const config =
+    configType === 'PRODUCTION' ? mcWebpackConfigProd : mcWebpackConfigDev;
+  return config.module.rules;
+};
+
+module.exports = (storybookBaseConfig, configType) => {
+  // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  storybookBaseConfig.module.rules = getRulesConfig(configType).concat(
+    uikitWebpackConfig.module.rules
+  );
+
+  return storybookBaseConfig;
 };
