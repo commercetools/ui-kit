ce49ed6471be55bb387fc8eaacc250bf30853210
Malcolm Laing <malcolm.laing@commercetools.de>
2018-07-04 11:51:41 +0200

    fix(ui-kit): fix rendering of datepickers in stories

--- date-picker/time-picker.story.js
+++ date-picker/time-picker.story.js
@@ -1,34 +1,45 @@
 import React from 'react';
+import { IntlProvider } from 'react-intl';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import withReadme from 'storybook-readme/with-readme';
 import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
 import Section from '../.storybook/decorators/section';
-import { DatePicker } from './date-picker';
+import DatePicker from './date-picker';
 import Readme from './README.md';
 
 storiesOf('Dates', module)
   .addDecorator(withKnobs)
   .addDecorator(withReadme(Readme))
   .add('TimePicker', () => (
-    <Section>
-      <DatePicker
-        placeholder={text('placeholder', 'Select time ...')}
-        mode={select('mode', ['single', 'multiple', 'range'], 'single')}
-        timeScale={select('timeScale', ['date', 'datetime', 'time'], 'time')}
-        isDisabled={boolean('isDisabled?', false)}
-        value={text('Time (UTC)', '16:02:50.000')}
-        onChange={action('on change')}
-        isInvalid={boolean('isInvalid?', false)}
-        /* Note that when you switch the locale in the interface it will not update completely,
-         * because the locale of flatpickr cannot be updated dynamically, you need to change the default (3rd parameter)
-         * below */
-        locale={select('locale', ['de', 'en'], 'de')}
-        size={select(
-          'size',
-          { scale: 'Full-Width', static: 'Static' },
-          'static'
-        )}
-      />
-    </Section>
+    <IntlProvider
+      locale="en"
+      messages={{
+        en: {
+          'UIKit.DatePicker.labelRange': 'to',
+        },
+      }}
+    >
+      <Section>
+        <DatePicker
+          placeholder={text('placeholder', 'Select time ...')}
+          mode={select('mode', ['single', 'multiple', 'range'], 'single')}
+          timeScale={select('timeScale', ['date', 'datetime', 'time'], 'time')}
+          isDisabled={boolean('isDisabled?', false)}
+          value={text('Time (UTC)', '16:02:50.000')}
+          onChange={action('on change')}
+          isInvalid={boolean('isInvalid?', false)}
+          timeZone={'Europe/Madrid'}
+          /* Note that when you switch the locale in the interface it will not update completely,
+           * because the locale of flatpickr cannot be updated dynamically, you need to change the default (3rd parameter)
+           * below */
+          locale={select('locale', ['de', 'en'], 'de')}
+          size={select(
+            'size',
+            { scale: 'Full-Width', static: 'Static' },
+            'static'
+          )}
+        />
+      </Section>
+    </IntlProvider>
   ));
