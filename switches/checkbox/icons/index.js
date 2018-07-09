ef4e6d2738073caa577ba19717387bec97cc38bd
lufego <luis.gomes@commercetools.de>
2018-07-09 15:07:07 +0200

    fix(checkbox): updates snapshot and removes unused mapping

--- switches/checkbox/icons/index.js
+++ switches/checkbox/icons/index.js
@@ -1,28 +1,10 @@
 // NOTE: explicit names for component names
 import CheckboxDefaultIcon from './checkbox-default.svg';
-import CheckboxDefaultHoverIcon from './checkbox-hover.svg';
-import CheckboxDefaultErrorIcon from './checkbox-error.svg';
 import CheckboxCheckedIcon from './checkbox-checked.svg';
-import CheckboxCheckedHoverIcon from './checkbox-checked-hover.svg';
-import CheckboxCheckedErrorIcon from './checkbox-checked-error.svg';
-import CheckboxDisabledIcon from './checkbox-disabled.svg';
-import CheckboxCheckedDisabledIcon from './checkbox-checked-disabled.svg';
 import CheckboxIndeterminateIcon from './checkbox-indeterminate.svg';
-import CheckboxIndeterminateHoverIcon from './checkbox-indeterminate-hover.svg';
-import CheckboxIndeterminateDisabledIcon from './checkbox-indeterminate-disabled.svg';
-import CheckboxIndeterminateErrorIcon from './checkbox-indeterminate-error.svg';
 
 export default {
   Default: CheckboxDefaultIcon,
-  Hover: CheckboxDefaultHoverIcon,
-  Error: CheckboxDefaultErrorIcon,
   Checked: CheckboxCheckedIcon,
-  CheckedHover: CheckboxCheckedHoverIcon,
-  CheckedError: CheckboxCheckedErrorIcon,
-  Disabled: CheckboxDisabledIcon,
-  CheckedDisabled: CheckboxCheckedDisabledIcon,
   Indeterminate: CheckboxIndeterminateIcon,
-  IndeterminateHover: CheckboxIndeterminateHoverIcon,
-  IndeterminateDisabled: CheckboxIndeterminateDisabledIcon,
-  IndeterminateError: CheckboxIndeterminateErrorIcon,
 };
