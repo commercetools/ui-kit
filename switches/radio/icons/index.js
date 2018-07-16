40ac2bad0f4865547db0569c9f688db058ddbd3d
lufego <luis.gomes@commercetools.de>
2018-07-16 11:27:28 +0200

    refactor(radio): adds new logic

--- switches/radio/icons/index.js
+++ switches/radio/icons/index.js
@@ -1,16 +1,8 @@
 // NOTE: explicit names for component names
 import RadioOptionDefaultIcon from './radio-option-default.svg';
-import RadioOptionDefaultHoverIcon from './radio-option-hover.svg';
 import RadioOptionCheckedIcon from './radio-option-checked.svg';
-import RadioOptionCheckedHoverIcon from './radio-option-checked-hover.svg';
-import RadioOptionDisabledIcon from './radio-option-disabled.svg';
-import RadioOptionCheckedDisabledIcon from './radio-option-checked-disabled.svg';
 
 export default {
   Default: RadioOptionDefaultIcon,
-  Hover: RadioOptionDefaultHoverIcon,
   Checked: RadioOptionCheckedIcon,
-  CheckedHover: RadioOptionCheckedHoverIcon,
-  Disabled: RadioOptionDisabledIcon,
-  CheckedDisabled: RadioOptionCheckedDisabledIcon,
 };
