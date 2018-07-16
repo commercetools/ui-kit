354963810e2cd6cd2bca423c2ccb9f8b624e9d3f
lufego <luis.gomes@commercetools.de>
2018-07-16 10:07:43 +0200

    feat(checkbox): adds react-value to story

--- switches/checkbox/checkbox.story.js
+++ switches/checkbox/checkbox.story.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { Value } from 'react-value';
 import withReadme from 'storybook-readme/with-readme';
 import Spacings from '../../materials/spacings';
 import Section from '../../.storybook/decorators/section';
@@ -14,16 +15,23 @@ storiesOf('Switches', module)
   .add('Checkbox', () => (
     <Section>
       <Spacings.Stack>
-        <Checkbox
-          onChange={action('onChange')}
-          value="foo-value"
-          isChecked={boolean('isChecked', false)}
-          isIndeterminate={boolean('isIndeterminate', false)}
-          isDisabled={boolean('isDisabled', false)}
-          hasError={boolean('hasError', false)}
-        >
-          {text('Label', 'This is a label')}
-        </Checkbox>
+        <Value
+          render={(value, onChange) => (
+            <Checkbox
+              onChange={event => {
+                action('onChange')(event);
+                onChange(event.target.value);
+              }}
+              value={value}
+              isChecked={boolean('isChecked', false)}
+              isIndeterminate={boolean('isIndeterminate', false)}
+              isDisabled={boolean('isDisabled', false)}
+              hasError={boolean('hasError', false)}
+            >
+              {text('Label', 'This is a label')}
+            </Checkbox>
+          )}
+        />
       </Spacings.Stack>
     </Section>
   ));
