a711d9c48a30d3ffc1fc03d78326e6d616794043
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2018-07-23 12:38:29 +0200

    docs(ui-kit/field-label): split story
    - remove inputs

--- fields/field-label/field-label.story.js
+++ fields/field-label/field-label.story.js
@@ -5,7 +5,6 @@ import withReadme from 'storybook-readme/with-readme';
 import Constraints from '@commercetools-frontend/ui-kit/materials/constraints';
 import Table from '@commercetools-frontend/ui-kit/table';
 import Checkbox from '@commercetools-frontend/ui-kit/switches/checkbox';
-import TextInput from '@commercetools-frontend/ui-kit/inputs/text-input';
 import IconButton from '@commercetools-frontend/ui-kit/buttons/icon-button';
 import {
   InformationIcon,
@@ -13,88 +12,87 @@ import {
 } from '@commercetools-frontend/ui-kit/icons';
 import FlatButton from '@commercetools-frontend/ui-kit/buttons/flat-button';
 import Section from '../../.storybook/decorators/section';
-import Spacings from '../../materials/spacings';
 import Readme from './README.md';
 import FieldLabel from './field-label';
 
-storiesOf('Fields', module)
+storiesOf('Fields/FieldLabel', module)
   .addDecorator(withKnobs)
   .addDecorator(withReadme(Readme))
-  .add('FieldLabel', () => (
+  .add('basic example', () => (
     <Section>
-      <Spacings.Stack scale="l">
-        <Spacings.Stack>
-          <Constraints.Horizontal constraint="m">
-            <FieldLabel
-              title={text('title', 'Label title')}
-              isBold={boolean('isBold', true)}
-              subtitle={text('subtitle', null)}
-              hint={text('hint', null)}
-              isRequired={boolean('isRequired', false)}
-              titleIcon={
-                select('titleIcon', ['', 'IconButton'], '') ? (
-                  <IconButton icon={<InformationIcon />} />
-                ) : null
-              }
-              subtitleIcon={
-                select('subtitleIcon', ['', 'WarningIcon'], '') ? (
-                  <WarningIcon />
-                ) : null
-              }
-              badge={
-                select('badge', ['', 'FlatButton'], '') ? (
-                  <FlatButton
-                    tone="primary"
-                    icon={<InformationIcon />}
-                    label="show"
-                  />
-                ) : null
-              }
-            />
-          </Constraints.Horizontal>
-          <TextInput placeholder="Placeholder" horizontalConstraint="m" />
-        </Spacings.Stack>
-        <Table
-          columns={[
-            {
-              key: 'name',
-              label: (
-                <FieldLabel
-                  title={text('title-table', 'Table title')}
-                  titleTone={select(
-                    'titleTone-table',
-                    ['', 'inverted'],
-                    'inverted'
-                  )}
-                  isRequired={boolean('isRequired-table', false)}
-                />
-              ),
-            },
-          ]}
-          rowCount={0}
-          itemRenderer={() => {}}
-          onRowClick={() => {}}
-          items={[]}
+      <Constraints.Horizontal constraint="m">
+        <FieldLabel
+          title={text('title', 'Label title')}
+          isBold={boolean('isBold', true)}
+          subtitle={text('subtitle', null)}
+          hint={text('hint', null)}
+          isRequired={boolean('isRequired', false)}
+          tone={select('tone', [
+            '',
+            'primary',
+            'information',
+            'secondary',
+            'positive',
+            'negative',
+            'inverted',
+          ])}
+          titleIcon={
+            select('titleIcon', ['', 'IconButton'], '') ? (
+              <IconButton icon={<InformationIcon />} />
+            ) : null
+          }
+          subtitleIcon={
+            select('subtitleIcon', ['', 'WarningIcon'], '') ? (
+              <WarningIcon />
+            ) : null
+          }
+          badge={
+            select('badge', ['', 'FlatButton'], '') ? (
+              <FlatButton
+                tone="primary"
+                icon={<InformationIcon />}
+                label="show"
+              />
+            ) : null
+          }
         />
-        <Checkbox
-          isDisabled={false}
-          isChecked={false}
-          onChange={() => {}}
-          value="foo-value"
-        >
-          <FieldLabel
-            title={text('title-checkbox', 'Checkbox title')}
-            isRequired={boolean('isRequired-checkbox', false)}
-          />
-        </Checkbox>
-        <Spacings.Inline alignItems="center" scale="s">
-          <FieldLabel
-            title={text('title-left', 'Label title')}
-            isBold={boolean('isBold-left', true)}
-            isRequired={boolean('isRequired-left', false)}
-          />
-          <TextInput placeholder="Placeholder" horizontalConstraint="m" />
-        </Spacings.Inline>
-      </Spacings.Stack>
+      </Constraints.Horizontal>
+    </Section>
+  ))
+  .add('in table', () => (
+    <Section>
+      <Table
+        columns={[
+          {
+            key: 'name',
+            label: (
+              <FieldLabel
+                title={text('title', 'Table title')}
+                tone={select('tone', ['', 'inverted'], 'inverted')}
+                isRequired={boolean('isRequired', false)}
+              />
+            ),
+          },
+        ]}
+        rowCount={0}
+        itemRenderer={() => {}}
+        onRowClick={() => {}}
+        items={[]}
+      />
+    </Section>
+  ))
+  .add('in checkbox', () => (
+    <Section>
+      <Checkbox
+        isDisabled={false}
+        isChecked={false}
+        onChange={() => {}}
+        value="foo-value"
+      >
+        <FieldLabel
+          title={text('title', 'Checkbox title')}
+          isRequired={boolean('isRequired', false)}
+        />
+      </Checkbox>
     </Section>
   ));
