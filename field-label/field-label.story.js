a372062f0f650f13566a1f1431a17a305c6c4bde
Dominik Ferber <dominik.ferber@gmail.com>
2018-08-08 12:48:48 +0200

    feat(field-label): show more features in story by default

--- field-label/field-label.story.js
+++ field-label/field-label.story.js
@@ -13,11 +13,11 @@ storiesOf('FieldLabel', module)
   .addDecorator(withKnobs)
   .addDecorator(withReadme(Readme))
   .add('FieldLabel', () => {
-    const hint = text('hint', '');
+    const hint = text('hint', 'Enter a number between 0 and 1');
 
     // hintIcon will only render when hint exists
     const iconNames = Object.keys(icons);
-    const icon = select('hintIcon', ['', ...iconNames], '');
+    const icon = select('hintIcon', ['', ...iconNames], 'SortingIcon');
     const hintIcon = icon ? React.createElement(icons[icon]) : undefined;
     return (
       <Section>
@@ -27,16 +27,19 @@ storiesOf('FieldLabel', module)
             ['xs', 's', 'm', 'l', 'xl', 'scale'],
             'scale'
           )}
-          title={text('title', 'Label title example')}
+          title={text('title', 'Sort Order')}
           hasRequiredIndicator={boolean('hasRequiredIndicator', false)}
           onInfoButtonClick={
-            boolean('show info button', false)
+            boolean('show info button', true)
               ? action('onInfoButtonClick')
               : undefined
           }
           hint={hint}
           hintIcon={hintIcon}
-          description={text('description', '')}
+          description={text(
+            'description',
+            'This order will be used to sort the categories.'
+          )}
           badge={
             boolean('show badge', false) ? (
               <FlatButton
