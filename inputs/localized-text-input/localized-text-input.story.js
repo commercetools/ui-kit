e9f7617fdc899acbffa782c1ba1539f8501a83d3
Dominik Ferber <dominik.ferber@gmail.com>
2018-08-13 17:02:18 +0200

    feat(ui-kit): remove onChangeValue from LocalizedTextInput
    And add Value handler to story

--- inputs/localized-text-input/localized-text-input.story.js
+++ inputs/localized-text-input/localized-text-input.story.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Value } from 'react-value';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
+import {
+  withKnobs,
+  boolean,
+  text,
+  select,
+  object,
+} from '@storybook/addon-knobs';
 import withReadme from 'storybook-readme/with-readme';
 import Section from '../../.storybook/decorators/section';
 import ErrorMessage from '../../messages/error-message';
@@ -19,47 +26,48 @@ storiesOf('Inputs', module)
     const key = isDefaultExpanded ? 'yes' : 'no';
     return (
       <Section>
-        <LocalizedTextInput
-          key={key}
-          id={text('id', undefined)}
-          name={text('name', undefined)}
-          value={{
-            en: text('value(en)', ''),
-            de: text('value(de)', ''),
-            'nan-Hant-TW': text('value(nan-Hant-TW)', ''),
-          }}
-          onChange={action('onChange')}
-          onChangeValue={action('onChangeValue')}
-          selectedLanguage={select('selectedLanguage', ['en', 'de'], 'en')}
-          onBlur={action('onBlur')}
-          onFocus={action('onFocus')}
-          hideExpansionControls={boolean('hideExpansionControls', false)}
-          isDefaultExpanded={
-            isDefaultExpanded
-              ? true
-              : // we need to set undefined instead of false to avoid prop-type
-                // warnings in case hideExpansionControls is true
-                undefined
-          }
-          isAutofocussed={boolean('isAutofocussed', false)}
-          isDisabled={boolean('isDisabled', false)}
-          isReadOnly={boolean('isReadOnly', false)}
-          placeholder={{
-            en: text('placeholder(en)', ''),
-            de: text('placeholder(de)', ''),
-          }}
-          horizontalConstraint={select(
-            'horizontalConstraint',
-            ['xs', 's', 'm', 'l', 'xl', 'scale'],
-            'm'
+        <Value
+          defaultValue={{ en: '', de: '', 'nan-Hant-TW': '' }}
+          render={(value, onChange) => (
+            <LocalizedTextInput
+              key={key}
+              id={text('id', undefined)}
+              name={text('name', undefined)}
+              value={object('value', value)}
+              onChange={event => {
+                action('onChange')(event);
+                onChange({
+                  ...value,
+                  [event.target.language]: event.target.value,
+                });
+              }}
+              selectedLanguage={select('selectedLanguage', ['en', 'de'], 'en')}
+              onBlur={action('onBlur')}
+              onFocus={action('onFocus')}
+              hideExpansionControls={boolean('hideExpansionControls', false)}
+              isDefaultExpanded={
+                // we need to set undefined instead of false to avoid prop-type
+                // warnings on the story in case hideExpansionControls is true
+                isDefaultExpanded || undefined
+              }
+              isAutofocussed={boolean('isAutofocussed', false)}
+              isDisabled={boolean('isDisabled', false)}
+              isReadOnly={boolean('isReadOnly', false)}
+              placeholder={object('placeholder', { en: '', de: '' })}
+              horizontalConstraint={select(
+                'horizontalConstraint',
+                ['xs', 's', 'm', 'l', 'xl', 'scale'],
+                'm'
+              )}
+              hasError={boolean('hasError', false)}
+              errors={
+                boolean('errors', false)
+                  ? { de: <ErrorMessage>Custom Error Message</ErrorMessage> }
+                  : undefined
+              }
+              data-test="foo"
+            />
           )}
-          hasError={boolean('hasError', false)}
-          errors={
-            boolean('errors', false)
-              ? { de: <ErrorMessage>Custom Error Message</ErrorMessage> }
-              : undefined
-          }
-          data-test="foo"
         />
       </Section>
     );
