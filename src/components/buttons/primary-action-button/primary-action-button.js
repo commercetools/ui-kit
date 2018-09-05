12baabd0da9fe22ee5f397ca731c025901397fc7
lufego <luis.gomes@commercetools.de>
2018-09-05 11:53:08 +0200

    feat(tokens): adds tokens to buttons

--- src/components/buttons/primary-action-button/primary-action-button.js
+++ src/components/buttons/primary-action-button/primary-action-button.js
@@ -4,7 +4,7 @@ import BaseButtonWrapper from '../base-button-wrapper';
 import styles from './primary-action-button.mod.css';
 
 const PrimaryActionButton = props => (
-  <BaseButtonWrapper>
+  <BaseButtonWrapper {...props}>
     <span className={styles.button}>{props.children}</span>
   </BaseButtonWrapper>
 );
@@ -12,7 +12,18 @@ const PrimaryActionButton = props => (
 PrimaryActionButton.displayName = 'PrimaryActionButton';
 
 PrimaryActionButton.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  ref: PropTypes.string,
   children: PropTypes.node.isRequired,
+  ariaLabel: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  dataAttr: PropTypes.object,
+  isDisabled: PropTypes.bool,
+  isToggled: PropTypes.bool,
+  icon: PropTypes.node,
+  type: PropTypes.oneOf(['button', 'reset', 'submit']),
+  size: PropTypes.oneOf(['small', 'big']),
 };
 
 export default PrimaryActionButton;
