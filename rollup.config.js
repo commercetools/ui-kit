514efaff8586f5d62adeab71cde09377045034d5
Malcolm Laing <malcolm.laing@commercetools.de>
2018-09-06 17:01:35 +0200

    feat(build-process): fix css bundling

--- rollup.config.js
+++ rollup.config.js
@@ -5,58 +5,61 @@ import svg from 'rollup-plugin-svg';
 import builtins from 'rollup-plugin-node-builtins';
 import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
-import { minify } from 'uglify-es';
+import postcssImport from 'postcss-import';
+import postcssPresetEnv from 'postcss-preset-env';
+import postcssReporter from 'postcss-reporter';
+import postcssCustomProperties from 'postcss-custom-properties';
+import postcssCustomMediaQueries from 'postcss-custom-media';
+import postcssPostcssColorModFunction from 'postcss-color-mod-function';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import { uglify } from 'rollup-plugin-uglify';
+// import { minify } from 'uglify-es';
 import babelOptions from '@commercetools-frontend/babel-preset-mc-app';
 import pkg from './package.json';
 
-const path = 'dist/ui-kit';
+// const path = 'dist/ui-kit';
 
-const external = [
-  'classnames',
-  'flatpickr',
-  'moment',
-  'moment-timezones',
-  'react',
-  'react-if',
-  'prop-types',
-  'invariant',
-  'react-is',
-  'react',
-  'react-dom',
-  'react-router-dom',
-  'react-virtualized',
-  'react-textarea-autosize',
-  'react-intl',
-  'recompose',
-  'styled-components',
-  'reselect',
-  'warning',
-];
-
-const globals = {
-  classnames: 'classNames',
-  'prop-types': 'PropTypes',
-  'react-dom': 'ReactDOM',
-  'react-router-dom': 'ReactRouterDOM',
-  react: 'React',
+const browserslist = {
+  development: ['chrome', 'firefox'].map(
+    browser => `last 2 ${browser} versions`
+  ),
+  production: ['>1%', 'not op_mini all', 'ie 11'],
 };
 
 const basePlugins = [
+  peerDepsExternal(),
   builtins(),
   commonjs({
     include: 'node_modules/**',
     namedExports: {
+      'node_modules/react-is/index.js': ['isValidElementType'],
       'node_modules/flatpickr/dist/l10n/de.js': ['German'],
     },
   }),
   resolve(),
   postcss({
     modules: true,
+    // extract: true,
+    importLoaders: 1,
+    localIdentName: '[name]__[local]___[hash:base64:5]',
+    plugins: [
+      postcssImport(),
+      postcssPresetEnv({
+        browsers: browserslist.development,
+        autoprefixer: { grid: true },
+      }),
+      postcssCustomProperties({
+        preserve: false,
+      }),
+      postcssCustomMediaQueries(),
+      postcssPostcssColorModFunction(),
+      postcssReporter(),
+    ],
   }),
   babel({
     runtimeHelpers: true,
     ...babelOptions(),
+    exclude: 'node_modules/**',
   }),
   json(),
   svg(),
@@ -69,40 +72,6 @@ export default [
       file: pkg.module,
       format: 'es',
     },
-    external,
     plugins: basePlugins,
-  },
-  {
-    input: 'src/index.js',
-    output: {
-      name: 'ui-kit',
-      file: `${path}.js`,
-      format: 'cjs',
-      globals,
-    },
-    external,
-    plugins: basePlugins,
-  },
-  {
-    input: 'src/index.js',
-    output: {
-      name: 'ui-kit',
-      file: `${path}.umd.js`,
-      format: 'umd',
-      globals,
-    },
-    external,
-    plugins: basePlugins,
-  },
-  {
-    input: 'src/index.js',
-    output: {
-      name: 'ui-kit',
-      file: `${path}.umd.min.js`,
-      format: 'umd',
-      globals,
-    },
-    external,
-    plugins: [...basePlugins, uglify({}, minify)],
   },
 ];
