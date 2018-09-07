7f2ea852317a9f48cc564ddedf3edde1a2c4398e
Dominik Ferber <dominik.ferber@gmail.com>
2018-09-07 15:17:24 +0200

    chore: inline plugins in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -36,66 +36,6 @@ const postcssPlugins = [
   postcssReporter(),
 ];
 
-const basePlugins = [
-  cleaner({
-    silent: true,
-    targets: ['./dist/'],
-  }),
-  peerDepsExternal(),
-  builtins(),
-  commonjs({
-    include: 'node_modules/**',
-    namedExports: {
-      'node_modules/react-is/index.js': ['isValidElementType'],
-      'node_modules/flatpickr/dist/l10n/de.js': ['German'],
-    },
-  }),
-  resolve(),
-  // this is used for importing our css modules
-  postcss({
-    include: ['**/*.mod.css'],
-    exclude: ['node_modules/**/*.css'],
-    modules: true,
-    importLoaders: 1,
-    localIdentName: '[name]__[local]___[hash:base64:5]',
-    plugins: postcssPlugins,
-  }),
-  // this is used for importing both vendor css (from node_modules) and for importing
-  // our global css which uses tokens that require postcss plugins (such as color-mod) to be compiled properly.
-  postcss({
-    exclude: ['**/*.mod.css'],
-    include: ['**/*.css'],
-    plugins: postcssPlugins,
-  }),
-  babel({
-    exclude: ['node_modules/**'],
-    runtimeHelpers: true,
-    ...babelOptions(),
-  }),
-  json(),
-  svg({
-    include: ['**/*.svg'],
-    exclude: ['**/*.react.svg'],
-  }),
-  svgrPlugin({
-    include: ['**/*.react.svg'],
-    icon: false,
-    svgoConfig: {
-      plugins: [
-        { removeViewBox: false },
-        // Keeps ID's of svgs so they can be targeted with CSS
-        { cleanupIDs: false },
-      ],
-    },
-  }),
-  // copy static files over
-  // We tried all copy plugins of rollup, and couldn't get a single one to do
-  // this. So, the command-line it is!
-  execute(
-    'cp -r package.json README.md LICENSE materials proxy-exports/ dist/'
-  ),
-];
-
 export default [
   {
     input: 'src/index.js',
@@ -109,6 +49,65 @@ export default [
         format: 'cjs',
       },
     ],
-    plugins: basePlugins,
+    plugins: [
+      cleaner({
+        silent: true,
+        targets: ['./dist/'],
+      }),
+      peerDepsExternal(),
+      builtins(),
+      commonjs({
+        include: 'node_modules/**',
+        namedExports: {
+          'node_modules/react-is/index.js': ['isValidElementType'],
+          'node_modules/flatpickr/dist/l10n/de.js': ['German'],
+        },
+      }),
+      resolve(),
+      // this is used for importing our css modules
+      postcss({
+        include: ['**/*.mod.css'],
+        exclude: ['node_modules/**/*.css'],
+        modules: true,
+        importLoaders: 1,
+        localIdentName: '[name]__[local]___[hash:base64:5]',
+        plugins: postcssPlugins,
+      }),
+      // this is used for importing both vendor css (from node_modules) and for
+      // importing our global css which uses tokens that require postcss plugins
+      // (such as color-mod) to be compiled properly.
+      postcss({
+        exclude: ['**/*.mod.css'],
+        include: ['**/*.css'],
+        plugins: postcssPlugins,
+      }),
+      babel({
+        exclude: ['node_modules/**'],
+        runtimeHelpers: true,
+        ...babelOptions(),
+      }),
+      json(),
+      svg({
+        include: ['**/*.svg'],
+        exclude: ['**/*.react.svg'],
+      }),
+      svgrPlugin({
+        include: ['**/*.react.svg'],
+        icon: false,
+        svgoConfig: {
+          plugins: [
+            { removeViewBox: false },
+            // Keeps ID's of svgs so they can be targeted with CSS
+            { cleanupIDs: false },
+          ],
+        },
+      }),
+      // copy static files over
+      // We tried all copy plugins of rollup, and couldn't get a single one to
+      // do this. So, the shell it is!
+      execute(
+        'cp -r package.json README.md LICENSE materials proxy-exports/ dist/'
+      ),
+    ],
   },
 ];
