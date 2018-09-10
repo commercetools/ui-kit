6d8abccc94efc9aa003f39da6c90a312baf82883
Fedor Kulikov <fedor.kulikov@commercetools.de>
2018-09-10 14:51:55 +0200

    feat(select-inputs): let user override customized components

--- src/components/inputs/creatable-select-input/creatable-select-input.js
+++ src/components/inputs/creatable-select-input/creatable-select-input.js
@@ -97,8 +97,8 @@ export class CreatableSelectInput extends React.Component {
               'react-select-warning': this.props.hasWarning,
             })}
             components={{
-              ...this.props.components,
               ...customizedComponents,
+              ...this.props.components,
             }}
             classNamePrefix="react-select"
             onChange={(value, info) =>
