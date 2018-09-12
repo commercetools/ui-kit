e2784e4a5f2160b5da227022b9ee47cf40387f17
Dominik Ferber <dominik.ferber@gmail.com>
2018-09-12 11:26:15 +0200

    feat: add sequentialId factory
    This factory is better than random numbers as we can ensure the ids in tests, and it guarantees unique ids, which ids based on random numbers would not have.

---
+++ src/utils/create-sequential-id.js
@@ -0,0 +1,15 @@
+// Whenever a field requires a unique id, this factory can be used
+// to create a function returning unique ids.
+//
+// Use it as
+//  const sequentialId = createSequentialId('text-field-');
+//  const element = <div id={sequentialId()}>foo</div>
+const createSequentialId = prefix => {
+  let id = 0;
+  return () => {
+    id += 1;
+    return `${prefix}${id}`;
+  };
+};
+
+export default createSequentialId;
