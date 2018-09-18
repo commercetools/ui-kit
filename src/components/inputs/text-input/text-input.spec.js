44229b4d8f8a85ab707ff5a2bc104f9280b060c9
Dominik Ferber <dominik.ferber@gmail.com>
2018-09-18 23:56:16 +0200

    test(text-input): rewrite with react-testing-library

--- src/components/inputs/text-input/text-input.spec.js
+++ src/components/inputs/text-input/text-input.spec.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, fireEvent } from 'react-testing-library';
 import TextInput from './text-input';
 
-const createTestProps = customProps => ({
-  value: '',
-  onChange: () => jest.fn(),
-  ...customProps,
-});
+const baseProps = { value: '', onChange: () => {} };
 
 describe('TextInput.isEmpty', () => {
   describe('when called with an empty value', () => {
@@ -23,202 +19,96 @@ describe('TextInput.isEmpty', () => {
   });
 });
 
-describe('rendering', () => {
-  describe('data attributes', () => {
-    let input;
-    beforeEach(() => {
-      const props = createTestProps({
-        name: 'text-field1',
-        value: '1',
-        'data-foo': 'bar',
-        'data-test': 'baz',
-      });
-      const wrapper = shallow(<TextInput {...props} />);
-      input = wrapper.find('input');
-    });
-    it('should forward the attributes', () => {
-      expect(input).toHaveProp('data-foo', 'bar');
-      expect(input).toHaveProp('data-test', 'baz');
-    });
+describe('TextInput', () => {
+  it('should forward data-attributes', () => {
+    const { container } = render(<TextInput {...baseProps} data-foo="bar" />);
+    expect(container.querySelector('input')).toHaveAttribute('data-foo', 'bar');
   });
-  describe('pristine', () => {
-    let input;
-    beforeEach(() => {
-      const props = createTestProps({
-        name: 'field1',
-        value: 'foo',
-      });
-      const wrapper = shallow(<TextInput {...props} />);
-      input = wrapper.find('input');
-    });
 
-    it('should have class for pristine styles', () => {
-      expect(input).toHaveClassName('pristine');
-    });
-
-    it('should have ARIA role', () => {
-      expect(input).toHaveProp('role', 'textbox');
-    });
-
-    it('input should have type text', () => {
-      expect(input).toHaveProp('type', 'text');
-    });
-
-    it('input have a HTML name', () => {
-      expect(input).toHaveProp('name', 'field1');
-    });
-
-    it('should have passed value', () => {
-      expect(input).toHaveProp('value', 'foo');
-    });
-    describe('with states', () => {
-      describe('warning', () => {
-        beforeEach(() => {
-          const props = createTestProps({
-            hasWarning: true,
-          });
-          const wrapper = shallow(<TextInput {...props} />);
-          input = wrapper.find('input');
-        });
-
-        it('should have warning styles', () => {
-          expect(input).toHaveClassName('warning');
-        });
-      });
-      describe('error', () => {
-        beforeEach(() => {
-          const props = createTestProps({
-            hasError: true,
-          });
-          const wrapper = shallow(<TextInput {...props} />);
-          input = wrapper.find('input');
-        });
-
-        it('should have error styles', () => {
-          expect(input).toHaveClassName('error');
-        });
-      });
-      describe('disabled', () => {
-        beforeEach(() => {
-          const props = createTestProps({
-            isDisabled: true,
-          });
-          const wrapper = shallow(<TextInput {...props} />);
-          input = wrapper.find('input');
-        });
-
-        it('should have class for the disabled state', () => {
-          expect(input).toHaveClassName('disabled');
-        });
-      });
-      describe('readonly', () => {
-        let wrapper;
-        beforeEach(() => {
-          const props = createTestProps({
-            isReadOnly: true,
-          });
-          wrapper = shallow(<TextInput {...props} />);
-          input = wrapper.find('input');
-        });
-
-        it('should have class for the readonly state', () => {
-          expect(input).toHaveClassName('readonly');
-        });
-
-        it('should have ARIA properties for the readonly state', () => {
-          expect(input).toHaveProp('aria-readonly', true);
-        });
-      });
-    });
+  it('should render a number input', () => {
+    const { container } = render(<TextInput {...baseProps} />);
+    expect(container.querySelector('input')).toHaveAttribute('type', 'text');
   });
-});
 
-describe('callbacks', () => {
-  describe('when changing value', () => {
-    let wrapper;
-    let props;
-    let input;
-    beforeEach(() => {
-      props = createTestProps({
-        value: 'foo',
-        onChange: jest.fn(),
-      });
-      wrapper = shallow(<TextInput {...props} />);
-      input = wrapper.find('input');
-      input.simulate('change', { target: { value: 'bar' } });
-    });
-
-    it('should call onChange', () => {
-      expect(props.onChange).toHaveBeenCalled();
-    });
-
-    it('should update with new value', () => {
-      expect(props.onChange).toHaveBeenCalledWith({
-        target: {
-          value: 'bar',
-        },
-      });
-    });
+  it('should have pristine style when untouched', () => {
+    const { container } = render(<TextInput {...baseProps} />);
+    expect(container.querySelector('input')).toHaveClass('pristine');
   });
-  describe('when input gains focus', () => {
-    let wrapper;
-    let props;
-    let input;
-    beforeEach(() => {
-      props = createTestProps({
-        value: 'foo',
-        onFocus: jest.fn(),
-      });
-      wrapper = shallow(<TextInput {...props} />);
-      input = wrapper.find('input');
-      input.simulate('focus');
-    });
 
-    it('should call onFocus', () => {
-      expect(props.onFocus).toHaveBeenCalled();
-    });
-
-    it('should keep the same value', () => {
-      expect(input).toHaveProp('value', 'foo');
-    });
+  it('should have warning style when warning is present', () => {
+    const { container } = render(<TextInput {...baseProps} hasWarning />);
+    expect(container.querySelector('input')).toHaveClass('warning');
   });
-  describe('when input loses focus', () => {
-    let wrapper;
-    let props;
-    let input;
-    beforeEach(() => {
-      props = createTestProps({
-        value: 'foo',
-        onBlur: jest.fn(),
-      });
-      wrapper = shallow(<TextInput {...props} />);
-      input = wrapper.find('input');
-      input.simulate('blur');
-    });
 
-    it('should call onBlur', () => {
-      expect(props.onBlur).toHaveBeenCalled();
-    });
-
-    it('should keep the same value', () => {
-      expect(input).toHaveProp('value', 'foo');
-    });
+  it('should have error style when error is present', () => {
+    const { container } = render(<TextInput {...baseProps} hasError />);
+    expect(container.querySelector('input')).toHaveClass('error');
   });
-  describe('when `isAutofocussed` is passed', () => {
-    let wrapper;
-    let props;
-    let input;
-    beforeEach(() => {
-      props = createTestProps({
-        isAutofocussed: true,
-        onFocus: jest.fn(),
-      });
-      wrapper = shallow(<TextInput {...props} />);
-      input = wrapper.find('input');
-    });
 
-    it('should autofocus prop be true', () => {
-      expect(input.prop('autoFocus')).toBe(true);
+  it('should have an HTML name', () => {
+    const { container } = render(<TextInput {...baseProps} name="foo" />);
+    expect(container.querySelector('input')).toHaveAttribute('name', 'foo');
+  });
+
+  it('should have ARIA role', () => {
+    const { container } = render(<TextInput {...baseProps} />);
+    expect(container.querySelector('input')).toHaveAttribute('role', 'textbox');
+  });
+
+  it('should forward the passed value', () => {
+    const { container } = render(<TextInput {...baseProps} value="foo" />);
+    expect(container.querySelector('input')).toHaveAttribute('value', 'foo');
+  });
+
+  it('should have ARIA properties for the readonly state', () => {
+    const { container } = render(<TextInput {...baseProps} isReadOnly />);
+    expect(container.querySelector('input')).toHaveAttribute(
+      'aria-readonly',
+      'true'
+    );
+  });
+
+  it('should call onChange when chaning the value', () => {
+    const onChange = jest.fn(event => {
+      expect(event.target.id).toEqual('some-id');
+      expect(event.target.name).toEqual('some-name');
+      expect(event.target.value).toEqual('foo');
     });
+    const { container } = render(
+      <TextInput
+        {...baseProps}
+        id="some-id"
+        name="some-name"
+        onChange={onChange}
+      />
+    );
+    const event = { target: { value: 'foo' } };
+    fireEvent.change(container.querySelector('input'), event);
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it('should call onFocus when the input is focused', () => {
+    const onFocus = jest.fn();
+    const { container } = render(
+      <TextInput {...baseProps} onFocus={onFocus} />
+    );
+    container.querySelector('input').focus();
+    expect(container.querySelector('input')).toHaveFocus();
+  });
+
+  it('should call onBlur when input loses focus', () => {
+    const onFocus = jest.fn();
+    const { container } = render(
+      <TextInput {...baseProps} onFocus={onFocus} />
+    );
+    container.querySelector('input').focus();
+    expect(container.querySelector('input')).toHaveFocus();
+    container.querySelector('input').blur();
+    expect(container.querySelector('input')).not.toHaveFocus();
+  });
+
+  it('should have focus automatically when isAutofocussed is passed', () => {
+    const { container } = render(<TextInput {...baseProps} isAutofocussed />);
+    expect(container.querySelector('input')).toHaveFocus();
   });
 });
