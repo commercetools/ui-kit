37504a361bba8a412fdf2afa5cc0bbc0daac95cc
Dominik Ferber <dominik.ferber@gmail.com>
2018-09-18 15:41:31 +0200

    feat(field-errors): add 'fractions' and 'negative' errors

--- src/components/field-errors/messages.js
+++ src/components/field-errors/messages.js
@@ -6,4 +6,14 @@ export default defineMessages({
     description: 'Error message for missing required value',
     defaultMessage: 'This field is required. Provide a value.',
   },
+  invalidNegativeNumber: {
+    id: 'UIKit.FieldErrors.invalidNegativeNumber',
+    description: 'Error message when negative number is used',
+    defaultMessage: 'Negative number is not supported.',
+  },
+  invalidFractionalNumber: {
+    id: 'UIKit.FieldErrors.invalidFractionalNumber',
+    description: 'Error message when fractional number is used',
+    defaultMessage: 'A whole number is required.',
+  },
 });
