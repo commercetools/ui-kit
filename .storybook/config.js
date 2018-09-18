0ab22f55c519389b34f991c478e2d0d34041571b
Dominik Ferber <dominik.ferber@gmail.com>
2018-09-18 15:04:50 +0200

    docs(storybook): readd forms example

--- .storybook/config.js
+++ .storybook/config.js
@@ -25,10 +25,12 @@ setOptions({
 
 const srcStories = require.context('../src', true, /\.story\.js$/);
 const materialsStories = require.context('../materials', true, /\.story\.js$/);
+const exampleStories = require.context('../examples', true, /\.story\.js$/);
 
 function loadStories() {
   srcStories.keys().forEach(filename => srcStories(filename));
   materialsStories.keys().forEach(filename => materialsStories(filename));
+  exampleStories.keys().forEach(filename => exampleStories(filename));
 }
 
 addDecorator(IntlDecorator);
