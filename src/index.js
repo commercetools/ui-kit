0f2fea2ffa8eb045cf5113ebf3c921b04620dcf1
Dominik Ferber <dominik.ferber@gmail.com>
2018-09-18 15:13:22 +0200

    feat(number-field): export from entry-point

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ export {
 export { default as FieldLabel } from './components/field-label';
 
 export { default as TextField } from './components/fields/text-field';
+export { default as NumberField } from './components/fields/number-field';
 
 export { Icons };
 // TODO: this type of export is deprecated and should be removed in the next major release
