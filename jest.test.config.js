1d3fce6b0590d58a539231acc3abd88cff1dcae8
Dominik Ferber <dominik.ferber@gmail.com>
2018-09-24 12:41:55 +0200

    add react testing library (#79)
    * chore: add react-testing-library setup
    
    * test(text-input): rewrite with react-testing-library
    
    * chore: setup wrapped react-testing-library test-utils
    
    * test(text-input): use test-utils

--- jest.test.config.js
+++ jest.test.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: '@commercetools-frontend/jest-preset-mc-app',
   moduleDirectories: ['src', 'node_modules'],
+  setupTestFrameworkScriptFile: '<rootDir>/scripts/setup-test-framework.js',
 };
