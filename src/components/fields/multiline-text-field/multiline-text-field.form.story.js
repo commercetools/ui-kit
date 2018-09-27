06e71e3f8159b40ebb383598d801f9575303f56e
Dominik Ferber <dominik.ferber@gmail.com>
2018-09-27 18:28:24 +0200

    feat: add MultilineTextField

---
+++ src/components/fields/multiline-text-field/multiline-text-field.form.story.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Formik } from 'formik';
+import { IntlProvider } from 'react-intl';
+import { storiesOf } from '@storybook/react';
+import omitEmpty from 'omit-empty';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, select } from '@storybook/addon-knobs';
+import withReadme from 'storybook-readme/with-readme';
+import Section from '../../../../.storybook/decorators/section';
+import FormikBox from '../../../../.storybook/decorators/formik-box';
+import PrimaryButton from '../../buttons/primary-button';
+import SecondaryButton from '../../buttons/secondary-button';
+import Spacings from '../../spacings';
+import Readme from './README.md';
+import MultilineTextField from './multiline-text-field';
+
+storiesOf('Examples|Forms/Fields', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withReadme(Readme))
+  .add('MultilineTextField', () => (
+    <Section>
+      <IntlProvider locale="en">
+        <Formik
+          initialValues={{ description: '' }}
+          validate={values => {
+            const errors = { description: {} };
+            if (MultilineTextField.isEmpty(values.description))
+              errors.description.missing = true;
+            if (values.description.trim().length > 160)
+              errors.description.exceedsMaxLength = true;
+            return omitEmpty(errors);
+          }}
+          onSubmit={(values, formik, ...rest) => {
+            action('onSubmit')(values, formik, ...rest);
+            formik.resetForm(values);
+          }}
+          render={formik => (
+            <Spacings.Stack scale="l">
+              <MultilineTextField
+                title="Description"
+                description="Information about the product."
+                hint="Use 160 spaces at most."
+                name="description"
+                isRequired={true}
+                value={formik.values.description}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                touched={formik.touched.description}
+                horizontalConstraint={select(
+                  'horizontalConstraint',
+                  ['xs', 's', 'm', 'l', 'xl', 'scale'],
+                  'm'
+                )}
+                errors={formik.errors.description}
+                renderError={key => {
+                  switch (key) {
+                    // these could also use <FormattedMessage />
+                    case 'exceedsMaxLength':
+                      return 'No more than 160 characters allowed';
+                    default:
+                      return null;
+                  }
+                }}
+              />
+              <Spacings.Inline>
+                <SecondaryButton
+                  onClick={formik.handleReset}
+                  isDisabled={formik.isSubmitting}
+                  label="Reset"
+                />
+                <PrimaryButton
+                  onClick={formik.handleSubmit}
+                  isDisabled={formik.isSubmitting || !formik.dirty}
+                  label="Submit"
+                />
+              </Spacings.Inline>
+              <hr />
+              <FormikBox formik={formik} />
+            </Spacings.Stack>
+          )}
+        />
+      </IntlProvider>
+    </Section>
+  ));
---
+++ src/components/fields/multiline-text-field/multiline-text-field.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import requiredIf from 'react-required-if';
+import Constraints from '../../constraints';
+import Spacings from '../../spacings';
+import FieldLabel from '../../field-label';
+import MultilineTextInput from '../../inputs/multiline-text-input';
+import createSequentialId from '../../../utils/create-sequential-id';
+import FieldErrors from '../../field-errors';
+import filterDataAttributes from '../../../utils/filter-data-attributes';
+
+const sequentialId = createSequentialId('text-field-');
+
+const hasErrors = errors => errors && Object.values(errors).some(Boolean);
+
+class MultilineTextField extends React.Component {
+  static displayName = 'MultilineTextField';
+
+  static isEmpty = MultilineTextInput.isEmpty;
+
+  static propTypes = {
+    // MultilineTextField
+    id: PropTypes.string,
+    horizontalConstraint: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl', 'scale']),
+    errors: PropTypes.shape({
+      missing: PropTypes.bool,
+    }),
+    renderError: PropTypes.func,
+    isRequired: PropTypes.bool,
+    touched: PropTypes.bool,
+
+    // TextInput
+    name: PropTypes.string,
+    value: PropTypes.string.isRequired,
+    onChange: requiredIf(PropTypes.func, props => !props.isReadOnly),
+    onBlur: PropTypes.func,
+    onFocus: PropTypes.func,
+    isAutofocussed: PropTypes.bool,
+    isDefaultClosed: PropTypes.bool,
+    isDisabled: PropTypes.bool,
+    isReadOnly: PropTypes.bool,
+    placeholder: PropTypes.string,
+
+    // LabelField
+    title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+    hint: requiredIf(
+      PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+      props => props.hintIcon
+    ),
+    description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    onInfoButtonClick: PropTypes.func,
+    hintIcon: PropTypes.node,
+    badge: PropTypes.node,
+  };
+
+  static defaultProps = {
+    horizontalConstraint: 'scale',
+  };
+
+  state = {
+    // We generate an id in case no id is provided by the parent to attach the
+    // label to the input component.
+    id: this.props.id,
+  };
+
+  static getDerivedStateFromProps = (props, state) => ({
+    id: do {
+      if (props.id) props.id;
+      else if (state.id) state.id;
+      else sequentialId();
+    },
+  });
+
+  render() {
+    const hasError = this.props.touched && hasErrors(this.props.errors);
+    return (
+      <Constraints.Horizontal constraint={this.props.horizontalConstraint}>
+        <Spacings.Stack scale="xs">
+          <FieldLabel
+            title={this.props.title}
+            hint={this.props.hint}
+            description={this.props.description}
+            onInfoButtonClick={this.props.onInfoButtonClick}
+            hintIcon={this.props.hintIcon}
+            badge={this.props.badge}
+            hasRequiredIndicator={this.props.isRequired}
+            htmlFor={this.state.id}
+          />
+          <MultilineTextInput
+            id={this.state.id}
+            name={this.props.name}
+            value={this.props.value}
+            onChange={this.props.onChange}
+            onBlur={this.props.onBlur}
+            onFocus={this.props.onFocus}
+            isAutofocussed={this.props.isAutofocussed}
+            isDefaultClosed={this.props.isDefaultClosed}
+            isDisabled={this.props.isDisabled}
+            isReadOnly={this.props.isReadOnly}
+            hasError={hasError}
+            placeholder={this.props.placeholder}
+            horizontalConstraint="scale"
+            {...filterDataAttributes(this.props)}
+          />
+          <FieldErrors
+            errors={this.props.errors}
+            isVisible={hasError}
+            renderError={this.props.renderError}
+          />
+        </Spacings.Stack>
+      </Constraints.Horizontal>
+    );
+  }
+}
+
+export default MultilineTextField;
