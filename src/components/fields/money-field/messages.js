f83306f72781444093701de118e79b125da901d6
Dominik Ferber <dominik.ferber@gmail.com>
2018-09-27 19:06:24 +0200

    feat(money-field): add high-precision price badge (#103)
    * feat(money-field): add high-precision price badge
    
    Removes the option to pass in a badge manually.
    
    * feat: showHighPrecisionBadge -> hasHighPrecisionBadge

---
+++ src/components/fields/money-field/messages.js
@@ -0,0 +1,9 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  highPrecision: {
+    id: 'UIKit.MoneyField.highPrecision',
+    description: 'Label of high precision bade in MoneyField',
+    defaultMessage: 'High Precision Price',
+  },
+});
