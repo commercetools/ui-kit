5dcfd2b433e1b56300181df634779a2439263f3f
Dominik Ferber <dominik.ferber@gmail.com>
2018-10-19 17:50:35 +0200

    docs(collapsible-motion): enhance story

--- src/components/collapsible-motion/collapsible-motion.story.js
+++ src/components/collapsible-motion/collapsible-motion.story.js
@@ -1,67 +1,117 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { storiesOf } from '@storybook/react';
 // import { action } from '@storybook/addon-actions';
-import { withKnobs, number } from '@storybook/addon-knobs';
+import { withKnobs, number, boolean } from '@storybook/addon-knobs';
 // import withReadme from 'storybook-readme/with-readme';
 import CollapsibleMotion from './collapsible-motion';
+import Spacings from '../spacings';
 
-export const CollapsibleMotionStoryBody = props => (
-  <div>
-    <div>
-      <button label={props.isOpen ? 'Close' : 'Open'} onClick={props.onToggle}>
-        {props.isOpen ? 'Close' : 'Open'}
-      </button>
-    </div>
-    <div style={props.style}>
-      <div ref={props.registerContentNode}>{props.children}</div>
-    </div>
-  </div>
-);
-
-CollapsibleMotionStoryBody.displayName = 'CollapsibleMotionStoryBody';
-CollapsibleMotionStoryBody.propTypes = {
-  children: PropTypes.node.isRequired,
-  style: PropTypes.object,
-  isOpen: PropTypes.bool.isRequired,
-  onToggle: PropTypes.func.isRequired,
-  registerContentNode: PropTypes.any,
-};
-
-export const CollapsibleMotionStory = () => (
-  <div>
-    <div>Some content before</div>
-    <CollapsibleMotion>
-      {({ isOpen, toggle, containerStyles, registerContentNode }) => (
-        <CollapsibleMotionStoryBody
-          registerContentNode={registerContentNode}
-          style={containerStyles}
-          isOpen={isOpen}
-          onToggle={toggle}
-        >
-          <div
-            style={{
-              backgroundColor: 'red',
-              width: '200px',
-              height: `${number('height', 250, {
-                range: true,
-                min: 10,
-                max: 5000,
-                step: 50,
-              })}px`,
-            }}
-          >
-            hello
+class CollapsibleMotionStory extends React.Component {
+  static displayName = 'CollapsibleMotionStory';
+  state = {
+    isClosed: false,
+  };
+  handleToggle = () =>
+    this.setState(prevState => ({ isClosed: !prevState.isClosed }));
+  render() {
+    const isDefaultClosed = boolean('isDefaultClosed', false);
+    return (
+      <Spacings.Inline>
+        <div>
+          <h2>Uncontrolled example</h2>
+          <div key={isDefaultClosed}>
+            <div>Some content before</div>
+            <CollapsibleMotion isDefaultClosed={isDefaultClosed}>
+              {({ isOpen, toggle, containerStyles, registerContentNode }) => (
+                <div>
+                  <div>
+                    <button onClick={toggle}>
+                      {isOpen ? 'Close' : 'Open'}
+                    </button>
+                  </div>
+                  <div style={containerStyles}>
+                    <div ref={registerContentNode}>
+                      <div
+                        style={{
+                          backgroundColor: 'red',
+                          width: '200px',
+                          height: `${number('height (uncontrolled)', 250, {
+                            range: true,
+                            min: 20,
+                            max: 5000,
+                            step: 50,
+                          })}px`,
+                        }}
+                      >
+                        hello
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </CollapsibleMotion>
+            <div>Some content afterwards</div>
           </div>
-        </CollapsibleMotionStoryBody>
-      )}
-    </CollapsibleMotion>
-    <div>Some content afterwards</div>
-  </div>
-);
-CollapsibleMotionStory.displayName = 'CollapsibleMotionStory';
+        </div>
+        <div>
+          <h2>Controlled example</h2>
+          <button onClick={() => this.setState({ isClosed: false })}>
+            Open
+          </button>
+          <button onClick={() => this.setState({ isClosed: true })}>
+            Close
+          </button>
+          <div>
+            <div>Some content before</div>
+            <CollapsibleMotion
+              isClosed={this.state.isClosed}
+              onToggle={this.handleToggle}
+            >
+              {({ toggle, containerStyles, registerContentNode }) => (
+                <div>
+                  <div>
+                    <button onClick={toggle}>Toggle</button>
+                  </div>
+                  <div style={containerStyles}>
+                    <div ref={registerContentNode}>
+                      <div
+                        style={{
+                          backgroundColor: 'red',
+                          width: '200px',
+                          height: `${number('height (controlled)', 250, {
+                            range: true,
+                            min: 20,
+                            max: 5000,
+                            step: 50,
+                          })}px`,
+                        }}
+                      >
+                        hello
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </CollapsibleMotion>
+            <div>Some content afterwards</div>
+          </div>
+        </div>
+      </Spacings.Inline>
+    );
+  }
+}
 
 storiesOf('Panels', module)
   .addDecorator(withKnobs)
   // .addDecorator(withReadme(Readme))
-  .add('CollapsibleMotion', () => <CollapsibleMotionStory />);
+  .add('CollapsibleMotion', () => {
+    // this prop is passed to rerender the whole story, which simulates a
+    // parent rerendering
+    const num = number('rerender', 0, {
+      range: true,
+      min: 0,
+      max: 5000,
+      step: 1,
+    });
+    return <CollapsibleMotionStory num={num} />;
+  });
