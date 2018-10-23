25bdce49191a0b1d9f3b02a436cedbbfb185e4df
Dominik Ferber <dominik.ferber@gmail.com>
2018-10-23 17:13:05 +0200

    refactor: consistently use react-emotion
    Drops direct usage of emotion. Drops emotion as a peer-dep as we're not using it directly anymore.

--- src/components/collapsible-motion/collapsible-motion.js
+++ src/components/collapsible-motion/collapsible-motion.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import invariant from 'tiny-invariant';
-import { keyframes } from 'emotion';
+import { keyframes } from 'react-emotion';
 import Collapsible from '../collapsible';
 
 const createOpeningAnimation = height =>
