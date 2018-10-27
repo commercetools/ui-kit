ddfcf1aa5bdb04b8f9ee8293fef792a47f70f7e3
Dominik Ferber <dominik.ferber@gmail.com>
2018-10-27 18:28:21 +0200

    chore: add visual regression testing setup

---
+++ src/components/buttons/primary-button/primary-button.bundlespec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { generateImage } from 'jsdom-screenshot';
+import { render } from 'react-testing-library';
+import { PrimaryButton } from 'ui-kit';
+
+it('has no visual regressions', async () => {
+  render(<PrimaryButton label="Submit" onClick={() => {}} />);
+  expect(await generateImage()).toMatchImageSnapshot();
+});
