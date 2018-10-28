703748d0f3bbcadd5a5b352bf2c06f08f8de9d05
Dominik Ferber <dominik.ferber@gmail.com>
2018-10-28 11:19:07 +0100

    chore: upgrade jsdom-screenshot to v3.1.0

--- scripts/setup-test-framework-for-bundle.js
+++ scripts/setup-test-framework-for-bundle.js
@@ -3,6 +3,12 @@ import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
 // set up visual regression testing
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
+import { setDefaultOptions } from 'jsdom-screenshot';
+
+// TravisCI requires --no-sandbox to be able to run the tests
+setDefaultOptions({
+  launch: { args: process.env.CI === 'true' ? ['--no-sandbox'] : [] },
+});
 
 // give tests more time as taking screenshots takes a while
 jest.setTimeout(15000);
