4cc2f0ae8393d26ba142a1ebfe693d4fc194ceb7
Dominik Ferber <dominik.ferber@gmail.com>
2018-10-29 12:28:14 +0100

    test(i18n): ensure some translations exist

--- src/index.bundlespec.js
+++ src/index.bundlespec.js
@@ -4,7 +4,16 @@ describe('exports', () => {
   it('should export i18n for three languages', () => {
     expect(Object.keys(i18n)).toEqual(['en', 'de', 'es']);
   });
+
   it('should export custom-properties', () => {
     expect(customProperties).toBeTruthy();
+  });
+
+  it('should have the translations', () => {
+    // This test ensures some translations exist, assuming the others would
+    // work as well then
+    expect(i18n.en['UIKit.DateInput.labelRange']).toEqual('to');
+    expect(i18n.de['UIKit.DateInput.labelRange']).toEqual('bis');
+    expect(i18n.es['UIKit.DateInput.labelRange']).toEqual('hasta');
   });
 });
