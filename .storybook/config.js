30d2540c76a584779d03f1123b899f96e4a9dbd1
Dominik Ferber <dominik.ferber@gmail.com>
2018-10-29 09:35:39 +0100

    chore: update dependencies

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,5 @@
 import { configure, setAddon, addDecorator } from '@storybook/react';
-import { setOptions } from '@storybook/addon-options';
+import { withOptions } from '@storybook/addon-options';
 import IntlDecorator from './decorators/intl';
 // import infoAddon from '@storybook/addon-info';
 
@@ -7,20 +7,22 @@ import IntlDecorator from './decorators/intl';
 
 import './main.mod.css';
 
-setOptions({
-  name: 'MC Styleguide',
-  url: 'https://uikit.commercetools.com',
-  goFullScreen: false,
-  showStoriesPanel: true,
-  showAddonPanel: true,
-  showSearchBox: false,
-  addonPanelInRight: true,
-  sortStoriesByKind: false,
-  hierarchySeparator: /\//,
-  hierarchyRootSeparator: /\|/,
+addDecorator(
+  withOptions({
+    name: 'MC Styleguide',
+    url: 'https://uikit.commercetools.com',
+    goFullScreen: false,
+    showStoriesPanel: true,
+    showAddonPanel: true,
+    showSearchBox: false,
+    addonPanelInRight: true,
+    sortStoriesByKind: false,
+    hierarchySeparator: /\//,
+    hierarchyRootSeparator: /\|/,
 
-  resolveStoryHierarchy: storyName => storyName.split('/'),
-});
+    resolveStoryHierarchy: storyName => storyName.split('/'),
+  })
+);
 
 const srcStories = require.context('../src', true, /\.story\.js$/);
 const materialsStories = require.context('../materials', true, /\.story\.js$/);
