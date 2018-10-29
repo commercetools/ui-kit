90ee819b87a6665b71741a32abab2c02cdf7d277
Dominik Ferber <dominik.ferber@gmail.com>
2018-10-29 15:59:55 +0100

    fix(date-range-input): use global locale knob in story

--- src/components/inputs/date-range-input/date-range-input.story.js
+++ src/components/inputs/date-range-input/date-range-input.story.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { IntlProvider, addLocaleData } from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import de from 'react-intl/locale-data/de';
-import es from 'react-intl/locale-data/es';
 import { action } from '@storybook/addon-actions';
 import withReadme from 'storybook-readme/with-readme';
 import { withKnobs, select, boolean } from '@storybook/addon-knobs';
@@ -11,53 +7,40 @@ import { Value } from 'react-value';
 import Section from '../../../../.storybook/decorators/section';
 import Readme from './README.md';
 import DateRangeInput from './date-range-input';
-import * as messages from '../../../../i18n';
-
-addLocaleData(en);
-addLocaleData(de);
-addLocaleData(es);
-const locales = Object.keys(messages);
-
-const getMessages = locale => messages[locale];
 
 storiesOf('Inputs', module)
   .addDecorator(withKnobs)
   .addDecorator(withReadme(Readme))
-  .add('DateRangeInput', () => {
-    const locale = select('locale', locales, locales[0]);
-    return (
-      <Section>
-        <IntlProvider locale={locale} messages={getMessages(locale)}>
-          <Value
-            defaultValue={[]}
-            render={(value, onChange) => (
-              <div>
-                <DateRangeInput
-                  value={value}
-                  onChange={date => {
-                    action('onChange')(date);
-                    onChange(date);
-                  }}
-                  isClearable={boolean('isClearable', true)}
-                  horizontalConstraint={select(
-                    'horizontalConstraint',
-                    ['xs', 's', 'm', 'l', 'xl', 'scale'],
-                    'm'
-                  )}
-                  isDisabled={boolean('isDisabled', false)}
-                  hasError={boolean('hasError', false)}
-                  hasWarning={boolean('hasWarning', false)}
-                />
-                {value && (
-                  <React.Fragment>
-                    <pre>From: {value[0]}</pre>
-                    <pre>To: {value[1]}</pre>
-                  </React.Fragment>
-                )}
-              </div>
+  .add('DateRangeInput', () => (
+    <Section>
+      <Value
+        defaultValue={[]}
+        render={(value, onChange) => (
+          <div>
+            <DateRangeInput
+              value={value}
+              onChange={date => {
+                action('onChange')(date);
+                onChange(date);
+              }}
+              isClearable={boolean('isClearable', true)}
+              horizontalConstraint={select(
+                'horizontalConstraint',
+                ['xs', 's', 'm', 'l', 'xl', 'scale'],
+                'm'
+              )}
+              isDisabled={boolean('isDisabled', false)}
+              hasError={boolean('hasError', false)}
+              hasWarning={boolean('hasWarning', false)}
+            />
+            {value && (
+              <React.Fragment>
+                <pre>From: {value[0]}</pre>
+                <pre>To: {value[1]}</pre>
+              </React.Fragment>
             )}
-          />
-        </IntlProvider>
-      </Section>
-    );
-  });
+          </div>
+        )}
+      />
+    </Section>
+  ));
