150e2bbd12dfe64a677a6e8872bc08da96a3a9d2
Dominik Ferber <dominik.ferber@gmail.com>
2018-10-29 12:28:41 +0100

    chore: warn when running bundlespec against old build

--- jest.bundle.config.js
+++ jest.bundle.config.js
@@ -1,5 +1,32 @@
 // Resolve the absolute path of the caller location.
 const rootPath = process.cwd();
+const fs = require('fs');
+
+// Ensure UI Kit build (ui-kit.esm.js) exists
+// and warn in case it is old.
+const info = (() => {
+  try {
+    return fs.statSync('./dist/ui-kit.esm.js');
+  } catch (e) {
+    return null;
+  }
+})();
+const twoMinutesAgo = Date.now() - 2 * 60 * 1000;
+if (!info) {
+  // We can only test ui-kit when it was built first
+  // eslint-disable-next-line no-console
+  console.info(
+    '\x1b[33m%s\x1b[0m', // log in yellow
+    '⚠️  You need to run "yarn build" or "yarn build:watch" before starting the bundle tests!'
+  );
+  process.exit(1);
+} else if (info.mtime < twoMinutesAgo) {
+  // eslint-disable-next-line no-console
+  console.info(
+    '\x1b[33m%s\x1b[0m', // log in yellow
+    "⏳  The ui-kit build is more than two minutes old! Are you sure you're testing the latest version?"
+  );
+}
 
 module.exports = {
   displayName: 'bundle',
