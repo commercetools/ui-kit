70f95908452e2b4ff7df86eda5329a8e22a62922
Dominik Ferber <dominik.ferber@gmail.com>
2018-11-12 13:39:11 +0100

    test: add locale data to tests

--- src/test-utils.js
+++ src/test-utils.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { render } from 'react-testing-library';
-import { IntlProvider } from 'react-intl';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import de from 'react-intl/locale-data/de';
+import es from 'react-intl/locale-data/es';
 import messages from '../i18n/core.json';
 
-const customRender = (node, ...options) =>
-  render(
-    <IntlProvider locale="en" messages={messages}>
+addLocaleData(en);
+addLocaleData(de);
+addLocaleData(es);
+
+const customRender = (node, opts = {}) => {
+  const { locale = 'en', ...options } = opts;
+
+  return render(
+    <IntlProvider locale={locale} messages={messages}>
       {node}
     </IntlProvider>,
-    ...options
+    options
   );
+};
 
 // re-export everything
 export * from 'react-testing-library';
