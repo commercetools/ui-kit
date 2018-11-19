ec43753c52aad6e7f916b64c6b428cc74de88c91
qmateub <qmateub@yahoo.es>
2018-11-19 15:47:13 +0100

    feat(story): add story for avatar component

---
+++ src/components/avatar/avatar.story.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
+import withReadme from 'storybook-readme/with-readme';
+import Section from '../../../.storybook/decorators/section';
+import Avatar from './avatar';
+import Readme from './README.md';
+
+storiesOf('Images', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withReadme(Readme))
+  .add('Avatar', () => {
+    const size = select('size', ['s', 'm', 'l'], 'l');
+
+    const wrapperSize = do {
+      if (size === 's') '26px';
+      else if (size === 'm') '48px';
+      else '100px';
+    };
+
+    return (
+      <Section>
+        <div
+          style={{
+            width: wrapperSize,
+            height: wrapperSize,
+          }}
+        >
+          <Avatar
+            firstName={text('firstName', 'Jon')}
+            lastName={text('lastName', 'Snow')}
+            gravatarHash={text('gravatarHash', '')}
+            isHighlighted={boolean('isHighlighted', false)}
+            size={size}
+          />
+        </div>
+      </Section>
+    );
+  });
