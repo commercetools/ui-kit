adbb5d39f8af3ba21ed0f5b2695e97268e25b02a
qmateub <qmateub@yahoo.es>
2018-11-22 16:07:21 +0100

    test(avatar): refactor to use react-testing-library

--- src/components/avatar/avatar.spec.js
+++ src/components/avatar/avatar.spec.js
@@ -1,87 +1,43 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import Avatar, { Initials, GravatarImg } from './avatar';
-import styles from './avatar.mod.css';
+import oneLineTrim from 'common-tags/lib/oneLineTrim';
+import Avatar from './avatar';
+import { render } from '../../test-utils';
 
 const createTestProps = customProps => ({
   firstName: '',
   lastName: '',
   gravatarHash: '20c9c1b252b46ab49d6f7a4cee9c3e68',
   isHighlighted: false,
-  scale: 'l',
+  size: 'l',
   ...customProps,
 });
 
-describe('render', () => {
-  let wrapper;
-  let props;
+describe('Avatar', () => {
+  const props = createTestProps();
+  it('should pass data-attributes all way to the DOM', () => {
+    const { container } = render(<Avatar data-foo="bar" {...props} />);
 
-  beforeEach(() => {
-    props = createTestProps();
-    wrapper = shallow(<Avatar {...props} />);
+    expect(container.querySelector("[data-foo='bar']")).toBeInTheDocument();
   });
 
-  it('should have base style', () => {
-    expect(wrapper).toHaveClassName(styles['avatar-base']);
+  it('should render an <img> with `src` attribute', () => {
+    const { container } = render(<Avatar data-foo="bar" {...props} />);
+
+    expect(container.querySelector('img')).toHaveAttribute(
+      'src',
+      'https://www.gravatar.com/avatar/20c9c1b252b46ab49d6f7a4cee9c3e68?s=100&d=blank'
+    );
   });
 
-  it('should render <GravatarImg>', () => {
-    expect(wrapper).toRender(GravatarImg);
-  });
+  it('should render an <img> with `srcset` attribute', () => {
+    const { container } = render(<Avatar data-foo="bar" {...props} />);
 
-  it('should render <Initials>', () => {
-    expect(wrapper).toRender(Initials);
-  });
-
-  describe('<GravatarImg />', () => {
-    describe('with regular email', () => {
-      beforeEach(() => {
-        wrapper = shallow(
-          <GravatarImg scale="s" hash="20c9c1b252b46ab49d6f7a4cee9c3e68" />
-        );
-      });
-
-      it('should match snapshot', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-    });
-  });
-
-  describe('when large scale', () => {
-    beforeEach(() => {
-      props = createTestProps();
-      wrapper = shallow(<Avatar {...props} />);
-    });
-
-    it('should contain style for large scale', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-  describe('when medium scale', () => {
-    beforeEach(() => {
-      props = createTestProps({ scale: 'm' });
-      wrapper = shallow(<Avatar {...props} />);
-    });
-    it('should contain style for medium scale', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-  describe('when small scale', () => {
-    beforeEach(() => {
-      props = createTestProps({ scale: 's' });
-      wrapper = shallow(<Avatar {...props} />);
-    });
-    it('should contain style for small scale', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-  describe('when highlighted', () => {
-    beforeEach(() => {
-      props = createTestProps({ isHighlighted: true });
-      wrapper = shallow(<Avatar {...props} />);
-    });
-    it('should contain style for highligting component', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
+    expect(container.querySelector('img')).toHaveAttribute(
+      'srcset',
+      oneLineTrim`
+        https://www.gravatar.com/avatar/20c9c1b252b46ab49d6f7a4cee9c3e68?s=100&d=blank 1x, 
+        https://www.gravatar.com/avatar/20c9c1b252b46ab49d6f7a4cee9c3e68?s=200&d=blank 2x
+      `
+    );
   });
 });
