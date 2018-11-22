f24552b90fc12b6e8a3877287f153cfe8a04fd80
qmateub <qmateub@yahoo.es>
2018-11-22 16:06:52 +0100

    refactor(avatar): rename to size prop and use properly srcset attribute of img

--- src/components/avatar/avatar.js
+++ src/components/avatar/avatar.js
@@ -1,31 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import oneLineTrim from 'common-tags/lib/oneLineTrim';
 import styles from './avatar.mod.css';
-import { getInitialsFromName, createGravatarImgUrl } from './utils';
+import filterDataAttributes from '../../utils/filter-data-attributes';
 
-export const GravatarImg = props => {
-  const imageUrl = createGravatarImgUrl(props.hash);
-  return (
-    <img
-      className={classnames(styles['gravatar-img'], {
-        [styles['gravatar-image-small']]: props.scale === 's',
-        [styles['gravatar-image-medium']]: props.scale === 'm',
-        [styles['gravatar-image-large']]: props.scale === 'l',
-      })}
-      src={imageUrl}
-      srcSet={imageUrl}
-    />
-  );
+const getFirstChar = str =>
+  typeof str === 'string'
+    ? str
+        .trim()
+        .slice(0, 1)
+        .toUpperCase()
+    : '';
+
+const getInitialsFromName = ({ firstName = '', lastName = '' }) =>
+  `${getFirstChar(firstName)}${getFirstChar(lastName)}`;
+
+/**
+ * `s` - defines the size. We want a bigger one if the user is on a retina-display
+ * `d` - defines the default if the user is not known to Gravatar. It returns a blank image,
+ *        which let the initials underneath shine through
+ *
+ * @see: https://de.gravatar.com/site/implement/images/
+ */
+const createGravatarImgUrl = (md5Hash, size) =>
+  `https://www.gravatar.com/avatar/${md5Hash}?s=${size}&d=blank`;
+
+const getSizeInPx = size => do {
+  if (size === 'l') 100;
+  else if (size === 'm') 48;
+  else 26;
 };
+
+const GravatarImg = props => (
+  <img
+    className={classnames(styles['gravatar-img'], {
+      [styles['gravatar-image-small']]: props.size === 's',
+      [styles['gravatar-image-medium']]: props.size === 'm',
+      [styles['gravatar-image-large']]: props.size === 'l',
+    })}
+    src={createGravatarImgUrl(props.hash, getSizeInPx(props.size))}
+    srcSet={oneLineTrim`
+      ${createGravatarImgUrl(props.hash, getSizeInPx(props.size))} 1x, 
+      ${createGravatarImgUrl(props.hash, getSizeInPx(props.size) * 2)} 2x
+    `}
+  />
+);
 
 GravatarImg.displayName = 'GravatarImg';
 GravatarImg.propTypes = {
   hash: PropTypes.string,
-  scale: PropTypes.oneOf(['s', 'm', 'l']).isRequired,
+  size: PropTypes.oneOf(['s', 'm', 'l']).isRequired,
 };
 
-export const Initials = props => (
+const Initials = props => (
   <div className={styles.initials}>
     {getInitialsFromName({
       firstName: props.firstName,
@@ -44,13 +72,14 @@ const Avatar = props => (
   <div
     className={classnames(
       styles['avatar-base'],
-      styles[`avatar-${props.scale}`],
+      styles[`avatar-${props.size}`],
       {
         [styles['avatar-hover']]: props.isHighlighted,
       }
     )}
+    {...filterDataAttributes(props)}
   >
-    <GravatarImg hash={props.gravatarHash} scale={props.scale} />
+    <GravatarImg hash={props.gravatarHash} size={props.size} />
     <Initials firstName={props.firstName} lastName={props.lastName} />
   </div>
 );
@@ -59,7 +88,7 @@ Avatar.defaultProps = {
   firstName: '',
   lastName: '',
   isHighlighted: false,
-  scale: 's',
+  size: 's',
 };
 
 Avatar.propTypes = {
@@ -67,7 +96,7 @@ Avatar.propTypes = {
   lastName: PropTypes.string,
   gravatarHash: PropTypes.string.isRequired,
   isHighlighted: PropTypes.bool,
-  scale: PropTypes.oneOf(['s', 'm', 'l']).isRequired,
+  size: PropTypes.oneOf(['s', 'm', 'l']).isRequired,
 };
 
 export default Avatar;
