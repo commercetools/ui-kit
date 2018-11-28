d540dba9e0130eaac44477e00d972847188f6247
Dominik Ferber <dominik.ferber@gmail.com>
2018-11-28 13:54:02 +0100

    test(text-input): add constraints

--- src/components/inputs/text-input/text-input.percy.js
+++ src/components/inputs/text-input/text-input.percy.js
@@ -3,14 +3,32 @@ import TextInput from './text-input';
 
 suite('TextInput', () => {
   percySnapshot('enabled', () => (
-    <TextInput value="Some value" onChange={() => {}} />
+    <TextInput
+      value="Some value"
+      onChange={() => {}}
+      horizontalConstraint="m"
+    />
   ));
 
   percySnapshot('disabled', () => (
-    <TextInput value="Some value" onChange={() => {}} isDisabled={true} />
+    <TextInput
+      value="Some value"
+      onChange={() => {}}
+      horizontalConstraint="m"
+      isDisabled={true}
+    />
   ));
 
   percySnapshot('placeholder', () => (
-    <TextInput value="" onChange={() => {}} placeholder="Some placeholder" />
+    <TextInput
+      value=""
+      onChange={() => {}}
+      horizontalConstraint="m"
+      placeholder="Some placeholder"
+    />
+  ));
+
+  percySnapshot('horizontalConstraint=l', () => (
+    <TextInput value="" onChange={() => {}} horizontalConstraint="l" />
   ));
 });
