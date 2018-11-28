a3d286f1af584cb59fa771736cde4939eaa42cae
Dominik Ferber <dominik.ferber@gmail.com>
2018-11-28 21:46:17 +0100

    chore: add storybook percy integration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,28 @@
-import { configure, setAddon, addDecorator } from '@storybook/react';
+import {
+  configure,
+  setAddon,
+  addDecorator,
+  getStorybook,
+} from '@storybook/react';
+import createPercyAddon from '@percy-io/percy-storybook';
 import { withOptions } from '@storybook/addon-options';
 import IntlDecorator from './decorators/intl';
-// import infoAddon from '@storybook/addon-info';
-
-// setAddon(infoAddon);
-
 import './main.mod.css';
+
+const { percyAddon, serializeStories } = createPercyAddon();
+setAddon(percyAddon);
+
+const srcStories = require.context('../src', true, /\.story\.js$/);
+const materialsStories = require.context('../materials', true, /\.story\.js$/);
+const exampleStories = require.context('../examples', true, /\.story\.js$/);
+
+function loadStories() {
+  srcStories.keys().forEach(filename => srcStories(filename));
+  materialsStories.keys().forEach(filename => materialsStories(filename));
+  exampleStories.keys().forEach(filename => exampleStories(filename));
+}
+
+addDecorator(IntlDecorator);
 
 addDecorator(
   withOptions({
@@ -24,16 +41,8 @@ addDecorator(
   })
 );
 
-const srcStories = require.context('../src', true, /\.story\.js$/);
-const materialsStories = require.context('../materials', true, /\.story\.js$/);
-const exampleStories = require.context('../examples', true, /\.story\.js$/);
-
-function loadStories() {
-  srcStories.keys().forEach(filename => srcStories(filename));
-  materialsStories.keys().forEach(filename => materialsStories(filename));
-  exampleStories.keys().forEach(filename => exampleStories(filename));
-}
-
-addDecorator(IntlDecorator);
-
 configure(loadStories, module);
+
+// NOTE: if you're using the Storybook options addon, call serializeStories
+//       *BEFORE* the setOptions call
+serializeStories(getStorybook);
