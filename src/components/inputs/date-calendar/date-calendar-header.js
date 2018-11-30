2a8c7b2f7ccfd55f4af2678dc0728957d3f11e84
Dominik Ferber <dominik.ferber@gmail.com>
2018-11-30 09:56:17 +0100

    feat(date-calendar): use circle icon for today button

--- src/components/inputs/date-calendar/date-calendar-header.js
+++ src/components/inputs/date-calendar/date-calendar-header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Text from '../../typography/text';
 import styles from './date-calendar-header.mod.css';
-import { AngleLeftIcon, AngleRightIcon, PinActiveIcon } from '../../icons';
+import { AngleLeftIcon, AngleRightIcon, CircleIcon } from '../../icons';
 import SecondaryIconButton from '../../buttons/secondary-icon-button';
 
 const DateCalendarHeader = props => (
@@ -16,7 +16,7 @@ const DateCalendarHeader = props => (
       <SecondaryIconButton
         label="next month"
         onClick={props.onTodayClick}
-        icon={<PinActiveIcon size="medium" />}
+        icon={<CircleIcon size="medium" />}
       />
       <SecondaryIconButton
         label="next month"
