fbf7eeaa797f895321e4d0b3b4d1c4ad114d1080
Malcolm Laing <malcolm.laing@commercetools.de>
2018-12-10 16:51:41 +0100

    chore: update comment on run-percy

--- scripts/run-percy.sh
+++ scripts/run-percy.sh
@@ -3,7 +3,10 @@
 # skip percy builds when this is not a pull request or when it's not master
 if [[ $TRAVIS_PULL_REQUEST != 'false' || ${TRAVIS_BRANCH} == 'master' ]]
   then
-  # skip percy builds when pull request is opened by a robot
+  # skip percy builds when pull request is opened by a renovate
+  # if renovate opens an average of 6 PRs per week, that means, 24 PRs per month.
+  # 24 * ~50 snapshots = 1200, or almost 25% of our current percy snapshot allowances.
+  # if in the future, we drop more 💸 on percy, we can remove this.
   regexp='^renovate\/'
   if ! [[ ${TRAVIS_PULL_REQUEST_BRANCH} =~ ${regexp} ]]
     then
