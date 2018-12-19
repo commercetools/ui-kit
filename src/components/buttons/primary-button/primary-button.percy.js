0ce15d49e3213aae0a5c12f9e19c46ad2c59b578
Malcolm Laing <malcolm.laing@commercetools.de>
2018-12-19 10:14:04 +0100

    chore: remove useless percy tests

--- src/components/buttons/primary-button/primary-button.percy.js
+++ src/components/buttons/primary-button/primary-button.percy.js
@@ -56,17 +56,5 @@ screenshot('PrimaryButton', () => (
     <Spec label='tone - when "primary"'>
       <PrimaryButton label="A label text" onClick={() => {}} tone="primary" />
     </Spec>
-
-    <Spec label='type - when "button"'>
-      <PrimaryButton label="A label text" onClick={() => {}} type="button" />
-    </Spec>
-
-    <Spec label='type - when "submit"'>
-      <PrimaryButton label="A label text" onClick={() => {}} type="submit" />
-    </Spec>
-
-    <Spec label='type - when "reset"'>
-      <PrimaryButton label="A label text" onClick={() => {}} type="reset" />
-    </Spec>
   </Suite>
 ));
