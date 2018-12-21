903f735f3aa0d53aac56e07975e81f9cb87ca6f5
Dominik Ferber <dominik.ferber@gmail.com>
2018-12-21 13:28:04 +0100

    test(tag): refactor to react-testing-library

--- src/components/tag/tag.spec.js
+++ src/components/tag/tag.spec.js
@@ -1,374 +1,120 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { shallow } from 'enzyme';
-import { CloseBoldIcon } from '../icons';
-import Tag, { TagNormalBody, TagLinkBody } from './tag';
+import { render } from '../../test-utils';
+import Tag from './tag';
 
-describe('`<TagLinkBody />`', () => {
-  const createTestProps = custom => ({
-    type: 'normal',
-    onClick: jest.fn(),
-    linkTo: 'foo-too',
-    isDisabled: false,
-    children: 'Icecream we scream!',
-    ...custom,
-  });
-  let props;
-  let wrapper;
+it('should render children', () => {
+  const { container } = render(<Tag>Bread</Tag>);
+  expect(container).toHaveTextContent('Bread');
+});
 
-  describe('rendering', () => {
-    beforeEach(() => {
-      props = createTestProps();
+it('should call onClick when clicked', () => {
+  const onClick = jest.fn();
+  const { getByText } = render(<Tag onClick={onClick}>Bread</Tag>);
+  getByText('Bread').click();
+  expect(onClick).toHaveBeenCalledTimes(1);
+});
 
-      wrapper = shallow(<TagLinkBody {...props} />);
-    });
+it('should call onRemove when remove icon is clicked', () => {
+  const onRemove = jest.fn();
+  const { getByLabelText } = render(<Tag onRemove={onRemove}>Bread</Tag>);
+  getByLabelText('Remove').click();
+  expect(onRemove).toHaveBeenCalledTimes(1);
+});
 
-    describe('when enabled (`isDisabled`)', () => {
-      it('should match the recoreded snapshot', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
+it('should call the right handler when onClick and onRemove are provided', () => {
+  const onClick = jest.fn();
+  const onRemove = jest.fn();
+  const { getByText, getByLabelText } = render(
+    <Tag onRemove={onRemove} onClick={onClick}>
+      Bread
+    </Tag>
+  );
 
-      it('should supply `to` to `Link`', () => {
-        expect(wrapper.find(Link)).toHaveProp('to', props.linkTo);
-      });
+  // clicking the Tag
+  getByText('Bread').click();
+  expect(onClick).toHaveBeenCalledTimes(1);
+  expect(onRemove).not.toHaveBeenCalled();
 
-      it('should apply clickable class name', () => {
-        expect(wrapper.find('div')).toHaveClassName('clickableContentWrapper');
-      });
+  // clicking the remove button
+  getByLabelText('Remove').click();
+  expect(onRemove).toHaveBeenCalledTimes(1);
+  expect(onClick).toHaveBeenCalledTimes(1);
+});
 
-      it('should apply link class name', () => {
-        expect(wrapper.find('div')).toHaveClassName('plainLink');
-      });
-    });
-
-    describe('when disabled (`isDisabled`)', () => {
-      beforeEach(() => {
-        props = createTestProps({ isDisabled: true });
-
-        wrapper = shallow(<TagLinkBody {...props} />);
-      });
-
-      it('should match the recoreded snapshot', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('should render a `Link`', () => {
-        expect(wrapper).not.toRender('Link');
-      });
-    });
-
-    describe('when removeable (`onRemove`)', () => {
-      beforeEach(() => {
-        props = createTestProps({ onRemove: jest.fn() });
-
-        wrapper = shallow(<TagLinkBody {...props} />);
-      });
-
-      it('should apply removeable class name', () => {
-        expect(wrapper.find('div')).toHaveClassName('removableContent');
-      });
-    });
+describe('when disabled', () => {
+  it('should not call onClick when clicked', () => {
+    const onClick = jest.fn();
+    const { getByText } = render(
+      <Tag onClick={onClick} isDisabled={true}>
+        Bread
+      </Tag>
+    );
+    getByText('Bread').click();
+    expect(onClick).not.toHaveBeenCalled();
   });
 
-  describe('callbacks', () => {
-    beforeEach(() => {
-      props = createTestProps();
-
-      wrapper = shallow(<TagLinkBody {...props} />);
-    });
-
-    describe('when enabled (`isDisabled`)', () => {
-      describe('of `<Link />`', () => {
-        describe('`onClick`', () => {
-          beforeEach(() => {
-            wrapper.find('Link').prop('onClick')();
-          });
-
-          it('should invoke `onClick`', () => {
-            expect(props.onClick).toHaveBeenCalled();
-          });
-        });
-      });
-    });
+  it('should not call onRemove when clicked', () => {
+    const onRemove = jest.fn();
+    const { getByLabelText } = render(
+      <Tag onRemove={onRemove} isDisabled={true}>
+        Bread
+      </Tag>
+    );
+    getByLabelText('Remove').click();
+    expect(onRemove).not.toHaveBeenCalled();
   });
 });
 
-describe('`<TagNormalBody />`', () => {
-  const createTestProps = custom => ({
-    type: 'normal',
-    onClick: jest.fn(),
-    isDisabled: false,
-    children: 'Icecream we scream!',
-    ...custom,
-  });
-  let props;
-  let wrapper;
+describe('when linkTo is set', () => {
+  it('should redirect when clicked', () => {
+    const { getByText, history } = render(<Tag linkTo="/foo">Bread</Tag>);
+    getByText('Bread').click();
 
-  describe('rendering', () => {
-    beforeEach(() => {
-      props = createTestProps();
-
-      wrapper = shallow(<TagNormalBody {...props} />);
-    });
-
-    it('should output correct tree', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    describe('when enabled (`isDisabled`)', () => {
-      it('should supply `onClick` to the main div', () => {
-        expect(wrapper.find('div')).toHaveProp('onClick', props.onClick);
-      });
-
-      describe('when clickable (`onClick`)', () => {
-        beforeEach(() => {
-          props = createTestProps({ onClick: jest.fn() });
-
-          wrapper = shallow(<TagNormalBody {...props} />);
-        });
-
-        it('should apply clickable class name', () => {
-          expect(wrapper.find('div')).toHaveClassName(
-            'clickableContentWrapper'
-          );
-        });
-      });
-    });
-
-    describe('when disabled (`isDisabled`)', () => {
-      beforeEach(() => {
-        props = createTestProps({ isDisabled: true });
-
-        wrapper = shallow(<TagNormalBody {...props} />);
-      });
-
-      it('should apply disabled class name', () => {
-        expect(wrapper.find('div')).toHaveClassName('disabledContent');
-      });
-    });
-
-    describe('when removable (`onRemove`)', () => {
-      beforeEach(() => {
-        props = createTestProps({ onRemove: jest.fn() });
-
-        wrapper = shallow(<TagNormalBody {...props} />);
-      });
-
-      it('should apply removable class name', () => {
-        expect(wrapper.find('div')).toHaveClassName('removableContent');
-      });
-    });
+    expect(history.location.pathname).toBe('/foo');
   });
 
-  describe('callbacks', () => {
-    beforeEach(() => {
-      props = createTestProps();
+  it('should still call onClick when clicked', () => {
+    const onClick = jest.fn();
+    const { getByText, history } = render(
+      <Tag onClick={onClick} linkTo="/foo">
+        Bread
+      </Tag>
+    );
+    getByText('Bread').click();
+    expect(onClick).toHaveBeenCalled();
 
-      wrapper = shallow(<TagNormalBody {...props} />);
-    });
-
-    describe('when enabled (`isDisabled`)', () => {
-      describe('of `<div />`', () => {
-        describe('`onClick`', () => {
-          beforeEach(() => {
-            wrapper.find('div').prop('onClick')();
-          });
-
-          it('should invoke `onClick`', () => {
-            expect(props.onClick).toHaveBeenCalled();
-          });
-        });
-      });
-    });
-  });
-});
-
-describe('<Tag />', () => {
-  const createTestProps = custom => ({
-    children: 'Icecream we scream!',
-    ...custom,
+    expect(history.location.pathname).toBe('/foo');
   });
 
-  describe('rendering', () => {
-    let props;
-    let wrapper;
-    let tagWrapper;
+  it('should not redirect when removed', () => {
+    const onRemove = jest.fn();
+    const { getByLabelText, history } = render(
+      <Tag onRemove={onRemove} linkTo="/foo">
+        Bread
+      </Tag>
+    );
 
-    describe('when of type `normal` (default)', () => {
-      beforeEach(() => {
-        props = createTestProps();
+    getByLabelText('Remove').click();
 
-        wrapper = shallow(<Tag {...props} />);
-        tagWrapper = wrapper.find('.wrapper');
-      });
+    // ensure "onRemove" is stil called
+    expect(onRemove).toHaveBeenCalled();
 
-      it('should output correct tree', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('should apply `wrapperTypeNormal` class name', () => {
-        expect(tagWrapper).toHaveClassName('wrapperTypeNormal');
-      });
-    });
-
-    describe('when of type `warning`', () => {
-      beforeEach(() => {
-        props = createTestProps({ type: 'warning' });
-
-        wrapper = shallow(<Tag {...props} />);
-        tagWrapper = wrapper.find('.wrapper');
-      });
-
-      it('should output correct tree', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('should apply `wrapperTypeWarning` class name', () => {
-        expect(tagWrapper).toHaveClassName('wrapperTypeWarning');
-      });
-    });
-
-    describe('when disabled', () => {
-      beforeEach(() => {
-        props = createTestProps({
-          isDisabled: true,
-        });
-
-        wrapper = shallow(<Tag {...props} />);
-        tagWrapper = wrapper.find('.wrapper');
-      });
-
-      it('should output correct tree', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('should apply `disabledWrapper` class name', () => {
-        expect(tagWrapper).toHaveClassName('disabledWrapper');
-      });
-    });
-
-    describe('when removable (`onRemove`)', () => {
-      beforeEach(() => {
-        props = createTestProps({
-          onRemove: jest.fn(),
-        });
-
-        wrapper = shallow(<Tag {...props} />);
-      });
-
-      it('should output correct tree', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('should render `CloseBoldIcon`', () => {
-        expect(wrapper).toRender(CloseBoldIcon);
-      });
-    });
-
-    describe('when `linkTo` is set', () => {
-      beforeEach(() => {
-        props = createTestProps({
-          linkTo: '/foo',
-          onClick: jest.fn(),
-          isDisabled: false,
-        });
-
-        wrapper = shallow(<Tag {...props} />);
-      });
-
-      it('should output correct tree', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('should render a `<TagLinkBody />`', () => {
-        expect(wrapper).toRender(TagLinkBody);
-      });
-
-      it('should supply `linkTo` to `<TagLinkBody />`', () => {
-        expect(wrapper.find(TagLinkBody)).toHaveProp('linkTo', props.linkTo);
-      });
-
-      it('should supply `onClick` to `<TagLinkBody />`', () => {
-        expect(wrapper.find(TagLinkBody)).toHaveProp('onClick', props.onClick);
-      });
-
-      it('should supply `onRemove` to `<TagLinkBody />`', () => {
-        expect(wrapper.find(TagLinkBody)).toHaveProp(
-          'onRemove',
-          props.onRemove
-        );
-      });
-
-      it('should supply `isDisabled` to `<TagLinkBody />`', () => {
-        expect(wrapper.find(TagLinkBody)).toHaveProp(
-          'isDisabled',
-          props.isDisabled
-        );
-      });
-    });
-
-    describe('when `linkTo` is not set', () => {
-      beforeEach(() => {
-        props = createTestProps({
-          onClick: jest.fn(),
-          onRemove: jest.fn(),
-          isDisabled: false,
-        });
-
-        wrapper = shallow(<Tag {...props} />);
-      });
-
-      it('should output correct tree', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('should render a `<TagNormalBody />`', () => {
-        expect(wrapper).toRender(TagNormalBody);
-      });
-
-      it('should supply `onRemove` to `<TagNormalBody />`', () => {
-        expect(wrapper.find(TagNormalBody)).toHaveProp(
-          'onRemove',
-          props.onRemove
-        );
-      });
-
-      it('should supply `onClick` to `<TagNormalBody />`', () => {
-        expect(wrapper.find(TagNormalBody)).toHaveProp(
-          'onClick',
-          props.onClick
-        );
-      });
-
-      it('should supply `isDisabled` to `<TagNormalBody />`', () => {
-        expect(wrapper.find(TagNormalBody)).toHaveProp(
-          'isDisabled',
-          props.isDisabled
-        );
-      });
-    });
+    // ensure the pathname is not "/foo", otherwise a redirect would have
+    // happend
+    expect(history.location.pathname).toBe('/');
   });
 
-  describe('callbacks', () => {
-    let props;
-    let wrapper;
+  it('should not redirect when disabled', () => {
+    const { getByText, history } = render(
+      <Tag linkTo="/foo" isDisabled={true}>
+        Bread
+      </Tag>
+    );
 
-    describe('of <AccessibleButton />', () => {
-      describe('when enabled (`isDisabled`)', () => {
-        describe('onClick', () => {
-          beforeEach(() => {
-            props = createTestProps({ onRemove: jest.fn() });
+    getByText('Bread').click();
 
-            wrapper = shallow(<Tag {...props} />);
-
-            wrapper.find('AccessibleButton').prop('onClick')();
-          });
-
-          it('should invoke `onRemove`', () => {
-            expect(props.onRemove).toHaveBeenCalled();
-          });
-        });
-      });
-    });
+    // ensure the pathname is not "/foo", otherwise a redirect would have
+    // happend
+    expect(history.location.pathname).toBe('/');
   });
 });
