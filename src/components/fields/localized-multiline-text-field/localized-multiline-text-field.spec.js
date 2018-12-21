98e5fa56bbb09464b9746a6d27465f412e31fe7c
Dominik Ferber <dominik.ferber@gmail.com>
2018-12-21 21:27:22 +0100

    test(localized-multiline-text-field): refactor to react-testing-library

--- src/components/fields/localized-multiline-text-field/localized-multiline-text-field.spec.js
+++ src/components/fields/localized-multiline-text-field/localized-multiline-text-field.spec.js
@@ -1,189 +1,245 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import PropTypes from 'prop-types';
+import { render, fireEvent } from '../../../test-utils';
 import LocalizedMultilineTextField from './localized-multiline-text-field';
-import FieldLabel from '../../field-label';
-import LocalizedMultilineTextInput from '../../inputs/localized-multiline-text-input';
-import FieldErrors from '../../field-errors';
-import { AddBoldIcon } from '../../icons';
 
-const createTestProps = customProps => ({
-  title: 'Description',
-  value: { en: '', de: '' },
-  selectedLanguage: 'en',
-  onChange: () => jest.fn(),
-  ...customProps,
+// This component is used to enable easy testing.
+// It overwrites the onChange function and places a label for the
+// input component. It also ensures an id so that the label can associate
+// the input. This allows tests to use getByLabelText.
+// It also makes sure the event's value passed to onChange flows back to the
+// component so that we can test it under real conditions.
+// As a convenience, we enable accessing a mocked onChange function.
+class Story extends React.Component {
+  static displayName = 'Story';
+  static propTypes = {
+    onChange: PropTypes.func,
+    value: PropTypes.objectOf(PropTypes.string),
+    id: PropTypes.string,
+  };
+  static defaultProps = {
+    title: 'foo',
+    id: 'text-field',
+    value: { en: '', de: '' },
+    selectedLanguage: 'en',
+  };
+  state = {
+    value: this.props.value,
+  };
+  handleChange = event => {
+    if (this.props.onChange) this.props.onChange(event);
+
+    this.setState(prevState => ({
+      value: {
+        ...prevState.value,
+        [event.target.language]: event.target.value,
+      },
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <label htmlFor={this.props.id}>LocalizedMultilineTextField</label>
+        <LocalizedMultilineTextField
+          id={this.props.id}
+          {...this.props}
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+const renderLocalizedMultilineTextField = (props, options) =>
+  render(<Story {...props} />, options);
+
+it('should render a text field', () => {
+  const { getByLabelText } = renderLocalizedMultilineTextField();
+  expect(getByLabelText('EN')).toBeInTheDocument();
 });
 
-describe('rendering', () => {
-  describe('data attributes', () => {
-    let textInput;
-    beforeEach(() => {
-      const props = createTestProps({
-        'data-foo': 'bar',
-        'data-test': 'baz',
-      });
-      const wrapper = shallow(<LocalizedMultilineTextField {...props} />);
-      textInput = wrapper.find(LocalizedMultilineTextInput);
-    });
-    it('should forward the attributes to the LocalizedMultilineTextInput', () => {
-      expect(textInput).toHaveProp('data-foo', 'bar');
-      expect(textInput).toHaveProp('data-test', 'baz');
-    });
+it('shoult render a title', () => {
+  const { getByText } = renderLocalizedMultilineTextField({
+    title: 'foo title',
   });
-  describe('when no id is provided', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({ id: undefined });
-      wrapper = shallow(<LocalizedMultilineTextField {...props} />);
-    });
-    it('should add a default id attribute', () => {
-      expect(wrapper.find(LocalizedMultilineTextInput)).toHaveProp(
-        'id',
-        expect.stringMatching(/.+/)
-      );
-    });
-    it('should add a default htmlFor attribute', () => {
-      expect(wrapper.find(FieldLabel)).toHaveProp('htmlFor');
-    });
-    it('should use the same value for the id and htmlFor attribute', () => {
-      expect(wrapper.find(LocalizedMultilineTextInput).prop('id')).toEqual(
-        wrapper.find(FieldLabel).prop('htmlFor')
-      );
-    });
-  });
-  describe('when touched', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({
-        // LocalizedMultilineTextField
-        id: 'foo',
-        // FieldLabel
-        title: 'Description',
-        hint: 'Some hint',
-        hintIcon: <AddBoldIcon />,
-        description: 'A description',
-        onInfoButtonClick: jest.fn(),
-        badge: <div>Some badge</div>,
+  expect(getByText('foo title')).toBeInTheDocument();
+});
 
-        // LocalizedMultilineTextField
-        name: 'field1',
-        value: {
-          en: 'Parrot that knows how to party',
-          de: 'Papagei der ordentlich abfeiert',
-        },
-        onChange: jest.fn(),
-        onBlur: jest.fn(),
-        onFocus: jest.fn(),
-        isAutofocussed: true,
-        isDisabled: false,
-        isReadOnly: false,
-        placeholder: { en: 'Some placeholder' },
+it('should forward data-attributes', () => {
+  const { container } = renderLocalizedMultilineTextField({
+    'data-foo': 'bar',
+  });
+  expect(container.querySelector('[data-foo="bar"]')).toBeInTheDocument();
+});
+
+it('should have an HTML name for input, according to the language', () => {
+  const { container } = renderLocalizedMultilineTextField({ name: 'foo' });
+  expect(container.querySelector('[name="foo.en"]')).toBeInTheDocument();
+});
+
+it('should have an HTML name for every input when all inputs are visible', () => {
+  const { container } = renderLocalizedMultilineTextField({
+    name: 'foo',
+    areLanguagesDefaultOpened: true,
+  });
+  expect(container.querySelector('[name="foo.en"]')).toBeInTheDocument();
+  expect(container.querySelector('[name="foo.de"]')).toBeInTheDocument();
+});
+
+it('should call onFocus when the input is focused', () => {
+  const onFocus = jest.fn();
+  const { getByLabelText } = renderLocalizedMultilineTextField({ onFocus });
+  getByLabelText('EN').focus();
+  expect(getByLabelText('EN')).toHaveFocus();
+  expect(onFocus).toHaveBeenCalled();
+});
+
+it('should call onBlur when input loses focus', () => {
+  const onBlur = jest.fn();
+  const { getByLabelText } = renderLocalizedMultilineTextField({ onBlur });
+  getByLabelText('EN').focus();
+  expect(getByLabelText('EN')).toHaveFocus();
+  getByLabelText('EN').blur();
+  expect(getByLabelText('EN')).not.toHaveFocus();
+  expect(onBlur).toHaveBeenCalled();
+});
+
+it('should have focus automatically when isAutofocussed is passed', () => {
+  const { getByLabelText } = renderLocalizedMultilineTextField({
+    isAutofocussed: true,
+  });
+  expect(getByLabelText('EN')).toHaveFocus();
+});
+
+it('should call onChange when changing the value', () => {
+  const onChange = jest.fn(event => {
+    event.persist();
+  });
+  const { getByLabelText } = renderLocalizedMultilineTextField({
+    name: 'name-of-pet',
+    onChange,
+  });
+  fireEvent.change(getByLabelText('EN'), {
+    target: { language: 'en', value: 'foo' },
+  });
+  expect(onChange).toHaveBeenCalledWith(
+    expect.objectContaining({
+      target: expect.objectContaining({
+        name: 'name-of-pet.en',
+        language: 'en',
+        value: 'foo',
+      }),
+    })
+  );
+});
+
+describe('when `description` is passed', () => {
+  it('should render a description', () => {
+    const { getByText } = renderLocalizedMultilineTextField({
+      description: 'foo description',
+    });
+    expect(getByText('foo description')).toBeInTheDocument();
+  });
+});
+
+describe('when `hint` is passed', () => {
+  it('should render a hint', () => {
+    const { getByText } = renderLocalizedMultilineTextField({
+      hint: 'foo hint',
+    });
+    expect(getByText('foo hint')).toBeInTheDocument();
+  });
+});
+
+describe('when `badge` is passed', () => {
+  it('should render a badge', () => {
+    const { getByText } = renderLocalizedMultilineTextField({
+      badge: 'foo badge',
+    });
+    expect(getByText('foo badge')).toBeInTheDocument();
+  });
+});
+
+describe('when disabled', () => {
+  it('should disable the inputs', () => {
+    const { getByLabelText } = renderLocalizedMultilineTextField({
+      isDisabled: true,
+    });
+    expect(getByLabelText('EN')).toHaveAttribute('disabled');
+  });
+  it('should disable all inputs when all languages  are visible', () => {
+    const { getByLabelText } = renderLocalizedMultilineTextField({
+      isDisabled: true,
+      areLanguagesDefaultOpened: true,
+    });
+    expect(getByLabelText('EN')).toHaveAttribute('disabled');
+    expect(getByLabelText('DE')).toHaveAttribute('disabled');
+  });
+});
+
+describe('when readOnly', () => {
+  it('should disable the input', () => {
+    const { getByLabelText } = renderLocalizedMultilineTextField({
+      isReadOnly: true,
+    });
+    expect(getByLabelText('EN')).toHaveAttribute('readonly');
+  });
+  it('should disable all inputs when all languages are visible', () => {
+    const { getByLabelText } = renderLocalizedMultilineTextField({
+      isReadOnly: true,
+      areLanguagesDefaultOpened: true,
+    });
+    expect(getByLabelText('EN')).toHaveAttribute('readonly');
+    expect(getByLabelText('DE')).toHaveAttribute('readonly');
+  });
+});
+
+describe('when required', () => {
+  it('should add `*` to title`', () => {
+    const { getByText } = renderLocalizedMultilineTextField({
+      isRequired: true,
+    });
+    expect(getByText('*')).toBeInTheDocument();
+  });
+});
+
+describe('when showing an info button', () => {
+  it('should render an info button', () => {
+    const onInfoButtonClick = jest.fn();
+    const { getByLabelText } = renderLocalizedMultilineTextField({
+      onInfoButtonClick,
+    });
+    expect(getByLabelText('More Info')).toBeInTheDocument();
+  });
+  it('should call onInfoButtonClick when button is clicked', () => {
+    const onInfoButtonClick = jest.fn();
+    const { getByLabelText } = renderLocalizedMultilineTextField({
+      onInfoButtonClick,
+    });
+    getByLabelText('More Info').click();
+    expect(onInfoButtonClick).toHaveBeenCalled();
+  });
+});
+
+describe('when field is touched and has errors', () => {
+  describe('when field empty', () => {
+    it('should render a default error', () => {
+      const { getByText } = renderLocalizedMultilineTextField({
+        touched: true,
         errors: { missing: true },
-        touched: true,
       });
-      wrapper = shallow(<LocalizedMultilineTextField {...props} />);
-    });
-
-    it('should forward the props for to the related components', () => {
-      const fieldLabel = wrapper.find(FieldLabel);
-      expect(fieldLabel).toHaveProp('title', props.title);
-      expect(fieldLabel).toHaveProp('hint', props.hint);
-      expect(fieldLabel).toHaveProp('hintIcon', props.hintIcon);
-      expect(fieldLabel).toHaveProp('description', props.description);
-      expect(fieldLabel).toHaveProp(
-        'onInfoButtonClick',
-        props.onInfoButtonClick
-      );
-      expect(fieldLabel).toHaveProp('badge', props.badge);
-      expect(fieldLabel).toHaveProp('htmlFor', props.id);
-
-      const textInput = wrapper.find(LocalizedMultilineTextInput);
-      expect(textInput).toHaveProp('name', props.name);
-      expect(textInput).toHaveProp('value', props.value);
-      expect(textInput).toHaveProp('onChange', props.onChange);
-      expect(textInput).toHaveProp('selectedLanguage', props.selectedLanguage);
-      expect(textInput).toHaveProp('onBlur', props.onBlur);
-      expect(textInput).toHaveProp('onFocus', props.onFocus);
-      expect(textInput).toHaveProp('isAutofocussed', props.isAutofocussed);
-      expect(textInput).toHaveProp('isDisabled', props.isDisabled);
-      expect(textInput).toHaveProp('isReadOnly', props.isReadOnly);
-      expect(textInput).toHaveProp('placeholder', props.placeholder);
-      expect(textInput).toHaveProp('hasError', true);
-
-      expect(wrapper).toRender(FieldErrors);
-      expect(wrapper.find(FieldErrors)).toHaveProp('errors', props.errors);
+      expect(getByText(/field is required/i)).toBeInTheDocument();
     });
   });
-
-  describe('when disabled', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({ isDisabled: true });
-      wrapper = shallow(<LocalizedMultilineTextField {...props} />);
-    });
-    it('should disable the LocalizedMultilineTextInput', () => {
-      expect(wrapper.find(LocalizedMultilineTextInput)).toHaveProp(
-        'isDisabled',
-        true
-      );
-    });
-  });
-
-  describe('when read-only', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({ isReadOnly: true });
-      wrapper = shallow(<LocalizedMultilineTextField {...props} />);
-    });
-    it('should mark the LocalizedMultilineTextInput as read-only', () => {
-      expect(wrapper.find(LocalizedMultilineTextInput)).toHaveProp(
-        'isReadOnly',
-        true
-      );
-    });
-  });
-
-  describe('when there are known errors', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({ touched: true, errors: { missing: true } });
-      wrapper = shallow(<LocalizedMultilineTextField {...props} />);
-    });
-    it('should mark the LocalizedMultilineTextInput as erroneous', () => {
-      expect(wrapper.find(LocalizedMultilineTextInput)).toHaveProp(
-        'hasError',
-        true
-      );
-    });
-    it('should render the known error', () => {
-      expect(wrapper).toRender(FieldErrors);
-    });
-  });
-
-  describe('when there are unknown errors', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({
+  describe('when there is a custom error', () => {
+    it('should render the custom error message', () => {
+      const { getByText } = renderLocalizedMultilineTextField({
         touched: true,
-        renderError: jest.fn(key => key),
-        errors: { customError: 5 },
+        errors: { custom: true },
+        renderError: () => 'Custom error',
       });
-      wrapper = shallow(<LocalizedMultilineTextField {...props} />);
-    });
-    it('should mark the NumberInput as erroneous', () => {
-      expect(wrapper.find(LocalizedMultilineTextInput)).toHaveProp(
-        'hasError',
-        true
-      );
-    });
-    it('should forward the error', () => {
-      expect(wrapper.find(FieldErrors)).toHaveProp('errors', props.errors);
+      expect(getByText('Custom error')).toBeInTheDocument();
     });
   });
 });
