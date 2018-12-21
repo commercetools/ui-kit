99481de995aaecb9693f5652c530ba555effec38
Dominik Ferber <dominik.ferber@gmail.com>
2018-12-21 23:08:20 +0100

    Convert MultilineTextField (#358)
    * test(text-field): simplify setup
    
    * test(multiline-text-field): refactor to react-testing-library

--- src/components/fields/multiline-text-field/multiline-text-field.spec.js
+++ src/components/fields/multiline-text-field/multiline-text-field.spec.js
@@ -1,189 +1,181 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import PropTypes from 'prop-types';
+import { render, fireEvent } from '../../../test-utils';
 import MultilineTextField from './multiline-text-field';
-import FieldLabel from '../../field-label';
-import MultilineTextInput from '../../inputs/multiline-text-input';
-import FieldErrors from '../../field-errors';
-import { AddBoldIcon } from '../../icons';
 
-const createTestProps = customProps => ({
-  title: 'Description',
-  value: '',
-  onChange: () => jest.fn(),
-  ...customProps,
+// This component is used to enable easy testing.
+// It overwrites the onChange function and places a label for the
+// input component. It also ensures an id so that the label can associate
+// the input. This allows tests to use getByLabelText.
+// It also makes sure the event's value passed to onChange flows back to the
+// component so that we can test it under real conditions.
+// As a convenience, we enable accessing a mocked onChange function.
+class Story extends React.Component {
+  static displayName = 'Story';
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.string,
+    id: PropTypes.string,
+  };
+  static defaultProps = {
+    id: 'text-field',
+    title: 'foo',
+    onChange: () => {},
+  };
+  state = {
+    value: this.props.value || '',
+  };
+  handleChange = event => {
+    this.props.onChange(event);
+    this.setState({ value: event.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <label htmlFor={this.props.id}>MultilineTextField</label>
+        <MultilineTextField
+          id={this.props.id}
+          {...this.props}
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+const renderMultilineTextField = (props, options) =>
+  render(<Story {...props} />, options);
+
+it('should render a text field', () => {
+  const { getByLabelText } = renderMultilineTextField();
+  expect(getByLabelText('MultilineTextField')).toBeInTheDocument();
 });
 
-describe('rendering', () => {
-  describe('data attributes', () => {
-    let textInput;
-    beforeEach(() => {
-      const props = createTestProps({
-        'data-foo': 'bar',
-        'data-test': 'baz',
-      });
-      const wrapper = shallow(<MultilineTextField {...props} />);
-      textInput = wrapper.find(MultilineTextInput);
-    });
-    it('should forward the attributes to the MultilineTextInput', () => {
-      expect(textInput).toHaveProp('data-foo', 'bar');
-      expect(textInput).toHaveProp('data-test', 'baz');
-    });
-  });
-  describe('when no id is provided', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({ id: undefined });
-      wrapper = shallow(<MultilineTextField {...props} />);
-    });
-    it('should add a default id attribute', () => {
-      expect(wrapper.find(MultilineTextInput)).toHaveProp(
-        'id',
-        expect.stringMatching(/.+/)
-      );
-    });
-    it('should add a default htmlFor attribute', () => {
-      expect(wrapper.find(FieldLabel)).toHaveProp('htmlFor');
-    });
-    it('should use the same value for the id and htmlFor attribute', () => {
-      expect(wrapper.find(MultilineTextInput).prop('id')).toEqual(
-        wrapper.find(FieldLabel).prop('htmlFor')
-      );
-    });
-  });
-  describe('when touched', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({
-        // MultilineTextField
-        id: 'foo',
-        // FieldLabel
-        title: 'Description',
-        hint: 'Some hint',
-        hintIcon: <AddBoldIcon />,
-        description: 'A description',
-        onInfoButtonClick: jest.fn(),
-        badge: <div>Some badge</div>,
+it('shoult render a title', () => {
+  const { getByText } = renderMultilineTextField({ title: 'foo title' });
+  expect(getByText('foo title')).toBeInTheDocument();
+});
 
-        // MultilineTextInput
-        name: 'field1',
-        value: 'foo',
-        onChange: jest.fn(),
-        onBlur: jest.fn(),
-        onFocus: jest.fn(),
-        isAutofocussed: true,
-        isDisabled: false,
-        isDefaultClosed: false,
-        isReadOnly: false,
-        placeholder: 'Some placeholder',
+it('should forward data-attributes', () => {
+  const { container } = renderMultilineTextField({ 'data-foo': 'bar' });
+  expect(container.querySelector('[data-foo="bar"]')).toBeInTheDocument();
+});
+
+it('should have an HTML name', () => {
+  const { container } = renderMultilineTextField({ name: 'foo' });
+  expect(container.querySelector('[name="foo"]')).toBeInTheDocument();
+});
+
+it('should call onFocus when the input is focused', () => {
+  const onFocus = jest.fn();
+  const { getByLabelText } = renderMultilineTextField({ onFocus });
+  getByLabelText('MultilineTextField').focus();
+  expect(getByLabelText('MultilineTextField')).toHaveFocus();
+  expect(onFocus).toHaveBeenCalled();
+});
+
+it('should call onBlur when input loses focus', () => {
+  const onBlur = jest.fn();
+  const { getByLabelText } = renderMultilineTextField({ onBlur });
+  getByLabelText('MultilineTextField').focus();
+  expect(getByLabelText('MultilineTextField')).toHaveFocus();
+  getByLabelText('MultilineTextField').blur();
+  expect(getByLabelText('MultilineTextField')).not.toHaveFocus();
+  expect(onBlur).toHaveBeenCalled();
+});
+
+it('should have focus automatically when isAutofocussed is passed', () => {
+  const { getByLabelText } = renderMultilineTextField({ isAutofocussed: true });
+  expect(getByLabelText('MultilineTextField')).toHaveFocus();
+});
+
+it('should call onChange when changing the value', () => {
+  const onChange = jest.fn();
+  const { getByLabelText } = renderMultilineTextField({ onChange });
+  const event = { target: { value: 'foo' } };
+  fireEvent.change(getByLabelText('MultilineTextField'), event);
+  expect(onChange).toHaveBeenCalled();
+});
+
+describe('when `description` is passed', () => {
+  it('should render a description', () => {
+    const { getByText } = renderMultilineTextField({
+      description: 'foo description',
+    });
+    expect(getByText('foo description')).toBeInTheDocument();
+  });
+});
+
+describe('when `hint` is passed', () => {
+  it('should render a hint', () => {
+    const { getByText } = renderMultilineTextField({ hint: 'foo hint' });
+    expect(getByText('foo hint')).toBeInTheDocument();
+  });
+});
+
+describe('when `badge` is passed', () => {
+  it('should render a badge', () => {
+    const { getByText } = renderMultilineTextField({ badge: 'foo badge' });
+    expect(getByText('foo badge')).toBeInTheDocument();
+  });
+});
+
+describe('when disabled', () => {
+  it('should disable the input', () => {
+    const { getByLabelText } = renderMultilineTextField({ isDisabled: true });
+    expect(getByLabelText('MultilineTextField')).toHaveAttribute('disabled');
+  });
+});
+
+describe('when readOnly', () => {
+  it('should disable the input', () => {
+    const { getByLabelText } = renderMultilineTextField({ isReadOnly: true });
+    expect(getByLabelText('MultilineTextField')).toHaveAttribute('readonly');
+  });
+});
+
+describe('when required', () => {
+  it('should add `*` to title`', () => {
+    const { getByText } = renderMultilineTextField({ isRequired: true });
+    expect(getByText('*')).toBeInTheDocument();
+  });
+});
+
+describe('when showing an info button', () => {
+  it('should render an info button', () => {
+    const onInfoButtonClick = jest.fn();
+    const { getByLabelText } = renderMultilineTextField({
+      onInfoButtonClick,
+    });
+    expect(getByLabelText('More Info')).toBeInTheDocument();
+  });
+  it('should call onInfoButtonClick when button is clicked', () => {
+    const onInfoButtonClick = jest.fn();
+    const { getByLabelText } = renderMultilineTextField({ onInfoButtonClick });
+    getByLabelText('More Info').click();
+    expect(onInfoButtonClick).toHaveBeenCalled();
+  });
+});
+
+describe('when field is touched and has errors', () => {
+  describe('when field empty', () => {
+    it('should render a default error', () => {
+      const { getByText } = renderMultilineTextField({
+        touched: true,
         errors: { missing: true },
-        touched: true,
       });
-      wrapper = shallow(<MultilineTextField {...props} />);
-    });
-
-    it('should forward the props for to the related components', () => {
-      const fieldLabel = wrapper.find(FieldLabel);
-      expect(fieldLabel).toHaveProp('title', props.title);
-      expect(fieldLabel).toHaveProp('hint', props.hint);
-      expect(fieldLabel).toHaveProp('hintIcon', props.hintIcon);
-      expect(fieldLabel).toHaveProp('description', props.description);
-      expect(fieldLabel).toHaveProp(
-        'onInfoButtonClick',
-        props.onInfoButtonClick
-      );
-      expect(fieldLabel).toHaveProp('badge', props.badge);
-      expect(fieldLabel).toHaveProp('htmlFor', props.id);
-
-      const textInput = wrapper.find(MultilineTextInput);
-      expect(textInput).toHaveProp('name', props.name);
-      expect(textInput).toHaveProp('value', props.value);
-      expect(textInput).toHaveProp('onChange', props.onChange);
-      expect(textInput).toHaveProp('onBlur', props.onBlur);
-      expect(textInput).toHaveProp('onFocus', props.onFocus);
-      expect(textInput).toHaveProp('isAutofocussed', props.isAutofocussed);
-      expect(textInput).toHaveProp('isDefaultClosed', props.isDefaultClosed);
-      expect(textInput).toHaveProp('isDisabled', props.isDisabled);
-      expect(textInput).toHaveProp('isReadOnly', props.isReadOnly);
-      expect(textInput).toHaveProp('placeholder', props.placeholder);
-      expect(textInput).toHaveProp('hasError', true);
-
-      expect(wrapper).toRender(FieldErrors);
-      expect(wrapper.find(FieldErrors)).toHaveProp('errors', props.errors);
+      expect(getByText(/field is required/i)).toBeInTheDocument();
     });
   });
-
-  describe('when disabled', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({ isDisabled: true });
-      wrapper = shallow(<MultilineTextField {...props} />);
-    });
-    it('should disable the MultilineTextInput', () => {
-      expect(wrapper.find(MultilineTextInput)).toHaveProp('isDisabled', true);
-    });
-  });
-
-  describe('when read-only', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({ isReadOnly: true });
-      wrapper = shallow(<MultilineTextField {...props} />);
-    });
-    it('should mark the MultilineTextInput as read-only', () => {
-      expect(wrapper.find(MultilineTextInput)).toHaveProp('isReadOnly', true);
-    });
-  });
-
-  describe('when input should be closed by default', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({ isDefaultClosed: true });
-      wrapper = shallow(<MultilineTextField {...props} />);
-    });
-    it('should mark the MultilineTextInput as default closed', () => {
-      expect(wrapper.find(MultilineTextInput)).toHaveProp(
-        'isDefaultClosed',
-        true
-      );
-    });
-  });
-
-  describe('when there are known errors', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({ touched: true, errors: { missing: true } });
-      wrapper = shallow(<MultilineTextField {...props} />);
-    });
-    it('should mark the MultilineTextInput as erroneous', () => {
-      expect(wrapper.find(MultilineTextInput)).toHaveProp('hasError', true);
-    });
-    it('should render the known error', () => {
-      expect(wrapper).toRender(FieldErrors);
-    });
-  });
-
-  describe('when there are unknown errors', () => {
-    let props;
-    let wrapper;
-    beforeEach(() => {
-      props = createTestProps({
+  describe('when there is a custom error', () => {
+    it('should render the custom error message', () => {
+      const { getByText } = renderMultilineTextField({
         touched: true,
-        renderError: jest.fn(key => key),
-        errors: { customError: 5 },
+        errors: { custom: true },
+        renderError: () => 'Custom error',
       });
-      wrapper = shallow(<MultilineTextField {...props} />);
-    });
-    it('should mark the NumberInput as erroneous', () => {
-      expect(wrapper.find(MultilineTextInput)).toHaveProp('hasError', true);
-    });
-    it('should forward the error', () => {
-      expect(wrapper.find(FieldErrors)).toHaveProp('errors', props.errors);
+      expect(getByText('Custom error')).toBeInTheDocument();
     });
   });
 });
