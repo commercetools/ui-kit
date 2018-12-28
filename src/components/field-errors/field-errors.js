1f2b894c7f48560829582ee27c3145456ea2280c
Dominik Ferber <dominik.ferber@gmail.com>
2018-12-28 12:18:15 +0100

    Export FieldErrors component (#371)
    * feat: export FieldErrors
    
    * test(field-errors): add tests

--- src/components/field-errors/field-errors.js
+++ src/components/field-errors/field-errors.js
@@ -36,19 +36,19 @@ const FieldErrors = props => {
             return <ErrorMessage key={key}>{defaultErrorElement}</ErrorMessage>;
 
           // Try to see if we know this error and render that error instead then
-          if (key === 'missing')
+          if (key === FieldErrors.errorTypes.MISSING)
             return (
               <ErrorMessage key={key}>
                 <FormattedMessage {...messages.missingRequiredField} />
               </ErrorMessage>
             );
-          if (key === 'negative')
+          if (key === FieldErrors.errorTypes.NEGATIVE)
             return (
               <ErrorMessage key={key}>
                 <FormattedMessage {...messages.invalidNegativeNumber} />
               </ErrorMessage>
             );
-          if (key === 'fractions')
+          if (key === FieldErrors.errorTypes.FRACTIONS)
             return (
               <ErrorMessage key={key}>
                 <FormattedMessage {...messages.invalidFractionalNumber} />
@@ -56,8 +56,6 @@ const FieldErrors = props => {
             );
           // Render nothing in case the error is not known and no custom error
           // was returned
-          // The input element will still have the red border to indicate an
-          // error in this case.
           return null;
         })}
     </React.Fragment>
@@ -71,6 +69,12 @@ FieldErrors.propTypes = {
   isVisible: PropTypes.bool,
   renderError: PropTypes.func,
   renderDefaultError: PropTypes.func,
+};
+
+FieldErrors.errorTypes = {
+  MISSING: 'missing',
+  NEGATIVE: 'negative',
+  FRACTIONS: 'fractions',
 };
 
 export default FieldErrors;
