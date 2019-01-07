67df3247966e2baba587e7ed0a0a7ec6880e6a7f
Dominik Ferber <dominik.ferber@gmail.com>
2019-01-07 14:57:45 +0100

    docs(storybook): fix PrimaryActionDropdown story (#403)

--- src/components/dropdowns/primary-action-dropdown/primary-action-dropdown.story.js
+++ src/components/dropdowns/primary-action-dropdown/primary-action-dropdown.story.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
 import withReadme from 'storybook-readme/with-readme';
 import Section from '../../../../.storybook/decorators/section';
-import * as icons from '../../icons';
+import { BoxIcon, BrainIcon, FlameIcon } from '../../icons';
 import Readme from './README.md';
 import PrimaryActionDropdown, { Option } from './primary-action-dropdown';
-
-const iconNames = Object.keys(icons);
 
 storiesOf('Components|Dropdowns', module)
   .addDecorator(withKnobs)
@@ -17,21 +15,21 @@ storiesOf('Components|Dropdowns', module)
     <Section>
       <PrimaryActionDropdown>
         <Option
-          iconLeft={React.createElement(
-            icons[select('icon', iconNames, iconNames[0])]
-          )}
+          iconLeft={<BoxIcon />}
           isDisabled={boolean('isPrimaryOptionDisabled', false)}
           onClick={action('onClick primary option')}
         >
           {text('Label of primary action', 'Primary option')}
         </Option>
         <Option
+          iconLeft={<BrainIcon />}
           isDisabled={boolean('isSecondOptionDisabled', false)}
           onClick={action('onClick second option')}
         >
           {text('Label of second action', 'Second option')}
         </Option>
         <Option
+          iconLeft={<FlameIcon />}
           isDisabled={boolean('isThirdOptionDisabled', false)}
           onClick={action('onClick third option')}
         >
