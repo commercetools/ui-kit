2443443e337fad25824544047f5053ad70f62c9b
Malcolm Laing <malcolm.laing@commercetools.de>
2019-01-10 17:03:58 +0100

    fix(error-message): support data-attributes

--- src/components/messages/error-message/error-message.js
+++ src/components/messages/error-message/error-message.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Text from '../../typography/text';
+import filterDataAttributes from '../../../utils/filter-data-attributes';
 
 const ErrorMessage = props => (
-  <Text.Detail tone="negative">{props.children}</Text.Detail>
+  <Text.Detail tone="negative" {...filterDataAttributes(props)}>
+    {props.children}
+  </Text.Detail>
 );
 ErrorMessage.displayName = 'ErrorMessage';
 ErrorMessage.propTypes = {
