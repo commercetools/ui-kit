28ed31712bed992355a3fa885889b258e58f4951
Malcolm Laing <malcolm.laing@commercetools.de>
2019-01-14 14:51:29 +0100

    chore(multiline-text-field): remove unsupported constraints

--- src/components/fields/multiline-text-field/multiline-text-field.story.js
+++ src/components/fields/multiline-text-field/multiline-text-field.story.js
@@ -44,7 +44,7 @@ storiesOf('Components|Fields', module)
               id={name.trim() === '' ? undefined : name}
               horizontalConstraint={select(
                 'horizontalConstraint',
-                ['xs', 's', 'm', 'l', 'xl', 'scale'],
+                ['m', 'l', 'xl', 'scale'],
                 'm'
               )}
               errors={object('errors', { missing: true, customError: true })}
