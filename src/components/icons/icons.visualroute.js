4704adbbdcfc1ab676f9832e4c9a66b43f034ec0
Malcolm Laing <malcolmclaing@gmail.com>
2019-01-16 11:31:32 +0100

    chore: update vrt tests for icons (#438)

--- src/components/icons/icons.visualroute.js
+++ src/components/icons/icons.visualroute.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { Switch, Route } from 'react-router-dom';
 import * as UIKit from 'ui-kit';
 import { Suite, Spec } from '../../../test/percy';
 
-const Inline = styled.div`
+const IconList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+`;
+
+const IconItem = styled.div`
+  padding: 16px;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+`;
+
+const IconContainer = styled.div`
+  margin: 8px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${props => (props.big ? '50px' : '25px')};
 `;
 
 const icons = Object.keys(UIKit).filter(thing => thing.endsWith('Icon'));
@@ -24,23 +41,41 @@ const themes = [
 
 export const routePath = '/icons';
 
+const renderIcon = (iconName, theme, size) => {
+  const Icon = UIKit[iconName];
+  return (
+    <IconItem key={iconName}>
+      <IconContainer big={size === 'scale'}>
+        <Icon theme={theme} size={size} />
+      </IconContainer>
+      <UIKit.Text.Body>{iconName}</UIKit.Text.Body>
+    </IconItem>
+  );
+};
+
 export const component = () => (
-  <Suite>
-    {icons.map(iconName => {
-      const Icon = UIKit[iconName];
-      return sizes.map(size => (
-        <Spec
-          key={`${iconName}-${size}`}
-          label={`${iconName} - ${size}`}
-          omitPropsList
-        >
-          <Inline>
-            {themes.map(theme => (
-              <Icon key={theme} size={size} theme={theme} />
+  <Switch>
+    {themes.map(theme => (
+      <Route
+        key={theme}
+        path={`${routePath}/${theme}`}
+        exact
+        render={() => (
+          <Suite>
+            {sizes.map(size => (
+              <Spec
+                key={size}
+                label={`All Icons - Theme: ${theme} / Size: ${size}`}
+                omitPropsList
+              >
+                <IconList>
+                  {icons.map(iconName => renderIcon(iconName, theme, size))}
+                </IconList>
+              </Spec>
             ))}
-          </Inline>
-        </Spec>
-      ));
-    })}
-  </Suite>
+          </Suite>
+        )}
+      />
+    ))}
+  </Switch>
 );
