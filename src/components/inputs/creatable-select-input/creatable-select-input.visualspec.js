33f83d2716b7aeffe23c0fd053693072a54e41a8
Dominik Ferber <dominik.ferber@gmail.com>
2019-01-16 13:52:02 +0100

    test(select-inputs): add visual-regression tests

--- src/components/inputs/creatable-select-input/creatable-select-input.visualspec.js
+++ src/components/inputs/creatable-select-input/creatable-select-input.visualspec.js
@@ -1,12 +1,16 @@
 import { percySnapshot } from '@percy/puppeteer';
 
 describe('CreatableSelectInput', () => {
-  beforeAll(async () => {
-    await page.goto('http://localhost:3001/creatable-select-input');
-  });
-
   it('CreatableSelectInput', async () => {
+    await page.goto('http://localhost:3001/creatable-select-input');
     await expect(page).toMatch('minimal');
     await percySnapshot(page, 'CreatableSelectInput');
+  });
+
+  it('Open', async () => {
+    await page.goto(`${HOST}/creatable-select-input-open`);
+    await page.click('#creatable-select-input');
+    await expect(page).toMatch('One');
+    await percySnapshot(page, 'CreatableSelectInput - open');
   });
 });
