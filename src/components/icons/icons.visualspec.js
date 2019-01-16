4bc36e891a0db26164dc49c5b5c06c962c86c3da
Malcolm Laing <malcolm.laing@commercetools.de>
2019-01-16 10:46:47 +0100

    chore: update vrt tests for icons

--- src/components/icons/icons.visualspec.js
+++ src/components/icons/icons.visualspec.js
@@ -1,12 +1,27 @@
 import { percySnapshot } from '@percy/puppeteer';
 
-describe('Icons', () => {
-  beforeAll(async () => {
-    await page.goto(`${HOST}/icons`);
-  });
+// let's go wide, to fit more icons.
+const snapshot = (page, description) =>
+  percySnapshot(page, description, { widths: [1600] });
 
-  it('Default', async () => {
-    await expect(page).toMatch('AddBoldIcon');
-    await percySnapshot(page, 'Icons');
-  });
+const capitalize = s => s[0].toUpperCase() + s.slice(1);
+const themes = [
+  'black',
+  'grey',
+  'white',
+  'blue',
+  'green',
+  'green-light',
+  'orange',
+  'red',
+];
+
+describe('Icons', () => {
+  themes.map(theme =>
+    it(capitalize(theme), async () => {
+      await page.goto(`${HOST}/icons/${theme}`);
+      await expect(page).toMatch(theme);
+      await snapshot(page, `Icons - Theme: ${theme}`);
+    })
+  );
 });
