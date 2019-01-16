edc341716e380c1744234aefdf175a3bd4a81d62
Dominik Ferber <dominik.ferber@gmail.com>
2019-01-16 16:18:20 +0100

    chore: remove playground

--- playground/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Playground from './playground';
-
-const node = document.getElementById('app');
-
-ReactDOM.render(<Playground />, node);
