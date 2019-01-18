973625441adb78930e29ac10276af69926d19b9e
Malcolm Laing <malcolm.laing@commercetools.de>
2019-01-18 14:54:40 +0100

    chore: revert inline

--- src/components/spacings/inline/inline.js
+++ src/components/spacings/inline/inline.js
@@ -1,55 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css } from '@emotion/core';
+import classnames from 'classnames';
 import filterDataAttributes from '../../../utils/filter-data-attributes';
-import vars from '../../../../materials/custom-properties';
-
-const getMargin = scale => {
-  switch (scale) {
-    case 'xs':
-      return vars['--spacing-4'];
-    case 's':
-      return vars['--spacing-8'];
-    case 'm':
-      return vars['--spacing-16'];
-    case 'l':
-      return vars['--spacing-24'];
-    case 'xl':
-      return vars['--spacing-32'];
-    default:
-      return vars['--spacing-4'];
-  }
-};
-
-const getAlignItems = alignItems => {
-  switch (alignItems) {
-    case 'center':
-      return 'center';
-
-    case 'flexStart':
-      return 'flex-start';
-
-    case 'flexEnd':
-      return 'flex-end';
-
-    case 'baseline':
-      return 'baseline';
-    case 'stretch':
-      return 'stretch';
-    default:
-      return '';
-  }
-};
+import styles from './inline.mod.css';
 
 const Inline = props => (
   <div
-    css={css`
-      display: flex;
-      align-items: ${getAlignItems(props.alignItems)};
-      > * + * {
-        margin: 0 0 0 ${getMargin(props.scale)};
-      }
-    `}
+    className={classnames(styles[props.scale], styles[props.alignItems])}
     {...filterDataAttributes(props)}
   >
     {props.children}
