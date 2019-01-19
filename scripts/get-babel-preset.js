0095ff187e483f2e4a3f126cfcc46aefb93b5606
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-19 16:05:05 +0100

    chore: remove do-expressions

--- scripts/get-babel-preset.js
+++ scripts/get-babel-preset.js
@@ -117,6 +117,7 @@ module.exports = function getBabelPresets() {
           async: false,
         },
       ],
+      require('@babel/plugin-proposal-optional-chaining').default,
       // Adds syntax support for import()
       require('@babel/plugin-syntax-dynamic-import').default,
       isEnvTest &&
