dd7e42fb1e91b0705eac5a79bef89f229e050bd7
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-21 11:28:27 +0100

    chore(storybook): compile both js and ts files with typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,6 @@
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BUILD_ROLLUP = true;
+
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
--- scripts/get-babel-preset.js
+++ scripts/get-babel-preset.js
@@ -10,6 +10,7 @@ module.exports = function getBabelPresets() {
   const isEnvDevelopment = env === 'development';
   const isEnvProduction = env === 'production';
   const isEnvTest = env === 'test';
+  const isRollup = process.env.BUILD_ROLLUP === true;
 
   if (!isEnvDevelopment && !isEnvProduction && !isEnvTest) {
     throw new Error(
@@ -104,9 +105,10 @@ module.exports = function getBabelPresets() {
       isEnvProduction && [
         // Remove PropTypes from production build
         require('babel-plugin-transform-react-remove-prop-types').default,
-        {
-          mode: 'wrap',
-        },
+        // In case of rollup bundles, we want to keep the prop types but wrap
+        // them into a `process.env.NODE_ENV !== "production"` so that when
+        // building the final application bundles, those codes parts can be removed.
+        isRollup ? { mode: 'wrap' } : { removeImport: true },
       ],
       // function* () { yield 42; yield 43; }
       !isEnvTest && [
