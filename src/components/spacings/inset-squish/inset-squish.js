17ea1a60d63dc4875f169549caa0e236a0fa3380
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-21 17:37:14 +0100

    refactor(spacings/inset-squish): to use emotion styles

--- src/components/spacings/inset-squish/inset-squish.js
+++ src/components/spacings/inset-squish/inset-squish.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+import vars from '../../../../materials/custom-properties';
 import filterDataAttributes from '../../../utils/filter-data-attributes';
-import styles from './inset-squish.mod.css';
+
+const getPadding = props => {
+  switch (props.scale) {
+    case 's':
+      return `${vars['--spacing-4']} ${vars['--spacing-8']}`;
+    case 'm':
+      return `${vars['--spacing-8']} ${vars['--spacing-16']}`;
+    case 'l':
+      return `${vars['--spacing-16']} ${vars['--spacing-32']}`;
+    default:
+      return 0;
+  }
+};
 
 const InsetSquish = props => (
-  <div className={styles[props.scale]} {...filterDataAttributes(props)}>
+  <div
+    css={css`
+      padding: ${getPadding(props.scale)};
+    `}
+    {...filterDataAttributes(props)}
+  >
     {props.children}
   </div>
 );
