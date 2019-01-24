eb50a6cb0e946cfb8a274fcb0e387841c2a3626b
Malcolm Laing <malcolm.laing@commercetools.de>
2019-01-24 13:50:41 +0100

    chore: start refactoring collapsible panel to use emotion

--- src/components/panels/collapsible-panel/collapsible-panel.js
+++ src/components/panels/collapsible-panel/collapsible-panel.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import isNil from 'lodash.isnil';
+import { css } from '@emotion/core';
 import classnames from 'classnames';
 import filterDataAttributes from '../../../utils/filter-data-attributes';
 import Spacings from '../../spacings';
@@ -8,6 +9,7 @@ import Text from '../../typography/text';
 import CollapsibleMotion from '../../collapsible-motion';
 import HeaderIcon from './header-icon';
 import styles from './collapsible-panel.mod.css';
+import vars from '../../../../materials/custom-properties';
 
 // When `isClosed` is provided the component behaves as a controlled component,
 // otherwise it will behave like an uncontrolled component.
@@ -83,33 +85,108 @@ export default class CollapsiblePanel extends React.PureComponent {
       >
         {({ isOpen, toggle, containerStyles, registerContentNode }) => (
           <div
-            className={classnames(
-              this.props.className,
-              styles[`container-theme-${this.props.theme}`],
-              {
-                [styles['container-condensed']]: this.props.condensed,
-                [styles['container-open']]: isOpen,
-              }
-            )}
+            css={[
+              css`
+                font-family: ${vars.fontFamilyDefault};
+                box-shadow: ${vars.shadow1Second};
+                color: ${vars.colorBlack};
+                border-radius: ${vars.borderRadius6};
+                position: relative;
+                min-width: 550px;
+                font-size: ${vars.fontSizeDefault};
+                padding: 0;
+                background-color: ${this.props.theme === 'light'
+                  ? vars.colorWhite
+                  : vars.colorGray95};
+              `,
+              this.props.condensed &&
+                css`
+                  min-width: 0;
+                `,
+            ]}
+            className={this.props.className}
           >
             <div
+              css={[
+                css`
+                  position: relative;
+                  cursor: pointer;
+                  border-top-left-radius: ${vars.borderRadius6};
+                  border-top-right-radius: ${vars.borderRadius6};
+                  background-color: ${this.props.theme === 'light'
+                    ? vars.colorWhite
+                    : vars.colorGray95};
+                `,
+                isOpen &&
+                  css`
+                    border-bottom: 1px ${vars.colorGray60} solid;
+                  `,
+                this.props.isDisabled &&
+                  css`
+                    cursor: default;
+                  `,
+                this.props.isSticky &&
+                  isOpen &&
+                  css`
+                    position: sticky;
+                    top: 0;
+                    z-index: 1;
+                    border-top-right-radius: ${vars.borderRadius6};
+                    border-top-left-radius: ${vars.borderRadius6};
+
+                    /* FIXME: this is supposed to fix a bug in Chrome with position: sticky in
+                           low-DPI screens. Once the bug is fixed, please remove this. (https://goo.gl/GVcJXf) */
+
+                    @media only screen and (-webkit-max-device-pixel-ratio: 1) {
+                      top: calc(${vars.spacing16} * -1);
+                    }
+                  `,
+                !isOpen &&
+                  css`
+                    border-bottom-left-radius: ${vars.borderRadius6};
+                    border-bottom-right-radius: ${vars.borderRadius6};
+                  `,
+              ]}
               onClick={this.props.isDisabled ? undefined : toggle}
-              className={classnames(
-                styles['base-header-container'],
-                styles[`header-container-theme-${this.props.theme}`],
-                {
-                  [styles.disabled]: this.props.isDisabled,
-                  [styles.sticky]: this.props.isSticky && isOpen,
-                  [styles['header-closed']]: !isOpen,
-                }
-              )}
+              className={classnames({
+                [styles['header-closed']]: !isOpen,
+              })}
             >
               <Spacings.InsetSquish scale={scale}>
                 <div
                   {...dataProps}
-                  className={classnames(styles.header, {
-                    [styles['header-disabled']]: this.props.isDisabled,
-                  })}
+                  css={[
+                    css`
+                      display: flex;
+                      flex: 1;
+                      align-items: center;
+                      list-style-type: none;
+                      justify-content: space-between;
+
+                      /*
+                    Two resource that explain why we need the min-width: 0; here
+                    By default, min-width is set to 'auto'. That means that this flex-child is not
+                    allowed to grow any smaller than the longest text inside. So it will stretch
+                    no matter how you set the flex-grow property
+                    To fix this you need to set min-width to 0. This tells the flex-child that
+                    it is ok to be narrower than the longest word inside
+                    https://hackernoon.com/11-things-i-learned-reading-the-flexbox-spec-5f0c799c776b
+                    https://css-tricks.com/flexbox-truncated-text/
+                    */
+
+                      min-width: 0;
+
+                      > * + * {
+                        /* would have loved to use Spacings.Inline here but that would require a
+                      complete overhaul of this components' structure */
+                        margin: 0 0 0 ${vars.spacing16};
+                      }
+                    `,
+                    this.props.isDisabled &&
+                      css`
+                        cursor: default;
+                      `,
+                  ]}
                 >
                   <div className={styles['truncate-header']}>
                     <Spacings.Inline alignItems="center" scale="s">
@@ -156,7 +233,16 @@ export default class CollapsiblePanel extends React.PureComponent {
                   </Spacings.Inset>
                 )}
                 <Spacings.Inset scale={scale}>
-                  <div className={styles.content}>{this.props.children}</div>
+                  <div
+                    css={css`
+                      width: 100%;
+                      display: flex;
+                      flex-wrap: wrap;
+                      align-items: flex-start;
+                    `}
+                  >
+                    {this.props.children}
+                  </div>
                 </Spacings.Inset>
               </div>
             </div>
