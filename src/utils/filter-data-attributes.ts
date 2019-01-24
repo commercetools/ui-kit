7654622a3e9fac8412b852c1d00e13d744c506c4
tdeekens <tobias.deekens@commercetools.de>
2019-01-24 11:57:12 +0100

    refactor(utils): type filter-data-attributes

---
+++ src/utils/filter-data-attributes.ts
@@ -0,0 +1,19 @@
+const regexpData = /^data-/;
+
+type PropsToBeFiltered = {
+  [key: string]: any;
+};
+export default function filterDataAttributes(
+  obj: PropsToBeFiltered
+): Partial<PropsToBeFiltered> {
+  return Object.keys(obj)
+    .filter(p => regexpData.test(p))
+    .reduce(
+      (acc, p) => {
+        // eslint-disable-next-line no-param-reassign
+        acc[p] = obj[p];
+        return acc;
+      },
+      {} as PropsToBeFiltered
+    );
+}
