d0e1a2e8d00f6dd274e80e162a1a2de629aacb1f
tdeekens <tobias.deekens@commercetools.de>
2019-01-24 18:34:33 +0100

    refactor(accessible-buttom): to type forwardRef invocation

--- src/components/buttons/accessible-button/accessible-button.tsx
+++ src/components/buttons/accessible-button/accessible-button.tsx
@@ -15,54 +15,58 @@ type AccessibleButtonProps = {
   className?: string;
   buttonAttributes?: Object;
 };
-const AccessibleButton: React.FC<AccessibleButtonProps> = React.forwardRef(
-  (props, ref) => {
-    invariant(
-      props.isToggleButton && typeof props.isToggled === 'undefined',
-      'ui-kit/AccessibleButton: when `isToggleButton` is passed `isToggled` is required.'
-    );
 
-    return (
-      <button
-        id={props.id}
-        ref={ref as React.RefObject<HTMLButtonElement>}
-        type={props.type}
-        aria-label={props.label}
-        onClick={props.onClick}
-        css={[
+type ButtonRef = HTMLButtonElement;
+
+const AccessibleButton: React.FC<AccessibleButtonProps> = React.forwardRef<
+  ButtonRef,
+  AccessibleButtonProps
+>((props, ref) => {
+  invariant(
+    props.isToggleButton && typeof props.isToggled === 'undefined',
+    'ui-kit/AccessibleButton: when `isToggleButton` is passed `isToggled` is required.'
+  );
+
+  return (
+    <button
+      id={props.id}
+      ref={ref}
+      type={props.type}
+      aria-label={props.label}
+      onClick={props.onClick}
+      css={[
+        css`
+          border: none;
+          background: none;
+          display: inline-block;
+          outline: 0;
+          padding: 0;
+          margin: 0;
+          white-space: nowrap;
+          cursor: pointer;
+          color: inherit;
+          font: inherit;
+          font-size: ${vars.fontSizeDefault};
+          font-family: ${vars.fontFamilyDefault};
+        `,
+        props.isDisabled &&
           css`
-            border: none;
-            background: none;
-            display: inline-block;
-            outline: 0;
-            padding: 0;
-            margin: 0;
-            white-space: nowrap;
-            cursor: pointer;
-            color: inherit;
-            font: inherit;
-            font-size: ${vars.fontSizeDefault};
-            font-family: ${vars.fontFamilyDefault};
+            cursor: not-allowed;
           `,
-          props.isDisabled &&
-            css`
-              cursor: not-allowed;
-            `,
-        ]}
-        // Allow to override the styles by passing a `className` prop.
-        // Custom styles can also be passed using the `css` prop from emotion.
-        // https://emotion.sh/docs/css-prop#style-precedence
-        className={props.className}
-        disabled={props.isDisabled}
-        aria-disabled={props.isDisabled}
-        {...(props.isToggleButton ? { 'aria-pressed': props.isToggled } : {})}
-        {...props.buttonAttributes}
-      >
-        {props.children}
-      </button>
-    );
-  }
-);
+      ]}
+      // Allow to override the styles by passing a `className` prop.
+      // Custom styles can also be passed using the `css` prop from emotion.
+      // https://emotion.sh/docs/css-prop#style-precedence
+      className={props.className}
+      disabled={props.isDisabled}
+      aria-disabled={props.isDisabled}
+      {...(props.isToggleButton ? { 'aria-pressed': props.isToggled } : {})}
+      {...props.buttonAttributes}
+    >
+      {props.children}
+    </button>
+  );
+});
 
 AccessibleButton.displayName = 'AccessibleButton';
 
