31fa7eb664aa754091857013c7ccbbdbf79e3e94
tdeekens <tobias.deekens@commercetools.de>
2019-01-24 15:57:14 +0100

    refactor(hocs): to not rely on recompose

--- src/hocs/with-mouse-down-state/with-mouse-down-state.js
+++ src/hocs/with-mouse-down-state/with-mouse-down-state.js
@@ -1,19 +1,36 @@
-import flowRight from 'lodash.flowright';
-import {
-  withState,
-  withHandlers,
-  wrapDisplayName,
-  setDisplayName,
-} from 'recompose';
+import React from 'react';
+import wrapDisplayName from '../wrap-display-name';
 
 export const stateHandlers = {
   handleMouseDown: ({ setMouseDown }) => () => setMouseDown(true),
   handleMouseUp: ({ setMouseDown }) => () => setMouseDown(false),
 };
 
-export default component =>
-  flowRight(
-    setDisplayName(wrapDisplayName(component, 'WithMouseDownState')),
-    withState('isMouseDown', 'setMouseDown', false),
-    withHandlers(stateHandlers)
-  )(component);
+const withMouseDownState = BaseComponent => {
+  class WithMouseDownState extends React.Component {
+    static displayName = wrapDisplayName(BaseComponent, 'withMouseDownState');
+
+    state = {
+      isMouseDown: false,
+    };
+
+    setMouseDown = nextMouseDown =>
+      this.setState({ isMouseDown: nextMouseDown });
+
+    handleMouseDown = () => this.setMouseDown(true);
+    handleMouseUp = () => this.setMouseDown(false);
+
+    render() {
+      return React.createElement(BaseComponent, {
+        ...this.props,
+        isMouseDown: this.state.isMouseDown,
+        handleMouseDown: this.handleMouseDown,
+        handleMouseUp: this.handleMouseUp,
+      });
+    }
+  }
+
+  return WithMouseDownState;
+};
+
+export default withMouseDownState;
--- src/hocs/with-mouse-over-state/with-mouse-over-state.js
+++ src/hocs/with-mouse-over-state/with-mouse-over-state.js
@@ -1,19 +1,31 @@
-import flowRight from 'lodash.flowright';
-import {
-  withState,
-  withHandlers,
-  wrapDisplayName,
-  setDisplayName,
-} from 'recompose';
+import React from 'react';
+import wrapDisplayName from '../wrap-display-name';
 
-export const stateHandlers = {
-  handleMouseOver: ({ setMouseOver }) => () => setMouseOver(true),
-  handleMouseOut: ({ setMouseOver }) => () => setMouseOver(false),
+const withMouseOverState = BaseComponent => {
+  class WithMouseOverState extends React.Component {
+    static displayName = wrapDisplayName(BaseComponent, 'withMouseOverState');
+
+    state = {
+      isMouseOver: false,
+    };
+
+    setMouseOver = nextMouseOver =>
+      this.setState({ isMouseOver: nextMouseOver });
+
+    handleMouseOver = () => this.setMouseOver(true);
+    handleMouseOut = () => this.setMouseOver(false);
+
+    render() {
+      return React.createElement(BaseComponent, {
+        ...this.props,
+        isMouseOver: this.state.isMouseOver,
+        handleMouseOver: this.handleMouseOver,
+        handleMouseOut: this.handleMouseOut,
+      });
+    }
+  }
+
+  return WithMouseOverState;
 };
 
-export default component =>
-  flowRight(
-    setDisplayName(wrapDisplayName(component, 'WithMouseOverState')),
-    withState('isMouseOver', 'setMouseOver', false),
-    withHandlers(stateHandlers)
-  )(component);
+export default withMouseOverState;
