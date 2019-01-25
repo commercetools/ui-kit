0d333818eb9fa80543f7cb4683b980a53b70e80f
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-25 09:35:31 +0100

    refactor(label): to use emotion styles (#476)

--- src/components/label/required-indicator.js
+++ src/components/label/required-indicator.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import styles from './required-indicator.mod.css';
+import { css } from '@emotion/core';
+import vars from '../../../materials/custom-properties';
 
-const RequiredIndicator = () => <em className={styles.highlighted}>{'*'}</em>;
+const RequiredIndicator = () => (
+  <em
+    css={css`
+      color: ${vars.fontColorWarning};
+    `}
+  >
+    {'*'}
+  </em>
+);
 RequiredIndicator.displayName = 'RequiredIndicator';
 
 export default RequiredIndicator;
