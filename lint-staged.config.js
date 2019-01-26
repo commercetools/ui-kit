a5ff6454c449a31cbc9c4ea876aa96a83d82ea66
Malcolm <malcolmclaing@gmail.com>
2019-01-26 14:35:04 +0100

    chore: remove unused code

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -12,15 +12,4 @@ module.exports = {
     // For that reason, we move the `--onlyChanged` flag next to it.
     'yarn lint:js --reporters=jest-silent-reporter --onlyChanged',
   ],
-  '*.css': [
-    // NOTE: apparently if you pass some argument that is not a flag AFTER the `reporters`
-    // flag, jest does not seem correctly parse the arguments.
-    //
-    //   No tests found related to files changed since last commit.
-    //   Run Jest without `-o` or with `--all` to run all tests.
-    //   Error: An error occurred while adding the reporter at path "/path/to/file".Reporter is not a constructor
-    //
-    // For that reason, we move the `--onlyChanged` flag next to it.
-    'yarn lint:css --reporters=jest-silent-reporter --onlyChanged',
-  ],
 };
