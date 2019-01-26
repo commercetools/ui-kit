1d535c7d2db784a8766eac236b6f360ea16a114d
Malcolm <malcolmclaing@gmail.com>
2019-01-26 14:54:31 +0100

    chore: make rem more precise for percy pass

--- src/components/typography/text/text.styles.js
+++ src/components/typography/text/text.styles.js
@@ -48,15 +48,15 @@ const getTone = tone => {
 const getElementFontSize = elementType => {
   switch (elementType) {
     case 'h1':
-      return '2.462rem';
+      return '2.461538461rem';
     case 'h2':
-      return '1.846rem';
+      return '1.8461538461rem';
     case 'h3':
-      return '1.538rem';
+      return '1.5384615384rem';
     case 'h4':
-      return '1.231rem';
+      return '1.2307692307rem';
     case 'h5':
-      return '1.077rem';
+      return '1.0769230769rem';
     default:
       return '';
   }
@@ -99,7 +99,7 @@ export const wrapStyles = () => css`
 export const detailStyles = props => css`
   ${baseStyles}
   display: block;
-  font-size: 0.9231rem;
+  font-size: 0.923076923rem;
   ${props.isInline && inline}
   ${props.isBold && bold}
   ${props.isItalic && italic}
