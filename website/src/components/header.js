08d2a5b3d06d256a56fdbb9375afe22ae0f9d1f5
Nicola Molinari <nicola.molinari@commercetools.de>
2019-01-29 13:45:54 +0100

    chore(website): add basic layout styles

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -2,37 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 import { Link } from 'gatsby';
+import { customProperties, Spacings } from 'ui-kit';
 
 const Header = props => (
   <div
+    className={props.className}
     css={css`
-      background: rebeccapurple;
-      margin-bottom: 1.45rem;
+      border-bottom: 1px solid ${customProperties.colorPurple};
+      display: flex;
+      align-items: center;
+      padding: 0 16px;
     `}
   >
-    <div
-      css={css`
-        margin: 0 auto;
-        max-width: 960px;
-        padding: 1.45rem 1.0875rem;
-      `}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {props.siteTitle}
-        </Link>
-      </h1>
-    </div>
+    <Spacings.Inline>
+      <Link to="/">{props.siteTitle}</Link>
+    </Spacings.Inline>
   </div>
 );
 Header.displayName = 'Header';
 Header.propTypes = {
+  className: PropTypes.string,
   siteTitle: PropTypes.string,
 };
 Header.defaultProps = {
--- website/src/components/layout.js
+++ website/src/components/layout.js
@@ -1,26 +1,105 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
+import { css, Global } from '@emotion/core';
+import { customProperties } from 'ui-kit';
 import Header from './header';
 
 const Layout = props => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+  <React.Fragment>
+    <Global
+      styles={css`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          height: 100vh;
+          font-family: ${customProperties.fontFamilyDefault};
+        }
+      `}
+    />
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        {props.children}
-      </>
-    )}
-  />
+      `}
+      render={data => (
+        <div
+          css={css`
+            height: 100vh;
+            display: grid;
+            grid-template-rows: auto 48px 1fr;
+            grid-template-columns: auto 1fr;
+          `}
+        >
+          <Header
+            css={css`
+              grid-row: 2;
+              grid-column: 1/3;
+            `}
+            siteTitle={data.site.siteMetadata.title}
+          />
+          <aside
+            css={css`
+              position: relative;
+              grid-row: 3;
+              display: flex;
+              flex-direction: column;
+              width: 128px;
+              border-right: 1px solid ${customProperties.colorPurple};
+            `}
+          >
+            {'Side'}
+          </aside>
+          <div
+            role="main"
+            css={css`
+              grid-column: 2;
+              grid-row: 3;
+
+              /*
+              Allow the this flex child to grow smaller than its smallest content.
+              This is needed when there is a really wide text inside that would stretch
+              this node to be wider than the parent.
+            */
+              min-width: 0;
+              overflow-x: hidden;
+              overflow-y: scroll;
+
+              /*
+              layout the children. There will always be the page and side notification
+              about the actual content. The content should stretch to fill the rest of
+              the page.
+            */
+              display: flex;
+              flex-direction: column;
+
+              /*
+              set position to relative to layout notifications and modals
+            */
+              position: relative;
+            `}
+          >
+            <div
+              css={css`
+                flex-grow: 1;
+                display: flex;
+                flex-direction: column;
+                padding: 16px;
+              `}
+            >
+              {props.children}
+            </div>
+          </div>
+        </div>
+      )}
+    />
+  </React.Fragment>
 );
 Layout.displayName = 'Layout';
 Layout.propTypes = {
