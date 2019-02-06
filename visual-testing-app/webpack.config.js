6030f3cf871a7fd2a3cb967fd16d09dbbb1446e0
Malcolm Laing <malcolm.laing@commercetools.de>
2019-02-06 11:28:29 +0100

    chore: revert webpack hange

--- visual-testing-app/webpack.config.js
+++ visual-testing-app/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   target: 'web',
   // note: we have to use 'development' for now, because when we use production
   // our emotion styles aren't displayed in our percy snapshots.
-  mode: 'production',
+  mode: 'development',
   stats: 'minimal',
   entry: './visual-testing-app/src/index.js',
   output: {
