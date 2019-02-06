f70665dac1003ea554fb1714d53ede3fab0d6f3a
Fedor Kulikov <fedor.kulikov@commercetools.de>
2019-02-06 14:18:16 +0100

    fix(tag): wrong css is appied in case linkTo prop is provided

--- src/components/tag/tag.story.js
+++ src/components/tag/tag.story.js
@@ -12,7 +12,7 @@ const Story = () => (
   <Section>
     <Tag
       type={select('type', ['normal', 'warning'], 'normal')}
-      linkTo={select('linkTo', ['/foo', undefined], undefined)}
+      linkTo={select('linkTo', ['/foo', undefined], '/foo')}
       isDisabled={boolean('isDisabled', false)}
       onClick={boolean('onClick', false) ? action('onClick') : undefined}
       horizontalConstraint={select(
