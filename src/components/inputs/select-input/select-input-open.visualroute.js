cfcdb80cb43cf3a286e6f07b7f99504a6b6e47b8
Vidura Rajapaksa <vidura.rajapaksa@commercetools.de>
2019-02-08 11:55:31 +0100

    test(select-input): cover all cases for options and dividers

--- src/components/inputs/select-input/select-input-open.visualroute.js
+++ src/components/inputs/select-input/select-input-open.visualroute.js
@@ -3,6 +3,10 @@ import { SelectInput } from 'ui-kit';
 import { Suite, Spec } from '../../../../test/percy';
 
 const options = [
+  { value: 'one', label: 'One' },
+  { value: 'two', label: 'Two' },
+];
+const optionsWithGroups = [
   { label: 'one', options: [{ value: 'one', label: 'One' }] },
   { options: [{ value: 'two', label: 'Two' }] },
 ];
@@ -15,12 +19,30 @@ export const routePath = '/select-input-open';
 
 export const component = () => (
   <Suite>
-    <Spec label="minimal">
+    <Spec label="option without option groups">
       <SelectInput
         id="select-input"
         value={value}
         onChange={() => {}}
         options={options}
+        horizontalConstraint="m"
+      />
+    </Spec>
+    <Spec label="option group with no divider">
+      <SelectInput
+        id="select-input"
+        value={value}
+        onChange={() => {}}
+        options={optionsWithGroups}
+        horizontalConstraint="m"
+      />
+    </Spec>
+    <Spec label="options groups with divider">
+      <SelectInput
+        id="select-input"
+        value={value}
+        onChange={() => {}}
+        options={optionsWithGroups}
         horizontalConstraint="m"
         showOptionGroupDivider={showOptionGroupDivider}
       />
