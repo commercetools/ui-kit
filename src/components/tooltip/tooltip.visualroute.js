5c6aad4edf404bb4125ec0b6ad19f1100781ee17
Malcolm Laing <malcolm.laing@commercetools.de>
2019-02-12 14:50:39 +0100

    chore: add VRT

--- src/components/tooltip/tooltip.visualroute.js
+++ src/components/tooltip/tooltip.visualroute.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 import { PrimaryButton, Tooltip } from 'ui-kit';
 import { Suite, Spec } from '../../../test/percy';
 
 const title = 'What kind of bear is best';
 const noop = () => {};
+
+const Body = styled.div`
+  color: red;
+  margin-top: 12px;
+`;
 
 export const routePath = '/tooltip';
 
@@ -22,6 +28,16 @@ export const component = () => (
         `}
       />
       <Tooltip title={title} isOpen={true}>
+        <PrimaryButton onClick={noop} label="Hello" />
+      </Tooltip>
+    </Spec>
+    <Spec label="Open with custom body component" omitPropsList>
+      <div
+        css={css`
+          margin-top: 50px;
+        `}
+      />
+      <Tooltip title={title} isOpen={true} components={{ BodyComponent: Body }}>
         <PrimaryButton onClick={noop} label="Hello" />
       </Tooltip>
     </Spec>
