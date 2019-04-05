8a998e66518b2690e3b9f2ffc474c393416fed97
Malcolm Laing <malcolm.laing@commercetools.de>
2019-04-05 12:44:15 +0200

    chore: use design tokens for select-inputs

--- src/components/internals/create-select-styles.js
+++ src/components/internals/create-select-styles.js
@@ -12,53 +12,55 @@ import vars from '../../../materials/custom-properties';
 
 const controlStyles = props => (base, state) => ({
   ...base,
-  fontSize: vars.fontSizeDefault,
+  fontSize: vars.fontSizeForInput,
   backgroundColor: state.isDisabled
-    ? vars.backgroundColorInputDisabled
-    : vars.backgroundColorInputPristine,
+    ? vars.backgroundColorForInputWhenDisabled
+    : vars.backgroundColorForInput,
   borderColor: (() => {
-    if (state.isDisabled) return vars.borderColorInputDisabled;
-    if (props.hasError) return vars.borderColorInputError;
-    if (props.hasWarning) return vars.borderColorInputWarning;
-    if (state.isFocused) return vars.borderColorInputFocus;
-    return vars.borderColorInputPristine;
+    if (state.isDisabled) return vars.borderColorForInputWhenDisabled;
+    if (props.hasError) return vars.borderColorForInputWhenError;
+    if (props.hasWarning) return vars.borderColorForInputWhenWarning;
+    if (state.isFocused) return vars.borderColorForInputWhenFocused;
+    return vars.borderColorForInput;
   })(),
-  borderRadius: vars.borderRadiusInput,
+  borderRadius: vars.borderRadiusForInput,
   minHeight: vars.sizeHeightInput,
   cursor: state.isDisabled ? 'not-allowed' : 'pointer',
-  padding: `0 ${vars.spacing8}`,
+  padding: `0 ${vars.spacingS}`,
 
   boxShadow: state.isFocused ? 'none' : base.boxShadow,
 
   '&:hover': {
-    borderColor: vars.borderColorInputFocus,
+    borderColor: vars.borderColorForInputWhenFocused,
     boxShadow: 'none',
   },
   '&:active': {
-    borderColor: vars.borderColorInputFocus,
+    borderColor: vars.borderColorForInputWhenFocused,
     boxShadow: 'none',
   },
   '&:focus': {
-    borderColor: vars.borderColorInputFocus,
+    borderColor: vars.borderColorForInputWhenFocused,
     boxShadow: 'none',
   },
 
   pointerEvents: state.isDisabled ? 'none' : base.pointerEvents,
-  color: state.isDisabled ? vars.fontColorDisabled : base.color,
+  color: state.isDisabled
+    ? vars.fontColorForInputWhenDisabled
+    : base.fontColorForInput,
 });
 
 const menuStyles = props => base => ({
   ...base,
-  border: `1px ${vars.borderColorInputFocus} solid`,
-  borderRadius: vars.borderRadiusInput,
-  backgroundColor: vars.backgroundColorInputPristine,
+  border: `1px ${vars.borderColorForInput} solid`,
+  borderRadius: vars.borderRadiusForInput,
+  backgroundColor: vars.backgroundColorForInput,
   boxShadow: vars.shadow7,
-  fontSize: vars.fontSizeDefault,
-  margin: `${vars.spacing4} 0 0 0`,
+  fontSize: vars.fontSizeForInput,
+  margin: `${vars.spacingXs} 0 0 0`,
   borderColor: (() => {
-    if (props.hasError) return vars.borderColorInputError;
-    if (props.hasWarning) return vars.borderColorInputWarning;
-    return base.borderColor;
+    if (props.hasError) return vars.borderColorForInputWhenError;
+    if (props.hasWarning) return vars.borderColorForInputWhenWarning;
+    return base.borderColorForInput;
   })(),
 });
 
@@ -67,15 +69,15 @@ const indicatorSeparatorStyles = () => base => ({
   display: 'none',
   margin: '0',
   padding: '0',
-  marginLeft: vars.spacing4,
+  marginLeft: vars.spacingXs,
 });
 
 const dropdownIndicatorStyles = () => base => ({
   ...base,
-  color: vars.fontColorDefault,
+  color: vars.fontColorForInput,
   margin: '0',
   padding: '0',
-  marginLeft: vars.spacing4,
+  marginLeft: vars.spacingXs,
 });
 
 const clearIndicatorStyles = () => base => ({
@@ -87,38 +89,38 @@ const clearIndicatorStyles = () => base => ({
 const menuListStyles = () => base => ({
   ...base,
   padding: '0',
-  borderRadius: vars.borderRadiusInput,
-  backgroundColor: vars.backgroundColorInputPristine,
+  borderRadius: vars.borderRadiusForInput,
+  backgroundColor: vars.backgroundColorForInput,
 });
 
 const optionStyles = () => (base, state) => ({
   ...base,
   transition: vars.transitionStandard,
-  paddingLeft: vars.spacing8,
-  paddingRight: vars.spacing8,
+  paddingLeft: vars.spacingS,
+  paddingRight: vars.spacingS,
   color: (() => {
-    if (!state.isDisabled) return vars.fontSizeDefault;
-    if (state.isSelected) return vars.fontColorDefault;
+    if (!state.isDisabled) return vars.fontColorForInput;
+    if (state.isSelected) return vars.fontColorForInput;
     return base.color;
   })(),
   backgroundColor: (() => {
-    if (state.isSelected) return vars.backgroundColorInputSelected;
-    if (state.isFocused) return vars.backgroundColorInputHover;
+    if (state.isSelected) return vars.backgroundColorForInputWhenSelected;
+    if (state.isFocused) return vars.backgroundColorForInputWhenHovered;
     return base.backgroundColor;
   })(),
 
   '&:active': {
     color: (() => {
-      if (!state.isDisabled) return vars.fontColorDefault;
+      if (!state.isDisabled) return vars.fontColorForInput;
       return base.color;
     })(),
-    backgroundColor: vars.backgroundColorInputSelected,
+    backgroundColor: vars.backgroundColorForInputWhenSelected,
   },
 });
 
 const placeholderStyles = () => base => ({
   ...base,
-  color: vars.fontColorPlaceholder,
+  color: vars.placeholderFontColorForInput,
 });
 
 const valueContainerStyles = () => base => ({
@@ -131,7 +133,7 @@ const valueContainerStyles = () => base => ({
 const multiValueStyles = () => base => ({
   ...base,
   height: vars.sizeHeightTag,
-  backgroundColor: vars.backgroundColorTagPristine,
+  backgroundColor: vars.backgroundColorForTag,
   padding: '0',
 });
 
@@ -142,22 +144,28 @@ const multiValueLabelStyles = () => (base, state) => ({
     if (state.isDisabled) return vars.fontColorForInputWhenDisabled;
     return base.color;
   })(),
-  padding: `${vars.spacing4} ${vars.spacing8}`,
-  borderRadius: vars.borderRadiusTag,
-  border: `1px ${vars.borderColorTagPristine} solid`,
+  padding: `${vars.spacingXs} ${vars.spacingS}`,
+  border: `1px ${vars.borderColorForTag} solid`,
+  borderRadius: `${vars.borderRadiusForTag} 0 0 ${vars.borderRadiusForTag}`,
+  borderRight: 0,
 });
 
 const multiValueRemoveStyles = () => base => ({
   ...base,
-  borderColor: vars.borderColorTagPristine,
-  padding: `0 ${vars.spacing4}`,
-  borderRadius: `0 ${vars.borderRadiusTag} ${vars.borderRadiusTag} 0`,
+  borderColor: vars.borderColorForTag,
+  padding: `0 ${vars.spacingXs}`,
+  borderRadius: `0 ${vars.borderRadiusForTag} ${vars.borderRadiusForTag} 0`,
   borderStyle: 'solid',
-  borderWidth: '1px 1px 1px 0',
+  borderWidth: '1px',
 
   '&:hover': {
-    backgroundColor: vars.backgroundColorTagNormalHover,
-    boxShadow: vars.shadowBoxTagHover,
+    boxShadow: 'none',
+    backgroundColor: vars.backgroundColorForTag,
+    borderColor: vars.borderColorForTagWarning,
+
+    '> svg *': {
+      fill: vars.borderColorForTagWarning,
+    },
   },
 });
 
@@ -173,12 +181,12 @@ const groupStyles = props => base => ({
 
 const groupHeadingStyles = () => base => ({
   ...base,
-  color: vars.fontColorReadonly,
+  color: vars.fontColorForInputWhenReadonly,
   fontSize: vars.fontSizeSmall,
   textTransform: 'none',
   fontWeight: 'bold',
-  margin: `0 ${vars.spacing4}`,
-  padding: `${vars.spacing8} ${vars.spacing4}`,
+  margin: `0 ${vars.spacingXs}`,
+  padding: `${vars.spacingS} ${vars.spacingXs}`,
   '&:empty': {
     padding: 0,
   },
@@ -188,9 +196,11 @@ const containerStyles = () => (base, state) => ({
   ...base,
   fontFamily: vars.fontFamilyDefault,
   minHeight: vars.sizeHeightInput,
-  borderRadius: vars.borderRadiusInput,
+  borderRadius: vars.borderRadiusForInput,
   cursor: state.isDisabled ? 'not-allowed' : base.cursor,
-  borderColor: state.isFocused ? vars.borderColorInputFocus : base.borderColor,
+  borderColor: state.isFocused
+    ? vars.borderColorForInputWhenFocused
+    : base.borderColor,
 
   boxShadow: state.isFocused ? 'none' : base.boxShadow,
 });
