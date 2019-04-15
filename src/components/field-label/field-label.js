abe42de0b598a1b2568723e22df9e42881cb7315
Malcolm Laing <malcolmclaing@gmail.com>
2019-04-15 13:59:48 +0200

    chore: revert changes in field label pr (#680)
    * chore: revert changes in field label pr
    
    * chore: update readme to show that the size cannot be changed
    
    * fix(field-label): add invariant for unsupported `size` prop

--- src/components/field-label/field-label.js
+++ src/components/field-label/field-label.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import invariant from 'tiny-invariant';
 import React from 'react';
 import requiredIf from 'react-required-if';
 import { css } from '@emotion/core';
@@ -9,66 +10,67 @@ import Label from '../label';
 import Constraints from '../constraints';
 import Spacings from '../spacings';
 
-export const FieldLabel = props => (
-  <Constraints.Horizontal constraint={props.horizontalConstraint}>
-    <Spacings.Stack scale="xs">
-      <Spacings.Inline alignItems="flexStart" scale="xs">
-        <Text.Wrap>
-          <Label
-            isBold={true}
-            isRequiredIndicatorVisible={props.hasRequiredIndicator}
-            htmlFor={props.htmlFor}
-          >
-            {props.title}
-          </Label>
-        </Text.Wrap>
-        {props.onInfoButtonClick && (
-          <IconButton
-            label="More Info"
-            icon={<InformationIcon size="medium" />}
-            size="small"
-            onClick={props.onInfoButtonClick}
-          />
-        )}
-      </Spacings.Inline>
-
-      {props.hint && (
-        <Spacings.Inline alignItems="center" scale="s">
-          {props.hintIcon && (
-            <div
-              css={css`
-                display: flex;
-              `}
+export const FieldLabel = props => {
+  if (props.hintIcon && props.hintIcon.props && props.hintIcon.props.size) {
+    invariant(
+      false,
+      `ui-kit/FieldLabel: setting an hintIcon size is not supported.`
+    );
+  }
+  return (
+    <Constraints.Horizontal constraint={props.horizontalConstraint}>
+      <Spacings.Stack scale="xs">
+        <Spacings.Inline alignItems="flexStart" scale="xs">
+          <Text.Wrap>
+            <Label
+              isBold={true}
+              isRequiredIndicatorVisible={props.hasRequiredIndicator}
+              htmlFor={props.htmlFor}
             >
-              {React.cloneElement(props.hintIcon, {
-                // FIXME: add proper tone when tones are refactored
-                size: props.hintIcon.props.size || 'medium',
+              {props.title}
+            </Label>
+          </Text.Wrap>
+          {props.onInfoButtonClick && (
+            <IconButton
+              label="More Info"
+              icon={<InformationIcon size="medium" />}
+              size="small"
+              onClick={props.onInfoButtonClick}
+            />
+          )}
+        </Spacings.Inline>
+
+        {props.hint && (
+          <Spacings.Inline alignItems="center" scale="xs">
+            {props.hintIcon &&
+              // FIXME: add proper tone when tones are refactored
+              React.cloneElement(props.hintIcon, {
+                size: 'medium',
                 theme: props.hintIcon.props.theme || 'orange',
               })}
-            </div>
-          )}
-          {props.hint && <Text.Detail>{props.hint}</Text.Detail>}
-        </Spacings.Inline>
-      )}
-      {props.description && (
-        <Text.Detail>
-          <Text.Wrap>{props.description}</Text.Wrap>
-        </Text.Detail>
-      )}
+            {props.hint && <Text.Detail>{props.hint}</Text.Detail>}
+          </Spacings.Inline>
+        )}
+        {props.description && (
+          <Text.Detail>
+            <Text.Wrap>{props.description}</Text.Wrap>
+          </Text.Detail>
+        )}
 
-      {props.badge && (
-        <div
-          css={css`
-            display: flex;
-            justify-content: flex-end;
-          `}
-        >
-          {props.badge}
-        </div>
-      )}
-    </Spacings.Stack>
-  </Constraints.Horizontal>
-);
+        {props.badge && (
+          <div
+            css={css`
+              display: flex;
+              justify-content: flex-end;
+            `}
+          >
+            {props.badge}
+          </div>
+        )}
+      </Spacings.Stack>
+    </Constraints.Horizontal>
+  );
+};
 
 FieldLabel.displayName = 'FieldLabel';
 
