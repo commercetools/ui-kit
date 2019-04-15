d294e85e880511bf4943a3e5b13100228cf9d5f5
Malcolm Laing <malcolm.laing@commercetools.de>
2019-04-15 12:57:32 +0200

    chore: revert changes in field label pr

--- src/components/field-label/field-label.visualroute.js
+++ src/components/field-label/field-label.visualroute.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { FieldLabel, WarningIcon, FlatButton } from 'ui-kit';
 import { Suite, Spec } from '../../../test/percy';
 
-const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`;
-
 export const routePath = '/field-label';
 
 export const component = () => (
@@ -16,14 +14,6 @@ export const component = () => (
         title="Hello"
         hint="a hint"
         hintIcon={<WarningIcon />}
-        horizontalConstraint="m"
-      />
-    </Spec>
-    <Spec label="with hint and small icon">
-      <FieldLabel
-        title="Hello"
-        hint="a hint"
-        hintIcon={<WarningIcon size="small" />}
         horizontalConstraint="m"
       />
     </Spec>
@@ -58,14 +48,6 @@ export const component = () => (
         badge={<FlatButton tone="primary" label="show" />}
         htmlFor="sampleInput"
         horizontalConstraint="l"
-      />
-    </Spec>
-    <Spec label="with long hint and big icon">
-      <FieldLabel
-        title="Hello"
-        hint={lorem}
-        hintIcon={<WarningIcon size="big" />}
-        horizontalConstraint="m"
       />
     </Spec>
   </Suite>
