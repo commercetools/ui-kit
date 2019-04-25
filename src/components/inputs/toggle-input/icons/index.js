ab198f770570684c78a0f5e666b26fcbdaee0063
Malcolm Laing <malcolm.laing@commercetools.de>
2019-04-25 16:02:59 +0200

    chore: remove unused file

--- src/components/inputs/toggle-input/icons/index.js
+++
@@ -1,7 +0,0 @@
-import ToggleButtonIcon from './toggle-button-default.react.svg';
-import ToggleBarIcon from './toggle-bar-default.react.svg';
-
-export default {
-  ButtonDefault: ToggleButtonIcon,
-  BarDefault: ToggleBarIcon,
-};
