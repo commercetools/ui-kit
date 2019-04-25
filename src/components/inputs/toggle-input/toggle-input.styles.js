a966257f028e048537307387086856567595fcc9
Malcolm Laing <malcolm.laing@commercetools.de>
2019-04-25 16:02:33 +0200

    chore: fix

--- src/components/inputs/toggle-input/toggle-input.styles.js
+++ src/components/inputs/toggle-input/toggle-input.styles.js
@@ -1,29 +1,20 @@
-import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 import vars from '../../../../materials/custom-properties';
 
-const toggleSwitchSizes = {
-  small: {
-    height: '16px',
-    width: '32px',
-  },
-  big: {
-    height: '32px',
-    width: '64px',
-  },
-};
 const toggleButtonSizes = {
   small: {
-    height: '16px',
-    width: '16px',
+    height: '13px',
+    width: '13px',
   },
   big: {
     height: '26px',
     width: '26px',
   },
 };
+
 const toggleBarSizes = {
   small: {
-    height: '12px',
+    height: '16px',
     width: '32px',
   },
   big: {
@@ -32,153 +23,115 @@ const toggleBarSizes = {
   },
 };
 
-const getToggleSwitchStyles = props => {
-  const baseStyles = [
-    css`
-      &:hover > * {
-        /* filter: drop-shadow(${vars.shadow15First})
-          drop-shadow(${vars.shadow15Second}); */
-      }
-    `,
-    props.isDisabled &&
-      css`
-        &:hover > * {
-          filter: none;
-        }
-      `,
-  ];
-  switch (props.size) {
-    case 'small':
-      return [
-        ...baseStyles,
-        css`
-          position: relative;
-          height: ${toggleSwitchSizes.small.height};
-          width: ${toggleSwitchSizes.small.width};
-        `,
-      ];
-    case 'big':
-      return [
-        ...baseStyles,
-        css`
-          position: relative;
-          height: ${toggleSwitchSizes.big.height};
-          width: ${toggleSwitchSizes.big.width};
-        `,
-      ];
-    default:
-      return baseStyles;
-  }
-};
+export const Button = styled.div`
+  position: absolute;
 
-const getToggleButtonStyles = props => {
-  const baseStyles = [
-    css`
-      position: absolute;
-      left: 2px;
-      top: 50%;
-      transform: translateY(-50%);
-      z-index: 1;
-      /* filter: drop-shadow(${vars.shadow11First})
-        drop-shadow(${vars.shadow11Second}); */
-      transition: transform 0.2s;
-      /*
-      svg .bt-fill {
-        transition: fill 0.2s ease;
-      } */
-    `,
+  left: 2px;
+  transform: translateY(-50%);
+  top: 50%;
+
+  background-color: ${vars.colorSurface};
+  box-shadow: ${vars.shadow7};
+  border-radius: 50%;
+  z-index: 1;
+  transition: transform 0.2s;
+
+  ${props =>
     props.isChecked &&
-      css`
-        /* The extra 7% shifts the button to the right by about 2px for big and 1px for small */
-        transform: translate(130%, -50%);
-        /* filter: drop-shadow(${vars.shadow12First}); */
+    `
+      transform: ${
+        props.size === 'small'
+          ? 'translate(117%, -50%)'
+          : 'translate(127%, -50%)'
+      };
+    `}
 
-        svg .bt-fill {
-          fill: ${vars.colorPrimary};
-        }
-      `,
+  ${props =>
     props.isDisabled &&
-      css`
-        /* filter: drop-shadow(${vars.shadow13First}); */
+    `
+    background-color: ${vars.colorAccent95};
+  `}
 
-        svg .bt-fill {
-          fill: ${vars.colorNeutral90};
-        }
-      `,
+  ${props =>
+    props.isDisabled &&
     props.isChecked &&
-      props.isDisabled &&
-      css`
-        /* filter: drop-shadow(${vars.shadow14First}); */
+    `
+      background-color: ${vars.colorNeutral};
+    `}
 
-        svg .bt-fill {
-          fill: ${vars.colorPrimary85};
-        }
-      `,
-  ];
-  switch (props.size) {
-    case 'small':
-      return [
-        ...baseStyles,
-        css`
+  ${props =>
+    props.size === 'small'
+      ? `
           height: ${toggleButtonSizes.small.height};
           width: ${toggleButtonSizes.small.width};
-        `,
-      ];
-    case 'big':
-      return [
-        ...baseStyles,
-        css`
-          height: ${toggleButtonSizes.big.height};
-          width: ${toggleButtonSizes.big.width};
-        `,
-      ];
-    default:
-      return baseStyles;
-  }
-};
+        `
+      : `
+        height: ${toggleButtonSizes.big.height};
+        width: ${toggleButtonSizes.big.width};
+      `}
+`;
 
-const getToggleBarStyles = props => {
-  const baseStyles = [
-    css`
-      position: absolute;
-      left: 0;
-      rect {
-        fill: ${vars.colorNeutral60};
-      }
-    `,
+export const Toggle = styled.div`
+  position: relative;
+  display: inline-flex;
+
+  ${props =>
+    props.size === 'small'
+      ? `
+      height: ${toggleBarSizes.small.height};
+      width: ${toggleBarSizes.small.width};
+      `
+      : `
+      height: ${toggleBarSizes.big.height};
+      width: ${toggleBarSizes.big.width};
+  `}
+
+  ${props =>
+    !props.isDisabled &&
+    `
+    &:hover ${Button} {
+      filter: drop-shadow(${vars.shadow15First})
+        drop-shadow(${vars.shadow15Second});
+    }
+    `}
+`;
+
+export const Track = styled.div`
+  position: absolute;
+  left: 0;
+  border-radius: 16px;
+  box-shadow: ${vars.shadow9};
+  background-color: ${vars.colorNeutral60};
+
+  ${props =>
     props.isChecked &&
-      css`
-        rect {
-          fill: ${vars.colorPrimary};
-        }
-      `,
+    `
+      background-color: ${vars.colorPrimary};
+    `}
+  ${props =>
     props.isDisabled &&
-      css`
-        rect {
-          fill: ${vars.colorAccent95};
-        }
-      `,
-  ];
-  switch (props.size) {
-    case 'small':
-      return [
-        ...baseStyles,
-        css`
-          height: ${toggleBarSizes.small.height};
-          width: ${toggleBarSizes.small.width};
-        `,
-      ];
-    case 'big':
-      return [
-        ...baseStyles,
-        css`
-          height: ${toggleBarSizes.big.height};
-          width: ${toggleBarSizes.big.width};
-        `,
-      ];
-    default:
-      return baseStyles;
-  }
-};
+    `
+      box-shadow: none;
+      background-color: ${vars.colorNeutral};
+    `}
 
-export { getToggleSwitchStyles, getToggleButtonStyles, getToggleBarStyles };
+  ${props =>
+    props.isDisabled &&
+    props.isChecked &&
+    `
+      background-color: ${vars.colorPrimary25};
+    `}
+
+
+  ${props =>
+    props.size === 'small'
+      ? `
+       height: ${toggleBarSizes.small.height};
+       width: ${toggleBarSizes.small.width};
+   `
+      : `
+      height: ${toggleBarSizes.big.height};
+      width: ${toggleBarSizes.big.width};
+
+      `}
+`;
--- src/components/inputs/toggle-input/toggle-switch.js
+++ src/components/inputs/toggle-input/toggle-switch.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Icons from './icons';
-import {
-  getToggleSwitchStyles,
-  getToggleButtonStyles,
-  getToggleBarStyles,
-} from './toggle-input.styles';
-
-export const ToggleButton = props => (
-  <Icons.ButtonDefault css={getToggleButtonStyles(props)} />
-);
-ToggleButton.displayName = 'ToggleButton';
-ToggleButton.propTypes = {
-  size: PropTypes.oneOf(['small', 'big']).isRequired,
-  isChecked: PropTypes.bool.isRequired,
-  isDisabled: PropTypes.bool.isRequired,
-};
-
-export const ToggleBar = props => (
-  <Icons.BarDefault css={getToggleBarStyles(props)} />
-);
-ToggleBar.displayName = 'ToggleBar';
-ToggleBar.propTypes = {
-  size: PropTypes.oneOf(['small', 'big']).isRequired,
-  isChecked: PropTypes.bool.isRequired,
-  isDisabled: PropTypes.bool.isRequired,
-};
+import { Button, Track, Toggle } from './toggle-input.styles';
 
 const ToggleSwitch = props => (
-  <div css={getToggleSwitchStyles(props)}>
-    <ToggleButton {...props} />
-    <ToggleBar {...props} />
-  </div>
+  <Toggle {...props}>
+    <Button {...props} />
+    <Track {...props} />
+  </Toggle>
 );
 
 ToggleSwitch.displayName = 'ToggleSwitch';
