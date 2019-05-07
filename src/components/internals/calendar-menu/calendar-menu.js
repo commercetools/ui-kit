e21375c78db082f318e983357f7c8601ffcb9fa7
Malcolm Laing <malcolm.laing@commercetools.de>
2019-05-07 11:02:07 +0200

    chore: use non deprecated prop

--- src/components/internals/calendar-menu/calendar-menu.js
+++ src/components/internals/calendar-menu/calendar-menu.js
@@ -23,7 +23,7 @@ export default class CalendarMenu extends Component {
           color: ${vars.colorSolid};
           font-family: ${vars.fontFamilyDefault};
           border: 1px solid ${vars.borderColorForInputWhenFocused};
-          border-radius: ${vars.borderRadiusInput};
+          border-radius: ${vars.borderRadiusForInput};
           margin-top: ${vars.spacingXs};
           font-size: ${vars.fontSizeDefault};
           position: absolute;
