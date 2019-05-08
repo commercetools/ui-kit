08aa576cdbfe7e6803878cc29e85931e095bcebe
jonnybel <jonnybel91@gmail.com>
2019-05-08 10:28:36 +0200

    refactor(input): remove wrapper div

--- src/components/inputs/text-input/text-input.js
+++ src/components/inputs/text-input/text-input.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import requiredIf from 'react-required-if';
-import Constraints from '../../constraints';
 import { getInputStyles } from '../styles';
+import { getConstraintSyles } from '../../constraints/horizontal';
 
 const TextInput = props => {
   const { horizontalConstraint, ...inputProps } = props;
+
   return (
-    <Constraints.Horizontal constraint={horizontalConstraint}>
-      <input
-        type="text"
-        css={theme => getInputStyles(props, theme)}
-        /* ARIA */
-        role="textbox"
-        aria-readonly={props.readOnly}
-        contentEditable={!props.readOnly}
-        {...inputProps}
-      />
-    </Constraints.Horizontal>
+    <input
+      type="text"
+      css={theme => [
+        getInputStyles(props, theme),
+        getConstraintSyles(props.horizontalConstraint),
+      ]}
+      /* ARIA */
+      role="textbox"
+      aria-readonly={props.readOnly}
+      contentEditable={!props.readOnly}
+      {...inputProps}
+    />
   );
 };
 
@@ -35,10 +37,6 @@ TextInput.propTypes = {
   placeholder: PropTypes.string,
   autoComplete: PropTypes.string,
   horizontalConstraint: PropTypes.oneOf(['s', 'm', 'l', 'xl', 'scale']),
-};
-
-TextInput.defaultProps = {
-  horizontalConstraint: 'scale',
 };
 
 TextInput.isEmpty = value => !value || value.trim().length === 0;
