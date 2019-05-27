faedce6a3ce1f997bf76ec3f90065db695e1a1bd
Malcolm <malcolmclaing@gmail.com>
2019-05-27 14:45:37 +0200

    chore: ignore some paths

--- jest.stylelint.config.js
+++ jest.stylelint.config.js
@@ -2,7 +2,13 @@ module.exports = {
   runner: 'jest-runner-stylelint',
   displayName: 'stylelint',
   moduleFileExtensions: ['js'],
-  modulePathIgnorePatterns: ['dist'],
-  testMatch: ['<rootDir>/**/*.js'],
+  modulePathIgnorePatterns: [
+    'dist',
+    '.spec.js',
+    '.visualspec.js',
+    '.visualroute.js',
+    '.story.js',
+  ],
+  testMatch: ['<rootDir>/**/*.js', ''],
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-master'],
 };
