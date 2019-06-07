4c1d965f65ed3ece5a7d7b5a31c0f4f34f05a591
jonnybel <jonnybel91@gmail.com>
2019-06-07 10:40:24 +0200

    feat(transforms): add codemod to automate refactor of prop renames

---
+++ transforms/react-component-rename-props.js
@@ -0,0 +1,57 @@
+/* eslint-disable guard-for-in */
+
+// Components whose props shall be renamed
+const componentNamesToRefactor = [
+  // 'TextInput'
+];
+
+// Props to rename
+const propertyRenameMap = {
+  // isDisabled: 'disabled',
+  // isAutofocussed: 'autoFocus',
+  // isReadOnly: 'readOnly',
+};
+
+const isAttributeInPropertyRenameMap = path =>
+  // eslint-disable-next-line no-prototype-builtins
+  propertyRenameMap.hasOwnProperty(path.value.name.name);
+
+const attributeBelongsToTargetComponent = path =>
+  componentNamesToRefactor.includes(path.parent.value.name.name);
+
+const shouldAttributeBeRenamed = path =>
+  attributeBelongsToTargetComponent(path) &&
+  isAttributeInPropertyRenameMap(path);
+
+const renameComponentAttribute = path => {
+  // eslint-disable-next-line no-param-reassign
+  path.value.name.name =
+    propertyRenameMap[path.value.name.name] || path.value.name.name;
+
+  return path.node;
+};
+
+// Renames properties from specified components
+module.exports = function transformer(file, api) {
+  const jscodeshift = api.jscodeshift;
+
+  let source = file.source;
+
+  // Rename variable references
+  // eslint-disable-next-line no-restricted-syntax
+  for (const property in propertyRenameMap) {
+    source = jscodeshift(source)
+      .findVariableDeclarators(property)
+      .renameTo(propertyRenameMap[property])
+      .toSource();
+  }
+
+  // Rename JSX attributes
+  source = jscodeshift(source)
+    .find(jscodeshift.JSXAttribute)
+    .filter(shouldAttributeBeRenamed)
+    .replaceWith(renameComponentAttribute)
+    .toSource();
+
+  return source;
+};
