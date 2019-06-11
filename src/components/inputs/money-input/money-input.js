a4cc73b32c50786eabe6632d31401392b745bb16
Malcolm Laing <malcolm.laing@commercetools.de>
2019-06-11 09:51:29 +0200

    chore: update deps

--- src/components/inputs/money-input/money-input.js
+++ src/components/inputs/money-input/money-input.js
@@ -679,9 +679,7 @@ class MoneyInput extends React.Component {
                   // so that the tooltip is flush with the component
                   styles={{
                     body: {
-                      margin: `${vars.spacing8} -${vars.spacing4} ${
-                        vars.spacing8
-                      } 0`,
+                      margin: `${vars.spacing8} -${vars.spacing4} ${vars.spacing8} 0`,
                     },
                   }}
                   title={this.props.intl.formatMessage(messages.highPrecision)}
