2b68dad0ff0542ecacc842b8ac7f6d8835de8d25
Malcolm Laing <malcolm.laing@commercetools.de>
2019-06-13 12:27:30 +0200

    fix(select-inputs): multi select disabled bug (#855)
    * fix(select-inputs): multi value removal bug

--- src/components/internals/tag-remove/tag-remove.js
+++ src/components/internals/tag-remove/tag-remove.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import omit from 'lodash/omit';
 import { CloseBoldIcon } from '../../icons';
 
-const TagRemove = props => (
-  <div {...props.innerProps}>
-    <CloseBoldIcon
-      theme={props.selectProps.isDisabled ? 'grey' : 'black'}
-      size="medium"
-    />
-  </div>
-);
+// see https://github.com/JedWatson/react-select/blob/44e9fb29b230e49a754a2f0d6f30c2250aa45009/src/components/MultiValue.js
+const removeProps = ['onClick', 'onTouchEnd', 'onMouseDown'];
+
+const TagRemove = props => {
+  // when the select input is disabled,
+  // we don't want to spread the removeProp event handlers
+  const innerProps = props.selectProps.isDisabled
+    ? omit(props.innerProps, removeProps)
+    : props.innerProps;
+
+  return (
+    <div {...innerProps}>
+      <CloseBoldIcon
+        theme={props.selectProps.isDisabled ? 'grey' : 'black'}
+        size="medium"
+      />
+    </div>
+  );
+};
 
 TagRemove.displayName = 'TagRemove';
 
