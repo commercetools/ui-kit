a186362383f1eb7d5edcaa2b79727b203d28d62d
Malcolm Laing <malcolm.laing@commercetools.de>
2019-06-13 12:17:24 +0200

    chore: add comment, rework omiting

--- src/components/internals/tag-remove/tag-remove.js
+++ src/components/internals/tag-remove/tag-remove.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import omit from 'lodash/omit';
 import { CloseBoldIcon } from '../../icons';
 
+// see https://github.com/JedWatson/react-select/blob/44e9fb29b230e49a754a2f0d6f30c2250aa45009/src/components/MultiValue.js
+const removeProps = ['onClick', 'onTouchEnd', 'onMouseDown'];
+
 const TagRemove = props => {
-  let innerProps = props.innerProps;
-  if (props.selectProps.isDisabled) {
-    innerProps = {
-      className: props.innerProps.className,
-    };
-  }
+  // when the select input is disabled,
+  // we don't want to spread the removeProp event handlers
+  const innerProps = props.selectProps.isDisabled
+    ? omit(props.innerProps, removeProps)
+    : props.innerProps;
 
   return (
     <div {...innerProps}>
