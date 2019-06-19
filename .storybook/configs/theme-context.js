28bb86a5578177996482b50af7bc6993414c0a07
Malcolm Laing <malcolm.laing@commercetools.de>
2019-06-19 11:36:34 +0200

    feat(storybook): add context add for theme

---
+++ .storybook/configs/theme-context.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import vars from '../../materials/custom-properties';
+import { ThemeProvider } from 'emotion-theming';
+
+const darkTheme = {
+  colorSolid: vars.colorSurface,
+  colorSurface: vars.colorSolid,
+};
+
+const defaultTheme = vars;
+
+const ThemeWrapper = props => {
+  return <ThemeProvider theme={props.theme}>{props.children}</ThemeProvider>;
+};
+
+ThemeWrapper.propTypes = {
+  theme: PropTypes.any,
+};
+
+const themeParams = [
+  {
+    name: 'Default Theme',
+    props: { theme: defaultTheme },
+    default: true,
+  },
+  {
+    name: 'Dark Theme (experimental)',
+    props: { theme: darkTheme },
+  },
+];
+
+const themeContext = {
+  icon: 'box', // a icon displayed in the Storybook toolbar to control contextual props
+  title: 'Themes', // an unique name of a contextual environment
+  components: [ThemeWrapper],
+  params: themeParams,
+  options: {
+    deep: true, // pass the `props` deeply into all wrapping components
+    disable: false, // disable this contextual environment completely
+    cancelable: false, // allow this contextual environment to be opt-out optionally in toolbar
+  },
+};
+
+export default themeContext;
--- .storybook/decorators/section/section.js
+++ .storybook/decorators/section/section.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { css } from '@emotion/core';
 import PropTypes from 'prop-types';
 
-const sectionStyles = {
-  padding: 16,
+const Section = props => {
+  return (
+    <div
+      css={theme => css`
+        background-color: ${theme.colorSurface};
+        padding: 16px;
+      `}
+    >
+      {props.children}
+    </div>
+  );
 };
-
-const Section = props => <div style={sectionStyles}>{props.children}</div>;
 
 Section.propTypes = { children: PropTypes.node.isRequired };
 
