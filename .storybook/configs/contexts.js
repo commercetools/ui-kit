c87d581e5aa01bfca76edb8495147c919902d13c
Malcolm Laing <malcolm.laing@commercetools.de>
2019-06-19 12:38:22 +0200

    feat(storybook): add context add for theme (#876)
    * feat(storybook): add context add for theme

--- .storybook/configs/contexts.js
+++ .storybook/configs/contexts.js
@@ -1,6 +1,8 @@
 import intlContext from './intl-context';
+import themeContext from './theme-context';
 
 export const contexts = [
   intlContext,
+  themeContext,
   /* ... */ // multiple contexts setups are supported
 ];
