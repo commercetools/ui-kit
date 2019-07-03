27900422781a1464d6f3bf9723c108e66be2117f
Malcolm Laing <malcolm.laing@commercetools.de>
2019-07-03 15:32:01 +0200

    feat(date-inputs): add tooltip to "today" button (#910)
    * feat(date-inputs): add tooltips to header controls

--- src/components/internals/calendar-header/calendar-header.js
+++ src/components/internals/calendar-header/calendar-header.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import { injectIntl } from 'react-intl';
 import vars from '../../../../materials/custom-properties';
 import Text from '../../typography/text';
 import Spacings from '../../spacings';
 import { AngleLeftIcon, AngleRightIcon, CircleIcon } from '../../icons';
+import Tooltip from '../../tooltip';
 import SecondaryIconButton from '../../buttons/secondary-icon-button';
+import messages from './messages';
+
+const WrapperComponent = styled.div`
+  display: flex;
+`;
 
 const CalendarHeader = props => (
   <div
@@ -18,37 +26,62 @@ const CalendarHeader = props => (
     `}
   >
     <Spacings.Inline scale="xs" alignItems="center">
-      <SecondaryIconButton
-        label="show prev month"
-        onClick={props.onPrevMonthClick}
-        icon={<AngleLeftIcon size="medium" />}
-      />
-      <SecondaryIconButton
-        label="show today"
-        onClick={props.onTodayClick}
-        icon={<CircleIcon size="medium" />}
-      />
-      <SecondaryIconButton
-        label="show next month"
-        onClick={props.onNextMonthClick}
-        icon={<AngleRightIcon size="medium" />}
-      />
+      <Tooltip
+        title={props.intl.formatMessage(messages.previousMonthTooltip)}
+        components={{ WrapperComponent }}
+      >
+        <SecondaryIconButton
+          label="show prev month"
+          onClick={props.onPrevMonthClick}
+          icon={<AngleLeftIcon size="medium" />}
+        />
+      </Tooltip>
+      <Tooltip
+        title={props.intl.formatMessage(messages.todayTooltip)}
+        components={{ WrapperComponent }}
+      >
+        <SecondaryIconButton
+          label="show today"
+          onClick={props.onTodayClick}
+          icon={<CircleIcon size="medium" />}
+        />
+      </Tooltip>
+      <Tooltip
+        title={props.intl.formatMessage(messages.nextMonthTooltip)}
+        components={{ WrapperComponent }}
+      >
+        <SecondaryIconButton
+          label="show next month"
+          onClick={props.onNextMonthClick}
+          icon={<AngleRightIcon size="medium" />}
+        />
+      </Tooltip>
       <Text.Body isInline={true} isBold={true}>
         {props.monthLabel}
       </Text.Body>
     </Spacings.Inline>
     <Spacings.Inline scale="xs" alignItems="center">
-      <SecondaryIconButton
-        label="show prev year"
-        onClick={props.onPrevYearClick}
-        icon={<AngleLeftIcon size="medium" />}
-      />
+      <Tooltip
+        title={props.intl.formatMessage(messages.previousYearTooltip)}
+        components={{ WrapperComponent }}
+      >
+        <SecondaryIconButton
+          label="show prev year"
+          onClick={props.onPrevYearClick}
+          icon={<AngleLeftIcon size="medium" />}
+        />
+      </Tooltip>
       <Text.Body isBold={true}>{props.yearLabel}</Text.Body>
-      <SecondaryIconButton
-        label="show next year"
-        onClick={props.onNextYearClick}
-        icon={<AngleRightIcon size="medium" />}
-      />
+      <Tooltip
+        title={props.intl.formatMessage(messages.nextYearTooltip)}
+        components={{ WrapperComponent }}
+      >
+        <SecondaryIconButton
+          label="show next year"
+          onClick={props.onNextYearClick}
+          icon={<AngleRightIcon size="medium" />}
+        />
+      </Tooltip>
     </Spacings.Inline>
   </div>
 );
@@ -63,6 +96,9 @@ CalendarHeader.propTypes = {
   onNextMonthClick: PropTypes.func.isRequired,
   onPrevYearClick: PropTypes.func.isRequired,
   onNextYearClick: PropTypes.func.isRequired,
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
-export default CalendarHeader;
+export default injectIntl(CalendarHeader);
---
+++ src/components/internals/calendar-header/messages.js
@@ -0,0 +1,29 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  todayTooltip: {
+    id: 'UIKit.CalendarHeader.todayTooltip',
+    description: 'Tooltip title when user hovers the today button',
+    defaultMessage: 'Today',
+  },
+  previousMonthTooltip: {
+    id: 'UIKit.CalendarHeader.previousMonthTooltip',
+    description: 'Tooltip title when user hovers the previous month button',
+    defaultMessage: 'Previous month',
+  },
+  nextMonthTooltip: {
+    id: 'UIKit.CalendarHeader.nextMonthTooltip',
+    description: 'Tooltip title when user hovers the next month button',
+    defaultMessage: 'Next month',
+  },
+  previousYearTooltip: {
+    id: 'UIKit.CalendarHeader.previousYearTooltip',
+    description: 'Tooltip title when user hovers the previous year button',
+    defaultMessage: 'Previous year',
+  },
+  nextYearTooltip: {
+    id: 'UIKit.CalendarHeader.nextYearTooltip',
+    description: 'Tooltip title when user hovers the next year button',
+    defaultMessage: 'Next year',
+  },
+});
