89f3e95706518ecacf1e0163857b3b53ab6d0763
Malcolm Laing <malcolm.laing@commercetools.de>
2019-07-25 11:40:54 -0700

    refactor(creatable-select-input): use hooks

--- src/components/inputs/creatable-select-input/creatable-select-input.js
+++ src/components/inputs/creatable-select-input/creatable-select-input.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { injectIntl } from 'react-intl';
-import { withTheme } from 'emotion-theming';
+import { useIntl } from 'react-intl';
+import { ThemeContext } from '@emotion/core';
 import { components as defaultComponents } from 'react-select';
 import CreatableSelect from 'react-select/creatable';
 import Constraints from '../../constraints';
@@ -14,236 +14,223 @@ import DropdownIndicator from '../../internals/dropdown-indicator';
 import createSelectStyles from '../../internals/create-select-styles';
 import messages from './messages';
 
+const useTheme = () => useContext(ThemeContext);
+
 const customizedComponents = {
   DropdownIndicator,
   ClearIndicator,
   MultiValueRemove: TagRemove,
 };
 
-export class CreatableSelectInput extends React.Component {
-  static displayName = 'SelectInput';
+const CreatableSelectInput = props => {
+  const intl = useIntl();
+  const theme = useTheme();
 
-  // Both "true" and an empty array [] represent a touched state. The Boolean
-  // conveniently handles both cases
-  static isTouched = touched => Boolean(touched);
+  const placeholder =
+    props.placeholder || intl.formatMessage(messages.placeholder);
 
-  static propTypes = {
-    horizontalConstraint: PropTypes.oneOf(['s', 'm', 'l', 'xl', 'scale']),
-    intl: PropTypes.shape({
-      formatMessage: PropTypes.func.isRequired,
-    }).isRequired,
-    // withTheme
-    theme: PropTypes.object,
-    hasError: PropTypes.bool,
-    hasWarning: PropTypes.bool,
+  return (
+    <Constraints.Horizontal constraint={props.horizontalConstraint}>
+      <div {...filterDataAttributes(props)}>
+        <CreatableSelect
+          aria-label={props['aria-label']}
+          aria-labelledby={props['aria-labelledby']}
+          autoFocus={props.isAutofocussed}
+          backspaceRemovesValue={props.backspaceRemovesValue}
+          components={{
+            ...customizedComponents,
+            ...props.components,
+          }}
+          styles={createSelectStyles(
+            {
+              hasWarning: props.hasWarning,
+              hasError: props.hasError,
+              showOptionGroupDivider: props.showOptionGroupDivider,
+              menuPortalZIndex: props.menuPortalZIndex,
+            },
+            theme
+          )}
+          filterOption={props.filterOption}
+          // react-select uses "id" (for the container) and "inputId" (for the input),
+          // but we use "id" (for the input) and "containerId" (for the container)
+          // instead.
+          // This makes it easier to less confusing to use with <label />s.
+          id={props.containerId}
+          inputId={props.id}
+          isClearable={props.isClearable}
+          isDisabled={props.isDisabled}
+          isOptionDisabled={props.isOptionDisabled}
+          isMulti={props.isMulti}
+          isSearchable={props.isSearchable}
+          maxMenuHeight={props.maxMenuHeight}
+          menuPortalTarget={props.menuPortalTarget}
+          menuShouldBlockScroll={props.menuShouldBlockScroll}
+          name={props.name}
+          noOptionsMessage={
+            props.noOptionsMessage ||
+            (({ inputValue }) =>
+              inputValue === ''
+                ? intl.formatMessage(messages.noOptionsMessageWithoutInputValue)
+                : intl.formatMessage(messages.noOptionsMessageWithInputValue, {
+                    inputValue,
+                  }))
+          }
+          onBlur={
+            typeof props.onBlur === 'function'
+              ? () => {
+                  const event = {
+                    target: {
+                      name: (() => {
+                        if (!props.isMulti) return props.name;
+                        // We append the ".0" to make Formik set the touched
+                        // state as an array instead of a boolean only.
+                        // Otherwise the shapes would clash on submission, as
+                        // Formik will create an array on submission anyways.
+                        return props.name ? `${props.name}.0` : undefined;
+                      })(),
+                    },
+                    persist: () => {},
+                  };
+                  props.onBlur(event);
+                }
+              : undefined
+          }
+          onChange={(value, info) => {
+            // selectedOptions is either an array, or a single option, or null
+            // depending on whether we're in multi-mode or not (isMulti)
 
-    // react-select base props
-    //
-    // Currently unsupported props are commented out. In case you need one of
-    // these props when using UI Kit, you can submit a PR and enable the
-    // prop. Don't forget to add it to the story, docs and other select input
-    // components as well!
-    //
-    // See https://react-select.com/props#select-props
-    'aria-label': PropTypes.string,
-    'aria-labelledby': PropTypes.string,
-    isAutofocussed: PropTypes.bool, // original: autoFocus
-    backspaceRemovesValue: PropTypes.bool,
-    components: PropTypes.objectOf(PropTypes.func),
-    filterOption: PropTypes.func,
-    // This forwarded as react-select's "inputId"
-    id: PropTypes.string,
-    // This is forwarded as react-select's "id"
-    containerId: PropTypes.string,
-    isClearable: PropTypes.bool,
-    isDisabled: PropTypes.bool,
-    isOptionDisabled: PropTypes.func,
-    isMulti: PropTypes.bool,
-    isSearchable: PropTypes.bool,
-    maxMenuHeight: PropTypes.number,
-    menuPortalTarget: PropTypes.instanceOf(SafeHTMLElement),
-    menuPortalZIndex: PropTypes.number.isRequired,
-    menuShouldBlockScroll: PropTypes.bool,
-    name: PropTypes.string,
-    noOptionsMessage: PropTypes.func,
-    onBlur: PropTypes.func,
-    onChange: PropTypes.func.isRequired,
-    onFocus: PropTypes.func,
-    onInputChange: PropTypes.func,
-    options: PropTypes.arrayOf(
-      PropTypes.oneOfType([
-        PropTypes.shape({ value: PropTypes.string.isRequired }),
-        PropTypes.shape({
-          options: PropTypes.arrayOf(
-            PropTypes.shape({ value: PropTypes.string.isRequired })
-          ),
-        }),
-      ])
-    ),
-    showOptionGroupDivider: PropTypes.bool,
-    placeholder: PropTypes.string,
-    tabIndex: PropTypes.string,
-    tabSelectsValue: PropTypes.bool,
-    value: (props, ...rest) =>
-      props.isMulti
-        ? PropTypes.arrayOf(
-            PropTypes.shape({ value: PropTypes.string.isRequired })
-          )(props, ...rest)
-        : PropTypes.shape({ value: PropTypes.string.isRequired })(
-            props,
-            ...rest
-          ),
+            let newValue = value;
 
-    // Creatable props
-    allowCreateWhileLoading: PropTypes.bool,
-    formatCreateLabel: PropTypes.func,
-    isValidNewOption: PropTypes.func,
-    getNewOptionData: PropTypes.func,
-    onCreateOption: PropTypes.func,
-    createOptionPosition: PropTypes.string,
-  };
+            if (props.isMulti && !newValue) {
+              newValue = [];
+            }
 
-  static defaultProps = {
-    // Using "null" will ensure that the currently selected value disappears in
-    // case "undefined" gets passed as the next value
-    value: null,
-    // The input needs to be searchable, otherwise it's not possible to create
-    // new options.
-    // We still allow consumers to pass isSearchable={false} so that they can
-    // use CreatableSelectInput as an alternative to SelectInput, which does
-    // not do the option/value mapping going on there and therefore provides
-    // the default API of react-select.
-    isSearchable: true,
-    menuPortalZIndex: 1,
-  };
-
-  render() {
-    const placeholder =
-      this.props.placeholder ||
-      this.props.intl.formatMessage(messages.placeholder);
-
-    return (
-      <Constraints.Horizontal constraint={this.props.horizontalConstraint}>
-        <div {...filterDataAttributes(this.props)}>
-          <CreatableSelect
-            aria-label={this.props['aria-label']}
-            aria-labelledby={this.props['aria-labelledby']}
-            autoFocus={this.props.isAutofocussed}
-            backspaceRemovesValue={this.props.backspaceRemovesValue}
-            components={{
-              ...customizedComponents,
-              ...this.props.components,
-            }}
-            styles={createSelectStyles(
+            props.onChange(
               {
-                hasWarning: this.props.hasWarning,
-                hasError: this.props.hasError,
-                showOptionGroupDivider: this.props.showOptionGroupDivider,
-                menuPortalZIndex: this.props.menuPortalZIndex,
+                target: { name: props.name, value: newValue },
+                persist: () => {},
               },
-              this.props.theme
-            )}
-            filterOption={this.props.filterOption}
-            // react-select uses "id" (for the container) and "inputId" (for the input),
-            // but we use "id" (for the input) and "containerId" (for the container)
-            // instead.
-            // This makes it easier to less confusing to use with <label />s.
-            id={this.props.containerId}
-            inputId={this.props.id}
-            isClearable={this.props.isClearable}
-            isDisabled={this.props.isDisabled}
-            isOptionDisabled={this.props.isOptionDisabled}
-            isMulti={this.props.isMulti}
-            isSearchable={this.props.isSearchable}
-            maxMenuHeight={this.props.maxMenuHeight}
-            menuPortalTarget={this.props.menuPortalTarget}
-            menuShouldBlockScroll={this.props.menuShouldBlockScroll}
-            name={this.props.name}
-            noOptionsMessage={
-              this.props.noOptionsMessage ||
-              (({ inputValue }) =>
-                inputValue === ''
-                  ? this.props.intl.formatMessage(
-                      messages.noOptionsMessageWithoutInputValue
-                    )
-                  : this.props.intl.formatMessage(
-                      messages.noOptionsMessageWithInputValue,
-                      { inputValue }
-                    ))
-            }
-            onBlur={
-              typeof this.props.onBlur === 'function'
-                ? () => {
-                    const event = {
-                      target: {
-                        name: (() => {
-                          if (!this.props.isMulti) return this.props.name;
-                          // We append the ".0" to make Formik set the touched
-                          // state as an array instead of a boolean only.
-                          // Otherwise the shapes would clash on submission, as
-                          // Formik will create an array on submission anyways.
-                          return this.props.name
-                            ? `${this.props.name}.0`
-                            : undefined;
-                        })(),
-                      },
-                      persist: () => {},
-                    };
-                    this.props.onBlur(event);
-                  }
-                : undefined
-            }
-            onChange={(value, info) => {
-              // selectedOptions is either an array, or a single option, or null
-              // depending on whether we're in multi-mode or not (isMulti)
+              info
+            );
+          }}
+          onFocus={props.onFocus}
+          onInputChange={props.onInputChange}
+          options={props.options}
+          placeholder={placeholder}
+          tabIndex={props.tabIndex}
+          tabSelectsValue={props.tabSelectsValue}
+          value={props.value}
+          // Creatable props
+          allowCreateWhileLoading={props.allowCreateWhileLoading}
+          formatCreateLabel={
+            props.formatCreateLabel ||
+            (inputValue =>
+              intl.formatMessage(messages.createLabel, {
+                inputValue,
+              }))
+          }
+          isValidNewOption={props.isValidNewOption}
+          getNewOptionData={props.getNewOptionData}
+          onCreateOption={props.onCreateOption}
+          createOptionPosition={props.createOptionPosition}
+        />
+      </div>
+    </Constraints.Horizontal>
+  );
+};
 
-              let newValue = value;
+CreatableSelectInput.displayName = 'CreatableSelectInput';
 
-              if (this.props.isMulti && !newValue) {
-                newValue = [];
-              }
+// Both "true" and an empty array [] represent a touched state. The Boolean
+// conveniently handles both cases
+CreatableSelectInput.isTouched = touched => Boolean(touched);
 
-              this.props.onChange(
-                {
-                  target: { name: this.props.name, value: newValue },
-                  persist: () => {},
-                },
-                info
-              );
-            }}
-            onFocus={this.props.onFocus}
-            onInputChange={this.props.onInputChange}
-            options={this.props.options}
-            placeholder={placeholder}
-            tabIndex={this.props.tabIndex}
-            tabSelectsValue={this.props.tabSelectsValue}
-            value={this.props.value}
-            // Creatable props
-            allowCreateWhileLoading={this.props.allowCreateWhileLoading}
-            formatCreateLabel={
-              this.props.formatCreateLabel ||
-              (inputValue =>
-                this.props.intl.formatMessage(messages.createLabel, {
-                  inputValue,
-                }))
-            }
-            isValidNewOption={this.props.isValidNewOption}
-            getNewOptionData={this.props.getNewOptionData}
-            onCreateOption={this.props.onCreateOption}
-            createOptionPosition={this.props.createOptionPosition}
-          />
-        </div>
-      </Constraints.Horizontal>
-    );
-  }
-}
+CreatableSelectInput.propTypes = {
+  horizontalConstraint: PropTypes.oneOf(['s', 'm', 'l', 'xl', 'scale']),
+  hasError: PropTypes.bool,
+  hasWarning: PropTypes.bool,
 
-const Enhanced = injectIntl(CreatableSelectInput);
-const Themed = withTheme(Enhanced);
+  // react-select base props
+  //
+  // Currently unsupported props are commented out. In case you need one of
+  // these props when using UI Kit, you can submit a PR and enable the
+  // prop. Don't forget to add it to the story, docs and other select input
+  // components as well!
+  //
+  // See https://react-select.com/props#select-props
+  'aria-label': PropTypes.string,
+  'aria-labelledby': PropTypes.string,
+  isAutofocussed: PropTypes.bool, // original: autoFocus
+  backspaceRemovesValue: PropTypes.bool,
+  components: PropTypes.objectOf(PropTypes.func),
+  filterOption: PropTypes.func,
+  // This forwarded as react-select's "inputId"
+  id: PropTypes.string,
+  // This is forwarded as react-select's "id"
+  containerId: PropTypes.string,
+  isClearable: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  isOptionDisabled: PropTypes.func,
+  isMulti: PropTypes.bool,
+  isSearchable: PropTypes.bool,
+  maxMenuHeight: PropTypes.number,
+  menuPortalTarget: PropTypes.instanceOf(SafeHTMLElement),
+  menuPortalZIndex: PropTypes.number.isRequired,
+  menuShouldBlockScroll: PropTypes.bool,
+  name: PropTypes.string,
+  noOptionsMessage: PropTypes.func,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
+  onFocus: PropTypes.func,
+  onInputChange: PropTypes.func,
+  options: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.shape({ value: PropTypes.string.isRequired }),
+      PropTypes.shape({
+        options: PropTypes.arrayOf(
+          PropTypes.shape({ value: PropTypes.string.isRequired })
+        ),
+      }),
+    ])
+  ),
+  showOptionGroupDivider: PropTypes.bool,
+  placeholder: PropTypes.string,
+  tabIndex: PropTypes.string,
+  tabSelectsValue: PropTypes.bool,
+  value: (props, ...rest) =>
+    props.isMulti
+      ? PropTypes.arrayOf(
+          PropTypes.shape({ value: PropTypes.string.isRequired })
+        )(props, ...rest)
+      : PropTypes.shape({ value: PropTypes.string.isRequired })(props, ...rest),
 
-addStaticFields(Enhanced, {
+  // Creatable props
+  allowCreateWhileLoading: PropTypes.bool,
+  formatCreateLabel: PropTypes.func,
+  isValidNewOption: PropTypes.func,
+  getNewOptionData: PropTypes.func,
+  onCreateOption: PropTypes.func,
+  createOptionPosition: PropTypes.string,
+};
+
+CreatableSelectInput.defaultProps = {
+  // Using "null" will ensure that the currently selected value disappears in
+  // case "undefined" gets passed as the next value
+  value: null,
+  // The input needs to be searchable, otherwise it's not possible to create
+  // new options.
+  // We still allow consumers to pass isSearchable={false} so that they can
+  // use CreatableSelectInput as an alternative to SelectInput, which does
+  // not do the option/value mapping going on there and therefore provides
+  // the default API of react-select.
+  isSearchable: true,
+  menuPortalZIndex: 1,
+};
+
+addStaticFields(CreatableSelectInput, {
   ...defaultComponents,
   ...customizedComponents,
   isTouched: CreatableSelectInput.isTouched,
 });
-export default Themed;
+
+export default CreatableSelectInput;
