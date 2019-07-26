c540c672a0dc14670faa873d3cc2c71e6c78c133
Malcolm Laing <malcolm.laing@commercetools.de>
2019-07-26 14:10:12 -0700

    chore: vrt fix

--- src/components/inputs/multiline-text-input/multiline-text-input.js
+++ src/components/inputs/multiline-text-input/multiline-text-input.js
@@ -20,7 +20,7 @@ const MultilineTextInput = props => {
     MultilineTextInput.MIN_ROW_COUNT
   );
 
-  const [isOpen, toggle] = useCollapsible(props.defaultExpandMultilineText);
+  const [isOpen, toggle] = useCollapsible(!!props.defaultExpandMultilineText);
 
   const handleHeightChange = React.useCallback((_, innerComponent) => {
     setContentRowCount(innerComponent.valueRowCount);
