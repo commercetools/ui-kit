99b8f928b8fd1574b0936bc4cb27825df8984078
Malcolm Laing <malcolm.laing@commercetools.de>
2019-07-26 11:02:15 -0700

    refactor(date-input): use hooks

--- src/components/inputs/date-input/date-input.js
+++ src/components/inputs/date-input/date-input.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Downshift from 'downshift';
-import { injectIntl } from 'react-intl';
+import { useIntl } from 'react-intl';
 import CalendarBody from '../../internals/calendar-body';
 import CalendarMenu from '../../internals/calendar-menu';
 import CalendarHeader from '../../internals/calendar-header';
@@ -25,267 +25,237 @@ import {
   parseInputToDate,
 } from '../../../utils/calendar';
 
-class DateInput extends React.Component {
-  static displayName = 'DateInput';
+const DateInput = props => {
+  const intl = useIntl();
+  const [calendarDate, setCalendarDate] = React.useState(
+    props.value || getToday()
+  );
+  const [suggestedItems, setSuggestedItems] = React.useState([]);
+  const [highlightedIndex, setHighlightedIndex] = React.useState(
+    props.value === '' ? null : getDateInMonth(props.value) - 1
+  );
+  const inputRef = React.useRef();
 
-  static isEmpty = value => value === '';
-
-  static propTypes = {
-    intl: PropTypes.shape({
-      locale: PropTypes.string.isRequired,
-      formatMessage: PropTypes.func.isRequired,
-    }).isRequired,
-    horizontalConstraint: PropTypes.oneOf(['m', 'l', 'xl', 'scale']),
-    value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    onFocus: PropTypes.func,
-    onBlur: PropTypes.func,
-    id: PropTypes.string,
-    name: PropTypes.string,
-    placeholder: PropTypes.string,
-    isDisabled: PropTypes.bool,
-    hasError: PropTypes.bool,
-    hasWarning: PropTypes.bool,
-  };
-
-  inputRef = React.createRef();
-
-  state = {
-    calendarDate: this.props.value || getToday(),
-    suggestedItems: [],
-    highlightedIndex:
-      this.props.value === '' ? null : getDateInMonth(this.props.value) - 1,
-  };
-
-  showPrevMonth = () => {
-    this.setState(prevState => ({
-      calendarDate: changeMonth(prevState.calendarDate, -1),
-      // select first day in next month
-      highlightedIndex: prevState.suggestedItems.length,
-    }));
-  };
-
-  jumpMonth = amount => {
-    this.setState(prevState => {
-      const nextDate = changeMonth(prevState.calendarDate, amount);
-      return { calendarDate: nextDate, highlightedIndex: 0 };
-    });
-  };
-
-  showToday = () => {
-    const today = getToday();
-    this.setState(
-      prevState => ({
-        calendarDate: today,
-        highlightedIndex:
-          prevState.suggestedItems.length + getDateInMonth(today) - 1,
-      }),
-      () => this.inputRef.current.focus()
-    );
-  };
-
-  handleBlur = () => {
-    if (this.props.onBlur)
-      this.props.onBlur({
+  const emit = React.useCallback(
+    value =>
+      props.onChange({
         target: {
-          id: this.props.id,
-          name: this.props.name,
+          id: props.id,
+          name: props.name,
+          // when cleared the value is null, but we always want it to be an
+          // empty string when there is no value.
+          value: value || '',
+        },
+      }),
+    [props.id, props.name]
+  );
+
+  const handleChange = React.useCallback(
+    date => {
+      inputRef.current.setSelectionRange(0, 100);
+      emit(date);
+    },
+    [inputRef]
+  );
+
+  const handleBlur = React.useCallback(() => {
+    if (props.onBlur)
+      props.onBlur({
+        target: {
+          id: props.id,
+          name: props.name,
         },
       });
+  }, [props.id, props.name]);
+
+  const showToday = () => {
+    const today = getToday();
+    setCalendarDate(today);
+    setHighlightedIndex(suggestedItems.length + getDateInMonth(today) - 1);
+    inputRef.current.focus();
   };
 
-  handleChange = date => {
-    this.inputRef.current.setSelectionRange(0, 100);
-    this.emit(date);
+  const jumpMonth = amount => {
+    const nextDate = changeMonth(calendarDate, amount);
+    setCalendarDate(nextDate);
+    setHighlightedIndex(0);
   };
 
-  emit = value =>
-    this.props.onChange({
-      target: {
-        id: this.props.id,
-        name: this.props.name,
-        // when cleared the value is null, but we always want it to be an
-        // empty string when there is no value.
-        value: value || '',
-      },
-    });
-
-  render() {
-    return (
-      <Constraints.Horizontal constraint={this.props.horizontalConstraint}>
-        <Downshift
-          key={this.props.intl.locale}
-          inputId={this.props.id}
-          itemToString={createItemToString(this.props.intl.locale)}
-          selectedItem={this.props.value === '' ? null : this.props.value}
-          highlightedIndex={this.state.highlightedIndex}
-          onChange={this.handleChange}
-          onStateChange={changes => {
-            /* eslint-disable no-prototype-builtins */
-            if (changes.hasOwnProperty('inputValue')) {
-              // input changed because user typed
-              if (changes.type === Downshift.stateChangeTypes.changeInput) {
-                const date = parseInputToDate(
-                  changes.inputValue,
-                  this.props.intl.locale
-                );
-                this.setState(
-                  date === ''
-                    ? {
-                        suggestedItems: [],
-                        highlightedIndex: null,
-                      }
-                    : {
-                        suggestedItems: [date],
-                        highlightedIndex: getDateInMonth(date) - 1,
-                        calendarDate: date,
-                      }
-                );
+  return (
+    <Constraints.Horizontal constraint={props.horizontalConstraint}>
+      <Downshift
+        key={intl.locale}
+        inputId={props.id}
+        itemToString={createItemToString(intl.locale)}
+        selectedItem={props.value === '' ? null : props.value}
+        highlightedIndex={highlightedIndex}
+        onChange={handleChange}
+        onStateChange={changes => {
+          /* eslint-disable no-prototype-builtins */
+          if (changes.hasOwnProperty('inputValue')) {
+            // input changed because user typed
+            if (changes.type === Downshift.stateChangeTypes.changeInput) {
+              const date = parseInputToDate(changes.inputValue, intl.locale);
+              if (date === '') {
+                setSuggestedItems([]);
+                setHighlightedIndex(null);
               } else {
-                // input changed because user selected a date
-                this.setState({
-                  suggestedItems: [],
-                  highlightedIndex: null,
-                });
+                setSuggestedItems([date]);
+                setHighlightedIndex(getDateInMonth(date) - 1);
+                setCalendarDate(date);
               }
+            } else {
+              // input changed because user selected a date
+              setSuggestedItems([]);
+              setHighlightedIndex(null);
             }
+          }
 
-            if (changes.hasOwnProperty('highlightedIndex')) {
-              this.setState({ highlightedIndex: changes.highlightedIndex });
-            }
+          if (changes.hasOwnProperty('highlightedIndex')) {
+            setHighlightedIndex(changes.highlightedIndex);
+          }
 
-            // ensure calendar always opens on selected item, or on current
-            // month when there is no selected item
-            if (changes.hasOwnProperty('isOpen') && changes.isOpen) {
-              this.setState({
-                calendarDate:
-                  this.props.value === '' ? getToday() : this.props.value,
-              });
-            }
-            /* eslint-enable no-prototype-builtins */
-          }}
-        >
-          {({
-            getInputProps,
-            getMenuProps,
-            getItemProps,
-            getToggleButtonProps,
+          // ensure calendar always opens on selected item, or on current
+          // month when there is no selected item
+          if (changes.hasOwnProperty('isOpen') && changes.isOpen) {
+            setCalendarDate(props.value === '' ? getToday() : props.value);
+          }
+          /* eslint-enable no-prototype-builtins */
+        }}
+      >
+        {({
+          getInputProps,
+          getMenuProps,
+          getItemProps,
+          getToggleButtonProps,
 
-            clearSelection,
+          clearSelection,
 
-            highlightedIndex,
-            openMenu,
-            setHighlightedIndex,
-            selectedItem,
-            isOpen,
-            inputValue,
-          }) => {
-            const calendarItems = createCalendarItems(
-              this.state.calendarDate,
-              this.props.intl
-            );
+          highlightedIndex: downshiftHighlightedIndex,
+          openMenu,
+          setHighlightedIndex: setDownshiftHighlightedIndex,
+          selectedItem,
+          isOpen,
+          inputValue,
+        }) => {
+          const calendarItems = createCalendarItems(calendarDate, intl);
 
-            const paddingDayCount = getPaddingDayCount(
-              this.state.calendarDate,
-              this.props.intl.locale
-            );
-            const paddingDays = Array(paddingDayCount).fill();
+          const paddingDayCount = getPaddingDayCount(calendarDate, intl.locale);
+          const paddingDays = Array(paddingDayCount).fill();
 
-            const weekdays = getWeekdayNames(this.props.intl.locale);
-            const today = getToday();
+          const weekdays = getWeekdayNames(intl.locale);
+          const today = getToday();
 
-            return (
-              <div onFocus={this.props.onFocus} onBlur={this.handleBlur}>
-                <CalendarBody
-                  inputRef={this.inputRef}
-                  inputProps={getInputProps({
-                    name: this.props.name,
-                    placeholder:
-                      typeof this.props.placeholder === 'string'
-                        ? this.props.placeholder
-                        : this.props.intl.formatMessage(messages.placeholder),
-                    disabled: this.props.isDisabled,
-                    onMouseEnter: () => {
-                      // we remove the highlight so that the user can use the
-                      // arrow keys to move the cursor when hovering
-                      if (isOpen) setHighlightedIndex(null);
-                    },
-                    onKeyDown: event => {
-                      if (event.key === 'Enter' && inputValue.trim() === '') {
-                        clearSelection();
-                      }
-                    },
-                    onFocus: openMenu,
-                    onClick: openMenu,
-                    ...filterDataAttributes(this.props),
-                  })}
-                  hasSelection={Boolean(selectedItem)}
-                  onClear={clearSelection}
-                  isOpen={isOpen}
-                  isDisabled={this.props.isDisabled}
-                  toggleButtonProps={getToggleButtonProps({
-                    disabled: this.props.isDisabled,
-                  })}
-                  hasError={this.props.hasError}
-                  hasWarning={this.props.hasWarning}
-                />
-                {isOpen && !this.props.isDisabled && (
-                  <CalendarMenu
-                    {...getMenuProps()}
-                    hasError={this.props.hasError}
-                    hasWarning={this.props.hasWarning}
-                  >
-                    <CalendarHeader
-                      monthLabel={getMonthCalendarLabel(
-                        this.state.calendarDate,
-                        this.props.intl.locale
-                      )}
-                      yearLabel={getYearCalendarLabel(
-                        this.state.calendarDate,
-                        this.props.intl.locale
-                      )}
-                      onPrevMonthClick={() => this.jumpMonth(-1)}
-                      onTodayClick={this.showToday}
-                      onNextMonthClick={() => this.jumpMonth(1)}
-                      onPrevYearClick={() => this.jumpMonth(-12)}
-                      onNextYearClick={() => this.jumpMonth(12)}
-                    />
-                    <CalendarContent>
-                      {weekdays.map(weekday => (
-                        <CalendarDay key={weekday} type="heading">
-                          {weekday}
-                        </CalendarDay>
-                      ))}
-                      {paddingDays.map((day, index) => (
-                        <CalendarDay key={index} type="spacing" />
-                      ))}
-                      {calendarItems.map((item, index) => (
-                        <CalendarDay
-                          key={item}
-                          isToday={isSameDay(today, item)}
-                          {...getItemProps({
-                            disabled: this.props.isDisabled,
-                            item,
-                            onMouseOut: () => {
-                              setHighlightedIndex(null);
-                            },
-                          })}
-                          isHighlighted={index === highlightedIndex}
-                          isSelected={isSameDay(item, this.props.value)}
-                        >
-                          {getCalendarDayLabel(item)}
-                        </CalendarDay>
-                      ))}
-                    </CalendarContent>
-                  </CalendarMenu>
-                )}
-              </div>
-            );
-          }}
-        </Downshift>
-      </Constraints.Horizontal>
-    );
-  }
-}
+          return (
+            <div onFocus={props.onFocus} onBlur={handleBlur}>
+              <CalendarBody
+                inputRef={inputRef}
+                inputProps={getInputProps({
+                  name: props.name,
+                  placeholder:
+                    typeof props.placeholder === 'string'
+                      ? props.placeholder
+                      : intl.formatMessage(messages.placeholder),
+                  disabled: props.isDisabled,
+                  onMouseEnter: () => {
+                    // we remove the highlight so that the user can use the
+                    // arrow keys to move the cursor when hovering
+                    if (isOpen) setDownshiftHighlightedIndex(null);
+                  },
+                  onKeyDown: event => {
+                    if (event.key === 'Enter' && inputValue.trim() === '') {
+                      clearSelection();
+                    }
+                  },
+                  onFocus: openMenu,
+                  onClick: openMenu,
+                  ...filterDataAttributes(props),
+                })}
+                hasSelection={Boolean(selectedItem)}
+                onClear={clearSelection}
+                isOpen={isOpen}
+                isDisabled={props.isDisabled}
+                toggleButtonProps={getToggleButtonProps({
+                  disabled: props.isDisabled,
+                })}
+                hasError={props.hasError}
+                hasWarning={props.hasWarning}
+              />
+              {isOpen && !props.isDisabled && (
+                <CalendarMenu
+                  {...getMenuProps()}
+                  hasError={props.hasError}
+                  hasWarning={props.hasWarning}
+                >
+                  <CalendarHeader
+                    monthLabel={getMonthCalendarLabel(
+                      calendarDate,
+                      intl.locale
+                    )}
+                    yearLabel={getYearCalendarLabel(calendarDate, intl.locale)}
+                    onPrevMonthClick={() => jumpMonth(-1)}
+                    onTodayClick={showToday}
+                    onNextMonthClick={() => jumpMonth(1)}
+                    onPrevYearClick={() => jumpMonth(-12)}
+                    onNextYearClick={() => jumpMonth(12)}
+                  />
+                  <CalendarContent>
+                    {weekdays.map(weekday => (
+                      <CalendarDay key={weekday} type="heading">
+                        {weekday}
+                      </CalendarDay>
+                    ))}
+                    {paddingDays.map((day, index) => (
+                      <CalendarDay key={index} type="spacing" />
+                    ))}
+                    {calendarItems.map((item, index) => (
+                      <CalendarDay
+                        key={item}
+                        isToday={isSameDay(today, item)}
+                        {...getItemProps({
+                          disabled: props.isDisabled,
+                          item,
+                          onMouseOut: () => {
+                            setDownshiftHighlightedIndex(null);
+                          },
+                        })}
+                        isHighlighted={index === downshiftHighlightedIndex}
+                        isSelected={isSameDay(item, props.value)}
+                      >
+                        {getCalendarDayLabel(item)}
+                      </CalendarDay>
+                    ))}
+                  </CalendarContent>
+                </CalendarMenu>
+              )}
+            </div>
+          );
+        }}
+      </Downshift>
+    </Constraints.Horizontal>
+  );
+};
 
-export default injectIntl(DateInput);
+DateInput.displayName = 'DateInput';
+
+DateInput.isEmpty = value => value === '';
+
+DateInput.propTypes = {
+  intl: PropTypes.shape({
+    locale: PropTypes.string.isRequired,
+    formatMessage: PropTypes.func.isRequired,
+  }).isRequired,
+  horizontalConstraint: PropTypes.oneOf(['m', 'l', 'xl', 'scale']),
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  hasError: PropTypes.bool,
+  hasWarning: PropTypes.bool,
+};
+
+export default DateInput;
