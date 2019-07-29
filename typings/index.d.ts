d888d972d3c7bb278526538aa2a374258195ced1
Nicola Molinari <nicola.molinari@commercetools.de>
2019-07-30 00:05:38 +0200

    refactor: add more typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,25 +6,78 @@ export const version: string;
 
 // <ContentNotification>
 export type ContentNotificationProps = {
-  type: "error" | "info" | "warning" | "success",
-  children: React.ReactNode
+  type: 'error' | 'info' | 'warning' | 'success';
+  children: React.ReactNode;
 };
-export function ContentNotification(props: ContentNotificationProps): JSX.Element;
+export function ContentNotification(
+  props: ContentNotificationProps
+): JSX.Element;
 
 // <Avatar>
 export const avatarDefaultProps: {
-  firstName: string,
-  lastName: string,
-  isHighlighted: boolean,
-  size: "s" | "m" | "l",
+  firstName: string;
+  lastName: string;
+  isHighlighted: boolean;
+  size: 's' | 'm' | 'l';
 };
 export type AvatarDefaultProps = typeof avatarDefaultProps;
 export type AvatarProps = {
-  firstName?: string,
-  lastName?: string,
-  gravatarHash: string,
-  isHighlighted?: boolean,
-  size?: "s" | "m" | "l",
-  children?: never
+  firstName?: string;
+  lastName?: string;
+  gravatarHash: string;
+  isHighlighted?: boolean;
+  size?: 's' | 'm' | 'l';
+  children?: never;
 } & AvatarDefaultProps;
 export function Avatar(props: AvatarProps): JSX.Element;
+
+// <Spacings.Stack>
+export const spacingsStackDefaultProps: {
+  scale: 's';
+  alignItems: 'stretch';
+};
+export type SpacingsStackDefaultProps = typeof spacingsStackDefaultProps;
+export type SpacingsStackProps = {
+  scale?: 'xs' | 's' | 'm' | 'l' | 'xl';
+  children?: React.ReactNode;
+  alignItems?:
+    | 'stretch'
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'flexStart'
+    | 'flexEnd';
+} & SpacingsStackDefaultProps;
+export function SpacingsStack(props: SpacingsStackProps): JSX.Element;
+
+// <Spacings.Inline>
+export const spacingsInlineDefaultProps: {
+  scale: 's';
+  alignItems: 'flex-start';
+  justifyContent: 'flex-start';
+};
+export type SpacingsInlineDefaultProps = typeof spacingsInlineDefaultProps;
+export type SpacingsInlineProps = {
+  scale?: 'xs' | 's' | 'm' | 'l' | 'xl';
+  children?: React.ReactNode;
+  alignItems?:
+    | 'stretch'
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'flexStart'
+    | 'flexEnd';
+  justifyContent?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
+} & SpacingsInlineDefaultProps;
+export function SpacingsInline(props: SpacingsInlineProps): JSX.Element;
+
+export type Spacings = {
+  Stack: ReturnType<typeof SpacingsStack>;
+  Inline: ReturnType<typeof SpacingsInline>;
+};
