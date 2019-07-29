3c3d712539f4d3a3bc214d71fc235bab707f11ae
Malcolm Laing <malcolm.laing@commercetools.de>
2019-07-29 02:13:55 -0700

    refactor(multiline-text-input): use hooks & expose `useCollapsible` hook (#966)
    * refactor(multiline-text-input): use hooks
    
    * chore: fixup test
    
    * chore: vrt fix
    
    * chore: fix
    
    * refactor(collapsible): use hook internally
    
    * chore: move files
    
    * chore: do not use hook if controlled
    
    * chore: fix
    
    * chore: use callback
    
    * chore: pr fixes
    
    * chore: fixup
    
    * fix: lint rule react hooks

--- src/components/collapsible/collapsible.js
+++ src/components/collapsible/collapsible.js
@@ -1,61 +1,76 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import isNil from 'lodash/isNil';
+import useToggleState from '../../hooks/use-toggle-state';
 
-class Collapsible extends React.PureComponent {
-  static displayName = 'Collapsible';
-  static propTypes = {
-    // This is only used to initialize the `isOpen` state once,
-    // when the component mounts. Therefore there should not be
-    // any `componentWillReceiveProps` to update the state from
-    // an external source.
-    isDefaultClosed: PropTypes.bool,
-    children: PropTypes.func.isRequired,
+const collapsiblePropTypes = {
+  // This is only used to initialize the `isOpen` state once,
+  // when the component mounts. Therefore there should not be
+  // any `componentWillReceiveProps` to update the state from
+  // an external source.
+  isDefaultClosed: PropTypes.bool,
+  children: PropTypes.func.isRequired,
 
-    // The component can be controlled or uncontrolled.
-    // when uncontrolled (no isClosed passed)
-    //  -> There may not be `onToggle`
-    // when controlled (isClosed passed)
-    //  -> `onToggle` is required
-    isClosed: PropTypes.bool,
-    onToggle(props, propName, componentName, ...rest) {
-      const isControlledComponent = !isNil(props.isClosed);
-      const hasOnToggle = !isNil(props.onToggle);
+  // The component can be controlled or uncontrolled.
+  // when uncontrolled (no isClosed passed)
+  //  -> There may not be `onToggle`
+  // when controlled (isClosed passed)
+  //  -> `onToggle` is required
+  isClosed: PropTypes.bool,
+  onToggle(props, propName, componentName, ...rest) {
+    const isControlledComponent = !isNil(props.isClosed);
+    const hasOnToggle = !isNil(props.onToggle);
 
-      // controlled
-      if (isControlledComponent)
-        return PropTypes.func.isRequired(
-          props,
-          propName,
-          componentName,
-          ...rest
-        );
+    // controlled
+    if (isControlledComponent)
+      return PropTypes.func.isRequired(props, propName, componentName, ...rest);
 
-      if (hasOnToggle)
-        return new Error(
-          `Invalid prop \`${propName}\` supplied to \`${componentName}\`. \`${propName}\` does not have any effect when the component is uncontrolled.`
-        );
+    if (hasOnToggle)
+      return new Error(
+        `Invalid prop \`${propName}\` supplied to \`${componentName}\`. \`${propName}\` does not have any effect when the component is uncontrolled.`
+      );
 
-      // uncontrolled component does not have `onToggle` so no validation needed.
-      return null;
-    },
-  };
+    // uncontrolled component does not have `onToggle` so no validation needed.
+    return null;
+  },
+};
 
-  static defaultProps = { isDefaultClosed: false };
-  state = { isOpen: !this.props.isDefaultClosed };
+const ControlledCollapsible = props => (
+  <>
+    {props.children({
+      isOpen: !props.isClosed,
+      toggle: props.onToggle,
+    })}
+  </>
+);
+ControlledCollapsible.displayName = 'ControlledCollapsible';
+ControlledCollapsible.propTypes = collapsiblePropTypes;
 
-  toggle = () => {
-    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
-  };
+const UncontrolledCollapsible = props => {
+  const [isOpen, toggle] = useToggleState(!props.isDefaultClosed);
+  return (
+    <>
+      {props.children({
+        isOpen,
+        toggle,
+      })}
+    </>
+  );
+};
+UncontrolledCollapsible.displayName = 'UncontrolledCollapsible';
+UncontrolledCollapsible.propTypes = collapsiblePropTypes;
 
-  render() {
-    const isControlledComponent = !isNil(this.props.isClosed);
+const Collapsible = props => {
+  const isControlledComponent = !isNil(props.isClosed);
 
-    return this.props.children({
-      isOpen: isControlledComponent ? !this.props.isClosed : this.state.isOpen,
-      toggle: isControlledComponent ? this.props.onToggle : this.toggle,
-    });
+  if (isControlledComponent) {
+    return <ControlledCollapsible {...props} />;
   }
-}
+  return <UncontrolledCollapsible {...props} />;
+};
+
+Collapsible.displayName = 'Collapsible';
+Collapsible.propTypes = collapsiblePropTypes;
+Collapsible.defaultProps = { isDefaultClosed: false };
 
 export default Collapsible;
---
+++ src/hooks/use-toggle-state/use-toggle-state.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import useToggleState from './use-toggle-state';
+import { render } from '../../test-utils';
+
+const TestComponent = props => {
+  // eslint-disable-next-line react/prop-types
+  const [isOpen, toggle] = useToggleState(props.isDefaultOpen);
+  return (
+    <div>
+      <div data-testid="openState">{isOpen ? 'open' : 'closed'}</div>
+      <button data-testid="toggle" onClick={toggle}>
+        Toggle
+      </button>
+    </div>
+  );
+};
+
+it('should be open by default', () => {
+  const { getByTestId } = render(<TestComponent />);
+  expect(getByTestId('openState')).toHaveTextContent('open');
+});
+
+it('should be possible to toggle the open state', () => {
+  const { getByTestId } = render(<TestComponent />);
+  expect(getByTestId('openState')).toHaveTextContent('open');
+  getByTestId('toggle').click();
+  expect(getByTestId('openState')).toHaveTextContent('closed');
+});
+
+it('should respect the default closed state', () => {
+  const { getByTestId } = render(<TestComponent isDefaultOpen={false} />);
+  expect(getByTestId('openState')).toHaveTextContent('closed');
+});
