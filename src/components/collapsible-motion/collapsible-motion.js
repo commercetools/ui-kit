1f544c08001ebb6ec26ea913f4f98df565147ed5
Malcolm Laing <malcolm.laing@commercetools.de>
2019-08-01 18:29:27 +0200

    refactor(collapsible-motion): use hooks

--- src/components/collapsible-motion/collapsible-motion.js
+++ src/components/collapsible-motion/collapsible-motion.js
@@ -4,6 +4,14 @@ import invariant from 'tiny-invariant';
 import { keyframes, ClassNames } from '@emotion/core';
 import Collapsible from '../collapsible';
 
+const usePrevious = value => {
+  const ref = React.useRef();
+  React.useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
 const createOpeningAnimation = height =>
   keyframes`
     0% { height: 0; overflow: hidden; }
@@ -17,80 +25,54 @@ const createClosingAnimation = height =>
     to { height: 0; overflow: hidden; }
   `;
 
-export class ToggleAnimation extends React.Component {
-  static getDerivedStateFromProps(props, state) {
-    const containerStyles = props.isOpen
-      ? { height: 'auto' }
-      : { height: 0, overflow: 'hidden' };
+const ToggleAnimation = props => {
+  const nodeRef = React.useRef();
+  const prevIsOpen = usePrevious(props.isOpen);
 
-    let animation = null;
-    if (props.isOpen !== state.isOpen) {
-      if (props.isOpen) {
-        animation = createOpeningAnimation(state.fullHeight);
-      } else {
-        animation = createClosingAnimation(state.fullHeight);
-      }
-    }
+  React.useEffect(
+    () => {
+      invariant(
+        nodeRef.current,
+        'You need to call `registerContentNode` in order to use this component'
+      );
+    },
+    // to match the componentDidMount behaviour
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [nodeRef]
+  );
 
-    return {
-      isOpen: props.isOpen,
-      containerStyles,
-      animation,
-    };
-  }
-
-  nodeRef = React.createRef();
-
-  state = {
-    isOpen: this.props.isOpen,
-    fullHeight: null,
-    animation: null,
-    containerStyles: this.props.isOpen
-      ? { height: 'auto' }
-      : { height: 0, overflow: 'hidden' },
-  };
-
-  componentDidMount() {
+  const { toggle } = props;
+  const handleToggle = React.useCallback(() => {
     invariant(
-      this.nodeRef.current,
-      'You need to call `registerContentNode` in order to use this component'
-    );
-
-    // Sets the full height to the content's height.
-    // We can't set this when initializing the state as the element in nodeRef
-    // would not have mounted yet.
-    if (this.props.isOpen) {
-      this.setState({
-        fullHeight: this.nodeRef.current.clientHeight,
-      });
-    }
-  }
-
-  handleToggle = () => {
-    invariant(
-      this.nodeRef.current,
+      nodeRef.current,
       'You need to call `registerContentNode` in order to use this component'
     );
 
     // set panel height to the height of the content,
     // so we can animate between the height and 0
-    this.setState(
-      {
-        fullHeight: this.nodeRef.current.clientHeight,
-      },
-      this.props.toggle
-    );
-  };
+    toggle();
+  }, [nodeRef, toggle]);
 
-  render() {
-    return this.props.children({
-      animation: this.state.animation,
-      containerStyles: this.state.containerStyles,
-      toggle: this.handleToggle,
-      registerContentNode: this.nodeRef,
-    });
+  const containerStyles = props.isOpen
+    ? { height: 'auto' }
+    : { height: 0, overflow: 'hidden' };
+
+  let animation = null;
+
+  // if state has changed
+  if (typeof prevIsOpen !== 'undefined' && prevIsOpen !== props.isOpen) {
+    animation = props.isOpen
+      ? createOpeningAnimation(nodeRef.current.clientHeight)
+      : createClosingAnimation(nodeRef.current.clientHeight);
   }
-}
+
+  return props.children({
+    animation,
+    containerStyles,
+    toggle: handleToggle,
+    registerContentNode: nodeRef,
+  });
+};
 
 ToggleAnimation.displayName = 'ToggleAnimation';
 ToggleAnimation.propTypes = {
