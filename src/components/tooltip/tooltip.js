b3491400f218fb4ba590d5e9ede607d74a3f781e
Malcolm Laing <malcolm.laing@commercetools.de>
2019-08-02 16:17:52 +0200

    refactor(tooltip): use hooks

--- src/components/tooltip/tooltip.js
+++ src/components/tooltip/tooltip.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { isValidElementType } from 'react-is';
 import styled from '@emotion/styled';
+import isNil from 'lodash/isNil';
 import { Manager, Reference, Popper } from 'react-popper';
 import getFieldId from '../../utils/get-field-id';
 import createSequentialId from '../../utils/create-sequential-id';
@@ -23,221 +24,231 @@ TooltipWrapper.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-class Tooltip extends React.Component {
-  static displayName = 'ToolTip';
+const Tooltip = props => {
+  const leaveTimer = React.useRef();
+  const childrenRef = React.useRef();
 
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-    horizontalConstraint: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl', 'scale'])
-      .isRequired,
-    closeAfter: PropTypes.number.isRequired,
-    styles: PropTypes.shape({
-      body: PropTypes.object.isRequired,
-    }).isRequired,
-    off: PropTypes.bool.isRequired,
-    isOpen: PropTypes.bool,
-    onClose: PropTypes.func,
-    onOpen: PropTypes.func,
-    placement: PropTypes.oneOf([
-      'top',
-      'top-start',
-      'top-end',
-      'right',
-      'right-start',
-      'right-end',
-      'bottom',
-      'bottom-start',
-      'bottom-end',
-      'left',
-      'left-start',
-      'left-end',
-    ]),
-    title: PropTypes.string.isRequired,
-    components: PropTypes.shape({
-      BodyComponent: (props, propName) => {
-        if (props[propName] && !isValidElementType(props[propName])) {
-          return new Error(
-            `Invalid prop 'components.BodyComponent' supplied to 'Tooltip': the prop is not a valid React component`
-          );
-        }
-        return null;
-      },
-      TooltipWrapperComponent: (props, propName) => {
-        if (props[propName] && !isValidElementType(props[propName])) {
-          return new Error(
-            `Invalid prop 'components.TooltipWrapperComponent' supplied to 'Tooltip': the prop is not a valid React component`
-          );
-        }
-        return null;
-      },
-      WrapperComponent: (props, propName) => {
-        if (props[propName] && !isValidElementType(props[propName])) {
-          return new Error(
-            `Invalid prop 'components.WrapperComponent' supplied to 'Tooltip': the prop is not a valid React component`
-          );
-        }
-        return null;
-      },
-    }),
-  };
-
-  static defaultProps = {
-    components: {},
-    styles: { body: {} },
-    closeAfter: 0,
-    horizontalConstraint: 'scale',
-    off: false,
-    placement: 'top',
-    type: 'info',
-  };
-
-  static getDerivedStateFromProps = (props, state) => ({
-    id: getFieldId(props, state, sequentialId),
-  });
-
-  state = {
-    open: this.isControlled ? this.props.isOpen : false,
-  };
-
-  componentWillUnmount() {
-    clearTimeout(this.leaveTimer);
-  }
-
-  handleEnter = event => {
-    const childrenProps = this.props.children.props;
-    // Remove the title ahead of time.
-    // We don't want to wait for the next render commit.
-    // We would risk displaying two tooltips at the same time (native + this one).
-    if (this.childrenRef && typeof this.childrenRef === 'function') {
-      this.childrenRef.setAttribute('title', '');
-    }
-
-    if (event.type === 'mouseover' && childrenProps.onMouseOver) {
-      childrenProps.onMouseOver(event);
-    }
-
-    if (event.type === 'focus' && childrenProps.onFocus) {
-      childrenProps.onFocus(event);
-    }
-
-    if (!this.state.open && !this.isControlled) {
-      this.setState({
-        open: true,
-      });
-    }
-
-    if (this.props.onOpen) {
-      this.props.onOpen(event);
-    }
-
-    event.preventDefault();
-    event.stopPropagation();
-  };
-
-  handleLeave = event => {
-    const childrenProps = this.props.children.props;
-    clearTimeout(this.leaveTimer);
-
-    if (event.type === 'mouseleave' && childrenProps.onMouseLeave) {
-      childrenProps.onMouseLeave(event);
-    }
-
-    if (event.type === 'blur' && childrenProps.onBlur) {
-      childrenProps.onBlur(event);
-    }
-
-    if (this.props.closeAfter) {
-      this.leaveTimer = setTimeout(() => {
-        this.handleClose();
-      }, this.props.closeAfter);
-    } else {
-      this.handleClose();
-    }
-  };
-
-  handleClose = event => {
-    if (!this.isControlled) {
-      this.setState({
-        open: false,
-      });
-    }
-    if (this.props.onClose) {
-      this.props.onClose(event);
-    }
-  };
-
-  setChildrenRef = ref => {
-    this.childrenRef = ref;
-  };
-
-  isControlled = this.props.isOpen != null;
-
-  render() {
-    const open = this.isControlled ? this.props.isOpen : this.state.open;
-
-    const childrenProps = {
-      'aria-describedby': open ? this.state.id : null,
-      // for seo and accessibility, we add the tooltip's title
-      // as a native title when the title is hidden
-      title:
-        !open && typeof this.props.title === 'string' ? this.props.title : null,
-      ...this.props.children.props,
-      // don't pass event listeners to children
-      onFocus: null,
-      onMouseOver: null,
-      onMouseLeave: null,
-      onBlur: null,
+  React.useEffect(() => {
+    return () => {
+      console.log('here leavetimer', leaveTimer);
+      if (leaveTimer.current) {
+        clearTimeout(leaveTimer.current);
+      }
     };
+  }, []);
 
-    const eventListeners = !this.props.off
-      ? {
-          onMouseOver: this.handleEnter,
-          onMouseLeave: this.handleLeave,
-          onFocus: this.handleEnter,
-          onBlur: this.handleLeave,
-        }
-      : {};
+  const [isOpen, setIsOpen] = React.useState(false);
+  const closeTooltip = React.useCallback(() => {
+    setIsOpen(false);
+  }, []);
+  const openTooltip = React.useCallback(() => {
+    setIsOpen(true);
+  }, []);
 
-    const WrapperComponent = this.props.components.WrapperComponent || Wrapper;
-    const BodyComponent = this.props.components.BodyComponent || Body;
-    const TooltipWrapperComponent =
-      this.props.components.TooltipWrapperComponent || TooltipWrapper;
-    return (
-      <Manager>
-        <Reference innerRef={this.setChildrenRef}>
-          {({ ref }) => (
-            <WrapperComponent {...eventListeners}>
-              <RootRef rootRef={ref}>
-                {React.cloneElement(this.props.children, { ...childrenProps })}
-              </RootRef>
-            </WrapperComponent>
-          )}
-        </Reference>
-        {open && (
-          <TooltipWrapperComponent>
-            <Popper placement={this.props.placement} positionFixed={true}>
-              {({ ref, style, placement }) => (
-                <div
-                  ref={ref}
-                  css={{
-                    ...style,
-                    ...getBodyStyles({
-                      constraint: this.props.horizontalConstraint,
-                      placement,
-                      customStyles: this.props.styles.body,
-                    }),
-                  }}
-                  data-placement={placement}
-                >
-                  <BodyComponent>{this.props.title}</BodyComponent>
-                </div>
-              )}
-            </Popper>
-          </TooltipWrapperComponent>
+  const isControlled = !isNil(props.isOpen);
+  const open = isControlled ? props.isOpen : isOpen;
+  const id = getFieldId(props, {}, sequentialId);
+
+  const { onClose } = props;
+  const handleClose = React.useCallback(
+    event => {
+      if (!isControlled) {
+        closeTooltip();
+      }
+      if (onClose) {
+        onClose(event);
+      }
+    },
+    [isControlled, closeTooltip, onClose]
+  );
+
+  const { onFocus, onMouseOver } = props.children.props;
+  const { onOpen } = props;
+  const handleEnter = React.useCallback(
+    event => {
+      // Remove the title ahead of time.
+      // We don't want to wait for the next render commit.
+      // We would risk displaying two tooltips at the same time (native + this one).
+      if (childrenRef && typeof childrenRef === 'function') {
+        childrenRef.setAttribute('title', '');
+      }
+
+      if (event.type === 'mouseover' && onMouseOver) {
+        onMouseOver(event);
+      }
+
+      if (event.type === 'focus' && onFocus) {
+        onFocus(event);
+      }
+
+      if (!isOpen && !isControlled) {
+        openTooltip();
+      }
+
+      if (onOpen) {
+        onOpen(event);
+      }
+
+      event.preventDefault();
+      event.stopPropagation();
+    },
+    [onFocus, onOpen, onMouseOver, isControlled, isOpen, openTooltip]
+  );
+
+  const { onBlur, onMouseLeave } = props.children.props;
+  const { closeAfter } = props;
+
+  const handleLeave = React.useCallback(
+    event => {
+      clearTimeout(leaveTimer);
+
+      if (event.type === 'mouseleave' && onMouseLeave) {
+        onMouseLeave(event);
+      }
+
+      if (event.type === 'blur' && onBlur) {
+        onBlur(event);
+      }
+
+      if (closeAfter) {
+        leaveTimer.current = setTimeout(() => {
+          handleClose();
+        }, closeAfter);
+      } else {
+        handleClose();
+      }
+    },
+    [closeAfter, onBlur, onMouseLeave, handleClose]
+  );
+
+  const childrenProps = {
+    'aria-describedby': open ? id : null,
+    // for seo and accessibility, we add the tooltip's title
+    // as a native title when the title is hidden
+    title: !open && typeof props.title === 'string' ? props.title : null,
+    ...props.children.props,
+    // don't pass event listeners to children
+    onFocus: null,
+    onMouseOver: null,
+    onMouseLeave: null,
+    onBlur: null,
+  };
+
+  const eventListeners = !props.off
+    ? {
+        onMouseOver: handleEnter,
+        onMouseLeave: handleLeave,
+        onFocus: handleEnter,
+        onBlur: handleLeave,
+      }
+    : {};
+
+  const WrapperComponent = props.components.WrapperComponent || Wrapper;
+  const BodyComponent = props.components.BodyComponent || Body;
+  const TooltipWrapperComponent =
+    props.components.TooltipWrapperComponent || TooltipWrapper;
+  return (
+    <Manager>
+      <Reference innerRef={childrenRef}>
+        {({ ref }) => (
+          <WrapperComponent {...eventListeners}>
+            <RootRef rootRef={ref}>
+              {React.cloneElement(props.children, { ...childrenProps })}
+            </RootRef>
+          </WrapperComponent>
         )}
-      </Manager>
-    );
-  }
-}
+      </Reference>
+      {open && (
+        <TooltipWrapperComponent>
+          <Popper placement={props.placement} positionFixed={true}>
+            {({ ref, style, placement }) => (
+              <div
+                ref={ref}
+                css={{
+                  ...style,
+                  ...getBodyStyles({
+                    constraint: props.horizontalConstraint,
+                    placement,
+                    customStyles: props.styles.body,
+                  }),
+                }}
+                data-placement={placement}
+              >
+                <BodyComponent>{props.title}</BodyComponent>
+              </div>
+            )}
+          </Popper>
+        </TooltipWrapperComponent>
+      )}
+    </Manager>
+  );
+};
+
+Tooltip.displayName = 'ToolTip';
+
+Tooltip.propTypes = {
+  children: PropTypes.node.isRequired,
+  horizontalConstraint: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl', 'scale'])
+    .isRequired,
+  closeAfter: PropTypes.number.isRequired,
+  styles: PropTypes.shape({
+    body: PropTypes.object.isRequired,
+  }).isRequired,
+  off: PropTypes.bool.isRequired,
+  isOpen: PropTypes.bool,
+  onClose: PropTypes.func,
+  onOpen: PropTypes.func,
+  placement: PropTypes.oneOf([
+    'top',
+    'top-start',
+    'top-end',
+    'right',
+    'right-start',
+    'right-end',
+    'bottom',
+    'bottom-start',
+    'bottom-end',
+    'left',
+    'left-start',
+    'left-end',
+  ]),
+  title: PropTypes.string.isRequired,
+  components: PropTypes.shape({
+    BodyComponent: (props, propName) => {
+      if (props[propName] && !isValidElementType(props[propName])) {
+        return new Error(
+          `Invalid prop 'components.BodyComponent' supplied to 'Tooltip': the prop is not a valid React component`
+        );
+      }
+      return null;
+    },
+    TooltipWrapperComponent: (props, propName) => {
+      if (props[propName] && !isValidElementType(props[propName])) {
+        return new Error(
+          `Invalid prop 'components.TooltipWrapperComponent' supplied to 'Tooltip': the prop is not a valid React component`
+        );
+      }
+      return null;
+    },
+    WrapperComponent: (props, propName) => {
+      if (props[propName] && !isValidElementType(props[propName])) {
+        return new Error(
+          `Invalid prop 'components.WrapperComponent' supplied to 'Tooltip': the prop is not a valid React component`
+        );
+      }
+      return null;
+    },
+  }),
+};
+
+Tooltip.defaultProps = {
+  components: {},
+  styles: { body: {} },
+  closeAfter: 0,
+  horizontalConstraint: 'scale',
+  off: false,
+  placement: 'top',
+  type: 'info',
+};
 
 export default Tooltip;
