77759c23ceed3487ddc8ea37d26ad623cdb1c796
Malcolm Laing <malcolm.laing@commercetools.de>
2019-08-08 14:44:27 +0200

    chore: try using hooks testing lib

--- src/hooks/use-toggle-state/use-toggle-state.spec.js
+++ src/hooks/use-toggle-state/use-toggle-state.spec.js
@@ -1,56 +1,51 @@
-import React from 'react';
-import { render } from '../../test-utils';
+import { renderHook, act } from '@testing-library/react-hooks';
 import useToggleState from './use-toggle-state';
 
-const TestComponent = props => {
-  // eslint-disable-next-line react/prop-types
-  const [isOpen, toggle] = useToggleState(props.isDefaultOpen);
-  const setOff = React.useCallback(() => {
-    toggle(false);
-  }, [toggle]);
-
-  const setOn = React.useCallback(() => {
-    toggle(true);
-  }, [toggle]);
-
-  return (
-    <div>
-      <div data-testid="openState">{isOpen ? 'open' : 'closed'}</div>
-      <button data-testid="toggle" onClick={toggle}>
-        Toggle
-      </button>
-      <button data-testid="setOff" onClick={setOff}>
-        setOff
-      </button>
-      <button data-testid="setOn" onClick={setOn}>
-        setOn
-      </button>
-    </div>
-  );
-};
-
-it('should be open by default', () => {
-  const { getByTestId } = render(<TestComponent />);
-  expect(getByTestId('openState')).toHaveTextContent('open');
+describe('useToggleState', () => {
+  describe('when set without default state', () => {
+    it('should be open by default', () => {
+      const { result } = renderHook(() => useToggleState());
+      expect(result.current[0]).toBe(true);
+    });
+    it('should be possible to toggle the open state', () => {
+      const { result } = renderHook(() => useToggleState());
+      const [, toggle] = result.current;
+      act(() => {
+        toggle();
+      });
+      expect(result.current[0]).toBe(false);
+    });
+  });
+  describe('when set to default false', () => {
+    it('should be closed by default', () => {
+      const { result } = renderHook(() => useToggleState(false));
+      const [isToggled] = result.current;
+      expect(isToggled).toBe(false);
+    });
+  });
 });
-
-it('should be possible to toggle the open state', () => {
-  const { getByTestId } = render(<TestComponent />);
-  expect(getByTestId('openState')).toHaveTextContent('open');
-  getByTestId('toggle').click();
-  expect(getByTestId('openState')).toHaveTextContent('closed');
-});
-
-it('should be possible to set the state on and off', () => {
-  const { getByTestId } = render(<TestComponent />);
-  expect(getByTestId('openState')).toHaveTextContent('open');
-  getByTestId('setOff').click();
-  expect(getByTestId('openState')).toHaveTextContent('closed');
-  getByTestId('setOn').click();
-  expect(getByTestId('openState')).toHaveTextContent('open');
-});
-
-it('should respect the default closed state', () => {
-  const { getByTestId } = render(<TestComponent isDefaultOpen={false} />);
-  expect(getByTestId('openState')).toHaveTextContent('closed');
-});
+// it('should be open by default', () => {
+//   const { getByTestId } = render(<TestComponent />);
+//   expect(getByTestId('openState')).toHaveTextContent('open');
+// });
+//
+// it('should be possible to toggle the open state', () => {
+//   const { getByTestId } = render(<TestComponent />);
+//   expect(getByTestId('openState')).toHaveTextContent('open');
+//   getByTestId('toggle').click();
+//   expect(getByTestId('openState')).toHaveTextContent('closed');
+// });
+//
+// it('should be possible to set the state on and off', () => {
+//   const { getByTestId } = render(<TestComponent />);
+//   expect(getByTestId('openState')).toHaveTextContent('open');
+//   getByTestId('setOff').click();
+//   expect(getByTestId('openState')).toHaveTextContent('closed');
+//   getByTestId('setOn').click();
+//   expect(getByTestId('openState')).toHaveTextContent('open');
+// });
+//
+// it('should respect the default closed state', () => {
+//   const { getByTestId } = render(<TestComponent isDefaultOpen={false} />);
+//   expect(getByTestId('openState')).toHaveTextContent('closed');
+// });
