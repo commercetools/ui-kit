93bca43c97dd41cbf84f51a6df5b8c6a232d63dc
Malcolm Laing <malcolm.laing@commercetools.de>
2019-09-02 10:47:19 +0200

    chore: update deps (#1041)
    * chore: update deps
    
    * chore: fix usage of dom-helpes

--- src/components/table/base-table/base-table.js
+++ src/components/table/base-table/base-table.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CellMeasurer, CellMeasurerCache, MultiGrid } from 'react-virtualized';
 import sortBy from 'lodash/sortBy';
-import getScrollbarSize from 'dom-helpers/util/scrollbarSize';
+import getScrollbarSize from 'dom-helpers/scrollbarSize';
 import { Global, ClassNames, css } from '@emotion/core';
 import vars from '../../../../materials/custom-properties';
 import Spacings from '../../spacings';
