b9c9f9f2f22e472964296922a85334b3ac8ffc08
Malcolm Laing <malcolm.laing@commercetools.de>
2019-09-18 10:36:10 +0200

    chore: add first puppeteer test for rich text

--- src/components/inputs/rich-text-input/rich-text-input.visualspec.js
+++ src/components/inputs/rich-text-input/rich-text-input.visualspec.js
@@ -14,11 +14,38 @@ describe('RichTextInput', () => {
     await page.goto(`${HOST}/rich-text-input/interactive`);
     const doc = await getDocument(page);
     const input = await getByLabelText(doc, 'Rich text');
-    console.log(input);
-    await input.focus();
-    // interact with puppeteer like usual
+
+    // make the text bold
+    const boldButton = await getByLabelText(doc, 'bold');
+    await boldButton.click();
+
     await input.type('Hello world');
-    // waiting works too!
     await wait(() => getByText(doc, 'Hello world'));
+
+    // check that there is now a strong tag in the document.
+    let numOfStrongTags = await page.evaluate(() => {
+      return document.querySelectorAll('strong').length;
+    });
+
+    expect(numOfStrongTags).toEqual(1);
+
+    // select the text
+    await page.evaluate(() => {
+      const richTextInput = document.querySelector('#rich-text');
+      const range = document.createRange();
+      range.selectNodeContents(richTextInput);
+      const sel = window.getSelection();
+      sel.removeAllRanges();
+      sel.addRange(range);
+    });
+
+    // click the bold button again
+    await boldButton.click();
+
+    // check there are no strong tags in the document.
+    numOfStrongTags = await page.evaluate(() => {
+      return document.querySelectorAll('strong').length;
+    });
+    expect(numOfStrongTags).toEqual(0);
   });
 });
