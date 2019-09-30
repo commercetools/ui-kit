383d4c50748702601b8a351eeabca7d9c931dd53
Malcolm Laing <malcolm.laing@commercetools.de>
2019-09-30 10:39:50 +0200

    chore: enable percy for renovate prs (#1080)

--- scripts/run-percy.sh
+++ scripts/run-percy.sh
@@ -3,17 +3,7 @@
 # skip percy builds when this is not a pull request or when it's not master
 if [[ -n $CIRCLE_PULL_REQUEST || ${CIRCLE_BRANCH} == 'master' ]]
   then
-  # skip percy builds when pull request is opened by a renovate
-  # if renovate opens an average of 6 PRs per week, that means, 24 PRs per month.
-  # 24 * ~50 snapshots = 1200, or almost 25% of our current percy snapshot allowances.
-  # if in the future, we drop more 💸 on percy, we can remove this.
-  regexp='^renovate\/'
-  if ! [[ ${CIRCLE_BRANCH} =~ ${regexp} ]]
-    then
-      yarn visual-testing-app:build && yarn percy --reporters jest-silent-reporter
-    else
-      echo 'Skipping percy - PR opened by `renovate`'
-  fi
+    yarn visual-testing-app:build && yarn percy --reporters jest-silent-reporter
 else
   echo 'Skipping percy build'
 fi
