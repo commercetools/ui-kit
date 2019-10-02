3aaa509a91048e774f94d0f2e18e4da733742c4f
Malcolm Laing <malcolm.laing@commercetools.de>
2019-10-02 15:39:22 +0200

    chore: remove mousedown from accessiblebutton

--- src/components/buttons/accessible-button/accessible-button.js
+++ src/components/buttons/accessible-button/accessible-button.js
@@ -10,26 +10,24 @@ const AccessibleButton = React.forwardRef((props, ref) => (
     type={props.type}
     aria-label={props.label}
     onClick={props.onClick}
-    css={[
-      css`
-        border: none;
-        background: none;
-        display: inline-block;
-        outline: 0;
-        padding: 0;
-        margin: 0;
-        white-space: nowrap;
-        cursor: pointer;
-        color: inherit;
-        font: inherit;
-        font-size: ${vars.fontSizeDefault};
-        font-family: inherit;
-      `,
-      props.isDisabled &&
-        css`
-          pointer-events: none;
-        `,
-    ]}
+    css={css`
+      border: none;
+      background: none;
+      display: inline-block;
+      outline: 0;
+      padding: 0;
+      margin: 0;
+      white-space: nowrap;
+      cursor: pointer;
+      color: inherit;
+      font: inherit;
+      font-size: ${vars.fontSizeDefault};
+      font-family: inherit;
+
+      &:disabled {
+        pointer-events: none;
+      }
+    `}
     // Allow to override the styles by passing a `className` prop.
     // Custom styles can also be passed using the `css` prop from emotion.
     // https://emotion.sh/docs/css-prop#style-precedence
@@ -38,7 +36,6 @@ const AccessibleButton = React.forwardRef((props, ref) => (
     aria-disabled={props.isDisabled}
     {...(props.isToggleButton ? { 'aria-pressed': props.isToggled } : {})}
     {...props.buttonAttributes}
-    onMouseDown={props.onMouseDown}
   >
     {props.children}
   </button>
@@ -60,7 +57,6 @@ AccessibleButton.propTypes = {
   isToggleButton: PropTypes.bool,
   isDisabled: PropTypes.bool,
   onClick: PropTypes.func,
-  onMouseDown: PropTypes.func,
   className: PropTypes.string,
   // allows setting custom attributes on the underlying button html element
   buttonAttributes: PropTypes.object,
