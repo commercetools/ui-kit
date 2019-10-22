648e3a89d5a75461b749f4a25cd12b6f2b45e64d
Malcolm Laing <malcolm.laing@commercetools.de>
2019-10-22 17:01:58 +0200

    chore: disabled fix

--- src/components/inputs/localized-rich-text-input/rich-text-input.js
+++ src/components/inputs/localized-rich-text-input/rich-text-input.js
@@ -141,7 +141,7 @@ class RichTextInput extends React.PureComponent {
         id={this.props.id}
         name={this.props.name}
         disabled={this.props.isDisabled}
-        readOnly={this.props.isReadOnly}
+        readOnly={this.props.isReadOnly || this.props.isDisabled}
         value={this.internalSlateValue}
         onFocus={this.onFocus}
         onBlur={this.onBlur}
