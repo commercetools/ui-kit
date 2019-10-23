df1d2ee52ce3a51514edae6b0f35e146c198d533
Malcolm Laing <malcolm.laing@commercetools.de>
2019-10-23 10:08:52 +0200

    chore: pr nits

--- src/components/internals/rich-text-utils/is-empty/is-empty.spec.js
+++ src/components/internals/rich-text-utils/is-empty/is-empty.spec.js
@@ -2,17 +2,17 @@ import isEmpty from './is-empty';
 
 describe('isEmpty', () => {
   describe('when totally empty', () => {
-    it('should return true', () => {
+    it('should indicate that the value is empty', () => {
       expect(isEmpty('')).toBeTruthy();
     });
   });
   describe('when empty but has empty html tags', () => {
-    it('should return true', () => {
+    it('should indicate that the value is empty', () => {
       expect(isEmpty('<h1></h1><ul><li></li></ul>')).toBeTruthy();
     });
   });
   describe('when not empty', () => {
-    it('should return false', () => {
+    it('should indicate that the value is not empty', () => {
       expect(isEmpty('<H1>Okay</h1>')).toBeFalsy();
     });
   });
