eda7b22d700ff2542c7f96a4d03a87bc4f4d973a
Malcolm Laing <malcolm.laing@commercetools.de>
2019-10-23 10:12:52 +0200

    chore: fixup

--- src/components/internals/rich-text-utils/localized/localized.spec.js
+++ src/components/internals/rich-text-utils/localized/localized.spec.js
@@ -40,22 +40,22 @@ describe('isEmpty', () => {
     });
   });
   describe('when calling with undefined values', () => {
-    it('should return true', () => {
+    it('should indicate that the value is empty', () => {
       expect(isEmpty({ en: null, de: null })).toBeTruthy();
     });
   });
   describe('when calling with one undefined value and one empty value', () => {
-    it('should return true', () => {
+    it('should indicate that the value is empty', () => {
       expect(isEmpty({ en: null, de: emptyValue })).toBeTruthy();
     });
   });
   describe('when calling with two empty values', () => {
-    it('should return true', () => {
+    it('should indicate that the value is empty', () => {
       expect(isEmpty({ en: emptyValue, de: emptyValue })).toBeTruthy();
     });
   });
   describe('when calling with one undefined value and one truthy value', () => {
-    it('should return true', () => {
+    it('should indicate that the value is not empty', () => {
       expect(isEmpty({ en: emptyValue, de: '<p>Okay</p>' })).toBeFalsy();
     });
   });
