a695e6efd52c8744c229894d2a090e158c255476
Malcolm Laing <malcolm.laing@commercetools.de>
2019-10-29 13:02:38 +0100

    refactor(rich-text-input): combine dropdowns (#1143)
    * refactor(rich-text-input): combine dropdowns
    
    * chore: proptype fix
    
    * chore: add display name
    
    * chore: change proptypes
    
    * chore: tobi comments

--- src/components/internals/rich-text-body/dropdown.js
+++ src/components/internals/rich-text-body/dropdown.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import Downshift from 'downshift';
-import styled from '@emotion/styled';
 import { css } from '@emotion/core';
-import { useIntl } from 'react-intl';
+import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
-import Spacings from '../../spacings';
-import { CaretDownIcon } from '../../icons';
 import Tooltip from '../../tooltip';
 import Button from './rich-text-body-button';
 import {
@@ -13,50 +10,19 @@ import {
   DropdownContainer,
   DropdownItem as StyledDropdownItem,
 } from './dropdown.styles';
-import { BLOCK_TAGS } from '../rich-text-utils/tags';
-import messages from './messages';
 
-const DropdownLabel = props => {
-  return (
-    <Spacings.Inline scale="xs" alignItems="center" justifyContent="center">
-      <span>{props.children}</span>
-      <CaretDownIcon size="small" />
-    </Spacings.Inline>
-  );
-};
+const getIsSelected = (props, item) =>
+  !props.isMulti
+    ? item.value === props.value
+    : props.value.find(selectedItem => selectedItem === item.value);
 
-DropdownLabel.displayName = 'DropdownLabel';
-DropdownLabel.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-const Item = styled.div`
-  margin: 0;
-  text-align: left;
-`;
-
-const DropdownItem = props => {
-  const { children } = props;
-  const as =
-    Object.keys(BLOCK_TAGS).find(key => BLOCK_TAGS[key] === props.value) ||
-    'div';
-
-  return (
-    <StyledDropdownItem {...props}>
-      <Item as={as}>{children}</Item>
-    </StyledDropdownItem>
-  );
-};
-
-DropdownItem.propTypes = {
-  children: PropTypes.node.isRequired,
-  value: PropTypes.string.isRequired,
-};
-
-DropdownItem.displayName = 'DropdownItem';
+const Label = styled.div;
 
 const Dropdown = props => {
-  const intl = useIntl();
+  const DropdownItem = props.components.Item;
+  const DropdownLabel = props.components.Label;
+  const isIndeterminate =
+    props.isMulti && (props.value && props.value.length > 0);
 
   return (
     <Downshift
@@ -64,13 +30,13 @@ const Dropdown = props => {
       selectedItem={props.value}
       itemToString={headings => (headings ? headings.label : '')}
     >
-      {({ isOpen, getToggleButtonProps, getItemProps, selectedItem }) => {
+      {({ isOpen, getToggleButtonProps, getItemProps }) => {
         const toggleButtonProps = getToggleButtonProps();
 
         return (
           <div>
             <Tooltip
-              title={intl.formatMessage(messages.styleDropdownLabel)}
+              title={props.label}
               placement="bottom"
               off={isOpen}
               style={{ body: { zIndex: 9999 } }}
@@ -80,7 +46,8 @@ const Dropdown = props => {
                 label={props.label}
                 css={getButtonStyles({
                   isOpen,
-                  isStyleButton: true,
+                  isIndeterminate,
+                  isStyleButton: !props.isMulti,
                   isDisabled: props.isDisabled,
                   isReadOnly: props.isReadOnly,
                 })}
@@ -101,13 +68,14 @@ const Dropdown = props => {
                       item,
                     });
                     const dropdownItemProps = itemProps;
+                    const isSelected = getIsSelected(props, item);
 
                     return (
                       <DropdownItem
                         {...dropdownItemProps}
                         key={index}
                         value={item.value}
-                        isSelected={item.value === selectedItem}
+                        isSelected={isSelected}
                       >
                         {item.label}
                       </DropdownItem>
@@ -125,9 +93,21 @@ const Dropdown = props => {
 
 Dropdown.displayName = 'Dropdown';
 
+Dropdown.defaultProps = {
+  components: {
+    Item: StyledDropdownItem,
+    Label,
+  },
+  isMulti: false,
+};
+
 Dropdown.propTypes = {
   label: PropTypes.string,
-  value: PropTypes.string,
+  isMulti: PropTypes.bool,
+  value: (props, ...rest) =>
+    props.isMulti
+      ? PropTypes.arrayOf(PropTypes.string).isRequired(props, ...rest)
+      : PropTypes.string(props, ...rest),
   isDisabled: PropTypes.bool,
   isReadOnly: PropTypes.bool,
   options: PropTypes.arrayOf(
@@ -136,6 +116,10 @@ Dropdown.propTypes = {
       value: PropTypes.string,
     })
   ).isRequired,
+  components: PropTypes.shape({
+    Item: PropTypes.elementType,
+    Label: PropTypes.elementType,
+  }),
   onChange: PropTypes.func.isRequired,
 };
 
--- src/components/internals/rich-text-body/rich-text-body.js
+++ src/components/internals/rich-text-body/rich-text-body.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 import requiredIf from 'react-required-if';
 import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 import Tooltip from '../../tooltip';
+import { CaretDownIcon } from '../../icons';
 import {
   Toolbar,
   ToolbarMainControls,
@@ -20,14 +22,93 @@ import {
   UnderlineIcon,
   RedoIcon,
   UndoIcon,
+  SubscriptIcon,
+  StrikethroughIcon,
+  SuperscriptIcon,
+  MoreStylesIcon,
 } from './icons';
+import Spacings from '../../spacings';
 import Button from './rich-text-body-button';
 import Divider from './divider';
-import MultiDropdown from './multi-dropdown';
 import Dropdown from './dropdown';
+import { DropdownItem } from './dropdown.styles';
 import { MARK_TAGS, BLOCK_TAGS } from '../rich-text-utils/tags';
 import hasBlock from '../rich-text-utils/has-block';
 import messages from './messages';
+
+const MoreStylesDropdownLabel = () => <MoreStylesIcon size="medium" />;
+MoreStylesDropdownLabel.displayName = 'MoreStylesDropdownLabel';
+
+const MoreStylesDropdownItem = props => {
+  let Icon;
+  switch (props.value) {
+    case 'subscript':
+      Icon = SubscriptIcon;
+      break;
+    case 'strikethrough':
+      Icon = StrikethroughIcon;
+      break;
+    default:
+      Icon = SuperscriptIcon;
+  }
+
+  return (
+    <DropdownItem {...props}>
+      <Spacings.Inline
+        scale="xs"
+        alignItems="center"
+        justifyContent="flex-start"
+      >
+        <Icon size="medium" />
+        <div>{props.children}</div>
+      </Spacings.Inline>
+    </DropdownItem>
+  );
+};
+
+MoreStylesDropdownItem.displayName = 'MoreStylesDropdownItem';
+MoreStylesDropdownItem.propTypes = {
+  value: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+const DropdownLabel = props => {
+  return (
+    <Spacings.Inline scale="xs" alignItems="center" justifyContent="center">
+      <span>{props.children}</span>
+      <CaretDownIcon size="small" />
+    </Spacings.Inline>
+  );
+};
+
+DropdownLabel.displayName = 'DropdownLabel';
+DropdownLabel.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const Item = styled.div`
+  margin: 0;
+  text-align: left;
+`;
+
+const StylesDropdownItem = props => {
+  const as =
+    Object.keys(BLOCK_TAGS).find(key => BLOCK_TAGS[key] === props.value) ||
+    'div';
+
+  return (
+    <DropdownItem {...props}>
+      <Item as={as}>{props.children}</Item>
+    </DropdownItem>
+  );
+};
+
+StylesDropdownItem.propTypes = {
+  children: PropTypes.node.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+StylesDropdownItem.displayName = 'StylesDropdownItem';
 
 const DEFAULT_NODE = BLOCK_TAGS.p;
 
@@ -192,6 +273,10 @@ const RichTextEditorBody = React.forwardRef((props, ref) => {
             value={activeBlock}
             onChange={onClickBlock}
             options={styleDropdownOptions}
+            components={{
+              Item: StylesDropdownItem,
+              Label: DropdownLabel,
+            }}
             isDisabled={props.isDisabled}
             isReadOnly={props.isReadOnly}
           />
@@ -249,13 +334,18 @@ const RichTextEditorBody = React.forwardRef((props, ref) => {
               <UnderlineIcon size="medium" />
             </Button>
           </Tooltip>
-          <MultiDropdown
+          <Dropdown
+            isMulti={true}
             label={intl.formatMessage(messages.moreStylesDropdownLabel)}
-            dropdownOptions={dropdownOptions}
-            selectedItems={activeMoreStyleMarks}
-            onSelect={onChangeMoreStyles}
+            options={dropdownOptions}
+            value={activeMoreStyleMarks}
+            onChange={onChangeMoreStyles}
             isDisabled={props.isDisabled}
             isReadOnly={props.isReadOnly}
+            components={{
+              Item: MoreStylesDropdownItem,
+              Label: MoreStylesDropdownLabel,
+            }}
           />
           <Divider />
           <Tooltip
