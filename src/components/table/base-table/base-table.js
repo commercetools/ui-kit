f317b6250660131ada52b07a323633ddbbef6b96
Malcolm Laing <malcolm.laing@commercetools.de>
2019-11-13 11:27:50 +0100

    refactor: try mono repo

--- src/components/table/base-table/base-table.js
+++ src/components/table/base-table/base-table.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { CellMeasurer, CellMeasurerCache, MultiGrid } from 'react-virtualized';
-import { sortBy } from 'lodash-es';
+import sortBy from 'lodash/sortBy';
 import getScrollbarSize from 'dom-helpers/util/scrollbarSize';
 import { Global, ClassNames, css } from '@emotion/core';
 import { customProperties as vars } from '@commercetools-uikit/design-system';
