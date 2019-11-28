0a1f4709a47e3bd1868a44b56d1ee38fbfe5ff06
Malcolm Laing <malcolm.laing@commercetools.de>
2019-11-28 14:59:42 +0100

    chore: try something

--- scripts/run-percy.sh
+++ scripts/run-percy.sh
@@ -3,7 +3,7 @@
 # skip percy builds when this is not a pull request or when it's not master
 if [[ -n $CIRCLE_PULL_REQUEST || ${CIRCLE_BRANCH} == 'master' ]]
   then
-    yarn percy --reporters jest-silent-reporter
+    yarn visual-testing-app:build && yarn percy --reporters jest-silent-reporter
 else
   echo 'Skipping percy build'
 fi
