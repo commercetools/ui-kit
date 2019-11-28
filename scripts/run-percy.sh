83f83ef8c80ad5f64a3f12f859a983cf630ee716
Malcolm Laing <malcolm.laing@commercetools.de>
2019-11-28 14:18:48 +0100

    chore: bundlesize fix

--- scripts/run-percy.sh
+++ scripts/run-percy.sh
@@ -3,8 +3,7 @@
 # skip percy builds when this is not a pull request or when it's not master
 if [[ -n $CIRCLE_PULL_REQUEST || ${CIRCLE_BRANCH} == 'master' ]]
   then
-    echo 'Skipping'
-    yarn visual-testing-app:build && yarn percy --reporters jest-silent-reporter
+    yarn percy --reporters jest-silent-reporter
 else
   echo 'Skipping percy build'
 fi
