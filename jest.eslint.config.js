8b7928a160e06980c441d3f187bbaa1a455929d4
Nicola Molinari <nicola.molinari@commercetools.de>
2019-12-05 14:32:58 +0100

    chore: move to zeit now

--- jest.eslint.config.js
+++ jest.eslint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   runner: 'jest-runner-eslint',
   displayName: 'eslint',
   testMatch: ['<rootDir>/**/*.js'],
-  testPathIgnorePatterns: ['/node_modules/', '.public/', 'dist/'],
+  testPathIgnorePatterns: ['/node_modules/', '.public/', 'public/', 'dist/'],
   watchPlugins: ['jest-watch-typeahead/filename'],
 };
