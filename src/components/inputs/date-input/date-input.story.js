ffa5674a3424b424d899aa95c466cb7282f35847
jonnybel <jonnybel91@gmail.com>
2019-12-10 18:13:23 +0100

    chore: refactor dateInput story

--- src/components/inputs/date-input/date-input.story.js
+++ src/components/inputs/date-input/date-input.story.js
@@ -3,62 +3,12 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, select, boolean, text } from '@storybook/addon-knobs/react';
 import { action } from '@storybook/addon-actions';
 import { getExampleDateStrings } from '@commercetools-uikit/calendar-utils';
+import { Value } from 'react-value';
 import Readme from './README.md';
 import DateInput from './date-input';
 import Section from '../../../../.storybook/decorators/section';
 
 const exampleDates = getExampleDateStrings();
-
-class DateInputStory extends React.Component {
-  static displayName = 'DateInputStory';
-
-  state = {
-    value: exampleDates.preselectedDate,
-  };
-
-  handleChange = event => {
-    action('onChange')(event);
-    this.setState({ value: event.target.value });
-  };
-
-  handleFocus = event => {
-    action('onFocus')(event);
-  };
-
-  handleBlur = event => {
-    action('onBlur')(event);
-  };
-
-  render() {
-    const placeholder = text('placeholder', '');
-    return (
-      <Section>
-        <div>
-          <DateInput
-            value={this.state.value}
-            onBlur={this.handleBlur}
-            onChange={this.handleChange}
-            onFocus={this.handleFocus}
-            horizontalConstraint={select(
-              'horizontalConstraint',
-              ['m', 'l', 'xl', 'scale'],
-              'l'
-            )}
-            id={text('id', '')}
-            name={text('name', '')}
-            placeholder={placeholder === '' ? undefined : placeholder}
-            isDisabled={boolean('isDisabled', false)}
-            isReadOnly={boolean('isReadOnly', false)}
-            hasError={boolean('hasError', false)}
-            hasWarning={boolean('hasWarning', false)}
-            minValue={text('minValue', exampleDates.minDate)}
-            maxValue={text('maxValue', exampleDates.maxDate)}
-          />
-        </div>
-      </Section>
-    );
-  }
-}
 
 storiesOf('Components|Inputs', module)
   .addDecorator(withKnobs)
@@ -68,4 +18,38 @@ storiesOf('Components|Inputs', module)
       sidebar: Readme,
     },
   })
-  .add('DateInput', () => <DateInputStory />);
+  .add('DateInput', () => (
+    <Section>
+      <Value
+        defaultValue={exampleDates.preselectedDate}
+        render={(value, onChange) => {
+          const placeholder = text('placeholder', 'Placeholder');
+          return (
+            <DateInput
+              value={value}
+              onBlur={action('onBlur')}
+              onFocus={action('onFocus')}
+              onChange={event => {
+                action('onChange')(event);
+                onChange(event.target.value);
+              }}
+              horizontalConstraint={select(
+                'horizontalConstraint',
+                ['m', 'l', 'xl', 'scale'],
+                'l'
+              )}
+              id={text('id', '')}
+              name={text('name', '')}
+              placeholder={placeholder === '' ? undefined : placeholder}
+              isDisabled={boolean('isDisabled', false)}
+              isReadOnly={boolean('isReadOnly', false)}
+              hasError={boolean('hasError', false)}
+              hasWarning={boolean('hasWarning', false)}
+              minValue={text('minValue', exampleDates.minDate)}
+              maxValue={text('maxValue', exampleDates.maxDate)}
+            />
+          );
+        }}
+      />
+    </Section>
+  ));
