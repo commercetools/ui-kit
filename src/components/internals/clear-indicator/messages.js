5f22731f4eb56190f3546423b6895b105ca4ff0d
jonnybel <jonnybel91@gmail.com>
2019-12-11 18:18:33 +0100

    fix(select-input): clear-indicator to be a proper button

---
+++ src/components/internals/clear-indicator/messages.js
@@ -0,0 +1,9 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  clearButtonLabel: {
+    id: 'UIKit.ClearButton.clearButtonLabel',
+    description: 'Label for the clear button',
+    defaultMessage: 'Clear',
+  },
+});
