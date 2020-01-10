6449a49aa4738caa3cb30a1518975e44c2928c7b
jonnybel <jonnybel91@gmail.com>
2020-01-10 16:10:24 +0100

    chore: change example dataset

--- packages/components/simple-table/src/simple-table.story.js
+++ packages/components/simple-table/src/simple-table.story.js
@@ -4,96 +4,121 @@ import { withKnobs, number, boolean, select } from '@storybook/addon-knobs';
 import { Value } from 'react-value';
 import SimpleTable from './simple-table';
 
+/* generated from https://next.json-generator.com with input:
+[
+  {
+    'repeat(9, 10)': {
+      key: '{{objectId()}}',
+      checked: false,
+      name: '{{firstName()}} {{surname()}}',
+      company: '{{company().toUpperCase()}}',
+      phone: '+1 {{phone()}}',
+      age: '{{integer(20, 40)}}',
+      about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.'
+    }
+  }
+]
+*/
 const items = [
   {
-    key: 'camilo-jimenez',
+    key: '5e188c29791747d9c54250e2',
     checked: false,
-    name: 'Camilo Jimenez',
-    role: 'Team lead',
-    nationality: 'Australian',
-    age: 12,
-    ':-)':
-      'asdsadsadsa dsa dsa dsa \n dsad asd sa \n dsa dsa \n d sadsadsadasdsa dsads',
+    name: 'Morgan Bean',
+    company: 'CYCLONICA',
+    phone: '+1 (895) 529-3300',
+    age: 23,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.',
   },
   {
-    key: 'jennifer-wong',
+    key: '5e188c295ae0bb19afbb115f',
     checked: false,
-    name: 'Jennifer Wong',
-    role: 'UX Designer',
-    nationality: 'American',
-    age: 11,
-    ':-)':
-      'asdsadsadsa dsa dsa dsa \n dsad asd sa \n dsa dsa \n d sadsadsadasdsa dsads',
+    name: 'Franklin Cochran',
+    company: 'TINGLES',
+    phone: '+1 (835) 571-3268',
+    age: 36,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.',
   },
   {
-    key: 'adnan-asani',
+    key: '5e188c298f0ea901553c517f',
     checked: false,
-    name: 'Adnan Asani',
-    role: 'Developer',
-    nationality: 'Swedish',
-    age: 14,
-    ':-)':
-      'asdsadsadsa dsa dsa dsa \n dsad asd sa \n dsa dsa \n d sadsadsadasdsa dsads',
+    name: 'Salazar Craig',
+    company: 'ECRAZE',
+    phone: '+1 (944) 445-2594',
+    age: 21,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.',
   },
   {
-    key: 'josh-bones',
+    key: '5e188c29b09bb748df833ed0',
     checked: false,
-    name: 'Josh Bones',
-    role: 'Developer',
-    nationality: 'Australian',
-    age: 12,
-    ':-)':
-      'asdsadsadsa dsa dsa dsa \n dsad asd sa \n dsa dsa \n d sadsadsadasdsa dsads',
+    name: 'Pamela Noble',
+    company: 'FILODYNE',
+    phone: '+1 (875) 421-3328',
+    age: 34,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.',
   },
   {
-    key: 'sven-heckler',
+    key: '5e188c29bc14e3b97ab2ad7d',
     checked: false,
-    name: 'Sven Heckler',
-    role: 'UI Designer',
-    nationality: 'German',
-    age: 10,
-    ':-)':
-      'asdsadsadsa dsa dsa dsa \n dsad asd sa \n dsa dsa \n d sadsadsadasdsa dsads',
+    name: 'Terra Morrow',
+    company: 'DAISU',
+    phone: '+1 (807) 436-2026',
+    age: 30,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.',
   },
   {
-    key: 'luis-gomes',
+    key: '5e188c296c9b7cf486a0479c',
     checked: false,
-    name: 'Luis Gomes',
-    role: 'Developer',
-    nationality: 'Brazilian',
-    age: 9,
-    ':-)':
-      'asdsadsadsa dsa dsa dsa \n dsad asd sa \n dsa dsa \n d sadsadsadasdsa dsads',
+    name: 'Cline Hansen',
+    company: 'ULTRIMAX',
+    phone: '+1 (934) 402-3675',
+    age: 21,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.',
   },
   {
-    key: 'nicola-molinari',
-    checked: false,
-    name: 'Nicola Molinari',
-    role: 'Developer',
-    nationality: 'Italian',
-    age: 11,
-    ':-)':
-      'asdsadsadsa dsa dsa dsa \n dsad asd sa \n dsa dsa \n d sadsadsadasdsa dsads',
+    key: '5e188c29b45c669d8e60303f',
+    checked: true,
+    name: 'Jefferson Rosario',
+    company: 'COMTOURS',
+    phone: '+1 (874) 437-2581',
+    age: 32,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.',
   },
   {
-    key: 'dominik-ferber',
+    key: '5e188c29ca865647af147b4a',
     checked: false,
-    name: 'Dominik Ferber',
-    role: 'Developer',
-    nationality: 'German',
-    age: 4,
-    ':-)':
-      'asdsadsadsa dsa dsa dsa \n dsad asd sa \n dsa dsa \n d sadsadsadasdsa dsads',
+    name: 'Tania Waller',
+    company: 'DOGSPA',
+    phone: '+1 (964) 585-3040',
+    age: 35,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.',
   },
   {
-    key: 'philipp-sporrer',
+    key: '5e188c2910b83f907e9c66ab',
     checked: false,
-    name: 'Philipp Sporrer',
-    role: 'Developer',
-    nationality: 'German',
-    age: 7,
-    ':-)':
-      'asdsadsadsa dsa dsa dsa \n dsad asd sa \n dsa dsa \n d sadsadsadasdsa dsads',
+    name: 'Butler Shepard',
+    company: 'HOUSEDOWN',
+    phone: '+1 (888) 434-2153',
+    age: 21,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.',
+  },
+  {
+    key: '5e188c29a9ece9123d6a87a1',
+    checked: false,
+    name: 'Diana Wise',
+    company: 'SPEEDBOLT',
+    phone: '+1 (992) 535-2912',
+    age: 27,
+    about:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu dictum varius duis at consectetur lorem donec.',
   },
 ];
 
@@ -103,12 +128,12 @@ const columns = [
     label: 'Name',
   },
   {
-    key: 'role',
-    label: 'Role',
+    key: 'company',
+    label: 'Company',
   },
   {
-    key: 'nationality',
-    label: 'Nationality',
+    key: 'phone',
+    label: 'Phone',
   },
   {
     key: 'age',
@@ -116,8 +141,8 @@ const columns = [
     align: 'right',
   },
   {
-    key: ':-)',
-    label: ':-)',
+    key: 'about',
+    label: 'About',
   },
 ];
 
@@ -135,24 +160,30 @@ storiesOf('Components|Table (NEW)', module)
               <Value
                 defaultValue={item.age}
                 render={(value, onChange) => (
-                  <input
-                    type="number"
-                    value={value}
-                    onChange={() => onChange(event.target.value)}
-                  />
+                  <div>
+                    <input
+                      type="number"
+                      value={value}
+                      onChange={() => onChange(event.target.value)}
+                    />
+                    <div>{value}</div>
+                  </div>
                 )}
               />
             );
-          case 'role':
+          case 'company':
             return (
               <Value
-                defaultValue={item.role}
+                defaultValue={item.company}
                 render={(value, onChange) => (
-                  <input
-                    type="text"
-                    value={value}
-                    onChange={() => onChange(event.target.value)}
-                  />
+                  <div>
+                    <input
+                      type="text"
+                      value={value}
+                      onChange={() => onChange(event.target.value)}
+                    />
+                    <div>{value}</div>
+                  </div>
                 )}
               />
             );
