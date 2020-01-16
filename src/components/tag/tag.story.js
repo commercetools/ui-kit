66bd5390de477836d5b9da471fb8224871bc94a4
Abel Almeida <jonnybel91@gmail.com>
2020-01-16 10:44:15 +0100

    chore(tag): fix storybook example (#1242)

--- src/components/tag/tag.story.js
+++ src/components/tag/tag.story.js
@@ -11,7 +11,7 @@ const Story = () => (
   <Section>
     <Tag
       type={select('type', ['normal', 'warning'], 'normal')}
-      linkTo={select('linkTo', ['/foo', null], '/foo')}
+      linkTo={select('linkTo', { '/foo': '/foo', Null: null }, '/foo')}
       isDisabled={boolean('isDisabled', false)}
       onClick={boolean('onClick', false) ? action('onClick') : undefined}
       horizontalConstraint={select(
