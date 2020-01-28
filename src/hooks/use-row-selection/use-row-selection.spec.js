652bc3814677990550ed2dafa92e88922cc41518
jonnybel <jonnybel91@gmail.com>
2020-01-28 16:45:28 +0100

    chore: cr suggestions

--- src/hooks/use-row-selection/use-row-selection.spec.js
+++ src/hooks/use-row-selection/use-row-selection.spec.js
@@ -62,83 +62,83 @@ const TestComponent = props => {
 };
 
 it('should have no selected items by default', () => {
-  const { getByTestId } = render(<TestComponent items={testData} />);
+  const rendered = render(<TestComponent items={testData} />);
 
   testData.forEach(item => {
-    expect(getByTestId(item.key)).toHaveTextContent('false');
+    expect(rendered.queryByTestId(item.key)).toHaveTextContent('false');
   });
 });
 
 it('should be possible to toggle a row selection state', () => {
-  const { getByTestId } = render(<TestComponent items={testData} />);
+  const rendered = render(<TestComponent items={testData} />);
 
-  expect(getByTestId('system-crasher')).toHaveTextContent('false');
+  expect(rendered.queryByTestId('system-crasher')).toHaveTextContent('false');
 
-  getByTestId('toggle system-crasher').click();
-  expect(getByTestId('system-crasher')).toHaveTextContent('true');
+  rendered.queryByTestId('toggle system-crasher').click();
+  expect(rendered.queryByTestId('system-crasher')).toHaveTextContent('true');
 });
 
 it('should be possible to select a row', () => {
-  const { getByTestId } = render(<TestComponent items={testData} />);
+  const rendered = render(<TestComponent items={testData} />);
 
-  expect(getByTestId('system-crasher')).toHaveTextContent('false');
+  expect(rendered.queryByTestId('system-crasher')).toHaveTextContent('false');
 
-  getByTestId('select system-crasher').click();
-  expect(getByTestId('system-crasher')).toHaveTextContent('true');
+  rendered.queryByTestId('select system-crasher').click();
+  expect(rendered.queryByTestId('system-crasher')).toHaveTextContent('true');
 });
 
 it('should be possible to deselect a row', () => {
-  const { getByTestId } = render(<TestComponent items={testData} />);
+  const rendered = render(<TestComponent items={testData} />);
 
-  getByTestId('select system-crasher').click();
-  expect(getByTestId('system-crasher')).toHaveTextContent('true');
+  rendered.queryByTestId('select system-crasher').click();
+  expect(rendered.queryByTestId('system-crasher')).toHaveTextContent('true');
 
-  getByTestId('deselect system-crasher').click();
-  expect(getByTestId('system-crasher')).toHaveTextContent('false');
+  rendered.queryByTestId('deselect system-crasher').click();
+  expect(rendered.queryByTestId('system-crasher')).toHaveTextContent('false');
 });
 
 it('should get the correct count of selected rows', () => {
-  const { getByTestId } = render(<TestComponent items={testData} />);
+  const rendered = render(<TestComponent items={testData} />);
 
-  expect(getByTestId('selectedCount')).toHaveTextContent('0');
+  expect(rendered.queryByTestId('selectedCount')).toHaveTextContent('0');
 
-  getByTestId('select system-crasher').click();
+  rendered.queryByTestId('select system-crasher').click();
 
-  expect(getByTestId('selectedCount')).toHaveTextContent('1');
+  expect(rendered.queryByTestId('selectedCount')).toHaveTextContent('1');
 
-  getByTestId('select birds-of-passage').click();
+  rendered.queryByTestId('select birds-of-passage').click();
 
-  expect(getByTestId('selectedCount')).toHaveTextContent('2');
+  expect(rendered.queryByTestId('selectedCount')).toHaveTextContent('2');
 
-  getByTestId('deselect system-crasher').click();
+  rendered.queryByTestId('deselect system-crasher').click();
 
-  expect(getByTestId('selectedCount')).toHaveTextContent('1');
+  expect(rendered.queryByTestId('selectedCount')).toHaveTextContent('1');
 });
 
 it('should be possible to select all rows', () => {
-  const { getByTestId } = render(<TestComponent items={testData} />);
+  const rendered = render(<TestComponent items={testData} />);
 
-  getByTestId('selectAll').click();
+  rendered.queryByTestId('selectAll').click();
 
-  expect(getByTestId('selectedCount')).toHaveTextContent('3');
+  expect(rendered.queryByTestId('selectedCount')).toHaveTextContent('3');
 
   testData.forEach(item => {
-    expect(getByTestId(item.key)).toHaveTextContent('true');
+    expect(rendered.queryByTestId(item.key)).toHaveTextContent('true');
   });
 });
 
 it('should be possible to deselect all rows', () => {
-  const { getByTestId } = render(<TestComponent items={testData} />);
+  const rendered = render(<TestComponent items={testData} />);
 
-  getByTestId('selectAll').click();
+  rendered.queryByTestId('selectAll').click();
 
-  expect(getByTestId('selectedCount')).toHaveTextContent('3');
+  expect(rendered.queryByTestId('selectedCount')).toHaveTextContent('3');
 
-  getByTestId('deselectAll').click();
+  rendered.queryByTestId('deselectAll').click();
 
-  expect(getByTestId('selectedCount')).toHaveTextContent('0');
+  expect(rendered.queryByTestId('selectedCount')).toHaveTextContent('0');
 
   testData.forEach(item => {
-    expect(getByTestId(item.key)).toHaveTextContent('false');
+    expect(rendered.queryByTestId(item.key)).toHaveTextContent('false');
   });
 });
