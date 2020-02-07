9c7d47035196a5ef97f252434cf2130081e5f1c5
Nicola Molinari <nicola.molinari@commercetools.de>
2020-02-07 14:25:29 +0100

    fix: vrt setup

--- visual-testing-app/src/App.js
+++ visual-testing-app/src/App.js
@@ -11,23 +11,40 @@ const darkTheme = {
   colorAccent98: 'rgba(0,0,0,0.98)',
 };
 
-const componentsContext = require.context(
+const srcComponentsContext = require.context(
   '../../src/components',
   true,
   /\.visualroute\.js$/
 );
-const allComponents = componentsContext.keys().reduce((components, file) => {
-  const Comp = componentsContext(file);
-  // trim leading slash
-  const label = Comp.routePath.substring(1);
-  if (components[label]) {
-    // eslint-disable-next-line no-console
-    console.error(`Duplicate route generated for: /${label}`);
-  }
-  // eslint-disable-next-line no-param-reassign
-  components[label] = Comp;
-  return components;
-}, {});
+const pkgComponentsContext = require.context(
+  '../../packages',
+  true,
+  /\.visualroute\.js$/
+);
+const srcRouteComponent = srcComponentsContext
+  .keys()
+  .map(id => srcComponentsContext(id));
+const pkgRouteComponent = pkgComponentsContext
+  .keys()
+  .map(id => pkgComponentsContext(id));
+const allRouteComponents = srcRouteComponent.concat(pkgRouteComponent);
+const allUniqueRouteComponents = allRouteComponents.reduce(
+  (allComponents, RouteComponent) => {
+    // trim leading slash
+    const label = RouteComponent.routePath.substring(1);
+    if (allComponents[label]) {
+      // eslint-disable-next-line no-console
+      console.error(`Duplicate route generated for: /${label}`);
+    }
+    // eslint-disable-next-line no-param-reassign
+    allComponents[label] = RouteComponent;
+    return allComponents;
+  },
+  {}
+);
+const allSortedComponents = Object.keys(allUniqueRouteComponents)
+  .sort()
+  .map(key => allUniqueRouteComponents[key]);
 
 class App extends React.Component {
   render() {
@@ -42,7 +59,7 @@ class App extends React.Component {
                 <div>
                   <h1>Visual Testing App</h1>
                   <ul>
-                    {Object.values(allComponents).map(Component => (
+                    {allSortedComponents.map(Component => (
                       <li key={Component.routePath}>
                         <a href={Component.routePath}>{Component.routePath}</a>
                       </li>
@@ -51,7 +68,7 @@ class App extends React.Component {
                 </div>
               )}
             />
-            {Object.values(allComponents).map(Component => (
+            {allSortedComponents.map(Component => (
               <Route
                 key={Component.routePath}
                 path={Component.routePath}
