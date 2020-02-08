27319f9104f6f1669fe619d898a9c8b5b1503dd0
Nicola Molinari <nicola.molinari@commercetools.de>
2020-02-08 17:29:33 +0100

    chore: force publish canary packages

--- scripts/release_canary.sh
+++ scripts/release_canary.sh
@@ -10,12 +10,6 @@ COMMIT_MESSAGE=$(git log --format=oneline -n 1 $CIRCLE_SHA1)
 # Only trigger the canary release when:
 # - the commit message does not contain `[skip publish]`
 if [[ ! "$COMMIT_MESSAGE" =~ \[skip\ publish\] ]]; then
-  echo "pwd"
-  echo $PWD
-
-  echo "ls"
-  ls -lah
-
   echo "Configuring npm for automation bot"
 
   cat > .npmrc << EOF
@@ -23,16 +17,8 @@ email=npmjs@commercetools.com
 //registry.npmjs.org/:_authToken=$NPM_TOKEN
 EOF
 
-  echo "Working tree status"
-  git status
-
-    # Release package only if lerna detected some changes in packages
-  if yarn run lerna changed &> /dev/null; then
-    echo "Releasing canary version"
-    yarn release:canary
-  else
-    echo "Skipping release, no changes in packages..."
-  fi
+  echo "Releasing canary version"
+  yarn release:canary
 
 else
   echo "Skipping release due to commit message..."
