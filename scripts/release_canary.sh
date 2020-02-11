8ee3c9970f619002921e06af9bb3d7f3a966c759
Nicola Molinari <nicola.molinari@commercetools.de>
2020-02-11 13:52:39 +0100

    fix: force canary releases to always publish all packages (#1259)
    * fix: publish canary only if lerna detects that packages changed
    
    * chore: force publish canary packages
    
    Co-authored-by: Abel Almeida <jonnybel91@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- scripts/release_canary.sh
+++ scripts/release_canary.sh
@@ -10,21 +10,12 @@ COMMIT_MESSAGE=$(git log --format=oneline -n 1 $CIRCLE_SHA1)
 # Only trigger the canary release when:
 # - the commit message does not contain `[skip publish]`
 if [[ ! "$COMMIT_MESSAGE" =~ \[skip\ publish\] ]]; then
-  echo "pwd"
-  echo $PWD
-
-  echo "ls"
-  ls -lah
-
   echo "Configuring npm for automation bot"
 
   cat > .npmrc << EOF
 email=npmjs@commercetools.com
 //registry.npmjs.org/:_authToken=$NPM_TOKEN
 EOF
-
-  echo "Working tree status"
-  git status
 
   echo "Releasing canary version"
   yarn release:canary
