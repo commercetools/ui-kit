d2b7d28b8caab04100108fcf0d7efcf3b93f7cd9
Abel Almeida <jonnybel91@gmail.com>
2020-03-04 12:12:43 +0100

    feat: add accessible-hidden component (#1271)
    * feat: add accessible-hidden component
    
    * chore: improve readme
    
    * Apply suggestions from code review
    
    Co-Authored-By: Tobias Deekens <tobias.deekens@commercetools.de>
    
    Co-authored-by: Tobias Deekens <nerd@tdeekens.name>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ packages/components/accessible-hidden/src/accessible-hidden.spec.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from '../../../../src/test-utils';
+import AccessibleHidden from './accessible-hidden';
+
+it('should find text inside AccessibleHidden', () => {
+  const rendered = render(<AccessibleHidden>Spies!</AccessibleHidden>);
+
+  expect(rendered.getByText('Spies!')).toBeInTheDocument();
+});
+
+it('should find the input of a label inside AccessibleHidden', () => {
+  const rendered = render(
+    <>
+      <AccessibleHidden>
+        <label htmlFor="maiden-name-input">Enter your Maiden Name</label>
+      </AccessibleHidden>
+      <input id="maiden-name-input" type="text"></input>
+    </>
+  );
+
+  expect(rendered.getByLabelText('Enter your Maiden Name')).toBeInTheDocument();
+});
---
+++ packages/components/accessible-hidden/src/index.js
@@ -0,0 +1,3 @@
+export { default } from './accessible-hidden';
+
+export { default as version } from './version';
