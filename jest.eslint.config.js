3c4016ca0e8832ed79037219d2181a8679411b47
tdeekens <tobias.deekens@commercetools.com>
2020-06-17 13:48:04 +0200

    fix: remove my stupidity

--- jest.eslint.config.js
+++ jest.eslint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   runner: 'jest-runner-eslint',
   displayName: 'eslint',
   testMatch: ['<rootDir>/**/*.js'],
-  testPathIgnorePatterns: ['/node_modules/', '.public/', 'public/', 'dist/es;'],
+  testPathIgnorePatterns: ['/node_modules/', '.public/', 'public/', 'dist;'],
   watchPlugins: ['jest-watch-typeahead/filename'],
 };
