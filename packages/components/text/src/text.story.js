e5880a37b0a6849eea5578e4683f1b81f0013bbd
mohib0306 <mohibullah.wardak@commercetools.com>
2020-07-06 15:17:02 +0200

    chore(packages/text): rename and move typography.story.js to src/text.story.js

--- packages/components/text/src/text.story.js
+++ packages/components/text/src/text.story.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, boolean, select, text } from '@storybook/addon-knobs/react';
-import Section from '../../../.storybook/decorators/section';
-import Text from './src/text';
-import Readme from './README.md';
+import Section from '../../../../.storybook/decorators/section';
+import Text from './text';
+import Readme from '../README.md';
 
 const InlineColorWrapper = styled.div`
   background-color: #e1ffdd;
