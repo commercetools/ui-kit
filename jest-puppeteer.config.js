303d9276f58b64979655ac6d0f30ba289839c5fa
Nicola Molinari <nicola.molinari@commercetools.de>
2020-07-10 10:42:56 +0200

    fix(puppeteer): use google chrome stable, update gh action to v3 (#1424)

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -7,6 +7,7 @@
 module.exports = {
   launch: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: 'google-chrome-stable',
   },
   server: {
     command: 'yarn visual-testing-app:serve',
