d604273b5b6c93078a53ed3b3bdce035b5773017
Nicolas Lucero <nico@devgurus.io>
2020-07-28 12:41:39 +0200

    feat(data-table-manager): adding main component

---
+++ packages/components/data-table-manager/src/constants.js
@@ -0,0 +1,7 @@
+export const UPDATE_ACTIONS = {
+  COLUMNS_UPDATE: 'columnsUpdate',
+  IS_TABLE_CONDENSED_UPDATE: 'isTableCondensedUpdate',
+  IS_TABLE_WRAPPING_TEXT_UPDATE: 'isTableWrappingTextUpdate',
+};
+export const COLUMN_MANAGER = 'columnManager';
+export const DISPLAY_SETTINGS = 'displaySettings';
---
+++ packages/components/data-table-manager/src/index.js
@@ -0,0 +1,4 @@
+export { default } from './managed-data-table';
+export { useRowSelection, useSorting } from '@commercetools-uikit/hooks';
+export { UPDATE_ACTIONS } from './constants';
+export { default as version } from './version';
