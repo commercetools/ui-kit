8c3001cf7599e307974924901be596e945367d9e
Nicola Molinari <nicola.molinari@commercetools.de>
2020-08-17 11:40:58 +0200

    refactor: include compile script in i18n package

--- husky.config.js
+++ husky.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
     'commit-msg': 'commitlint -e $HUSKY_GIT_PARAMS',
-    'pre-commit': 'yarn i18n:compile && lint-staged',
+    'pre-commit': 'lint-staged',
   },
 };
