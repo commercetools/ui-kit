d5d3f64f2e8795aa55d1e1fcb20486fab8140e46
Adnan Asani <adnasa@users.noreply.github.com>
2020-09-03 16:09:40 +0200

    docs(fields/date-field): add `generate-readme`

---
+++ packages/components/fields/date-field/docs/usage-example.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DateField from '@commercetools-uikit/date-field';
+
+const Example = () => (
+  <DateField
+    title="Release Date"
+    value="2018-10-30"
+    onChange={(event) => alert(event.target.value)}
+  />
+);
+
+export default Example;
