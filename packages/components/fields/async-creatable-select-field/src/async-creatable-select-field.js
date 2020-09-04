484e51de842f1a0afd18d044e98062e0e7ccaab1
Adnan Asani <adnasa@users.noreply.github.com>
2020-09-04 11:10:32 +0200

    docs(fields/async-creatable-select-field): add `generate-readme`

--- packages/components/fields/async-creatable-select-field/src/async-creatable-select-field.js
+++ packages/components/fields/async-creatable-select-field/src/async-creatable-select-field.js
@@ -22,43 +22,147 @@ export default class AsyncCreatableSelectField extends React.Component {
 
   static propTypes = {
     // AsyncCreatableSelectField
+    /**
+     * Used as HTML id property. An id is auto-generated when it is not specified.
+     */
     id: PropTypes.string,
+    /**
+     * Horizontal size limit of the input fields.
+     */
     horizontalConstraint: PropTypes.oneOf(['s', 'm', 'l', 'xl', 'scale']),
+    /**
+     * A map of errors. Error messages for known errors are rendered automatically.
+     * <br />
+     * Unknown errors will be forwarded to `renderError`
+     */
     errors: PropTypes.shape({
       missing: PropTypes.bool,
     }),
+    /**
+     * Called with custom errors. This function can return a message which will be wrapped in an ErrorMessage. It can also return null to show no error.
+     * <br />
+     * Signature: `(key, error) => React.node`
+     */
     renderError: PropTypes.func,
+    /**
+     * Indicates if the value is required. Shows an the "required asterisk" if so.
+     */
     isRequired: PropTypes.bool,
+    /**
+     * Indicates whether the field was touched. Errors will only be shown when the field was touched.
+     */
     touched: (props, ...rest) =>
       props.isMulti
         ? PropTypes.arrayOf(PropTypes.bool, ...rest)(props, ...rest)
         : PropTypes.bool(props, ...rest),
 
     // AsyncCreatableSelectInput
+    /**
+     * Aria label (for assistive tech)
+     */
     'aria-label': PropTypes.string,
+    /**
+     * HTML ID of an element that should be used as the label (for assistive tech)
+     */
     'aria-labelledby': PropTypes.string,
+    /**
+     * 	Focus the control when it is mounted
+     */
     isAutofocussed: PropTypes.bool,
+    /**
+     * Remove the currently focused option when the user presses backspace
+     */
     backspaceRemovesValue: PropTypes.bool,
+    /**
+     * Map of components to overwrite the default ones, see [what components you can override](https://react-select.com/components)
+     */
     components: PropTypes.objectOf(PropTypes.func),
+    /**
+     * Custom method to filter whether an option should be displayed in the menu
+     */
     filterOption: PropTypes.func,
+    /**
+     * The id to set on the SelectContainer component
+     */
     containerId: PropTypes.string,
+    /**
+     * Is the select value clearable
+     */
     isClearable: PropTypes.bool,
+    /**
+     * Is the select disabled
+     */
     isDisabled: PropTypes.bool,
+    /**
+     * Is the select read-only
+     */
     isReadOnly: PropTypes.bool,
+    /**
+     * Override the built-in logic to detect whether an option is disabled
+     * <br />
+     * Signature: `(option, options) => boolean`
+     */
     isOptionDisabled: PropTypes.func,
+    /**
+     * Support multiple selected options
+     */
     isMulti: PropTypes.bool,
+    /**
+     * Whether to enable search functionality
+     */
     isSearchable: PropTypes.bool,
     hasWarning: PropTypes.bool,
+    /**
+     * 	Maximum height of the menu before scrolling
+     */
     maxMenuHeight: PropTypes.number,
+    /**
+     * 	Dom element to portal the select menu to
+     */
     menuPortalTarget: PropTypes.instanceOf(SafeHTMLElement),
+    /**
+     * z-index value for the menu portal
+     */
     menuPortalZIndex: PropTypes.number,
+    /**
+     * whether the menu should block scroll while open
+     */
     menuShouldBlockScroll: PropTypes.bool,
+    /**
+     * 	Name of the HTML Input (optional - without this, no input will be rendered)
+     */
     name: PropTypes.string,
+    /**
+     * Can be used to render a custom value when there are no options (either because of no search results, or all options have been used, or there were none in the first place).
+     * <br/>
+     * Gets called with `{ inputValue: String }`. `inputValue` will be an empty string when no search text is present.
+     * <br />
+     * Signature: `({ inputValue }) => string`
+     */
     noOptionsMessage: PropTypes.func,
+    /**
+     * Handle blur events on the control
+     */
     onBlur: PropTypes.func,
+    /**
+     * Called with a fake event when value changes.
+     * <br />
+     * The event's `target.name` will be the name supplied in props. The event's `target.value` will hold the value. The value will be the selected option, or an array of options in case `isMulti` is `true`.
+     * <br />
+     * Signature: `(event) => void`
+     */
     onChange: PropTypes.func,
+    /**
+     * Handle focus events on the control
+     */
     onFocus: PropTypes.func,
+    /**
+     * Handle change events on the input
+     */
     onInputChange: PropTypes.func,
+    /**
+     * Array of options that populate the select menu
+     */
     options: PropTypes.arrayOf(
       PropTypes.oneOfType([
         PropTypes.shape({ value: PropTypes.string.isRequired }),
@@ -69,9 +173,21 @@ export default class AsyncCreatableSelectField extends React.Component {
         }),
       ])
     ),
+    /**
+     * Placeholder text for the select value
+     */
     placeholder: PropTypes.string,
+    /**
+     * Sets the tabIndex attribute on the input
+     */
     tabIndex: PropTypes.string,
+    /**
+     * Select the currently focused option when the user presses tab
+     */
     tabSelectsValue: PropTypes.bool,
+    /**
+     * The value of the select; reflected by the selected option
+     */
     value: (props, ...rest) =>
       props.isMulti
         ? PropTypes.arrayOf(
@@ -84,6 +200,11 @@ export default class AsyncCreatableSelectField extends React.Component {
     showOptionGroupDivider: PropTypes.bool,
 
     // Async props
+    /**
+     * The default set of options to show before the user starts searching.
+     * <br/>
+     * When set to `true`, the results for `loadOptions('')` will be autoloaded.
+     */
     defaultOptions: PropTypes.oneOfType([
       PropTypes.bool,
       PropTypes.arrayOf(
@@ -92,27 +213,84 @@ export default class AsyncCreatableSelectField extends React.Component {
         })
       ),
     ]),
+    /**
+     * Function that returns a promise, which is the set of options to be used once the promise resolves.
+     * <br />
+     * Signature: `(inputValue, callback) => void)) => Promise | void`
+     * <br />
+     * `callback` can be called with `options`
+     */
     loadOptions: PropTypes.func.isRequired,
+    /**
+     * If cacheOptions is truthy, then the loaded data will be cached. The cache will remain until cacheOptions changes value.
+     */
     cacheOptions: PropTypes.any,
 
     // Creatable props
+    /**
+     * Allow options to be created while the isLoading prop is true. Useful to prevent the "create new ..." option being displayed while async results are still being loaded.
+     */
     allowCreateWhileLoading: PropTypes.bool,
+    /**
+     * Gets the label for the "create new ..." option in the menu. Is given the current input value.
+     */
     formatCreateLabel: PropTypes.func,
+    /**
+     * Determines whether the "create new ..." option should be displayed based on the current input value, select value and options array.
+     */
     isValidNewOption: PropTypes.func,
+    /**
+     * Returns the data for the new option when it is created. Used to display the value, and is passed to onChange.
+     */
     getNewOptionData: PropTypes.func,
+    /**
+     * If provided, this will be called with the input value when a new option is created, and onChange will not be called. Use this when you need more control over what happens when new options are created.
+     */
     onCreateOption: PropTypes.func,
+    /**
+     * Sets the position of the createOption element in your options list.
+     */
     createOptionPosition: PropTypes.string,
 
     // LabelField
+    /**
+     * Title of the label
+     */
     title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+    /**
+     * Hint for the label. Provides a supplementary but important information regarding the behaviour of the input (e.g warn about uniqueness of a field, when it can only be set once), whereas `description` can describe it in more depth. Can also receive a `hintIcon`.
+     */
     hint: requiredIf(
       PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
       (props) => props.hintIcon
     ),
+    /**
+     * Provides a description for the title.
+     */
     description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    /**
+     * Function called when info button is pressed.
+     * <br />
+     * Info button will only be visible when this prop is passed.
+     * <br />
+     * Signature: `(event) => void`
+     */
     onInfoButtonClick: PropTypes.func,
+    /**
+     * Icon to be displayed beside the hint text.
+     * <br />
+     * Will only get rendered when `hint` is passed as well.
+     */
     hintIcon: PropTypes.node,
+    /**
+     * Badge to be displayed beside the label.
+     * <br />
+     * Might be used to display additional information about the content of the field (E.g verified email)
+     */
     badge: PropTypes.node,
+    /**
+     * Icon to display on the left of the placeholder text and selected value. Has no effect when isMulti is enabled.
+     */
     iconLeft: PropTypes.node,
   };
 
