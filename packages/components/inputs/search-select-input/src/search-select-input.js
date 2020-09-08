70f3ff8deebe42e74b4dd9bafb8d89346b203d05
mohib0306 <mohibullah.wardak@commercetools.com>
2020-09-08 12:31:09 +0200

    chore(search-select-input): add default loading and no-option messages

--- packages/components/inputs/search-select-input/src/search-select-input.js
+++ packages/components/inputs/search-select-input/src/search-select-input.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useIntl } from 'react-intl';
 import {
   SafeHTMLElement,
   filterAriaAttributes,
@@ -7,9 +8,15 @@ import {
 import SearchIconDropdownIndicator from '../../../../../src/components/internals/search-icon-dropdown-indicator';
 import AsyncSelectInput from '../../async-select-input';
 import { SearchSelectInputOption } from './search-select-input-option';
-import { SELECT_DROPDOWN_OPTION_TYPES } from './constants';
+import messages from './messages';
 
 const SearchSelectInput = (props) => {
+  const intl = useIntl();
+  const noOptionsMessage = () =>
+    props.noOptionsMessage || intl.formatMessage(messages.noOptionsMessage);
+  const loadingMessage = () =>
+    props.loadingMessage || intl.formatMessage(messages.loadingOptionsMessage);
+
   return (
     <AsyncSelectInput
       {...props}
@@ -25,79 +32,170 @@ const SearchSelectInput = (props) => {
         ...props.components,
         DropdownIndicator: SearchIconDropdownIndicator,
       }}
-      noOptionsMessage={props.noOptionsMessage}
+      loadingMessage={loadingMessage}
+      noOptionsMessage={noOptionsMessage}
       isSearchable={true}
       showOptionGroupDivider={false}
     />
   );
 };
 
-SearchSelectInput.displayName = 'SearchSelectInput';
-
-SearchSelectInput.defaultProps = {
-  // Using "null" will ensure that the currently selected value disappears in
-  // case "undefined" gets passed as the next value
-  value: null,
-  isSearchable: true,
-  menuPortalZIndex: 1,
-  maxMenuHeight: '220',
-};
-
 SearchSelectInput.propTypes = {
+  /**
+   *Horizontal size limit of the input fields.
+   */
   horizontalConstraint: PropTypes.oneOf(['s', 'm', 'l', 'xl', 'scale']),
-  // react-select base props
+  /**
+   * Aria label (for assistive tech)
+   */
   'aria-label': PropTypes.string,
+  /**
+   * HTML ID of an element that should be used as the label (for assistive tech)
+   */
   'aria-labelledby': PropTypes.string,
-  // This forwarded as react-select's "inputId"
+  /**
+   * The id of the search input. This forwarded as react-select's "inputId"
+   */
   id: PropTypes.string,
-  // This is forwarded as react-select's "id"
+  /**
+   * The id to set on the SelectContainer component. This is forwarded as react-select's "id"
+   */
   containerId: PropTypes.string,
+  /**
+   * Name of the HTML Input (optional - without this, no input will be rendered)
+   */
   name: PropTypes.string,
+  /**
+   * Placeholder text for the select value
+   */
   placeholder: PropTypes.string,
+  /**
+   * Map of components to overwrite the default ones, see [what components you can override](https://react-select.com/components)
+   */
   components: PropTypes.objectOf(PropTypes.func),
+  /**
+   * Sets the tabIndex attribute on the input
+   */
   tabIndex: PropTypes.string,
+  /**
+   * Icon to display on the left of the placeholder text and selected value. Has no effect when isMulti is enabled.
+   */
   iconLeft: PropTypes.node,
-  inputValue: PropTypes.string,
+  /**
+   * The value of the select; reflected by the selected option
+   */
   value: (props, ...rest) =>
     props.isMulti
       ? PropTypes.arrayOf(
           PropTypes.shape({ value: PropTypes.string.isRequired })
         )(props, ...rest)
       : PropTypes.shape({ value: PropTypes.string.isRequired })(props, ...rest),
+  /**
+   * Remove the currently focused option when the user presses backspace
+   */
   backspaceRemovesValue: PropTypes.bool,
+  /**
+   * Indicates the input field has an error
+   */
   hasError: PropTypes.bool,
+  /**
+   * Indicates the input field has a warning
+   */
   hasWarning: PropTypes.bool,
+  /**
+   * Is the select read-only
+   */
   isReadOnly: PropTypes.bool,
+  /**
+   * Is the select disabled
+   */
   isDisabled: PropTypes.bool,
+  /**
+   * Is the select value clearable
+   */
   isClearable: PropTypes.bool,
+  /**
+   * Override the built-in logic to detect whether an option is disabled
+   */
   isOptionDisabled: PropTypes.func,
+  /**
+   * Support multiple selected options
+   */
   isMulti: PropTypes.bool,
-  // renamed autoFocus of react-select
+  /**
+   * Focus the control when it is mounted. Renamed autoFocus of react-select
+   */
   isAutofocussed: PropTypes.bool,
-
-  // dropdown menu props
+  /**
+   * Can be used to render a custom value when there are no options (either because of no search results, or all options have been used, or there were none in the first place). Gets called with `{ inputValue: String }`. `inputValue` will be an empty string when no search text is present.
+   */
   noOptionsMessage: PropTypes.func,
-  initialSuggestionMessage: PropTypes.func,
+  /**
+   * Maximum height of the menu before scrolling
+   */
   maxMenuHeight: PropTypes.number,
+  /**
+   * Dom element to portal the select menu to
+   */
   menuPortalTarget: PropTypes.instanceOf(SafeHTMLElement),
+  /**
+   * z-index value for the menu portal
+   */
   menuPortalZIndex: PropTypes.number.isRequired,
+  /**
+   * whether the menu should block scroll while open
+   */
   menuShouldBlockScroll: PropTypes.bool,
-
-  // input actions
+  /**
+   * Handle blur events on the control
+   */
   onBlur: PropTypes.func,
+  /**
+   * Called with a fake event when value changes. The event's `target.name` will be the `name` supplied in props. The event's `target.value` will hold the value. The value will be the selected option, or an array of options in case `isMulti` is `true`.
+   */
   onChange: PropTypes.func.isRequired,
+  /**
+   * Handle focus events on the control
+   */
   onFocus: PropTypes.func,
+  /**
+   * Handle change events on the input
+   */
   onInputChange: PropTypes.func,
+  /**
+   * Select the currently focused option when the user presses tab
+   */
   tabSelectsValue: PropTypes.bool,
-
-  // Async props
+  /**
+   * Function that returns a promise, which is the set of options to be used once the promise resolves.
+   */
   loadOptions: PropTypes.func.isRequired,
+  /**
+   * The text shown while the options are being loaded
+   */
   loadingMessage: PropTypes.func,
+  /**
+   * If cacheOptions is truthy, then the loaded data will be cached. The cache will remain until cacheOptions changes value.
+   */
   cacheOptions: PropTypes.any,
+  /**
+   * Custom method to filter whether an option should be displayed in the menu
+   */
   filterOption: PropTypes.func,
-
-  // options porps
-  optionType: PropTypes.oneOf(Object.values(SELECT_DROPDOWN_OPTION_TYPES)),
+  /**
+   * The style of the an option in the dropdown menu. It could be single lined option or an option with more and custom info
+   */
+  optionType: PropTypes.oneOf([
+    'single-lined',
+    'brief-detailed',
+    'extended-detailed',
+  ]),
 };
+SearchSelectInput.defaultProps = {
+  value: null,
+  menuPortalZIndex: 1,
+  maxMenuHeight: '220',
+};
+SearchSelectInput.displayName = 'SearchSelectInput';
 
 export default SearchSelectInput;
