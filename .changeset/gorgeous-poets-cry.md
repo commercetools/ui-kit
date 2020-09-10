f2cd16928b0ee001fa544a78cc196d177273581c
Rombelirk <roman.beloborodov@commercetools.com>
2020-09-10 13:16:21 +0500

    feat: to add table icon to data-table-manager dropdown (#1549)
    * feat: to add table icon to data-table-manager dropdown
    
    * Create gorgeous-poets-cry.md

---
+++ .changeset/gorgeous-poets-cry.md
@@ -0,0 +1,6 @@
+---
+"@commercetools-uikit/data-table-manager": minor
+"@commercetools-uikit/data-table": minor
+---
+
+Added table icon to data-table-manager dropdown
--- packages/components/data-table-manager/src/data-table-settings/data-table-settings.js
+++ packages/components/data-table-manager/src/data-table-settings/data-table-settings.js
@@ -5,6 +5,7 @@ import { useIntl } from 'react-intl';
 import styled from '@emotion/styled';
 import AccessibleHidden from '@commercetools-uikit/accessible-hidden';
 import SelectInput from '@commercetools-uikit/select-input';
+import { TableIcon } from '@commercetools-uikit/icons';
 import Spacings from '@commercetools-uikit/spacings';
 import { UPDATE_ACTIONS, COLUMN_MANAGER, DISPLAY_SETTINGS } from '../constants';
 import DisplaySettingsManager, {
@@ -108,6 +109,7 @@ const DataTableSettings = (props) => {
               placeholder={intl.formatMessage(messages.placeholder)}
               onChange={handleDropdownChange}
               options={dropdownOptions}
+              iconLeft={<TableIcon />}
             />
           </SelectContainer>
         )}
