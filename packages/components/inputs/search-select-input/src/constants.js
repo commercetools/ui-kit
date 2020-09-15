d84ff34dcb4d0f38f68d4d00870ffd0348898d4b
mohib0306 <mohibullah.wardak@commercetools.com>
2020-09-15 16:55:34 +0200

    chore(search-select-input): remove unused constants

--- packages/components/inputs/search-select-input/src/constants.js
+++
@@ -1,7 +0,0 @@
-export const NO_VALUE_FALLBACK = '----';
-
-export const SELECT_DROPDOWN_OPTION_TYPES = {
-  SINGLE_LINED_OPTION: 'single-lined',
-  BRIEF_DETAILED_OPTION: 'brief-detailed',
-  EXTENDED_DETAILED_OPTION: 'extended-detailed',
-};
