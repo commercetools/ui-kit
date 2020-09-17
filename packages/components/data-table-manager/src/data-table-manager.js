4a3f1f0ec7fb33f3beb0caaab9be30ca6b86ac9f
Abel Almeida <jonnybel91@gmail.com>
2020-09-17 12:14:09 +0200

    feat(data-table-manager): add prop `managerTheme` (#1566)
    * feat(data-table-manager): add settingsContainerTheme prop
    
    * chore: add changeset
    
    * fix: add missing deps
    
    * chore: make prop not required
    
    * chore: rename prop

--- packages/components/data-table-manager/src/data-table-manager.js
+++ packages/components/data-table-manager/src/data-table-manager.js
@@ -28,6 +28,7 @@ const DataTableManager = (props) => {
         onSettingsChange={props.onSettingsChange}
         columnManager={props.columnManager}
         displaySettings={props.displaySettings}
+        managerTheme={props.managerTheme}
       />
       {React.cloneElement(props.children, {
         columns,
@@ -234,6 +235,10 @@ DataTableManager.propTypes = {
    * A React node for rendering additional information within the table manager.
    */
   topBar: PropTypes.node,
+  /**
+   * Sets the background theme of the Card that contains the settings
+   */
+  managerTheme: PropTypes.oneOf(['light', 'dark']),
 };
 DataTableManager.displayName = 'DataTableManager';
 
