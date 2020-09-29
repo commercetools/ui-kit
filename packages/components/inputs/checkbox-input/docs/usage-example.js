75cbcf687a82deaee061834af99ee9875e0cf363
jonnybel <jonnybel91@gmail.com>
2020-09-29 15:46:39 +0200

    docs: update docs and add generate-readme script

---
+++ packages/components/inputs/checkbox-input/docs/usage-example.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import CheckboxInput from '@commercetools-uikit/checkbox-input';
+
+const Example = () => (
+  <div>
+    <CheckboxInput
+      value="foo-radio-value"
+      onChange={(event) => alert(event.target.value)}
+      isChecked={true}
+    >
+      A pre-checked option
+    </CheckboxInput>
+    <CheckboxInput
+      value="bar-radio-value"
+      onChange={(event) => alert(event.target.value)}
+      isDisabled={true}
+    >
+      A disabled option
+    </CheckboxInput>
+    <CheckboxInput
+      value="unknown-radio-value"
+      onChange={(event) => alert(event.target.value)}
+      aria-label={'An Option Without a Visible Label'}
+    />
+  </div>
+);
+
+export default Example;
