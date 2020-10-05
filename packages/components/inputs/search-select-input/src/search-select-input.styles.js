7f6602cf6882829af3dc1d0838c535a3276487f4
mohib0306 <mohibullah.wardak@commercetools.com>
2020-10-05 10:40:50 +0200

    refactor(search-select-input): simplify styles logic

--- packages/components/inputs/search-select-input/src/search-select-input.styles.js
+++ packages/components/inputs/search-select-input/src/search-select-input.styles.js
@@ -1,28 +1,22 @@
 /* eslint-disable import/prefer-default-export */
 import styled from '@emotion/styled';
-import { css } from '@emotion/core';
 import { customProperties as vars } from '@commercetools-uikit/design-system';
 
-const getWrapperStyles = (props) => {
-  if (props.isDisabled || props.isReadOnly) {
-    return '';
-  }
-  return css`
-    div[class$='control'] {
-      &:hover {
-        cursor: text;
-      }
-      div[class$='indicatorContainer' i] {
-        cursor: pointer;
-        svg * {
-          fill: ${vars.colorSolid};
-        }
-      }
-    }
-  `;
-};
 const SearchSelectInputWrapper = styled.div`
-  ${getWrapperStyles}
+  ${(props) =>
+    !props.isDisabled && !props.isReadOnly
+      ? `div[class$='control'] {
+        &:hover {
+          cursor: text;
+        }
+        div[class$='indicatorContainer' i] {
+          cursor: pointer;
+          svg * {
+            fill: ${vars.colorSolid};
+          }
+        }
+      }`
+      : ''}
 `;
 
 export { SearchSelectInputWrapper };
