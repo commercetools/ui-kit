43ad32392e63b891707e39d57795a45c0d84d4e8
mohib0306 <mohibullah.wardak@commercetools.com>
2020-10-05 08:10:24 +0200

    fix(search-select-input): fix isDisabled and isReadOnly states

--- packages/components/inputs/search-select-input/src/search-select-input.styles.js
+++ packages/components/inputs/search-select-input/src/search-select-input.styles.js
@@ -1,19 +1,28 @@
 /* eslint-disable import/prefer-default-export */
 import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 import { customProperties as vars } from '@commercetools-uikit/design-system';
 
-const SearchSelectInputWrapper = styled.div`
-  div[class$='control'] {
-    &:hover {
-      cursor: text;
-    }
-    div[class$='indicatorContainer' i] {
-      cursor: pointer;
-      svg * {
-        fill: ${vars.colorSolid};
+const getWrapperStyles = (props) => {
+  if (props.isDisabled || props.isReadOnly) {
+    return '';
+  }
+  return css`
+    div[class$='control'] {
+      &:hover {
+        cursor: text;
+      }
+      div[class$='indicatorContainer' i] {
+        cursor: pointer;
+        svg * {
+          fill: ${vars.colorSolid};
+        }
       }
     }
-  }
+  `;
+};
+const SearchSelectInputWrapper = styled.div`
+  ${getWrapperStyles}
 `;
 
 export { SearchSelectInputWrapper };
