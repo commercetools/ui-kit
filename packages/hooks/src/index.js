833bba5c1e9de30898ed4acbe4f05012430757af
Nicola Molinari <nicola.molinari@commercetools.de>
2020-10-28 20:01:58 +0100

    fix: add missing description field in all remaining packages

--- packages/hooks/src/index.js
+++ packages/hooks/src/index.js
@@ -1,6 +1,7 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as useToggleState } from './use-toggle-state';
 export { default as usePrevious } from './use-previous';
 export { default as useFieldId } from './use-field-id';
 export { default as useRowSelection } from './use-row-selection';
 export { default as useSorting } from './use-sorting';
+
+export { default as version } from './version';
---
+++ presets/ui-kit/README.md
@@ -0,0 +1,1 @@
+# Preset: all UI-Kit components
