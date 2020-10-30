055aa63bdf9c28813b9a2bf7bd1811e7cd47c784
jonnybel <jonnybel91@gmail.com>
2020-10-30 10:11:39 +0100

    refactor: simplify getMaxPropTokenValue

--- packages/components/constraints/src/helpers.js
+++ packages/components/constraints/src/helpers.js
@@ -1,24 +1,7 @@
 import { customProperties as vars } from '@commercetools-uikit/design-system';
 
 const getMaxPropTokenValue = (max) => {
-  if (max === 1) return vars.constraint1;
-  if (max === 2) return vars.constraint2;
-  if (max === 3) return vars.constraint3;
-  if (max === 4) return vars.constraint4;
-  if (max === 5) return vars.constraint5;
-  if (max === 6) return vars.constraint6;
-  if (max === 7) return vars.constraint7;
-  if (max === 8) return vars.constraint8;
-  if (max === 9) return vars.constraint9;
-  if (max === 10) return vars.constraint10;
-  if (max === 11) return vars.constraint11;
-  if (max === 12) return vars.constraint12;
-  if (max === 13) return vars.constraint13;
-  if (max === 14) return vars.constraint14;
-  if (max === 15) return vars.constraint15;
-  if (max === 16) return vars.constraint16;
-
-  return null;
+  return vars[`constraint${max}`] ?? null;
 };
 
 /* Useful for mapping a constraint prop to its equivalent max prop for ui-kit components
