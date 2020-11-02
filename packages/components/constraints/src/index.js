ad26dd42ac968abe4d42615a3cc3c2c18cc948f3
jonnybel <jonnybel91@gmail.com>
2020-11-02 16:30:01 +0100

    chore: remove unecessary export

--- packages/components/constraints/src/index.js
+++ packages/components/constraints/src/index.js
@@ -1,14 +1,12 @@
 // eslint-disable-next-line import/prefer-default-export
 import Horizontal from './horizontal';
 import {
-  getMaxPropEquivalent,
   parseHorizontalConstraintProp,
   getAcceptedMaxPropValues,
 } from './helpers';
 
 export default {
   Horizontal,
-  getMaxPropEquivalent,
   parseHorizontalConstraintProp,
   getAcceptedMaxPropValues,
 };
