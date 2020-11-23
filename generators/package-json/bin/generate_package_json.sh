cf249c6afdfb35959d5716af091d37317a5f4653
Nicola Molinari <nicola.molinari@commercetools.de>
2020-11-23 14:31:16 +0100

    fix: prepare tsconfig.json for TS migration (#1687)

--- generators/package-json/bin/generate_package_json.sh
+++ generators/package-json/bin/generate_package_json.sh
@@ -12,4 +12,4 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 # Execute the script using the `ts-node` CLI.
-npx ts-node "$DIR/generate-package-json.ts" "$@"
+npx ts-node --compiler-options '{"module": "commonjs"}' "$DIR/generate-package-json.ts" "$@"
