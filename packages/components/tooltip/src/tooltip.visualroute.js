77ecf1cfe0fc209a7d12267563eccf2c82cc3f17
jonnybel <jonnybel91@gmail.com>
2020-11-24 16:31:23 +0100

    chore: attempt interactive vrt

--- packages/components/tooltip/src/tooltip.visualroute.js
+++ packages/components/tooltip/src/tooltip.visualroute.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { PrimaryButton, Tooltip } from '@commercetools-frontend/ui-kit';
+import { Switch, Route } from 'react-router-dom';
 import { Suite, Spec } from '../../../../test/percy';
 
 const title = 'What kind of bear is best';
 const noop = () => {};
 
-const Body = styled.div`
-  color: red;
-  margin-top: 12px;
-`;
+// const Body = styled.div`
+//   color: red;
+//   margin-top: 12px;
+// `;
 
 const ContainerWithPadding = styled.div`
   padding-top: 50px;
@@ -17,7 +18,7 @@ const ContainerWithPadding = styled.div`
 
 export const routePath = '/tooltip';
 
-export const component = () => {
+export const DefaultRoute = () => {
   return (
     <Suite>
       <Spec label="Closed">
@@ -25,23 +26,16 @@ export const component = () => {
           <PrimaryButton onClick={noop} label="Hello" />
         </Tooltip>
       </Spec>
-      <Spec label="Open" listPropsOfNestedChild={true}>
+    </Suite>
+  );
+};
+
+export const InteractiveRoute = () => {
+  return (
+    <Suite>
+      <Spec label="Open" omitPropsList>
         <ContainerWithPadding>
-          <Tooltip title={title} isOpen={true}>
-            <PrimaryButton onClick={noop} label="Hello" />
-          </Tooltip>
-        </ContainerWithPadding>
-      </Spec>
-      <Spec
-        label="Open with custom body component"
-        listPropsOfNestedChild={true}
-      >
-        <ContainerWithPadding>
-          <Tooltip
-            title={title}
-            isOpen={true}
-            components={{ BodyComponent: Body }}
-          >
+          <Tooltip title={title} closeAfter={5000}>
             <PrimaryButton onClick={noop} label="Hello" />
           </Tooltip>
         </ContainerWithPadding>
@@ -49,5 +43,10 @@ export const component = () => {
     </Suite>
   );
 };
-/*
- */
+
+export const component = () => (
+  <Switch>
+    <Route path={`${routePath}/interactive`} component={InteractiveRoute} />
+    <Route path={routePath} component={DefaultRoute} />
+  </Switch>
+);
--- packages/components/tooltip/src/tooltip.visualspec.js
+++ packages/components/tooltip/src/tooltip.visualspec.js
@@ -1,12 +1,26 @@
 import { percySnapshot } from '@percy/puppeteer';
+import { getDocument, queries } from 'pptr-testing-library';
 
 describe('Tooltip', () => {
-  beforeAll(async () => {
+  it('Default', async () => {
     await page.goto(`${HOST}/tooltip`);
+
+    expect(page).toMatch('Closed');
+
+    await percySnapshot(page, 'Tooltip');
   });
 
-  it('Default', async () => {
-    await expect(page).toMatch('Closed');
-    await percySnapshot(page, 'Tooltip');
+  it('Interactive', async () => {
+    await page.goto(`${HOST}/tooltip/interactive`);
+
+    expect(page).toMatch('Open');
+
+    const doc = await getDocument(page);
+
+    await page.hover('button');
+
+    await queries.findByText(doc, 'What kind of bear is best');
+
+    await percySnapshot(page, 'Tooltip - hovered');
   });
 });
