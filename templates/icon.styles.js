7531d9e4ef07b1af2d2a8bde3192ea453f725b1a
Nicola Molinari <nicola.molinari@commercetools.de>
2020-11-25 11:33:12 +0100

    refactor: generate icons using svgr cli (#1699)
    * refactor: generate icons using svgr cli
    
    * refactor: rename template file
    
    * docs: changeset
    
    * refactor: use better naming, use generated folder
    
    * fix: keep svg dimensions
    
    * fix: keep svg dimensions
    
    * chore: update gitattributes path

---
+++ templates/icon.styles.js
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+import invariant from 'tiny-invariant';
+import { css, useTheme } from '@emotion/react';
+import { customProperties as vars } from '@commercetools-uikit/design-system';
+
+const iconSizes = {
+  small: 12,
+  medium: 16,
+  big: 24,
+};
+
+const getSizeStyle = (size) => {
+  switch (size) {
+    case 'scale':
+      return `
+        &:not(:root) {
+          width: 100%;
+          height: auto;
+        }
+      `;
+    case 'small':
+    case 'medium':
+    case 'big':
+      return `
+        width: ${iconSizes[size]}px;
+        height: ${iconSizes[size]}px;
+      `;
+    default:
+      return `
+        width: ${iconSizes.big}px;
+        height: ${iconSizes.big}px;
+      `;
+  }
+};
+
+const capitalize = (s) => s[0].toUpperCase() + s.slice(1);
+
+const getColor = (color, theme) => {
+  if (!color) return 'inherit';
+  const overwrittenVars = {
+    ...vars,
+    ...theme,
+  };
+
+  const iconColor = overwrittenVars[`color${capitalize(color)}`];
+
+  if (!iconColor) {
+    invariant(
+      color,
+      `ui-kit/Icon: the specified color '${color}' is not supported.`
+    );
+    return 'inherit';
+  }
+
+  return iconColor;
+};
+
+const getIconStyles = (props, theme) => css`
+  * {
+    fill: ${getColor(props.color, theme)};
+  }
+  ${getSizeStyle(props.size)};
+  flex-shrink: 0;
+`;
+
+const iconPropTypes = {
+  color: PropTypes.oneOf([
+    'solid',
+    'neutral60',
+    'surface',
+    'info',
+    'primary',
+    'primary40',
+    'warning',
+    'error',
+  ]),
+  size: PropTypes.oneOf(['small', 'medium', 'big', 'scale']),
+};
