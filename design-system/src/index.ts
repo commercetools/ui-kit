c4f6686113fa8833a601c08eb003d85f7479200c
Nicola Molinari <nicola.molinari@commercetools.de>
2020-12-22 15:30:06 +0100

    fix(design-system): correctly bundle and export type declarations

--- design-system/src/index.ts
+++ design-system/src/index.ts
@@ -1,3 +1,5 @@
+import customProperties from '../materials/custom-properties';
+
 export { default as customProperties } from '../materials/custom-properties';
 export { default as designTokens } from '../materials/design-tokens';
 
