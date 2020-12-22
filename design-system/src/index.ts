2784cabea3d239f9cde03bee9c4b55ffb21ed8f1
Nicola Molinari <nicola.molinari@commercetools.de>
2020-12-22 15:42:06 +0100

    chore: remove wrong import

--- design-system/src/index.ts
+++ design-system/src/index.ts
@@ -1,5 +1,3 @@
-import customProperties from '../materials/custom-properties';
-
 export { default as customProperties } from '../materials/custom-properties';
 export { default as designTokens } from '../materials/design-tokens';
 
