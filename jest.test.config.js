692cabfeb5a467244e65e1e32733595b21a0002b
Nicola Molinari <nicola.molinari@commercetools.de>
2020-12-30 20:18:40 +0100

    fix: jest config

--- jest.test.config.js
+++ jest.test.config.js
@@ -8,7 +8,7 @@ module.exports = {
       NODE_ENV: 'test',
     },
   },
-  moduleDirectories: ['src', 'packages', 'node_modules'],
+  moduleDirectories: ['packages', 'node_modules'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/transform-file.js',
@@ -20,10 +20,23 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/scripts/setup-test-framework.js'],
   testEnvironment: 'jest-environment-jsdom-sixteen',
   testURL: 'https://mc.commercetools.com/',
-  testPathIgnorePatterns: ['node_modules'],
-  testRegex: '\\.spec\\.js$',
+  testPathIgnorePatterns: ['generators', '/node_modules/'],
+  testRegex: '\\.spec\\.[j|t]sx?$',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
   transform: {
-    '^.+\\.[t|j]sx?$': 'babel-jest',
+    '^.+\\.js?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
   },
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-master'],
+  // Without this option, somehow CI fails to run the tests with the following error:
+  //   TypeError: Unable to require `.d.ts` file.
+  //   This is usually the result of a faulty configuration or import. Make sure there is a `.js`, `.json` or another executable extension available alongside `core.ts`.
+  // Fix is based on this comment:
+  // - https://github.com/kulshekhar/ts-jest/issues/805#issuecomment-456055213
+  // - https://github.com/kulshekhar/ts-jest/blob/master/docs/user/config/isolatedModules.md
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
 };
--- jest.ts-test.config.js
+++ jest.ts-test.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
+  displayName: 'test-node',
   testEnvironment: 'node',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['generators', 'node_modules'],
+  testPathIgnorePatterns: ['packages', '/node_modules/'],
 };
