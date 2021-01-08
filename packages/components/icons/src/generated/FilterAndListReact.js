68b4bdd92946f80fa5583163065a2608097078e7
Nicholas Bryant <nicholas.bryant@commercetools.com>
2021-01-08 10:25:01 -0500

    refactor(icons): updated icon size
    relates icons

--- packages/components/icons/src/generated/FilterAndListReact.js
+++ packages/components/icons/src/generated/FilterAndListReact.js
@@ -82,30 +82,24 @@ const iconPropTypes = {
 
 const SvgFilterAndList = (props) => (
   <svg
-    width={16}
-    height={16}
-    viewBox="0 0 16 16"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <g
-      id="filter-and-list_react_svg__Symbols"
+      id="filter-and-list_react_svg__Component-/-icon-/-24px-/-filter_and_-list"
       stroke="none"
       strokeWidth={1}
       fill="none"
       fillRule="evenodd"
-      fillOpacity={0.85}
     >
-      <g
-        id="filter-and-list_react_svg__Component-/-icon-/-16px-/fliter-&amp;-lsit"
-        fill="#000"
-        fillRule="nonzero"
-      >
-        <path
-          d="M13.5 12a.5.5 0 110 1h-3a.5.5 0 110-1h3zM12.017 2c.234 0 .391.144.457.339a.747.747 0 01-.057.536l-.064.098-3.67 4.401v6.034c0 .25-.096.425-.29.526a.49.49 0 01-.125.04l-.06.005a.396.396 0 01-.28-.109l-.055-.061-1.906-2.285a.595.595 0 01-.132-.29l-.009-.112V7.374L2.158 2.973c-.154-.173-.19-.458-.132-.645.08-.198.218-.298.382-.322L2.492 2h9.525zm-.989.999L3.48 2.997l3.346 4.015-.003 3.977.86 1.032v-5.01L11.029 3zM13.5 9a.5.5 0 110 1h-3a.5.5 0 110-1h3zm0-3a.5.5 0 110 1h-3a.5.5 0 110-1h3z"
-          id="filter-and-list_react_svg__Shape"
-        />
-      </g>
+      <path
+        d="M18.264 3c.356 0 .596.217.696.509.07.23.053.546-.086.806l-.098.147-5.591 6.613v9.066c0 .376-.147.64-.442.791a.686.686 0 01-.284.068.608.608 0 01-.425-.164l-.085-.092-2.903-3.433a.889.889 0 01-.202-.435l-.014-.169v-5.632L3.24 4.462c-.234-.26-.288-.688-.2-.97.12-.296.332-.446.582-.483L3.75 3h14.514zM4 4l5.747 6.928-.002 5.955.005.099L12.253 20l.001-9.072 5.699-6.86.047-.066L4 4zm16.5 14.98a.5.5 0 110 1h-5a.5.5 0 010-1h5zm0-4a.5.5 0 110 1h-5a.5.5 0 010-1h5zm0-4a.5.5 0 110 1h-5a.5.5 0 010-1h5z"
+        id="filter-and-list_react_svg__Combined-Shape"
+        fill="#1A1A1A"
+      />
     </g>
   </svg>
 );
