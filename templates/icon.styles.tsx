4e3570b8ac245ff5fbaadff2da0bbba8a5609e7c
Nicola Molinari <nicola.molinari@commercetools.de>
2021-01-12 17:19:50 +0100

    refactor(icons): migrate to typescript

---
+++ templates/icon.styles.tsx
@@ -0,0 +1,84 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { Theme } from '@emotion/react';
+
+// @ts-ignore
+import React, { FC } from 'react';
+import invariant from 'tiny-invariant';
+// @ts-ignore
+import { css, useTheme } from '@emotion/react';
+import { customProperties as vars } from '@commercetools-uikit/design-system';
+
+type Props = {
+  color:
+    | 'solid'
+    | 'neutral60'
+    | 'surface'
+    | 'info'
+    | 'primary'
+    | 'primary40'
+    | 'warning'
+    | 'error';
+  size: 'small' | 'medium' | 'big' | 'scale';
+};
+
+const iconSizes = {
+  small: 12,
+  medium: 16,
+  big: 24,
+} as const;
+
+const getSizeStyle = (size: Props['size']) => {
+  switch (size) {
+    case 'scale':
+      return `
+        &:not(:root) {
+          width: 100%;
+          height: auto;
+        }
+      `;
+    case 'small':
+    case 'medium':
+    case 'big':
+      return `
+        width: ${iconSizes[size]}px;
+        height: ${iconSizes[size]}px;
+      `;
+    default:
+      return `
+        width: ${iconSizes.big}px;
+        height: ${iconSizes.big}px;
+      `;
+  }
+};
+
+const capitalize = (value: string) => value[0].toUpperCase() + value.slice(1);
+
+const getColor = (color: Props['color'], theme: Theme) => {
+  if (!color) return 'inherit';
+  const overwrittenVars = {
+    ...vars,
+    ...theme,
+  };
+
+  // @ts-expect-error
+  const iconColor = overwrittenVars[`color${capitalize(color)}`];
+
+  if (!iconColor) {
+    invariant(
+      color,
+      `ui-kit/Icon: the specified color '${color}' is not supported.`
+    );
+    return 'inherit';
+  }
+
+  return iconColor;
+};
+
+// @ts-ignore
+const getIconStyles = (props: Props, theme: Theme) => css`
+  * {
+    fill: ${getColor(props.color, theme)};
+  }
+  ${getSizeStyle(props.size)};
+  flex-shrink: 0;
+`;
