1a6a0fbc6ab47e5f33db57222a821f232aa1b4f3
Adnan Asani <adnasa@users.noreply.github.com>
2021-01-13 13:42:33 +0100

    refactor(add-static-fields): prefix types

--- packages/utils/src/add-static-fields.ts
+++ packages/utils/src/add-static-fields.ts
@@ -1,15 +1,15 @@
 import type { FC } from 'react';
 
-type StaticFields = Record<string, unknown>;
+type TStaticFields = Record<string, unknown>;
 
 export default function addStaticFields<
-  Props extends {},
-  Fields extends StaticFields
->(Component: FC<Props>, fields: Fields): FC<Props> & Fields {
+  TProps extends {},
+  TFields extends TStaticFields
+>(Component: FC<TProps>, fields: TFields): FC<TProps> & TFields {
   Object.entries(fields).forEach(([key, value]) => {
     // eslint-disable-next-line no-param-reassign
     // @ts-expect-error
     Component[key] = value;
   });
-  return Component as FC<Props> & Fields;
+  return Component as FC<TProps> & TFields;
 }
