b4fbbf306db5819f63a48b6e7f10667f9b217a1f
Nicola Molinari <nicola.molinari@commercetools.de>
2021-01-21 20:03:51 +0100

    fix(spacings): be explicit in typing Spacings object

---
+++ .changeset/tidy-mails-reflect.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-uikit/spacings': patch
+---
+
+Be explicit in typing `Spacings` object as otherwise inferred type declarations are not generated correctly.
--- presets/spacings/src/index.ts
+++ presets/spacings/src/index.ts
@@ -3,4 +3,12 @@ import Inset from '@commercetools-uikit/spacings-inset';
 import InsetSquish from '@commercetools-uikit/spacings-inset-squish';
 import Stack from '@commercetools-uikit/spacings-stack';
 
-export default { Inline, Inset, InsetSquish, Stack };
+type TSpacings = {
+  Inline: typeof Inline;
+  Inset: typeof Inset;
+  InsetSquish: typeof InsetSquish;
+  Stack: typeof Stack;
+};
+const Spacings: TSpacings = { Inline, Inset, InsetSquish, Stack };
+
+export default Spacings;
