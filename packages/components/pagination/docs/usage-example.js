30526872f163a1afe01821f65a1c0460b17056e0
Nicola Molinari <nicola.molinari@commercetools.de>
2021-01-28 10:35:19 +0100

    fix(pagination): example

--- packages/components/pagination/docs/usage-example.js
+++ packages/components/pagination/docs/usage-example.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Pagination } from '@commercetools-uikit/pagination';
 
-const Example = ({ items }) => (
-  <Pagination
-    totalItems={items.length}
-    currentPage={1}
-    onPageChange={() => {}}
-    onPageSizeChange={() => {}}
-  />
-);
+const Example = () => {
+  const items = [{ id: '1' }, { id: '2' }];
+  return (
+    <Pagination
+      totalItems={items.length}
+      currentPage={1}
+      onPageChange={() => {}}
+      onPageSizeChange={() => {}}
+    />
+  );
+};
 
 export default Example;
