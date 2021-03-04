5b2604eb315f1c8b6a4c63e32ccec9af625751fb
tdeekens <tobias.deekens@commercetools.com>
2021-03-04 23:43:35 +0100

    fix: try to install missing dep

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -7,10 +7,11 @@
 module.exports = {
   launch: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
   },
   server: {
     command: 'yarn visual-testing-app:serve',
     port: 3001,
-    launchTimeout: 20000,
+    launchTimeout: 10000,
   },
 };
