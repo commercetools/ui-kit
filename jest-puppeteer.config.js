2940fd8adb70015be3605f0460a74b633f903c23
Tobias Deekens <tobias.deekens@commercetools.com>
2021-03-05 09:12:43 +0100

    fix: to adjust executable path via percy v2 (#1813)
    * fix: to remove executable path via percy v2
    
    * fix: try to install missing dep
    
    * fix: install it all
    
    * fix: try to pass vars
    
    * fix: try without container

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,15 +4,15 @@
  *
  *  Ref: https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
  */
+
 module.exports = {
   launch: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: 'google-chrome-stable',
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
   },
   server: {
     command: 'yarn visual-testing-app:serve',
-    launchTimeout: 30000,
     port: 3001,
-    launchTimeout: 20000,
+    launchTimeout: 10000,
   },
 };
