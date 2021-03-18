4d05beefdebc5b5c0212f7b2f4b4ef68c10530a2
Adnan Asani <adnasa@users.noreply.github.com>
2021-03-18 14:28:28 +0100

    refactor(constraints): remove unused helpers

--- packages/components/constraints/src/index.ts
+++ packages/components/constraints/src/index.ts
@@ -1,14 +1,8 @@
 // eslint-disable-next-line import/prefer-default-export
 import Horizontal from './horizontal';
-import {
-  parseHorizontalConstraintProp,
-  getAcceptedMaxPropValues,
-} from './helpers';
 
 export default {
   Horizontal,
-  parseHorizontalConstraintProp,
-  getAcceptedMaxPropValues,
 };
 
 export { default as version } from './version';
