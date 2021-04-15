89f254b90be4abeb637e69b5a8abfd93b45318e7
Adnan Asani <adnasa@users.noreply.github.com>
2021-04-15 15:40:25 +0200

    refactor(secondary-icon-button): convert to TypeSript, use generate script for readme

---
+++ packages/components/buttons/secondary-icon-button/docs/usage-example.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SecondaryIconButton from '@commercetools-uikit/secondary-icon-button';
+import { InformationIcon } from '@commercetools-uikit/icons';
+
+const Example = () => (
+  <SecondaryIconButton
+    icon={<InformationIcon />}
+    label="A label text"
+    onClick={() => alert('Button clicked')}
+  />
+);
+
+export default Example;
---
+++ packages/components/buttons/secondary-icon-button/src/secondary-icon-button.tsx
@@ -0,0 +1,83 @@
+import React, { ElementType, MouseEvent, KeyboardEvent } from 'react';
+import { useTheme } from '@emotion/react';
+import omit from 'lodash/omit';
+import { warning, filterInvalidAttributes } from '@commercetools-uikit/utils';
+import AccessibleButton from '@commercetools-uikit/accessible-button';
+import { getBaseStyles } from './secondary-icon-button.styles';
+
+const propsToOmit = ['type'];
+
+export type TSecondaryButtonProps = {
+  /**
+   * an `ElementType`. <br />
+   * You may pass in a string like "a" to have the button render as an anchor tag instead.
+   */
+  as?: string | ElementType;
+  /**
+   * Used as the HTML type attribute.
+   */
+  type?: 'submit' | 'reset' | 'button';
+  /**
+   * An <Icon /> component.
+   */
+  icon?: React.ReactElement;
+  /**
+   * Sets the color of the icon
+   */
+  color?: 'solid' | 'primary'; // used in `getBaseStyles`
+  /**
+   * Should describe what the button does, for accessibility purposes (screen-reader users)
+   */
+  label: string;
+  /**
+   * Tells when the button should present a disabled state
+   */
+  isDisabled?: boolean;
+  /**
+   * Handler when the button is clicked.
+   * <br />
+   * This is required if `as` is not defined.
+   */
+  onClick?: (
+    event: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
+  ) => void;
+};
+
+const SecondaryIconButton = (props: TSecondaryButtonProps) => {
+  if (!Boolean(props.as)) {
+    warning(
+      typeof props.onClick === 'function',
+      'SecondaryIconButton: "onClick" is required when "as" is not defined.'
+    );
+  }
+  const buttonAttributes = {
+    ...filterInvalidAttributes(omit(props, propsToOmit)),
+    'data-track-component': 'SecondaryIconButton',
+    // if there is a divergence between `isDisabled` and `disabled`,
+    // we fall back to `isDisabled`
+    disabled: props.isDisabled,
+  };
+  const theme = useTheme();
+  return (
+    <AccessibleButton
+      as={props.as}
+      type={props.type}
+      buttonAttributes={buttonAttributes}
+      label={props.label}
+      onClick={props.onClick}
+      isDisabled={props.isDisabled}
+      css={getBaseStyles(props, theme)}
+    >
+      {props.icon}
+    </AccessibleButton>
+  );
+};
+
+SecondaryIconButton.displayName = 'SecondaryIconButton';
+SecondaryIconButton.defaultProps = {
+  color: 'solid',
+  type: 'button',
+  isDisabled: false,
+};
+
+export default SecondaryIconButton;
