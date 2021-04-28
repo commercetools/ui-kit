043e9d2f36a06b01fe2ce6172a0708de2f70d35d
Adnan Asani <adnasa@users.noreply.github.com>
2021-04-28 10:50:31 +0200

    refactor(hook): migrate to TypeScript (#1872)
    * refactor(hook): migrate to TypeScript
    
    * chore: changeset
    
    * fix(use-toggle-state): do not assign value as type

---
+++ packages/hooks/src/use-toggle-state/use-toggle-state.ts
@@ -0,0 +1,18 @@
+import { useCallback, useState } from 'react';
+
+const useToggleState = (defaultValue?: boolean) => {
+  const initialValue = typeof defaultValue === 'boolean' ? defaultValue : true;
+
+  const [isToggled, setIsToggled] = useState(initialValue);
+  const toggle = useCallback<(forceIsToggled?: boolean) => void>(
+    (forceIsToggled?: boolean) => {
+      setIsToggled(
+        typeof forceIsToggled === 'boolean' ? forceIsToggled : !isToggled
+      );
+    },
+    [isToggled, setIsToggled]
+  );
+  return [isToggled, toggle];
+};
+
+export default useToggleState;
