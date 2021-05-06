e4d31feb8c709839ac9ed03fa03569bb1295f4ad
Adnan Asani <adnasa@users.noreply.github.com>
2021-05-06 15:20:16 +0200

    refactor(inputs): export types through entry-point

--- packages/components/inputs/number-input/src/index.ts
+++ packages/components/inputs/number-input/src/index.ts
@@ -1,3 +1,5 @@
-export { default } from './number-input';
+import type { TNumberInputProps as NumberInputProps } from './number-input';
 
+export { default } from './number-input';
 export { default as version } from './version';
+export type TNumberInputProps = NumberInputProps;
