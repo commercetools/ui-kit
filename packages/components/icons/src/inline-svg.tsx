97f1e784aa2283093efd2f52fd0351bb10713326
Nicola Molinari <nicola.molinari@commercetools.com>
2021-07-09 13:57:03 +0200

    refactor(icons): memoize sanitizing svg data

--- packages/components/icons/src/inline-svg.tsx
+++ packages/components/icons/src/inline-svg.tsx
@@ -1,6 +1,6 @@
 import type { Props } from './templates/icon.styles';
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import DOMPurify from 'dompurify';
 import SVG from 'react-inlinesvg';
 import { useTheme } from '@emotion/react';
@@ -12,13 +12,17 @@ type InlineSvgProps = Props & {
 
 const InlineSvg = (props: InlineSvgProps) => {
   const theme = useTheme();
-  const sanitized = DOMPurify.sanitize(props.data, {
-    USE_PROFILES: { svg: true },
-    FORBID_ATTR: [
-      // To avoid injection by using `style="filter:url(\"data:image/svg+xml,<svg`
-      'style',
-    ],
-  });
+  const sanitized = useMemo(
+    () =>
+      DOMPurify.sanitize(props.data, {
+        USE_PROFILES: { svg: true },
+        FORBID_ATTR: [
+          // To avoid injection by using `style="filter:url(\"data:image/svg+xml,<svg`
+          'style',
+        ],
+      }),
+    [props.data]
+  );
   return <SVG src={sanitized} css={getIconStyles(props, theme)} />;
 };
 
