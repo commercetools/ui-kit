da3ff03db20bd3a9126ffd66e8b4c71c9dcbf5a4
Nicola Molinari <emmenko@gmail.com>
2021-08-04 22:50:50 +0200

    Revert "fix(deps): update dependency unified to v10 (#1969)"
    This reverts commit e88def0d56252b33be4484f51657151502ebb1d6.

--- generators/readme/test/generate-readme.spec.ts
+++ generators/readme/test/generate-readme.spec.ts
@@ -1,7 +1,7 @@
 import os from 'os';
 import path from 'path';
 import shelljs from 'shelljs';
-import { VFile } from 'vfile';
+import vfile from 'vfile';
 import { transformDocument } from '../src';
 
 const tmpFolder = os.tmpdir();
@@ -20,7 +20,7 @@ beforeAll(() => {
 describe('generate README (for JS file)', () => {
   it('should generate README based on package info', async () => {
     const file = await transformDocument(
-      new VFile(),
+      vfile(),
       path.join(testPackages, 'avenger')
     );
     const content = file.toString();
@@ -100,7 +100,7 @@ describe('generate README (for JS file)', () => {
 describe('generate README (for TS file)', () => {
   it('should generate README based on package info', async () => {
     const file = await transformDocument(
-      new VFile(),
+      vfile(),
       path.join(testPackages, 'justice-league')
     );
     const content = file.toString();
