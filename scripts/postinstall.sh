c5dd40385294ca4e59a1b81145b1c881d60362da
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-25 11:43:20 +0100

    Cleanup and missing deps (#2028)
    * chore: update package.jsons due to yarn
    
    * fix: no need for private: false
    
    * chore(docs): declare missing deps
    
    * chore(docs): declare missing deps
    
    * chore(docs): try to provide manual symlinks for all packages, in order for webpack to resolve them
    
    * chore: remove script
    
    * test: adjust snapshot

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,6 +6,9 @@ echo "Preparing development setup."
 yarn husky install
 yarn manypkg check
 yarn preconstruct dev
+# We need to run prettier to avoid unnecessary formatting changes to package.json (due to Yarn install).
+yarn prettier --write --parser json '**/package.json' &>/dev/null
+
 yarn patch-package
 
 if [ -n "$SKIP_INSTALL_STORYBOOK" ]; then
