6cddddf9ff75db411895c87af08be029b8261079
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2021-12-07 15:50:14 +0100

    refactor(react-select): add PR link

--- packages/components/inputs/select-utils/src/create-select-styles.js
+++ packages/components/inputs/select-utils/src/create-select-styles.js
@@ -238,6 +238,7 @@ const valueContainerStyles = (props, theme) => (base) => {
     display:
       // Display property should be grid when isMulti and has no value so the Placeholder component is positioned correctly with the Input
       // Display property should be Flex when there is an iconLeft, also when the input has some values when isMulti.
+      // See PR from react select for more insight https://github.com/JedWatson/react-select/pull/4833
       (props.iconLeft && !props.isMulti) || (props.isMulti && props.hasValue)
         ? 'flex'
         : 'grid',
