19db4cf4c83219fea01b0a2a6ce1021e50499130
Douglas Egiemeh <douglasegiemeh@Douglass-MacBook-Pro-2.local>
2022-01-07 14:04:59 +0100

    refactor(multiline-text-field): migrate to typescript

---
+++ packages/components/fields/multiline-text-field/src/multiline-text-field.tsx
@@ -0,0 +1,222 @@
+import {
+  ChangeEventHandler,
+  Component,
+  FocusEventHandler,
+  ReactElement,
+  ReactNode,
+} from 'react';
+import {
+  createSequentialId,
+  filterDataAttributes,
+  getFieldId,
+} from '@commercetools-uikit/utils';
+import Constraints from '@commercetools-uikit/constraints';
+import Spacings from '@commercetools-uikit/spacings';
+import FieldLabel from '@commercetools-uikit/field-label';
+import MultilineTextInput from '@commercetools-uikit/multiline-text-input';
+import FieldErrors from '@commercetools-uikit/field-errors';
+
+const sequentialId = createSequentialId('multiline-text-field-');
+
+const hasErrors = (errors: { [s: string]: unknown } | ArrayLike<unknown>) =>
+  errors && Object.values(errors).some(Boolean);
+
+type TErrorRenderer = (key: string, error?: boolean) => ReactNode;
+
+export type TMultiTextFieldProps = {
+  // FieldLabel
+  /**
+   * Used as HTML id property. An id is auto-generated when it is not specified.
+   */
+  id: string;
+  /**
+   * Horizontal size limit of the input fields.
+   */
+  horizontalConstraint?:
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | 13
+    | 14
+    | 15
+    | 16
+    | 'scale'
+    | 'auto';
+  /**
+   * A map of errors. Error messages for known errors are rendered automatically.
+   * <br />
+   * Unknown errors will be forwarded to `renderError`
+   */
+  errors: {
+    missing: boolean;
+  };
+  /**
+   * Called with custom errors. This function can return a message which will be wrapped in an ErrorMessage. It can also return null to show no error.
+   * <br />
+   * Signature: `(key, error) => React.node`
+   */
+  renderError?: TErrorRenderer;
+  /**
+   * Indicates if the value is required. Shows an the "required asterisk" if so.
+   */
+  isRequired: boolean;
+  /**
+   * Indicates whether the field was touched. Errors will only be shown when the field was touched.
+   */
+  touched: boolean;
+
+  // TextInput
+  /**
+   * Used as HTML `autocomplete` property
+   */
+  autoComplete: string;
+  /**
+   * Used as HTML name of the input component. property
+   */
+  name: string;
+  /**
+   * Value of the input component.
+   */
+  value: string;
+  /**
+   * Called with an event containing the new value. Required when input is not read only. Parent should pass it back as value.
+   * <br />
+   * Signature: `(event) => void`
+   */
+  onChange?: ChangeEventHandler;
+  /**
+   * Called when input is blurred
+   * <br />
+   * Signature: `(event) => void`
+   */
+  onBlur?: FocusEventHandler;
+  /**
+   * Called when input is focused
+   * <br />
+   * Signature: `(event) => void`
+   */
+  onFocus?: FocusEventHandler;
+  /**
+   * Focus the input on initial render
+   */
+  isAutofocussed?: boolean;
+  /**
+   * Expands multiline text input initially
+   */
+  defaultExpandMultilineText: boolean;
+  /**
+   * Indicates that the input cannot be modified (e.g not authorized, or changes currently saving).
+   */
+  isDisabled?: boolean;
+  /**
+   * Indicates that the field is displaying read-only content
+   */
+  isReadOnly?: boolean;
+  /**
+   * Placeholder text for the input
+   */
+  placeholder?: string;
+
+  // LabelField
+  /**
+   * Title of the label
+   */
+  title?: string;
+  /**
+   * 	Hint for the label. Provides a supplementary but important information regarding the behaviour of the input (e.g warn about uniqueness of a field, when it can only be set once), whereas `description` can describe it in more depth. Can also receive a `hintIcon`.
+   */
+  hint?: string;
+  /**
+   * Provides a description for the title.
+   */
+  description?: string;
+  /**
+   * Function called when info button is pressed.
+   * <br />
+   * Info button will only be visible when this prop is passed.
+   * <br />
+   * Signature: `(event) => void`
+   */
+  onInfoButtonClick: () => void;
+  /**
+   * Icon to be displayed beside the hint text.
+   * <br />
+   * Will only get rendered when `hint` is passed as well.
+   */
+  hintIcon?: ReactElement;
+  /**
+   * Badge to be displayed beside the label.
+   * <br />
+   * Might be used to display additional information about the content of the field (E.g verified email)
+   */
+  badge: () => void;
+};
+
+class MultilineTextField extends Component<TMultiTextFieldProps> {
+  static displayName = 'MultilineTextField';
+
+  static defaultProps = {
+    horizontalConstraint: 'scale',
+  };
+
+  state = {
+    // We generate an id in case no id is provided by the parent to attach the
+    // label to the input component.
+    id: this.props.id,
+  };
+
+  static getDerivedStateFromProps = (
+    props: TMultiTextFieldProps,
+    state: { id?: string | undefined }
+  ) => ({
+    id: getFieldId(props, state, sequentialId),
+  });
+
+  render() {
+    const hasError = this.props.touched && hasErrors(this.props.errors);
+    return (
+      <Constraints.Horizontal max={this.props.horizontalConstraint}>
+        <Spacings.Stack scale="xs">
+          <FieldLabel
+            title={this.props.title}
+            hint={this.props.hint}
+            description={this.props.description}
+            onInfoButtonClick={this.props.onInfoButtonClick}
+            hintIcon={this.props.hintIcon}
+            badge={this.props.badge}
+            hasRequiredIndicator={this.props.isRequired}
+            htmlFor={this.state.id}
+          />
+          <MultilineTextInput
+            id={this.state.id}
+            name={this.props.name}
+            autoComplete={this.props.autoComplete}
+            value={this.props.value}
+            onChange={this.props.onChange}
+            onBlur={this.props.onBlur}
+            onFocus={this.props.onFocus}
+            isAutofocussed={this.props.isAutofocussed}
+            defaultExpandMultilineText={this.props.defaultExpandMultilineText}
+            isDisabled={this.props.isDisabled}
+            isReadOnly={this.props.isReadOnly}
+            hasError={hasError}
+            placeholder={this.props.placeholder}
+            horizontalConstraint="scale"
+            {...filterDataAttributes(this.props)}
+          />
+          <FieldErrors
+            errors={this.props.errors}
+            isVisible={hasError}
+            renderError={this.props.renderError}
+          />
+        </Spacings.Stack>
+      </Constraints.Horizontal>
+    );
+  }
+}
+
+export default MultilineTextField;
