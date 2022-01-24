c5ef7f9a17a0756f0e17e05d090d7e860b8171ba
Douglas Egiemeh <douglasegiemeh@gmail.com>
2022-01-24 15:57:02 +0100

    refactor(rich-text-utils): update file name

--- packages/components/inputs/rich-text-utils/src/index.ts
+++ packages/components/inputs/rich-text-utils/src/index.ts
@@ -2,7 +2,6 @@ export * as localized from './localized';
 export { default as html } from './html';
 export { default as isEmpty } from './is-empty';
 export { default as richTextPlugins } from './rich-text-plugins';
-// eslint-disable-next-line import/no-unresolved
 export { HiddenInput, RichTextBody } from './rich-text-body';
 
 export { default as version } from './version';
