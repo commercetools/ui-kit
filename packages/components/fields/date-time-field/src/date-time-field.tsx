f85b99d35f67e4429736ffab0442271b696e00b6
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2022-02-16 18:24:44 +0100

    refactor(date-time-field): migrate to typescript

---
+++ packages/components/fields/date-time-field/src/date-time-field.tsx
@@ -0,0 +1,238 @@
+import {
+  Component,
+  FocusEventHandler,
+  isValidElement,
+  ReactNode,
+  MouseEvent,
+  KeyboardEvent,
+  ReactElement,
+} from 'react';
+import {
+  filterDataAttributes,
+  getFieldId,
+  createSequentialId,
+  warning,
+} from '@commercetools-uikit/utils';
+import Constraints from '@commercetools-uikit/constraints';
+import Spacings from '@commercetools-uikit/spacings';
+import FieldLabel from '@commercetools-uikit/field-label';
+import DateTimeInput from '@commercetools-uikit/date-time-input';
+import FieldErrors from '@commercetools-uikit/field-errors';
+
+type TErrorRenderer = (key: string, error?: boolean) => ReactNode;
+
+type TFieldErrors = Record<string, boolean>;
+
+const sequentialId = createSequentialId('date-time-field-');
+
+const hasErrors = (errors?: TFieldErrors) =>
+  errors && Object.values(errors).some(Boolean);
+
+type TEvent = {
+  target: {
+    id?: string;
+    name?: string;
+    value?: string;
+  };
+};
+
+type TDateTimeFieldProps = {
+  // DateTimeField
+  /**
+   * Used as HTML id property. An id is auto-generated when it is not specified.
+   */
+  id?: string;
+  /**
+   * Horizontal size limit of the input fields.
+   */
+  horizontalConstraint?:
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | 13
+    | 14
+    | 15
+    | 16
+    | 'scale'
+    | 'auto';
+  /**
+   * A map of errors. Error messages for known errors are rendered automatically.
+   * <br />
+   * Unknown errors will be forwarded to `renderError`
+   */
+  errors?: TFieldErrors;
+  /**
+   * Called with custom errors. This function can return a message which will be wrapped in an ErrorMessage. It can also return null to show no error.
+   */
+  renderError?: TErrorRenderer;
+  /**
+   * Indicates if the value is required. Shows an the "required asterisk" if so.
+   */
+  isRequired?: boolean;
+  /**
+   * Indicates whether the field was touched. Errors will only be shown when the field was touched.
+   */
+  touched?: boolean;
+
+  // DateTimeInput
+  /**
+   * Used as HTML name of the input component.
+   */
+  name?: string;
+  /**
+   * Value of the input
+   */
+  value: string;
+  /**
+   * Called with an event holding the new value.
+   * <br/>
+   * Required when input is not read only. Parent should pass it back as `value`-
+   */
+  onChange: (event: TEvent) => void;
+  /**
+   * Called when input is blurred
+   */
+  onBlur?: (event: TEvent) => void;
+  /**
+   * Called when input is focused
+   */
+  onFocus?: FocusEventHandler;
+  /**
+   * Indicates that the input cannot be modified (e.g not authorized, or changes currently saving).
+   */
+  isDisabled?: boolean;
+  /**
+   * Indicates that the field is displaying read-only content
+   */
+  isReadOnly?: boolean;
+  /**
+   * Placeholder text for the input
+   */
+  placeholder?: string;
+  /**
+   * Specifies the time zone in which the calendar and selected values are shown. It also influences how entered dates and times are parsed.
+   * <br />
+   * See the `DateTimeInput` docs for more information.
+   */
+  timeZone: string;
+
+  // LabelField
+  /**
+   * Title of the label
+   */
+  title: ReactNode;
+  /**
+   * Hint for the label. Provides a supplementary but important information regarding the behaviour of the input (e.g warn about uniqueness of a field, when it can only be set once), whereas `description` can describe it in more depth. Can also receive a `hintIcon`.
+   */
+  hint?: ReactNode;
+  /**
+   * Provides a description for the title.
+   */
+  description?: ReactNode;
+  /**
+   * Function called when info button is pressed.
+   * <br />
+   * Info button will only be visible when this prop is passed.
+   */
+  onInfoButtonClick?: (
+    event: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
+  ) => void;
+  /**
+   * Icon to be displayed beside the hint text.
+   * <br />
+   * Will only get rendered when `hint` is passed as well.
+   */
+  hintIcon?: ReactElement;
+  /**
+   * Badge to be displayed beside the label.
+   * <br />
+   * Might be used to display additional information about the content of the field (E.g verified email)
+   */
+  badge?: ReactNode;
+};
+
+type TDateTimeFieldState = Pick<TDateTimeFieldProps, 'id'>;
+
+class DateTimeField extends Component<
+  TDateTimeFieldProps,
+  TDateTimeFieldState
+> {
+  static displayName = 'DateTimeField';
+
+  static defaultProps = {
+    horizontalConstraint: 'scale',
+  };
+
+  state = {
+    // We generate an id in case no id is provided by the parent to attach the
+    // label to the input component.
+    id: this.props.id,
+  };
+
+  static getDerivedStateFromProps = (
+    props: TDateTimeFieldProps,
+    state: TDateTimeFieldState
+  ) => ({
+    id: getFieldId(props, state, sequentialId),
+  });
+
+  render() {
+    if (!this.props.isReadOnly) {
+      warning(
+        typeof this.props.onChange === 'function',
+        'DateTimeField: `onChange` is required when it is not read only.'
+      );
+    }
+
+    if (this.props.hintIcon) {
+      warning(
+        typeof this.props.hint === 'string' || isValidElement(this.props.hint),
+        'DateTimeField: `hint` is required to be string or ReactNode if hintIcon is present'
+      );
+    }
+
+    const hasError = this.props.touched && hasErrors(this.props.errors);
+    return (
+      <Constraints.Horizontal max={this.props.horizontalConstraint}>
+        <Spacings.Stack scale="xs">
+          <FieldLabel
+            title={this.props.title}
+            hint={this.props.hint}
+            description={this.props.description}
+            onInfoButtonClick={this.props.onInfoButtonClick}
+            hintIcon={this.props.hintIcon}
+            badge={this.props.badge}
+            hasRequiredIndicator={this.props.isRequired}
+            htmlFor={this.state.id}
+          />
+          <DateTimeInput
+            id={this.state.id}
+            name={this.props.name}
+            timeZone={this.props.timeZone}
+            value={this.props.value}
+            onChange={this.props.onChange}
+            onFocus={this.props.onFocus}
+            onBlur={this.props.onBlur}
+            isDisabled={this.props.isDisabled}
+            isReadOnly={this.props.isReadOnly}
+            hasError={hasError}
+            placeholder={this.props.placeholder}
+            horizontalConstraint="scale"
+            {...filterDataAttributes(this.props)}
+          />
+          <FieldErrors
+            errors={this.props.errors}
+            isVisible={hasError}
+            renderError={this.props.renderError}
+          />
+        </Spacings.Stack>
+      </Constraints.Horizontal>
+    );
+  }
+}
+
+export default DateTimeField;
