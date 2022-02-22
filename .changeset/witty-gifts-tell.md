e7523deabcf733815d6bf8bcfafdc35129c870c2
Douglas Egiemeh <douglasegiemeh@gmail.com>
2022-02-22 13:27:33 +0100

    Migrate rich text utils to TS (#2072)
    * refactor(rich-text-utils): migrate to typescript
    
    * refactor(rich-text-utils): improve type definition
    
    * refactor(rich-text-utils): define getparent property for editor prop
    
    * refactor(rich-text-utils): add missing dependencies and improve types
    
    * refactor(rich-text-utils): update editor type
    
    * refactor(rich-text-utils): update file name
    
    * refactor(rich-text-utils): improve types for dropdown component
    
    * refactor(rich-text-utils): remove ts-nocheck
    
    * refactor(rich-text-utils): fix index file name case error
    
    * refactor(rich-text-utils): improve variable names
    
    * refactor(rich-text-utils): improve types definition
    
    * refactor(rich-text-utils): type defaultprops
    
    * refactor(rich-text-utils): migrate html js file to ts
    
    * refactor(rich-text-utils): proper types naming
    
    * refactor(rich-text-utils): add warning warning for conditionally required prop types
    
    * refactor(rich-text-utils): add warning warning for conditionally required prop types
    
    * refactor(rich-text-utils): update yarn lock
    
    * refactor(rich-text-utils): proper type definition for isEmpty function parameter
    
    * refactor(rich-text-utils): proper type definition and cleanup
    
    * refactor(rich-text-utils): point all editor types to one editor type reference
    
    * refactor(rich-text-utils): pick props from rich-text-body types definition
    
    * refactor(rich-text-utils): proper types naming
    
    * refactor(rich-text-utils): specific name for props type
    
    * refactor(rich-text-utils): general improvement on types and imports
    
    * refactor(rich-text-utils): more definitive props types naming
    
    * refactor(rich-text-utils): remove comments
    
    * refactor(rich-text-utils): fix typo
    
    * refactor(rich-text-utils): avoid forcing non-null assertions
    
    * refactor(rich-text-utils): avoid unnecessary castings
    
    * refactor(rich-text-utils): style props
    
    * fix(rich-text-utils): prop type
    
    * refactor(rich-text-utils): add ElementType to react imports
    
    Co-authored-by: Nicola Molinari <nicola.molinari@commercetools.com>

---
+++ .changeset/witty-gifts-tell.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-uikit/rich-text-utils': patch
+---
+
+changes for rich text utils migration to typescript
--- packages/components/inputs/rich-text-utils/src/has-block.js
+++
@@ -1,4 +0,0 @@
-const hasBlock = (type, editor) =>
-  editor.value.blocks.some((node) => node.type === type);
-
-export default hasBlock;
