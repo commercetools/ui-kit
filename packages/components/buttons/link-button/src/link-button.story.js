bc8f5f44007417a4e8fd5502eacd6fd4cbd084f4
Kacper Krzywiec <krzywiec@gmail.com>
2022-02-22 10:30:35 +0100

    refactor: remove unwanted parts of the story

--- packages/components/buttons/link-button/src/link-button.story.js
+++ packages/components/buttons/link-button/src/link-button.story.js
@@ -1,33 +1,10 @@
-import { createElement } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, boolean, text, select } from '@storybook/addon-knobs/react';
-import * as icons from '@commercetools-uikit/icons';
-import Section from '../../../../../docs/.storybook/decorators/section';
 import Readme from '../README.md';
-import LinkButton from './link-button';
-
-const iconNames = Object.keys(icons);
 
 storiesOf('Components|Buttons', module)
-  .addDecorator(withKnobs)
   .addParameters({
     readme: {
       sidebar: Readme,
     },
   })
-  .add('LinkButton', () => (
-    <Router>
-      <Section>
-        <LinkButton
-          label={text('label', 'Accessibility text')}
-          to={text('to', '/foo/bar')}
-          iconLeft={createElement(
-            icons[select('icon', iconNames, iconNames[0])]
-          )}
-          isDisabled={boolean('isDisabled', false)}
-          isExternal={boolean('isExternal', false)}
-        />
-      </Section>
-    </Router>
-  ));
+  .add('LinkButton', () => {});
