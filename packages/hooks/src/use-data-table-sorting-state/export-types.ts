0479b7a01e6e7688be294370fbffde7c8f1e9f75
Carlos Martinez <carlos.martines@commercetools.com>
2022-03-18 14:12:04 +0100

    refactor: export ts types related to tables

---
+++ packages/hooks/src/use-data-table-sorting-state/export-types.ts
@@ -0,0 +1,4 @@
+export type {
+  TSortDefinition,
+  TDataTableSortingState,
+} from './use-data-table-sorting-state';
--- packages/hooks/src/use-data-table-sorting-state/index.ts
+++ packages/hooks/src/use-data-table-sorting-state/index.ts
@@ -1,1 +1,2 @@
 export { default } from './use-data-table-sorting-state';
+export * from './export-types';
