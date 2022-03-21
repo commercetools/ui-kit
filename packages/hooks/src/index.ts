b31956abfaa3855ece61e57a532205774607271d
Carlos Martinez <carlos.martines@commercetools.com>
2022-03-21 14:55:10 +0100

    refactor(hooks): better exporting hook types

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -5,6 +5,6 @@ export { default as useRowSelection } from './use-row-selection';
 export { default as useSorting } from './use-sorting';
 export { default as usePaginationState } from './use-pagination-state';
 export { default as useDataTableSortingState } from './use-data-table-sorting-state';
-export * from './use-data-table-sorting-state';
+export * from './use-data-table-sorting-state/export-types';
 
 export { default as version } from './version';
