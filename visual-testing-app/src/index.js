47b67299aed6775f87667023078c18f4dff83015
Nicola Molinari <nicola.molinari@commercetools.com>
2022-03-22 22:32:25 +0100

    refactor: use Vite for VRT app

--- visual-testing-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-
-const node = document.createElement('div');
-node.id = 'app';
-document.body.appendChild(node);
-
-ReactDOM.render(<App />, node);
---
+++ visual-testing-app/vite.config.js
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'process.env.CI': JSON.stringify(process.env.CI),
+  },
+  plugins: [react()],
+});
