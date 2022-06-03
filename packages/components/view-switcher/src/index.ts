f2f96416be37ae8aeb9fa49e40e58c0bd7cac837
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2022-06-03 11:13:09 +0200

    Implement ViewSwitcher component (#2198)
    * feat(view-switcher): implement component
    
    * feat(view-switcher): add docs
    
    * feat(view-switcher): add tests
    
    * chore(view-switcher): add changeset
    
    * chore(view-switcher): update types
    
    * chore(view-switcher): fix console error with emotion react
    
    * chore(view-switcher): update messages
    
    * chore(view-switcher): add vrt
    
    * chore(view-switcher): add ViewSwitcher to presets
    
    * chore(view-switcher): remove filtering children and useffect hook
    
    * chore(view-switcher): add route to visualroute
    
    * refactor(mc-script): prevent background change for disabled buttons
    
    * refactor(mc-script): enable removal of icon on storybook
    
    * refactor(mc-script): remove border for focused state
    
    * refactor(view-switcher): use 2 story for view switcher
    
    * refactor(view-switcher): update vrt
    
    * refactor(view-switcher): update vrt

---
+++ packages/components/view-switcher/src/index.ts
@@ -0,0 +1,7 @@
+import Group from './view-switcher';
+import Button from './view-switcher-button';
+
+const ViewSwitcher = { Group, Button };
+export default ViewSwitcher;
+
+export { default as version } from './version';
