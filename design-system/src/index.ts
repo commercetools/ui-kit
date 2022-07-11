823aac8b3533846869e95d6274703fe8d79687e7
Kacper Krzywiec <krzywiec@gmail.com>
2022-07-11 12:40:01 +0200

    feat: poc of theme support in uikit
    fix: add fallback values to css variables
    
    fix: attempt to fix vrts
    
    refactor: themeprovider improvements

--- design-system/src/index.ts
+++ design-system/src/index.ts
@@ -1,4 +1,5 @@
 export { default as customProperties } from './custom-properties';
 export { default as designTokens } from './design-tokens';
+export { ThemeProvider, useTheme } from './theme-provider';
 
 export { default as version } from './version';
