386066cab0017bce3534669e8ccf61654f9378e5
Carlos Martinez <carlos.martines@commercetools.com>
2022-07-14 15:56:40 +0200

    fix(loading-spinner): update removed puppeteer function call

--- packages/components/loading-spinner/src/loading-spinner.visualspec.js
+++ packages/components/loading-spinner/src/loading-spinner.visualspec.js
@@ -3,7 +3,7 @@ import percySnapshot from '@percy/puppeteer';
 describe('LoadingSpinner', () => {
   beforeAll(async () => {
     await page.goto(`${HOST}/loading-spinner`);
-    await page.waitFor(1001);
+    await page.waitForTimeout(1001);
   });
 
   it('Default', async () => {
