c59284aa12fd7df002e723f5a0e93b92a57ae0a1
Kacper Krzywiec <krzywiec@gmail.com>
2022-08-30 14:56:05 +0200

    fix: try to fix theme provider vrts #2

--- design-system/src/theme-provider.visualspec.js
+++ design-system/src/theme-provider.visualspec.js
@@ -1,5 +1,5 @@
 import percySnapshot from '@percy/puppeteer';
-import { getDocument, queries, waitFor } from 'pptr-testing-library';
+import { getDocument, queries } from 'pptr-testing-library';
 
 describe('ThemeProvider', () => {
   it('Default', async () => {
@@ -21,9 +21,8 @@ describe('Interactive', () => {
     );
 
     await globalThemeChangeButton.click();
-    await waitFor(async () => {
-      await percySnapshot(page, 'ThemeProvider - after global theme change');
-    });
+    await page.waitForSelector('[data-theme="dark"]');
+    await percySnapshot(page, 'ThemeProvider - after global theme change');
 
     // change local theme
     const localThemeChangeButton = await queries.findByText(
@@ -31,8 +30,7 @@ describe('Interactive', () => {
       'change local theme'
     );
     await localThemeChangeButton.click();
-    await waitFor(async () => {
-      await percySnapshot(page, 'ThemeProvider - after local theme change');
-    });
+    await page.waitForSelector('[data-theme="dark"]');
+    await percySnapshot(page, 'ThemeProvider - after local theme change');
   });
 });
