08a677b04deeb7ae131c8a65acc0d6533b00c745
Carlos Martinez <carlos.martines@commercetools.com>
2022-11-07 12:36:58 +0100

    feat(docs): make theme switcher visible in preview deployments

--- docs/.storybook/configs/contexts.js
+++ docs/.storybook/configs/contexts.js
@@ -4,9 +4,8 @@ import themeContext from './theme-context';
 // We only want to show the theme switcher in local environment
 // or in preview deployments
 const shouldIncludeThemeContext = () =>
-  process.env.NODE_ENV !== 'production' || process.env.VERCEL_URL === 'preview';
-
-console.log('vne:', process.env.VERCEL_URL);
+  process.env.NODE_ENV !== 'production' ||
+  window.location.hostname.includes('commercetools.vercel.app');
 
 export const contexts = [
   intlContext,
