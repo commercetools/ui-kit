7e92149929dedbf7e9a4e1fbc5adfdd587dc37f4
Nicola Molinari <nicola.molinari@commercetools.com>
2022-11-08 15:31:22 +0100

    fix(visual-app): keep minimal global styles

--- visual-testing-app/src/index.jsx
+++ visual-testing-app/src/index.jsx
@@ -1,5 +1,4 @@
 import '@commercetools-uikit/design-system/materials/fonts';
-import '@commercetools-uikit/design-system/materials/resets.css';
 import './globals.css';
 import ReactDOM from 'react-dom';
 import App from './App';
