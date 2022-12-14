42d9ea5a251c1fac374c819f17438fbaa254260b
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2022-12-14 10:12:15 +0100

    Add new tokens for Input component (#2347)
    * refactor(design-system): add tokens for new theme
    
    * refactor(design-system): update tokens for input for new theme
    
    * refactor(design-system): update token for input
    
    * refactor(design-system): update font size for input
    
    * refactor(design-system): add changeset
    
    * refactor(design-system): update font size for input
    
    * refactor(design-system): use correct font size for input
    
    * refactor(design-system): use correct padding for input
    
    * refactor(design-system): use correct borderRadius for input
    
    * refactor(design-system): add new token choices for border width
    
    * refactor(design-system): use correct tokens for input font-color
    
    * refactor(design-system): update design tokens for input
    
    * refactor(design-system): disable hover styles when input is focused
    
    * refactor(design-system): rebuild tokens
    
    * refactor(design-system): use boxshadow to prevent input resize, update tokens
    
    * refactor(design-system): update default theme for input to be color-surface background
    
    * refactor(design-system): use appopraite background color for text input default theme
    
    * refactor(design-system): fix regression for input
    
    * refactor(design-system): replace transparent with unset
    
    * refactor(design-system): add token bg token to fix selct input regression

--- packages/components/inputs/input-utils/src/styles.ts
+++ packages/components/inputs/input-utils/src/styles.ts
@@ -10,7 +10,10 @@ type TInputProps = {
   readOnly?: boolean;
 };
 
-const getInputBorderColor = (props: TInputProps) => {
+const getInputBorderColor = (
+  props: TInputProps,
+  defaultBorderColor: string = designTokens.borderColorForInput
+) => {
   if (props.isDisabled || props.disabled) {
     return designTokens.borderColorForInputWhenDisabled;
   }
@@ -23,7 +26,7 @@ const getInputBorderColor = (props: TInputProps) => {
   if (props.isReadOnly || props.readOnly) {
     return designTokens.borderColorForInputWhenReadonly;
   }
-  return designTokens.borderColorForInput;
+  return defaultBorderColor;
 };
 
 const getInputFontColor = (props: TInputProps) => {
@@ -42,29 +45,50 @@ const getInputFontColor = (props: TInputProps) => {
   return designTokens.fontColorForInput;
 };
 
+const getInputBorderWidth = (props: TInputProps) => {
+  if (props.hasError) {
+    return designTokens.borderWidthForInputWhenError;
+  }
+  if (props.hasWarning) {
+    return designTokens.borderWidthForInputWhenWarning;
+  }
+  return designTokens.borderWidthForInput;
+};
+
+const getInputBoxShadow = (props: TInputProps) => {
+  if (props.hasError) {
+    return designTokens.shadowForInputWhenError;
+  }
+  if (props.hasWarning) {
+    return designTokens.shadowForInputWhenWarning;
+  }
+  return designTokens.shadowForInput;
+};
+
 const getInputStyles = (props: TInputProps) => {
   return css`
     appearance: none;
     background-color: ${props.isDisabled || props.disabled
       ? designTokens.backgroundColorForInputWhenDisabled
       : designTokens.backgroundColorForInput};
-    border: 1px solid ${getInputBorderColor(props)};
+    border: ${getInputBorderWidth(props)} solid ${getInputBorderColor(props)};
     border-radius: ${designTokens.borderRadiusForInput};
     box-sizing: border-box;
+    box-shadow: ${getInputBoxShadow(props)};
     color: ${getInputFontColor(props)};
     cursor: ${props.isDisabled ? 'not-allowed' : 'default'};
     display: flex;
     flex: 1;
     font-family: inherit;
     font-size: ${designTokens.fontSizeForInput};
-    height: ${designTokens.sizeHeightInput};
-    min-height: ${designTokens.sizeHeightInput};
+    height: ${designTokens.heightForInput};
+    min-height: ${designTokens.heightForInput};
     opacity: ${props.isDisabled || props.disabled
       ? '1'
       : 'unset'}; /* fix for mobile safari */
     outline: none;
     overflow: hidden;
-    padding: 0 ${designTokens.spacing20};
+    padding: 0 ${designTokens.paddingForInput};
     transition: border-color ${designTokens.transitionStandard},
       background-color ${designTokens.transitionStandard},
       color ${designTokens.transitionStandard},
@@ -76,11 +100,17 @@ const getInputStyles = (props: TInputProps) => {
     }
     :active,
     :focus,
-    :hover:not(:disabled):not(:read-only) {
-      border-color: ${designTokens.borderColorForInputWhenFocused};
+    :hover:not(:disabled):not(:read-only):not(:focus) {
+      border-color: ${getInputBorderColor(
+        props,
+        designTokens.borderColorForInputWhenHovered
+      )};
+      background-color: ${designTokens.backgroundColorForInputWhenHovered};
     }
     :focus {
-      box-shadow: inset 0 0 0 2px ${designTokens.borderColorForInputWhenFocused};
+      box-shadow: ${designTokens.shadowForInputWhenFocused};
+      border-color: ${designTokens.borderColorForInputWhenFocused};
+      background-color: ${designTokens.backgroundColorForInputWhenFocused};
     }
   `;
 };
