95416884f8774e662dffec0213b1fc8b6329e9f5
Yemitan Isaiah Olurotimi <isaiah.yemitan@commercetools.com>
2023-03-14 15:04:57 +0100

    refactor(text): remove unnecessary curly bracket

--- packages/components/text/src/text.styles.ts
+++ packages/components/text/src/text.styles.ts
@@ -21,7 +21,7 @@ const truncate = `
 `;
 
 const bold = `
-  font-weight: ${designTokens.fontWeightForTextWhenBold}};
+  font-weight: ${designTokens.fontWeightForTextWhenBold};
 `;
 
 const italic = `
