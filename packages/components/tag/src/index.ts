b1df854096d92c5a224fd1d3f5becbe85c35a255
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2023-04-12 09:03:45 +0100

    chore: fixing the export `TagList`

--- packages/components/tag/src/index.ts
+++ packages/components/tag/src/index.ts
@@ -1,3 +1,8 @@
+/** @deprecated use new way below of exporting the Tag component*/
 export { default } from './tag';
+
+export { default as Tag } from './tag';
+export { default as TagList } from './tag-list';
+
 export { default as version } from './version';
 export * from './export-types';
