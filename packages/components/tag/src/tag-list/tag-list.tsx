23709ce77c535fd8e447308c80d05598345c3b1c
Klaudija Ljevar <klaudija.ljevar@gmail.com>
2023-04-12 09:20:46 +0100

    chore: code improvement

--- packages/components/tag/src/tag-list/tag-list.tsx
+++ packages/components/tag/src/tag-list/tag-list.tsx
@@ -1,33 +1,29 @@
 import { ReactNode, Children } from 'react';
-import { css } from '@emotion/react';
 import { designTokens } from '@commercetools-uikit/design-system';
+import styled from '@emotion/styled';
 
 export type TTagListProps = {
   children: ReactNode;
   tag: string;
 };
 
-const getTagListContainerStyles = () => {
-  return css`
-    width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-  `;
-};
+const TagListContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
 
-const getTagListItemStyles = () => {
-  return css`
-    margin: ${designTokens.marginForTagList};
-  `;
-};
+const TagListItem = styled.div`
+  margin: ${designTokens.marginForTagList};
+`;
 
 const TagList = (props: TTagListProps) => {
   return (
-    <div css={[getTagListContainerStyles]}>
+    <TagListContainer>
       {Children.map(props.children, (tag) => (
-        <div css={[getTagListItemStyles]}>{tag}</div>
+        <TagListItem>{tag}</TagListItem>
       ))}
-    </div>
+    </TagListContainer>
   );
 };
 
