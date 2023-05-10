37464be10ab05c6e445c136e70e7df464e005ffc
Carlos Martinez <carlos.martines@commercetools.com>
2023-05-10 10:45:04 +0200

    refactor(docs): change theme selector for a theme wrapper

--- docs/.storybook/decorators/index.js
+++ docs/.storybook/decorators/index.js
@@ -1,3 +1,4 @@
 export { default as FormikBox } from './formik-box';
 export { default as Section } from './section';
 export { default as NeighbouringStackingContext } from './neighbouring-stacking-context';
+export { default as ThemeWrapper } from './theme-wrapper';
