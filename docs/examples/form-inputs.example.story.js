fb1b6350020a6e76ad01938ac770354fef69147a
Kacper Krzywiec <krzywiec@gmail.com>
2023-07-21 09:23:48 +0200

    chore: use latest pnpm version

--- docs/examples/form-inputs.example.story.js
+++ docs/examples/form-inputs.example.story.js
@@ -17,7 +17,7 @@ import {
   PrimaryButton,
   SecondaryButton,
   Spacings,
-} from '@commercetools-frontend/ui-kit';
+} from '../../presets/ui-kit';
 import { FormikBox, Section } from '../.storybook/decorators';
 import Forms from './form-inputs.md';
 
