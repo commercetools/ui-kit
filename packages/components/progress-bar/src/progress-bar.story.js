39b0b41c19444aff2cf7a8af0f610df394af45fa
Tyler Ford <tyler.ford@commercetools.com>
2024-01-30 08:24:00 -0700

    fix(progressbar): update lockfile and story for isinverted

--- packages/components/progress-bar/src/progress-bar.story.js
+++ packages/components/progress-bar/src/progress-bar.story.js
@@ -19,36 +19,43 @@ storiesOf('Components|ProgressBar', module)
     },
   })
   .addDecorator(withKnobs)
-  .add('ProgressBar', () => (
-    <Section>
-      <SpacingsStack>
-        <ProgressBar
-          label={text('label', '50% completed')}
-          progress={number('Progress', 50, {
-            range: true,
-            min: 0,
-            max: 100,
-            step: 1,
-          })}
-          isAnimated={boolean('isAnimated', true)}
-          isInverted={boolean('isInverted', false)}
-          labelPosition={select(
-            'labelPosition',
-            ['left', 'right', 'top', 'bottom'],
-            'top'
-          )}
-          height={select('height', ['10', '20'], '20')}
-          labelWidth={select(
-            'labelWidth',
-            ['auto', 'scale', 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-            6
-          )}
-          barWidth={select(
-            'barWidth',
-            ['auto', 'scale', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-            6
-          )}
-        />
-      </SpacingsStack>
-    </Section>
-  ));
+  .add('ProgressBar', () => {
+    const isInverted = boolean('isInverted', false);
+    const backgroundColor = isInverted ? 'black' : 'transparent';
+
+    return (
+      <Section>
+        <SpacingsStack>
+          <div style={{ backgroundColor, height: '100px' }}>
+            <ProgressBar
+              label={text('label', '50% completed')}
+              progress={number('Progress', 50, {
+                range: true,
+                min: 0,
+                max: 100,
+                step: 1,
+              })}
+              isAnimated={boolean('isAnimated', true)}
+              isInverted={isInverted}
+              labelPosition={select(
+                'labelPosition',
+                ['left', 'right', 'top', 'bottom'],
+                'top'
+              )}
+              height={select('height', ['10', '20'], '20')}
+              labelWidth={select(
+                'labelWidth',
+                ['auto', 'scale', 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+                6
+              )}
+              barWidth={select(
+                'barWidth',
+                ['auto', 'scale', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+                6
+              )}
+            />
+          </div>
+        </SpacingsStack>
+      </Section>
+    );
+  });
