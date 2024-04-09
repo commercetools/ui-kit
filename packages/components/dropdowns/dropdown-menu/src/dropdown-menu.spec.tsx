716852ed4f28380d0bbff08a547774f2b14ff07a
Carlos Cortizas <carlos.martines@commercetools.com>
2024-04-09 13:03:21 +0200

    refactor(dropdown-menu): integrated tests included

---
+++ packages/components/dropdowns/dropdown-menu/src/dropdown-menu.spec.tsx
@@ -0,0 +1,62 @@
+import SecondaryButton from '@commercetools-uikit/secondary-button';
+
+import { screen, render } from '../../../../../test/test-utils';
+import DropdownMenu from './dropdown-menu';
+
+describe('DropdownMenu', () => {
+  it('should render list menu', async () => {
+    const firstOptionOnClick = jest.fn();
+    const secondOptionOnClick = jest.fn();
+
+    await render(
+      <DropdownMenu triggerElement={<SecondaryButton label="Trigger" />}>
+        <DropdownMenu.ListMenuItem onClick={firstOptionOnClick}>
+          Option 1
+        </DropdownMenu.ListMenuItem>
+        <DropdownMenu.ListMenuItem onClick={secondOptionOnClick} isDisabled>
+          Option 2
+        </DropdownMenu.ListMenuItem>
+      </DropdownMenu>
+    );
+
+    // Dropdown shold be closed by default
+    expect(screen.queryByText('Option 1')).not.toBeVisible();
+
+    // Open the dropdown
+    screen.getByLabelText('Trigger').click();
+    expect(screen.getByText('Option 1')).toBeVisible();
+    expect(screen.getByText('Option 2')).toBeVisible();
+
+    // Clicking in the disabled options should do nothing
+    screen.getByText('Option 2').click();
+    expect(secondOptionOnClick).toHaveBeenCalledTimes(0);
+    expect(screen.getByText('Option 1')).toBeVisible();
+
+    // Clicking in the enabled option should close the dropdown
+    screen.getByText('Option 1').click();
+    expect(firstOptionOnClick).toHaveBeenCalledTimes(1);
+    expect(screen.queryByText('Option 1')).not.toBeVisible();
+  });
+
+  it('should render the default menu', async () => {
+    await render(
+      <>
+        <h2>Header</h2>
+        <DropdownMenu triggerElement={<SecondaryButton label="Trigger" />}>
+          <div>Content</div>
+        </DropdownMenu>
+      </>
+    );
+
+    // Dropdown shold be closed by default
+    expect(screen.queryByText('Content')).not.toBeVisible();
+
+    // Open the dropdown
+    screen.getByLabelText('Trigger').click();
+    expect(screen.getByText('Content')).toBeVisible();
+
+    // Clicking outside the dropdown should close it
+    screen.getByText('Header').click();
+    expect(screen.queryByText('Content')).not.toBeVisible();
+  });
+});
