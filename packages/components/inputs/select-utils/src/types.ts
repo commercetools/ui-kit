a22f7eeff50828cf74fefb6d5f313aa94f5c2e16
Sarah Brolley <sarahbrolley@gmail.com>
2024-04-12 09:00:51 -0400

    feat(select-input): update types to be more dynamic/maintainable and add visual spec for new isCondensed prop

--- packages/components/inputs/select-utils/src/types.ts
+++ packages/components/inputs/select-utils/src/types.ts
@@ -23,27 +23,11 @@ import type {
   ValueContainerProps,
 } from 'react-select';
 
-export type TReactSelectCustomComponentsProps =
-  | ClearIndicatorProps
-  | ControlProps
-  | DropdownIndicatorProps
-  | GroupProps
-  | GroupHeadingProps
-  | IndicatorsContainerProps
-  | IndicatorSeparatorProps
-  | InputProps
-  | LoadingIndicatorProps
-  | MenuProps
-  | MenuListProps
-  | NoticeProps
-  | MultiValueProps
-  | MultiValueGenericProps
-  | MultiValueRemoveProps
-  | OptionProps
-  | PlaceholderProps
-  | ContainerProps
-  | SingleValueProps
-  | ValueContainerProps;
+type ComponentProps<T> = T extends React.ComponentType<infer P> ? P : never;
+
+export type TReactSelectCustomComponentsProps = {
+  [K in keyof SelectComponents]: ComponentProps<SelectComponents[K]>;
+}[keyof SelectComponents];
 
 export type TSelectInputCustomComponentProps<
   T extends TReactSelectCustomComponentsProps
