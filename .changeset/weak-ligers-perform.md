82b5db83881d1fc77d8491cea6439c6fc9c58fb1
Byron Wall <87667330+ByronDWall@users.noreply.github.com>
2024-05-14 09:49:25 -0400

    FCT-808: Create `CustomIcon` component (#2803)
    * feat(CustomIcon): implement initial CustomIcon component with tests and documentation
    
    * feat(CustomIcon): update README, remove height/width from fixture, copy sizing values into style file directly, remove warning
    
    * feat(customIcon): add overflow:hidden to styles so that svg does not get clipped by border radius
    
    * feat(customIcon): update readme based on feedback
    
    * feat(customIcon): update flex properties to address resize concerns
    
    * feat(customIcon): use inline-block instead of flex due to safari computing svg width as 0px in flex containers where there is no w/h specified for the svg
    
    * fix(leadingIcon styles): remove export
    
    ---------
    
    Co-authored-by: Carlos Cortizas <97907068+CarlosCortizasCT@users.noreply.github.com>

---
+++ .changeset/weak-ligers-perform.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-uikit/icons': minor
+---
+
+Adds new CustomIcon component for displaying non-ui-kit-svgs.
---
+++ packages/components/icons/src/fixtures/CustomIconReact.tsx
@@ -0,0 +1,30 @@
+const SvgCustomIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" role="img">
+    <g clipPath="url(#custom-icon_react_svg__a)">
+      <path fill="#fff" d="M0 0h35v35H0z" />
+      <path
+        fill="#6359FF"
+        fillRule="evenodd"
+        d="M16.059 20q-.45 0-.755-.305a1.02 1.02 0 0 1-.304-.754V3.06q0-.45.304-.755.305-.304.755-.304h7.597q.37 0 .662.238.29.239.37.61l.265 1.27h4.87q.45-.001.754.304.305.304.305.754v8.471q0 .45-.305.754a1.02 1.02 0 0 1-.754.305h-5.479q-.37 0-.662-.238a1.05 1.05 0 0 1-.37-.61l-.265-1.27h-5.93v6.353q0 .45-.303.754a1.02 1.02 0 0 1-.755.305"
+        clipRule="evenodd"
+      />
+      <path
+        fill="#000"
+        d="M9.554 22.481-1 37.5h37L22.45 21.833a8.19 8.19 0 0 0-12.896.648"
+      />
+      <path
+        stroke="#fff"
+        strokeWidth={1.5}
+        d="m5 36.5 13.66-4.673c1.782-.61 1.81-3.121.04-3.77l-5.252-1.926c-1.66-.61-1.772-2.916-.177-3.681L21.5 18.5"
+      />
+    </g>
+    <defs>
+      <clipPath id="custom-icon_react_svg__a">
+        <path fill="#fff" d="M0 0h35v35H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+SvgCustomIcon.displayName = 'SvgCustomIcon';
+
+export default SvgCustomIcon;
