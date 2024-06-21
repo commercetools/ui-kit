6bf4fe982fbd60fb2142743c48bbe3245dd308d6
Ddouglasz <douglas.egiemeh@gmail.com>
2024-06-21 15:24:18 +0200

    feat(multiline text input): increase max rows in storybook

--- packages/components/inputs/multiline-text-input/src/multiline-text-input.story.js
+++ packages/components/inputs/multiline-text-input/src/multiline-text-input.story.js
@@ -10,7 +10,9 @@ import MultilineTextInput from './multiline-text-input';
 import * as icons from '@commercetools-uikit/icons';
 
 const iconNames = Object.keys(icons);
-const rows = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const rows = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
 
 storiesOf('Components|Inputs', module)
   .addDecorator(withKnobs)
