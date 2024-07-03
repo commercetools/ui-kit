6869e3380a5050cfa73f4d12f371a88fe86992fd
Michael Salzmann <michael.salzmann@commercetools.com>
2024-07-03 15:56:11 +0200

    feat: add IconSearch component to storybook helpers

--- storybook/src/helpers/index.ts
+++ storybook/src/helpers/index.ts
@@ -3,3 +3,4 @@ export { default as iconArgType } from './icon-arg-type';
 export { default as horizontalConstraintArgType } from './horizontal-constraint-arg-type';
 export { default as categorize } from './categorize';
 export { default as hideControls } from './hide-controls';
+export * from './icon-search';
