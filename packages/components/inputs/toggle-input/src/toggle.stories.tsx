c37b6da1f2e0479eb402423dee7b97de4b2ec017
Michael Salzmann <michael.salzmann@commercetools.com>
2024-07-04 11:40:20 +0200

    feat: add Toggle component stories

---
+++ packages/components/inputs/toggle-input/src/toggle.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Toggle, { TToggleInputProps } from './toggle-input';
+import { useState } from 'react';
+
+const meta: Meta<typeof Toggle> = {
+  title: 'components/Toggle',
+  component: Toggle,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Toggle>;
+
+export const BasicExample: Story = ({
+  isChecked,
+  ...args
+}: TToggleInputProps) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <Toggle
+      isChecked={isChecked === undefined ? isActive : isChecked}
+      {...args}
+      onChange={() => setIsActive(!isActive)}
+    />
+  );
+};
+
+BasicExample.args = {};
