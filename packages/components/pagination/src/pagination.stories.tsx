0f6a9295af1a7615f668e2f7a8341cb5aee04666
Michael Salzmann <michael.salzmann@commercetools.com>
2024-07-05 10:36:25 +0200

    feat: add pagination component stories

---
+++ packages/components/pagination/src/pagination.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import styled from '@emotion/styled';
+import Pagination from './pagination';
+
+const meta: Meta<typeof Pagination> = {
+  title: 'components/Pagination/Pagination',
+  component: Pagination,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Pagination>;
+
+const Container = styled.div`
+  display: block;
+  margin-top: 96px;
+  margin-bottom: 96px;
+  align-items: center;
+`;
+
+export const BasicExample: Story = {
+  args: {
+    totalItems: 200,
+    page: 1,
+    onPageChange: () => alert('onPageChange Request'),
+    onPerPageChange: () => alert('onPerPageChange Request'),
+    perPage: 20,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Container>
+          <Story />
+        </Container>
+      );
+    },
+  ],
+};
