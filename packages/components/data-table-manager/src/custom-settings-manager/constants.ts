4e609b2d32d157cda6ecf305083b3f596b34f466
Ddouglasz <douglas.egiemeh@gmail.com>
2024-07-08 21:33:11 +0200

    feat(data table manager): remove unused files

--- packages/components/data-table-manager/src/custom-settings-manager/constants.ts
+++
@@ -1,4 +0,0 @@
-export const WRAPPED_TEXT_VISIBLE = 'wrapped-text-visible';
-export const SHOW_HIDE_ON_DEMAND = 'show-hide-on-demand';
-export const DENSITY_COMFORTABLE = 'density-comfortable';
-export const DENSITY_COMPACT = 'density-compact';
