413a6ce68dd94bea06a8b8765a9c1a0da0b4090b
Michael Salzmann <michael.salzmann@commercetools.com>
2024-08-02 14:07:24 +0200

    chore: add FieldErrors component readme + stories

---
+++ packages/components/field-errors/src/field-errors.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import FieldErrors from './field-errors';
+
+const meta: Meta<typeof FieldErrors> = {
+  title: 'field/Field___/FieldErrors',
+  component: FieldErrors,
+};
+export default meta;
+
+type Story = StoryObj<typeof FieldErrors>;
+
+export const BasicExample: Story = {
+  args: {
+    id: 'error-id',
+    errors: {
+      missing: true,
+      duplicate: true,
+      minLength: true,
+    },
+    renderError: (key) => {
+      switch (key) {
+        case 'duplicate':
+          return 'This is already in use. It must be unique.';
+        default:
+          // When null is returned then the default error handling from
+          // renderDefaultError will kick in for that error.
+          return null;
+      }
+    },
+
+    renderDefaultError: (key) => {
+      switch (key) {
+        case 'minLength':
+          return 'This is too short.';
+        default:
+          // When null is returned then the error handling defined in
+          // FieldError itself will kick in
+          return null;
+      }
+    },
+    isVisible: true,
+  },
+};
