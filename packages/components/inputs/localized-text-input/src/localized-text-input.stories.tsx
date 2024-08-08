9e2c71dad4d1addedf2f6d930f83dbc7e24263a5
Michael Salzmann <michael.salzmann@commercetools.com>
2024-08-08 13:04:47 +0200

    chore: add LocalizedTextInput component readme + stories

---
+++ packages/components/inputs/localized-text-input/src/localized-text-input.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryFn } from '@storybook/react';
+import LocalizedTextInput from './localized-text-input';
+import { useState } from 'react';
+
+const meta: Meta<typeof LocalizedTextInput> = {
+  title: 'Form/Inputs/LocalizedTextInput',
+  // @ts-ignore
+  component: LocalizedTextInput,
+};
+export default meta;
+
+type Story = StoryFn<typeof LocalizedTextInput>;
+
+export const BasicExample: Story = ({
+  defaultExpandLanguages,
+  value: propsValue,
+  ...args
+}) => {
+  const [value, onChange] = useState(
+    propsValue || { en: '', de: '', 'nan-Hant-TW': '' }
+  );
+
+  // We need to force the component to rerender in case a default value
+  // is changed. Otherwise the knob would have no effect.
+  // We do this by changing the key.
+  const key = defaultExpandLanguages ? 'yes' : 'no';
+
+  return (
+    <LocalizedTextInput
+      key={key}
+      value={value}
+      defaultExpandLanguages={defaultExpandLanguages}
+      {...args}
+      onChange={(event) => {
+        onChange({
+          ...value,
+          [event.target.language]: event.target.value,
+        });
+      }}
+    />
+  );
+};
+
+BasicExample.args = {
+  defaultExpandLanguages: false,
+  selectedLanguage: 'en',
+  hideLanguageExpansionControls: false,
+  isAutofocussed: false,
+  isCondensed: false,
+  isDisabled: false,
+  isReadOnly: false,
+  placeholder: { en: 'Placeholder', de: 'Platzhalter' },
+  horizontalConstraint: 7,
+  hasError: false,
+  hasWarning: false,
+};
