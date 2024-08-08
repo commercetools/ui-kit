c81d1e62fe9774a046bacdd59af46b962e95b0a9
Michael Salzmann <michael.salzmann@commercetools.com>
2024-08-08 13:17:17 +0200

    chore: add SelectInput component readme + stories

---
+++ packages/components/inputs/select-input/src/select-input.stories.tsx
@@ -0,0 +1,92 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SelectInput from './select-input';
+
+const meta: Meta<typeof SelectInput> = {
+  title: 'Form/Inputs/SelectInput',
+  component: SelectInput,
+};
+export default meta;
+
+type Story = StoryObj<typeof SelectInput>;
+
+const options = [
+  {
+    label: 'Animals 1',
+    options: [
+      { value: 'platypus', label: 'Platypus' },
+      { value: 'goat', label: 'Goat' },
+      { value: 'giraffe', label: 'Giraffe' },
+      { value: 'whale', label: 'Whale' },
+      { value: 'killer-whale', label: 'Killer Whale', isDisabled: true },
+      { value: 'otter', label: 'Otter' },
+      { value: 'elephant', label: 'Elephant' },
+      { value: 'rat', label: 'Rat' },
+      { value: 'anteater', label: 'Anteater' },
+      { value: 'alligator', label: 'Alligator' },
+      { value: 'dog', label: 'Dog' },
+      { value: 'pig', label: 'Pig' },
+      { value: 'hippopotamus', label: 'Hippopotamus' },
+      { value: 'lion', label: 'Lion' },
+      { value: 'monkey', label: 'Monkey' },
+      { value: 'kangaroo', label: 'Kangaroo' },
+      { value: 'flamingo', label: 'Flamingo' },
+      { value: 'moose', label: 'Moose' },
+    ],
+  },
+  {
+    label: 'Animals 2',
+    options: [
+      { value: 'prairie-dog', label: 'Prairie Dog', isDisabled: true },
+      { value: 'snake', label: 'Snake' },
+      { value: 'porcupine', label: 'Porcupine' },
+      { value: 'stingray', label: 'Stingray' },
+      { value: 'panther', label: 'Panther' },
+      { value: 'lizard', label: 'Lizard' },
+      { value: 'parrot', label: 'Parrot' },
+      { value: 'dolphin', label: 'Dolphin' },
+      { value: 'chicken', label: 'Chicken' },
+      { value: 'sloth', label: 'Sloth' },
+      { value: 'shark', label: 'Shark' },
+      { value: 'ape', label: 'Ape' },
+      { value: 'bear', label: 'Bear' },
+      { value: 'cheetah', label: 'Cheetah' },
+      { value: 'camel', label: 'Camel' },
+      { value: 'beaver', label: 'Beaver' },
+      { value: 'armadillo', label: 'Armadillo' },
+      { value: 'tiger', label: 'Tiger' },
+    ],
+  },
+  {
+    label: 'Animals 3',
+    options: [
+      { value: 'llama', label: 'Llama' },
+      { value: 'seal', label: 'Seal' },
+      { value: 'hawk', label: 'Hawk' },
+      { value: 'wolf', label: 'Wolf' },
+      { value: 'yak', label: 'Yak' },
+      { value: 'rhinoceros', label: 'Rhinoceros' },
+      { value: 'alpaca', label: 'Alpaca' },
+      { value: 'zebra', label: 'Zebra' },
+      { value: 'cat', label: 'Cat' },
+      { value: 'rabbit', label: 'Rabbit' },
+      { value: 'turtle', label: 'Turtle' },
+      { value: 'cow', label: 'Cow' },
+      { value: 'turkey', label: 'Turkey' },
+      { value: 'deer', label: 'Deer' },
+    ],
+  },
+];
+
+export const BasicExample: Story = {
+  decorators: [
+    (Story) => (
+      <div style={{ height: 350 }}>
+        <Story />
+      </div>
+    ),
+  ],
+  args: {
+    options,
+    horizontalConstraint: 7,
+  },
+};
