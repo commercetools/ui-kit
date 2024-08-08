53be6acea6bb4ee8807cd1de7c5a3fad65d9d87b
Michael Salzmann <michael.salzmann@commercetools.com>
2024-08-08 12:59:42 +0200

    chore: add CheckboxInput component readme + stories

---
+++ packages/components/inputs/checkbox-input/src/checkbox-input.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import CheckboxInput from './checkbox-input';
+import styled from '@emotion/styled';
+
+const meta: Meta<typeof CheckboxInput> = {
+  title: 'Form/Inputs/CheckboxInput',
+  component: CheckboxInput,
+};
+export default meta;
+
+type Story = StoryObj<typeof CheckboxInput>;
+
+export const BasicExample: Story = {
+  args: {
+    isChecked: true,
+    children: 'I am a checkbox',
+  },
+};
+
+const DemoContainer = styled.div`
+  display: flex;
+  margin-bottom: 0.5em;
+  align-items: center;
+  gap: 1em;
+`;
+
+export const StatesAndVariants: Story = () => {
+  return (
+    <div>
+      {['', 'isDisabled', 'isReadOnly', 'hasError'].map((prop) => {
+        return (
+          <DemoContainer key={prop}>
+            <div style={{ width: '8em', fontWeight: 600 }}>
+              {prop || 'Default'}
+            </div>
+            <CheckboxInput
+              value="1"
+              isChecked={true}
+              onChange={() => {}}
+              {...{ [prop]: true }}
+            >
+              Checkbox Label
+            </CheckboxInput>
+            <CheckboxInput value="1" isChecked={false} onChange={() => {}}>
+              Checkbox Label
+            </CheckboxInput>
+            <CheckboxInput value="1" isIndeterminate={true} onChange={() => {}}>
+              Checkbox Label
+            </CheckboxInput>
+          </DemoContainer>
+        );
+      })}
+    </div>
+  );
+};
+
+StatesAndVariants.args = {
+  isChecked: true,
+};
