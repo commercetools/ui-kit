aaa23feb1ff40ed6ca9811ff4337b2f606fe75dc
Michael Salzmann <michael.salzmann@commercetools.com>
2024-08-08 13:00:22 +0200

    chore: add DateInput component readme + stories

---
+++ packages/components/inputs/date-input/src/date-input.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import DateInput from './date-input';
+
+const meta: Meta<typeof DateInput> = {
+  title: 'Form/Inputs/DateInput',
+  component: DateInput,
+};
+export default meta;
+
+type Story = StoryObj<typeof DateInput>;
+
+export const BasicExample: Story = {
+  args: {
+    horizontalConstraint: 7,
+  },
+};
