2979343f1ccda765d6f2ea98712b76229e249b1c
Michael Salzmann <michael.salzmann@commercetools.com>
2024-08-13 08:28:12 +0200

    chore: remove docs/ folder exceptions from jest configs

--- jest.eslint.config.js
+++ jest.eslint.config.js
@@ -5,14 +5,7 @@ module.exports = {
   runner: 'jest-runner-eslint',
   displayName: 'eslint',
   moduleFileExtensions: ['js', 'ts', 'tsx'],
-  modulePathIgnorePatterns: [
-    '.yarn',
-    '.cache',
-    'docs/.public',
-    'docs/public',
-    'storybook',
-    'dist',
-  ],
+  modulePathIgnorePatterns: ['.yarn', '.cache', 'storybook', 'dist'],
   testMatch: ['<rootDir>/**/*.js', '<rootDir>/**/*.ts', '<rootDir>/**/*.tsx'],
   watchPlugins: [
     'jest-watch-typeahead/filename',
