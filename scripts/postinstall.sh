55c770532436a7a742447a52a6d59b134b3830b8
Michael Salzmann <michael.salzmann@commercetools.com>
2024-09-06 13:25:48 +0200

    chore: more yarn removals

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -7,15 +7,14 @@ if [ -n "$SKIP_POSTINSTALL_DEV_SETUP" ]; then
 
 else
   echo "Preparing development setup."
-  yarn husky install
-  yarn manypkg check
-  yarn preconstruct dev
+  pnpm husky install
+  pnpm manypkg check
+  pnpm preconstruct dev
 
 fi
 
 echo "Running prettier on package.json files"
-# We need to run prettier to avoid unnecessary formatting changes to package.json (due to Yarn install).
-yarn prettier --write --parser json '**/package.json' &>/dev/null
+pnpm prettier --write --parser json '**/package.json' &>/dev/null
 
 echo "Patching packages"
-yarn patch-package
+pnpm patch-package
