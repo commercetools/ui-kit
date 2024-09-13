568c0dec9bb583ca43626f4795d834f623115730
Byron Wall <87667330+ByronDWall@users.noreply.github.com>
2024-09-13 12:18:22 -0400

    FCT-1112 | display story only when running storybook locally (#2902)
    * feat(local only storybook): add ability to only display a story in the sidebar when running storybook locally by adding a 'local-dev' tag to the story meta
    
    * feat(local only storybook): use tag in mdx files as well
    
    * feat(local only storybook): add local story stubs for FiltersList, add documentation for local-dev tag in storybook/README.md
    
    * fix(tag): remove local-dev tag from dropdownmenu readme

--- storybook/.storybook/manager.ts
+++ storybook/.storybook/manager.ts
@@ -3,4 +3,18 @@ import { cocoTheme } from './theme';
 
 addons.setConfig({
   theme: cocoTheme,
+  sidebar: {
+    filters: {
+      // if the item is tagged with local-dev-only, don't show it in the sidebar in production
+      // see: https://github.com/storybookjs/storybook/issues/9209#issuecomment-1866309500
+      // this cannot be done with `!dev` tag because the Storybook.Meta tag array will only take string literals
+      // see: https://github.com/storybookjs/storybook/discussions/24192
+      patterns: (item) => {
+        if (process.env.NODE_ENV === 'production') {
+          return !item.tags?.includes('local-dev');
+        }
+        return true;
+      },
+    },
+  },
 });
