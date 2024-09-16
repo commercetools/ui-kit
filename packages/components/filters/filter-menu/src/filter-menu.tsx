5fc0720ae235fc410785ef506d2f4c6a6e570d55
Byron Wall <byron.wall@commercetools.com>
2024-09-16 11:37:43 -0400

    feat(filter-menu): add jsdoc comments to props, add sort button to header, update conditional onclicks to give an alert for POC purposes

--- packages/components/filters/filter-menu/src/filter-menu.tsx
+++ packages/components/filters/filter-menu/src/filter-menu.tsx
@@ -1,6 +1,7 @@
 import { type ReactElement } from 'react';
-import DropdownMenu from '@commercetools-uikit/dropdown-menu';
-import { CaretDownIcon } from '../../../icons';
+import DropdownMenu from '../../../dropdowns/dropdown-menu';
+import IconButton from '../../../buttons/icon-button';
+import { CaretDownIcon, CloseIcon } from '../../../icons';
 import { Badge } from './badge';
 import { Chip } from './chip';
 import { Footer } from './footer';
@@ -8,22 +9,73 @@ import { Header } from './header';
 
 export type TAppliedFilterValue = {
   key?: string;
-  label: string;
+  label: string | ReactElement;
 };
 
 export type TFilterMenuProps = {
+  /**
+   * unique identifier for the filter
+   */
   key: string;
-  label: string;
-  filter: ReactElement; // it's this or get into passing different input types - which is restrictive and much higher maintenance
-  operatorsInput?: ReactElement; // could also pass array of values and handler function
+  /**
+   * formatted message to display the filter's name
+   */
+  label: string | ReactElement;
+  /**
+   * the input in which the user selects values for the filter
+   *
+   * NOTE / OPINIONS WELCOME IN PR COMMENTS:
+   * can either pass a react element and let the consuming app choose what input to use,
+   * or start defining different menu bodies based on props and only make predefined input
+   * types available, which is restrictive and much higher maintenance
+   */
+  filter: ReactElement;
+  /**
+   * the input in which the user can select which operator should be used for this filter
+   *
+   * NOTE / OPINIONS WELCOME IN PR COMMENTS:
+   * the alternative to a rect element is passing in an array of operators and a handler function,
+   * which is more achievable here than for the input in the body, but could still be restrictive
+   */
+  operatorsInput?: ReactElement;
+  /**
+   * the values applied to this filter by the user
+   *
+   * NOTE / OPINIONS WELCOME IN PR COMMENTS:
+   * this will almost certainly be some sort of generic w/validation - all the `Chip` really
+   * needs is the selected option's `label`, but these options need to contain a lot more data
+   */
   appliedFilterValues:
     | TAppliedFilterValue
     | TAppliedFilterValue[]
     | undefined
     | null;
+  /**
+   * indicates whether FilterMenu can be removed from the filtersList
+   */
+  isPersistent?: boolean;
+  /**
+   * whether or not the filter is disabled
+   */
+  isDisabled?: boolean;
+  /**
+   * controls whether `x` in Trigger Button is displayed - required if `isPersistent` is `false`
+   *
+   * NOTES / OPINIONS WELCOME IN PR COMMENTS:
+   * is the presence/absence of the function sufficient for display logic, or do we need a separate `show-` prop?
+   */
   onRemoveFilter?: Function;
+  /**
+   * controls whether `apply` button in Menu Body Footer is displayed
+   */
   onApplyFilter?: Function;
+  /**
+   * controls whether `clear` button in Menu Body Footer is displayed
+   */
   onClearFilter?: Function;
+  /**
+   * controls whether `sort` button in Menu Body Header is displayed
+   */
   onFilterOptionsSortClick?: Function;
 };
 function FilterMenu(props: TFilterMenuProps) {
@@ -55,16 +107,16 @@ function FilterMenu(props: TFilterMenuProps) {
             Array.isArray(props.appliedFilterValues) && (
               <Badge label={`+${props.appliedFilterValues.length}`} />
             )}
-          {props.onRemoveFilter && (
-            <button
-              style={{ margin: '0 4px' }}
+          {props.onRemoveFilter && !props.isPersistent && (
+            <IconButton
+              icon={<CloseIcon />}
+              label={`close ${props.label} filter`}
               onClick={(e) => {
                 e.stopPropagation();
                 props.onRemoveFilter!();
               }}
-            >
-              x
-            </button>
+              size="20"
+            />
           )}
           <CaretDownIcon size="small" color="info" />
         </button>
@@ -72,7 +124,11 @@ function FilterMenu(props: TFilterMenuProps) {
       menuHorizontalConstraint={7}
       menuMaxHeight={1000}
     >
-      <Header label={props.label} operatorsInput={props.operatorsInput} />
+      <Header
+        label={props.label}
+        operatorsInput={props.operatorsInput}
+        onFilterOptionsSortClick={props.onFilterOptionsSortClick}
+      />
       {props.filter}
       <Footer
         onApplyFilter={props.onApplyFilter}
