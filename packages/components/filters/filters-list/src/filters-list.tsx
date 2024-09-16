34cf9f3849fbdc4718d83a4d74b4abedea121a74
Byron Wall <byron.wall@commercetools.com>
2024-09-16 16:05:55 -0400

    chore(readmes): run yarn generate-readmes and commit results

---
+++ packages/components/filters/filters-list/src/filters-list.tsx
@@ -0,0 +1,11 @@
+export type TFiltersListProps = {
+  /**
+   * This is a stub prop
+   */
+  label?: string;
+};
+
+function FiltersList(props: TFiltersListProps) {
+  return <div>{props.label} - I am a List of Filters!</div>;
+}
+export default FiltersList;
