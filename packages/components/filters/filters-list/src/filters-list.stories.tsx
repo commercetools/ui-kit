4445746b52674fda5edb546a942136aa6aae06a4
Ddouglasz <douglas.egiemeh@gmail.com>
2024-09-27 10:08:44 +0200

    feat(filterlist): filter list config via children poc

--- packages/components/filters/filters-list/src/filters-list.stories.tsx
+++ packages/components/filters/filters-list/src/filters-list.stories.tsx
@@ -1,22 +1,162 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryFn } from '@storybook/react';
+import { MenuProps, MenuListProps } from 'react-select';
+import { useState, useCallback } from 'react';
 import FiltersList from './filters-list';
+import SelectInput from '../../../inputs/select-input';
 
 const meta: Meta<typeof FiltersList> = {
   title: 'components/Filters/FiltersList',
   component: FiltersList,
   tags: ['local-dev'],
-  argTypes: {
-    label: {
-      control: 'text',
-    },
-  },
+  argTypes: {},
 };
 export default meta;
 
-type Story = StoryObj<typeof FiltersList>;
+type Story = StoryFn<typeof FiltersList>;
 
-export const BasicExample: Story = {
-  args: {
-    label: 'A list of filters',
-  },
+const CustomSelectMenu = ({
+  children,
+  innerProps: { ref, ...restInnerProps },
+}: MenuProps) => (
+  <div ref={ref} {...restInnerProps}>
+    {children}
+  </div>
+);
+
+const CustomMenuList = ({
+  children,
+  innerProps: { ref, ...restInnerProps },
+}: MenuListProps) => (
+  <div ref={ref} {...restInnerProps}>
+    {children}
+  </div>
+);
+
+const primaryColorOptions = [
+  { label: 'Blue', value: 'blue', key: 'blue', id: '2' },
+  { label: 'Red', value: 'red', key: 'red', id: '4' },
+  { label: 'Orange', value: 'orange', key: 'orange', id: '5' },
+  { label: 'Yellow', value: 'yellow', key: 'yellow', id: '6' },
+  { label: 'Green', value: 'green', key: 'green', id: '7' },
+];
+
+const secondaryColorOptions = [
+  { label: 'Purple', value: 'purple', key: 'purple', id: '3' },
+  { label: 'Forest', value: 'forest', key: 'forest', id: '8' },
+  { label: 'Slate', value: 'slate', key: 'slate', id: '9' },
+  { label: 'Silver', value: 'silver', key: 'silver', id: '10' },
+];
+
+export const BasicExample: Story = () => {
+  const [primaryColorValue, setPrimaryColorValue] = useState<string[]>([]);
+  const [secondaryColorValue, setSecondaryColorValue] = useState<string[]>([]);
+
+  const clearPrimaryColorFilter = () => setPrimaryColorValue([]);
+  const clearSecondaryColorFilter = () => setSecondaryColorValue([]);
+
+  const getPrimaryColorTags = useCallback(() => {
+    return primaryColorValue.length > 0
+      ? primaryColorValue.map((value) => ({
+          key: value,
+          label: value.toUpperCase(),
+        }))
+      : [];
+  }, [primaryColorValue]);
+
+  const getSecondaryColorTags = useCallback(() => {
+    return secondaryColorValue.length > 0
+      ? secondaryColorValue.map((value) => ({
+          key: value,
+          label: value.toUpperCase(),
+        }))
+      : [];
+  }, [secondaryColorValue]);
+
+  const clearAllFilters = () => {
+    clearPrimaryColorFilter();
+    clearSecondaryColorFilter();
+  };
+
+  const filters = [
+    {
+      key: 'primaryColors',
+      label: 'Primary Colors',
+      groupKey: 'primaryColors',
+      getTags: getPrimaryColorTags,
+      onClearRequest: clearPrimaryColorFilter,
+    },
+    {
+      key: 'secondaryColors',
+      label: 'Secondary Colors',
+      groupKey: 'secondaryColors',
+      getTags: getSecondaryColorTags,
+      onClearRequest: clearSecondaryColorFilter,
+    },
+  ];
+
+  const filterConfigs = [
+    {
+      filterKey: 'primaryColors',
+      name: 'primaryColorsSelect',
+      options: primaryColorOptions,
+      value: primaryColorValue,
+      setValue: setPrimaryColorValue,
+      isMulti: false,
+    },
+    {
+      filterKey: 'secondaryColors',
+      name: 'secondaryColorsSelect',
+      options: secondaryColorOptions,
+      value: secondaryColorValue,
+      setValue: setSecondaryColorValue,
+      isMulti: true,
+    },
+  ];
+
+  const filterGroups = [
+    { key: 'secondaryColors', label: <div>SECONDARY COLORS</div> },
+    { key: 'primaryColors', label: <div>PRIMARY!!!</div> },
+  ];
+
+  return (
+    <div
+      style={{
+        height: 500,
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <FiltersList
+        filters={filters}
+        onClearAllRequest={clearAllFilters}
+        filterGroups={filterGroups}
+      >
+        {filterConfigs.map(
+          ({ filterKey, name, options, value, setValue, isMulti = false }) => (
+            <div key={filterKey} id={filterKey}>
+              <SelectInput
+                name={name}
+                options={options}
+                value={value}
+                onChange={(e) =>
+                  setValue(
+                    Array.prototype.concat(e.target.value ? e.target.value : [])
+                  )
+                }
+                components={{
+                  Menu: CustomSelectMenu,
+                  MenuList: CustomMenuList,
+                }}
+                menuIsOpen={true}
+                controlShouldRenderValue={false}
+                isMulti={isMulti}
+              />
+            </div>
+          )
+        )}
+      </FiltersList>
+    </div>
+  );
 };
--- packages/components/filters/filters-list/src/filters-list.tsx
+++ packages/components/filters/filters-list/src/filters-list.tsx
@@ -1,11 +1,176 @@
-export type TFiltersListProps = {
-  /**
-   * This is a stub prop
-   */
-  label?: string;
+import { ReactNode, useState, Children, isValidElement } from 'react';
+import { MenuProps, MenuListProps } from 'react-select';
+import { css } from '@emotion/react';
+import DropdownMenu from '@commercetools-uikit/dropdown-menu';
+import FilterMenu from '@commercetools-uikit/filter-menu';
+import FlatButton from '@commercetools-uikit/flat-button';
+import { PlusThinIcon, CloseIcon } from '@commercetools-uikit/icons';
+import SelectInput from '@commercetools-uikit/select-input';
+
+export type TAppliedFilterValue = {
+  label: ReactNode;
 };
 
-function FiltersList(props: TFiltersListProps) {
-  return <div>{props.label}</div>;
+export type TFiltersConfiguration = {
+  key: string;
+  groupKey?: string;
+  label: ReactNode;
+  isPersistent?: boolean;
+  isDisabled?: boolean;
+  getTags: () => TAppliedFilterValue[];
+  onClearRequest: Function;
+  onApplyRequest?: Function;
+  onSortRequest?: Function;
+};
+
+export type TFilterGroup = {
+  key: string;
+  label: ReactNode;
+};
+
+export type TAddFilterSelectOption = {
+  label: string;
+  value: string;
+  isDisabled: boolean;
+};
+
+export type TFiltersListProps = {
+  filters: TFiltersConfiguration[];
+  filterGroups?: TFilterGroup[];
+  onClearAllRequest: Function;
+  onAddFilterRequest?: Function;
+  children: ReactNode; // Now accept children
+};
+
+const CustomSelectMenu = ({
+  children,
+  innerProps: { ref, ...restInnerProps },
+}: MenuProps) => (
+  <div ref={ref} {...restInnerProps}>
+    {children}
+  </div>
+);
+
+const CustomMenuList = ({
+  children,
+  innerProps: { ref, ...restInnerProps },
+}: MenuListProps) => (
+  <div ref={ref} {...restInnerProps}>
+    {children}
+  </div>
+);
+
+function FiltersList({
+  filters,
+  filterGroups,
+  onClearAllRequest,
+  children,
+}: TFiltersListProps) {
+  const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
+  const persistedFilters = filters
+    .filter((filter) => filter.isPersistent)
+    .map((filter) => filter.key);
+
+  // Get options for the dropdown
+  const getFilterOptions = () => {
+    if (filterGroups) {
+      return filterGroups.map((filterGroup) => ({
+        label: filterGroup.label,
+        options: filters
+          .filter((filter) => filter.groupKey === filterGroup.key)
+          .map((filter) => ({
+            value: filter.key,
+            label: filter.label,
+            isDisabled: filter.isDisabled,
+          })),
+      }));
+    } else {
+      return filters.map((filter) => ({
+        value: filter.key,
+        label: filter.label,
+        isDisabled: filter.isDisabled,
+      }));
+    }
+  };
+
+  const removeFilter = (filterKey: string) =>
+    setSelectedFilters((selectedFilters) =>
+      selectedFilters.filter((selectedFilter) => selectedFilter !== filterKey)
+    );
+
+  return (
+    <div
+      css={css`
+        width: 100%;
+        display: flex;
+        align-items: center;
+        gap: 8px;
+      `}
+    >
+      {persistedFilters.concat(selectedFilters).map((activeFilter) => {
+        const activeFilterConfig = filters.find(
+          (filter) => filter.key === activeFilter
+        )!;
+
+        // Find corresponding child component
+        const matchingChild = Children.toArray(children).find(
+          (child) =>
+            isValidElement(child) && child.props.id === activeFilterConfig.key
+        );
+
+        return (
+          <FilterMenu
+            key={activeFilterConfig.key}
+            filterKey={activeFilterConfig.key}
+            label={activeFilterConfig.label}
+            renderMenuBody={() => matchingChild}
+            appliedFilterValues={activeFilterConfig.getTags()}
+            onApplyFilter={activeFilterConfig.onApplyRequest}
+            onClearFilter={activeFilterConfig.onClearRequest}
+            onRemoveFilter={() => {
+              removeFilter(activeFilter);
+              activeFilterConfig.onClearRequest();
+            }}
+          />
+        );
+      })}
+
+      <DropdownMenu
+        triggerElement={
+          <FlatButton label="add filters" icon={<PlusThinIcon />} />
+        }
+      >
+        <SelectInput
+          name="add filters"
+          options={getFilterOptions()}
+          onChange={(e) => {
+            setSelectedFilters(
+              Array.prototype.concat(e.target.value ? e.target.value : [])
+            );
+          }}
+          value={selectedFilters}
+          menuIsOpen={true}
+          isMulti={true}
+          controlShouldRenderValue={false}
+          components={{ Menu: CustomSelectMenu, MenuList: CustomMenuList }}
+          // @ts-ignore
+          isOptionDisabled={(option: unknown) => option.isDisabled}
+        />
+      </DropdownMenu>
+
+      {selectedFilters.length > 0 && (
+        <FlatButton
+          tone="secondary"
+          onClick={() => {
+            setSelectedFilters([]);
+            onClearAllRequest();
+          }}
+          label="Clear all"
+          icon={<CloseIcon />}
+        />
+      )}
+    </div>
+  );
 }
+
 export default FiltersList;
