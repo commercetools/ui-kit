e692902aa9d7e0bf4da6adc113cde5f4057660df
Valorie <valorie.carli@commercetools.com>
2024-10-07 11:48:02 -0500

    chore(filter): add footer component

--- packages/components/filters/src/filter-menu/footer/footer.tsx
+++ packages/components/filters/src/filter-menu/footer/footer.tsx
@@ -1,5 +1,73 @@
-function Footer() {
-  return <div>footer</div>;
-}
+import FlatButton from '@commercetools-uikit/flat-button';
+import messages from './messages';
+import { CloseBoldIcon } from '@commercetools-uikit/icons';
+import { ReactNode } from 'react';
+import { css } from '@emotion/react';
+import { designTokens } from '@commercetools-uikit/design-system';
+import { useIntl } from 'react-intl';
 
+type TFooterProps = {
+  /**
+   * controls whether `apply` button in Menu Body Footer is displayed
+   */
+  renderApplyButton?: () => ReactNode;
+  /**
+   * controls whether a `clear all` button in Menu Body Footer is displayed
+   */
+  onClearAllRequest?: () => void;
+};
+
+const mainContainerStyles = css`
+  height: auto;
+  width: 252px;
+   };
+`;
+
+const footerContainerStyles = css`
+  /* horizontal divider */
+  height: 1px;
+  background-color: ${designTokens.colorWarning};
+  margin-top: ${designTokens.spacingS};
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+
+  & > div:first-of-type {
+    margin-top: ${designTokens.spacingS};
+    padding-left: ${designTokens.spacingM};
+  }
+
+  & > div:last-of-type {
+    margin-top: ${designTokens.spacingS};
+    padding-right: ${designTokens.spacingM};
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
+
+const Footer = ({ renderApplyButton, onClearAllRequest }: TFooterProps) => {
+  const intl = useIntl();
+  if (!renderApplyButton && !onClearAllRequest) return null;
+
+  return (
+    <div css={mainContainerStyles}>
+      <div css={footerContainerStyles}>
+        <div>{renderApplyButton && renderApplyButton()}</div>
+        <div>
+          {onClearAllRequest && (
+            <FlatButton
+              icon={<CloseBoldIcon />}
+              tone="secondary"
+              onClick={onClearAllRequest}
+              label={intl.formatMessage(messages.clearAllButtonLabel)}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Footer.displayName = 'Footer';
 export default Footer;
--- packages/components/filters/src/filters.stories.tsx
+++ packages/components/filters/src/filters.stories.tsx
@@ -1,10 +1,12 @@
 import type { Meta, StoryFn } from '@storybook/react';
 import Filters from './filters';
+import Footer from './filter-menu/footer/footer';
+import { PrimaryButton } from '@commercetools-uikit/buttons';
 
 const meta: Meta<typeof Filters> = {
   title: 'components/Filters',
   component: Filters,
-  tags: ['local-dev'],
+  // tags: ['local-dev'],
   argTypes: {
     label: {
       control: 'text',
