4c2941618daf6071ac76b7a8cd4f750e79924755
Byron Wall <87667330+ByronDWall@users.noreply.github.com>
2024-10-09 15:29:32 -0400

    fix(storybook): make sure filters storybook isnt published in prod (#2955)

--- packages/components/filters/src/filters.tsx
+++ packages/components/filters/src/filters.tsx
@@ -1,5 +1,4 @@
 import { designTokens } from '@commercetools-uikit/design-system';
-import FilterMenu from './filter-menu';
 
 export type TFiltersProps = {
   /**
@@ -17,103 +16,6 @@ function Filters(props: TFiltersProps) {
       }}
     >
       {props.label && <span>{props.label}</span>}
-      <span
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: designTokens.spacing20,
-        }}
-      >
-        <FilterMenu
-          filterKey="test"
-          label="Testing"
-          renderMenuBody={() => <div>im the body</div>}
-          appliedFilterValues={undefined}
-          onRemoveRequest={() => {}}
-          defaultOpen={true}
-        />
-        <ul>
-          <li>no applied values</li>
-          <li>open on mount</li>
-        </ul>
-      </span>
-      <span
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: designTokens.spacing20,
-        }}
-      >
-        <FilterMenu
-          filterKey="test"
-          label="Testing"
-          renderMenuBody={() => <div>im the body</div>}
-          appliedFilterValues={[{ label: 'hello', value: 'hello' }]}
-          onRemoveRequest={() => {}}
-        />
-        <ul>
-          <li>applied values</li>
-        </ul>
-      </span>
-      <span
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: designTokens.spacing20,
-        }}
-      >
-        <FilterMenu
-          filterKey="test"
-          label="Testing"
-          renderMenuBody={() => <div>im the body</div>}
-          appliedFilterValues={undefined}
-          onRemoveRequest={() => {}}
-          isPersistent
-        />
-        <ul>
-          <li>no applied values</li>
-          <li>persistent</li>
-        </ul>
-      </span>
-      <span
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: designTokens.spacing20,
-        }}
-      >
-        <FilterMenu
-          filterKey="test"
-          label="Testing"
-          renderMenuBody={() => <div>im the body</div>}
-          appliedFilterValues={[{ label: 'hello', value: 'hello' }]}
-          onRemoveRequest={() => {}}
-          isPersistent
-        />
-        <ul>
-          <li>applied values</li>
-          <li>persistent</li>
-        </ul>
-      </span>
-      <span
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: designTokens.spacing20,
-        }}
-      >
-        <FilterMenu
-          filterKey="test"
-          label="Testing"
-          renderMenuBody={() => <div>im the body</div>}
-          appliedFilterValues={[{ label: 'hello', value: 'hello' }]}
-          onRemoveRequest={() => {}}
-          isDisabled
-        />
-        <ul>
-          <li>disabled</li>
-        </ul>
-      </span>
     </div>
   );
 }
