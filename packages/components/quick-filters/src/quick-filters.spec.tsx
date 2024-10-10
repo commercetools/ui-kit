a704e1bd42546ca7c553ae850543dc998b2353b2
Michael Salzmann <michael.salzmann@commercetools.com>
2024-10-10 12:49:58 +0200

    test(quick-filters): add unit-test

--- packages/components/quick-filters/src/quick-filters.spec.tsx
+++ packages/components/quick-filters/src/quick-filters.spec.tsx
@@ -1,21 +1,26 @@
 import { screen, render } from '../../../../test/test-utils';
 import QuickFilters from './quick-filters';
 
-/**
- * THIS IS A PLACEHOLDER, PLEASE UPDATE IT
- */
 describe('QuickFilters', () => {
-  it('should render the quick-filters', async () => {
-    await render(
-      <QuickFilters
-        items={[
-          { id: '1', label: 'Fooo', isActive: true },
-          { id: '2', label: 'Bar', isActive: false },
-        ]}
-        onItemClick={() => {}}
-      />
-    );
-    await screen.findByText('Fooo');
-    await screen.findByText('Bar');
+  it('should render items and propagate item-clicks', async () => {
+    const items = [
+      { id: '1', label: 'Foo', isActive: true },
+      { id: '2', label: 'Bar', isActive: false },
+    ];
+
+    const cb = jest.fn();
+
+    await render(<QuickFilters items={items} onItemClick={cb} />);
+
+    const button1 = (await screen.findByText('Foo')).closest('button');
+    const button2 = (await screen.findByText('Bar')).closest('button');
+
+    expect(button1).toBeInTheDocument();
+    expect(button2).toBeInTheDocument();
+
+    button2?.click();
+    expect(cb).toHaveBeenCalledWith(items[1]);
+    button1?.click();
+    expect(cb).toHaveBeenCalledWith(items[0]);
   });
 });
