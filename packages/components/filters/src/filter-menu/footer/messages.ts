7e4edc762efa10ae15402cdbd6db540d3f7ebbda
Valorie <47182700+valoriecarli@users.noreply.github.com>
2024-10-10 11:22:20 -0500

    FCT 1084- Filter Menu - Footer (#2947)
    * chore(filter): add footer component
    
    * chore(filter): adjust main container width
    
    * chore(changeset): add changeset
    
    * chore(filter): removed unnecessary styles after refactor
    
    * chore(filter): used available design token
    
    * chore(changeset): make more specific
    
    * chore(filter): adjusted styling
    
    * chore(changeset): after all of that we dont need a changeset
    
    * chore(filter): removed footer from filters.stories

---
+++ packages/components/filters/src/filter-menu/footer/messages.ts
@@ -0,0 +1,9 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  clearAllButtonLabel: {
+    id: 'UIKit.ClearAllButton.clearAllButtonLabel',
+    description: 'Label for the clear all button',
+    defaultMessage: 'Clear all',
+  },
+});
