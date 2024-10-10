d7e2eee39cb77aa29a31428fc2316812fdf5723b
Byron Wall <byron.wall@commercetools.com>
2024-10-10 13:38:57 -0400

    feat(selected values in trigger button): use callback ref and useCallback instead of useRef to measure values container, get rid of timeouts

--- packages/components/filters/src/filter-menu/trigger-button/trigger-button.tsx
+++ packages/components/filters/src/filter-menu/trigger-button/trigger-button.tsx
@@ -2,9 +2,8 @@ import {
   type ReactNode,
   forwardRef,
   type LegacyRef,
-  type RefObject,
-  useEffect,
   useLayoutEffect,
+  useCallback,
   useRef,
   useState,
 } from 'react';
@@ -15,47 +14,41 @@ import * as styles from './trigger-button.styles';
 import { Badge } from '../../badge';
 import { Chip } from '../chip';
 
-type TVisibleHiddenCount = number | undefined;
-
-const useScrollObserver = (ref: RefObject<HTMLElement>, totalCount: number) => {
+const useScrollObserver = (totalCount: number) => {
   const [isOverflowing, setIsOverflowing] = useState(false);
   const [visibleCount, setVisibleCount] = useState(0);
-  const numRef = useRef(totalCount); // Store the current num value in a ref
+  const containerRef = useRef<HTMLUListElement | null>(null);
 
-  useEffect(() => {
-    numRef.current = totalCount; // Update the ref whenever num changes
-  }, [totalCount]);
+  const setContainerRef = useCallback((node: HTMLUListElement) => {
+    if (node) {
+      containerRef.current = node;
+    }
+  }, []);
 
   useLayoutEffect(() => {
     if (isOverflowing) {
-      let counts;
-      setTimeout(() => {
-        const node = ref.current;
-        const { clientWidth, children } = node!;
-        let widthOfAllChildren = 0;
-        counts = Array.from(children).reduce<TVisibleHiddenCount>(
-          (acc, child, idx) => {
-            const { width } = child.getBoundingClientRect();
-            widthOfAllChildren = widthOfAllChildren += width;
-            console.log(width);
-            if (widthOfAllChildren <= clientWidth) {
-              console.log(idx);
-              acc = idx + 1;
-            }
-            return acc;
-          },
-          0
-        );
-        console.log(counts);
-        setVisibleCount(counts ? counts : 0);
-      }, 100);
+      const node = containerRef.current;
+      if (!node) return;
+      const { clientWidth, children } = node;
+      let count;
+      let widthOfAllChildren = 0;
+      count = Array.from(children).reduce<number>((acc, child, idx) => {
+        const { width } = child.getBoundingClientRect();
+        widthOfAllChildren = widthOfAllChildren += width;
+
+        if (widthOfAllChildren <= clientWidth) {
+          acc = idx + 1;
+        }
+        return acc;
+      }, 0);
+      setVisibleCount(count);
     } else {
       setVisibleCount(0);
     }
-  }, [isOverflowing, ref]);
+  }, [isOverflowing]);
 
-  useEffect(() => {
-    const node = ref.current;
+  useLayoutEffect(() => {
+    const node = containerRef.current;
     if (!node) return;
 
     const checkOverflow = () => {
@@ -76,9 +69,13 @@ const useScrollObserver = (ref: RefObject<HTMLElement>, totalCount: number) => {
     };
     // doesn't work without ref.current
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ref, ref.current]);
-  console.log(visibleCount);
-  return { isOverflowing, overflowCount: totalCount - visibleCount + 1 };
+  }, [containerRef, containerRef.current]);
+  console.log(visibleCount, totalCount, isOverflowing);
+  return {
+    isOverflowing,
+    overflowCount: totalCount - visibleCount,
+    setContainerRef,
+  };
 };
 
 export type TFilterMenuTriggerButtonProps = {
@@ -124,9 +121,7 @@ const TriggerButton = forwardRef(function TriggerButton(
   const values = appliedFilterValues || [];
   const filtersApplied: boolean = values.length > 0;
 
-  const appliedValuesContainer = useRef<HTMLUListElement>(null);
-  const { isOverflowing, overflowCount } = useScrollObserver(
-    appliedValuesContainer,
+  const { isOverflowing, overflowCount, setContainerRef } = useScrollObserver(
     values.length
   );
 
@@ -135,9 +130,8 @@ const TriggerButton = forwardRef(function TriggerButton(
       <label css={styles.label} htmlFor={`${filterKey}-menu-trigger`}>
         {label}:
       </label>
-      {/** THESE CONTAINERS ARE FOR THE NEXT PR, when the `Chip` and `Badge` are merged */}
       {filtersApplied && (
-        <ul ref={appliedValuesContainer} css={styles.valuesContainer}>
+        <ul ref={setContainerRef} css={styles.valuesContainer}>
           {values.map((value) => (
             <Chip key={value.value} label={value.label} />
           ))}
