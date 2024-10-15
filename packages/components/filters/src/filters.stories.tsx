19d76574289c7a0aa14f121f82a6204bb5f3fa52
Byron Wall <byron.wall@commercetools.com>
2024-10-15 13:17:20 -0400

    chore(storybook): update storybook so it deploys to preview branch

--- packages/components/filters/src/filters.stories.tsx
+++ packages/components/filters/src/filters.stories.tsx
@@ -1,13 +1,10 @@
 import type { Meta, StoryFn } from '@storybook/react';
 import Filters from './filters';
-import FilterMenu from './filter-menu';
-import { ReactNode, useState } from 'react';
-import SelectInput from '@commercetools-uikit/select-input';
 
 const meta: Meta<typeof Filters> = {
   title: 'components/Filters',
   component: Filters,
-  tags: ['local-dev'],
+  // tags: ['local-dev'],
   argTypes: {
     label: {
       control: 'text',
@@ -19,43 +16,5 @@ export default meta;
 type Story = StoryFn<typeof Filters>;
 
 export const BasicExample: Story = () => {
-  const [headerSelectOptions, setHeaderSelectOptions] = useState<string>();
-  const renderOperatorsInput = (): ReactNode => {
-    const operatorOptions = [
-      { value: 'is', label: 'is' },
-      { value: 'is not', label: 'is NOT' },
-      {
-        value: 'is not quite a short option',
-        label: 'is NOT quite a short option',
-      },
-    ];
-
-    return (
-      <SelectInput
-        appearance="quiet"
-        isCondensed={true}
-        isSearchable={false}
-        value={
-          // Default to the first option if no value is passed
-          headerSelectOptions ? headerSelectOptions : operatorOptions[0].value
-        }
-        options={operatorOptions}
-        onChange={(event) => {
-          setHeaderSelectOptions(event.target.value as string);
-        }}
-      />
-    );
-  };
-
-  // return <Filters label={'test'} />;
-  return (
-    <FilterMenu
-      label={'Size'}
-      filterKey="filterKey"
-      renderMenuBody={() => <>Hello!</>}
-      appliedFilterValues={[{ label: 'hello', value: 'hello' }]}
-      renderOperatorsInput={renderOperatorsInput}
-      onSortRequest={() => {}}
-    />
-  );
+  return <Filters label={'test'} />;
 };
