b19451fd6c69fb5edf8032b2638abd2b4989c194
Valorie <valorie.carli@commercetools.com>
2024-10-16 13:50:10 -0500

    chore(filters): init api and styling for filter component

--- packages/components/filters/src/filters.spec.tsx
+++ packages/components/filters/src/filters.spec.tsx
@@ -1,12 +1,86 @@
-import { screen, render } from '../../../../test/test-utils';
-import Filters from './filters';
+import { fireEvent, render, screen } from '../../../../test/test-utils';
+import Filters, { TFiltersProps } from './filters';
+import SelectInput from '@commercetools-uikit/select-input';
+import { designTokens } from '@commercetools-uikit/design-system';
 
-/**
- * THIS IS A PLACEHOLDER, PLEASE UPDATE IT
- */
-describe('FilterMenu FiltersList', () => {
-  it('should render the filters-list', async () => {
-    await render(<Filters label="filters list" />);
-    await screen.findByText('filters list');
+const mockRenderSearchComponent = () => <SelectInput />;
+const mockOnClearAllRequest = jest.fn();
+
+const getDefaultProps = (custom?: Partial<TFiltersProps>): TFiltersProps => ({
+  appliedFilters: [],
+  filters: [],
+  onClearAllRequest: mockOnClearAllRequest,
+  renderSearchComponent: mockRenderSearchComponent,
+  ...custom,
+});
+
+// filters list === add filter & clear all
+describe('Filters', () => {
+  it('should display the filter list when `filters` button is toggled', async () => {
+    await render(<Filters {...getDefaultProps()} />);
+    const filtersButton = await screen.findByTestId('filters-button');
+
+    fireEvent.click(filtersButton);
+
+    const addFilterButton = await screen.findByTestId('add-filter-button');
+    const clearAllFiltersButton = await screen.findByText(/clear all/i);
+
+    expect(addFilterButton).toBeInTheDocument();
+    expect(clearAllFiltersButton).toBeInTheDocument();
+  });
+
+  it('should hide the filter list when `filters` button is toggled twice', async () => {
+    await render(<Filters {...getDefaultProps()} />);
+    const filtersButton = await screen.findByTestId('filters-button');
+    fireEvent.click(filtersButton);
+    fireEvent.click(filtersButton);
+    const addFilterButton = screen.queryByTestId('add-filter-button');
+    const clearAllFiltersButton = screen.queryByText(/clear all/i);
+
+    expect(addFilterButton).not.toBeInTheDocument();
+    expect(clearAllFiltersButton).not.toBeInTheDocument();
+  });
+
+  it('should call onClearAllRequest when the `clear all` filters button is clicked', async () => {
+    await render(<Filters {...getDefaultProps()} />);
+    const filtersButton = await screen.findByTestId('filters-button');
+    fireEvent.click(filtersButton);
+    const clearAllFiltersButton = await screen.findByText(/clear all/i);
+    fireEvent.click(clearAllFiltersButton);
+    expect(mockOnClearAllRequest).toHaveBeenCalled();
+  });
+
+  it('should render a SelectInput component when the `add filter` button is clicked', async () => {
+    await render(<Filters {...getDefaultProps()} />);
+    const filtersButton = await screen.findByTestId('filters-button');
+    fireEvent.click(filtersButton);
+
+    const addFilterButton = await screen.findByTestId('add-filter-button');
+    fireEvent.click(addFilterButton);
+
+    const selectOption = await screen.findByText(/select/);
+    expect(selectOption).toBeInTheDocument();
+  });
+
+  it('renders the custom search component when passed as a prop', () => {
+    render(
+      <Filters
+        {...getDefaultProps()}
+        renderSearchComponent={() => (
+          <div
+            css={{
+              maxWidth: `${designTokens.constraint13}`,
+              border: `1px solid ${designTokens.colorNeutral90}`,
+            }}
+          >
+            SearchPlaceholderSearchPlaceholder
+          </div>
+        )}
+      />
+    );
+
+    expect(
+      screen.getByText('SearchPlaceholderSearchPlaceholder')
+    ).toBeInTheDocument();
   });
 });
--- packages/components/filters/src/filters.tsx
+++ packages/components/filters/src/filters.tsx
@@ -1,22 +1,223 @@
+// import { forwardRef, type LegacyRef } from 'react';
+import * as Popover from '@radix-ui/react-popover';
+import FlatButton from '@commercetools-uikit/flat-button';
+import SelectInput from '@commercetools-uikit/select-input';
+import {
+  CloseBoldIcon,
+  FilterIcon,
+  PlusBoldIcon,
+} from '@commercetools-uikit/icons';
+import { ReactElement, ReactNode, useState } from 'react';
+import { type TAppliedFilterValue } from './filter-menu/filter-menu';
+import { type TPrimaryButtonProps } from '@commercetools-uikit/primary-button';
+import { type TSelectInputProps } from '@commercetools-uikit/select-input';
+import Spacings from '@commercetools-uikit/spacings/src/spacings';
 import { designTokens } from '@commercetools-uikit/design-system';
+import { css } from '@emotion/react';
+import messages from './messages';
+import { useIntl } from 'react-intl';
+
+// This will be updated in a future PR to use a ui kit button once ui kit buttons can forward refs
+// const AddFilterButtonNEW = forwardRef(function AddFilterButton(
+//   props: Record<string, unknown>,
+//   ref: LegacyRef<HTMLButtonElement>
+// ) {
+//   return (
+//     <button ref={ref} {...props}>
+//       add filters
+//     </button>
+//   );
+// });
+
+type TAppliedFilter = {
+  /**
+   * unique identifier for the filter
+   */
+  filterKey: string;
+  /**
+   * the values applied to this filter by the user
+   */
+  values: TAppliedFilterValue[];
+};
+
+type TFilterConfiguration = {
+  /**
+   * configuration object for the filter menu.
+   */
+  filterMenuConfiguration: {
+    /**
+     * the input in which the user selects values for the filter
+     */
+    renderMenuBody: () => ReactNode;
+    /**
+     * the input in which the user can select which operator should be used for this filter
+     */
+    renderOperatorsInput?: () => ReactElement<TSelectInputProps>;
+    /**
+     * optional button that allows the user to apply selected filter values
+     */
+    renderApplyButton?: () => ReactElement<TPrimaryButtonProps>;
+    /**
+     * controls whether `clear` button in Menu Body Footer is displayed
+     */
+    onClearRequest: Function;
+    /**
+     * controls whether `sort` button in Menu Body Header is displayed
+     */
+    onSortRequest?: Function;
+  };
+  /**
+   * optional key to group filters together.
+   */
+  groupKey?: string;
+  /**
+   * indicates whether filter menu can be removed from filters
+   */
+  isPersistent?: boolean;
+  /**
+   * whether or not the filter is disabled
+   */
+  isDisabled?: boolean;
+  /**
+   * unique identifier for the filter
+   */
+  key: string;
+  /**
+   * formatted message to display the filter name
+   */
+  label: ReactNode;
+};
+
+type TFilterGroupConfiguration = {
+  /**
+   * unique identifier for the filter group
+   */
+  key: string;
+  /**
+   * formatted message to display the filter group name
+   */
+  label: ReactNode;
+};
 
 export type TFiltersProps = {
   /**
-   * Label for the filter
+   * array of applied filters, each containing a unique key and an array of values.
    */
-  label?: string;
+  appliedFilters: TAppliedFilter[];
+  /**
+   * configuration for the available filters.
+   */
+  filters: TFilterConfiguration[];
+  /**
+   * optional configuration for filter groups.
+   */
+  filterGroups?: TFilterGroupConfiguration[];
+  /**
+   * controls the `clear all` (added filters) button from the menu list
+   */
+  onClearAllRequest: () => void;
+  /**
+   * optional callback trigger to add a new filter.
+   */
+  onAddFilterRequest?: Function;
+  /**
+   * function to render a search input, selectable from applicable UI Kit components.
+   */
+  renderSearchComponent: () => ReactNode;
+};
+
+const horizontalDividerStyles = css`
+  width: 100%;
+  height: 1px;
+  border: 1px solid ${designTokens.colorNeutral90};
+  margin-top: ${designTokens.spacing25};
+  margin-bottom: ${designTokens.spacing30};
+`;
+
+const verticalDividerStyles = css`
+  width: 1px;
+  height: ${designTokens.spacing30};
+  background-color: ${designTokens.colorNeutral90};
+  margin: 0 ${designTokens.spacing30} 0 ${designTokens.spacing30}!important;
+`;
+
+const FiltersButton = ({ onAddFilters }: { onAddFilters: () => void }) => {
+  const intl = useIntl();
+  return (
+    <FlatButton
+      data-testid="filters-button"
+      label={intl.formatMessage(messages.filtersButtonLabel)}
+      icon={<FilterIcon />}
+      onClick={onAddFilters}
+    />
+  );
+};
+
+const AddFilterButton = () => {
+  const intl = useIntl();
+  return (
+    <FlatButton
+      data-testid="add-filter-button"
+      label={intl.formatMessage(messages.addFiltersButtonLabel)}
+      icon={<PlusBoldIcon />}
+    />
+  );
+};
+
+//Removes filters from being visible & clears all data
+const ClearAllFiltersButton = ({
+  onClearAllRequest,
+}: {
+  onClearAllRequest: () => void;
+}) => {
+  const intl = useIntl();
+  return (
+    <>
+      <div css={verticalDividerStyles}> </div>
+      <FlatButton
+        icon={<CloseBoldIcon />}
+        label={intl.formatMessage(messages.clearAllFiltersButtonLabel)}
+        onClick={onClearAllRequest}
+        tone="secondary"
+      />
+    </>
+  );
 };
 
 function Filters(props: TFiltersProps) {
+  const [showAddFilters, setShowAddFilters] = useState(false);
+
+  const handleFiltersClick = () => {
+    setShowAddFilters((currState) => !currState);
+  };
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        gap: designTokens.spacing40,
-      }}
-    >
-      {props.label && <span>{props.label}</span>}
-    </div>
+    <>
+      <Spacings.Inline scale="m" alignItems="center">
+        {props.renderSearchComponent()}
+        <FiltersButton onAddFilters={handleFiltersClick} />
+      </Spacings.Inline>
+      <div css={horizontalDividerStyles} />
+      {showAddFilters && (
+        <Spacings.Inline scale="s" alignItems="center">
+          <Popover.Root>
+            <Popover.Trigger asChild>
+              <div>
+                <AddFilterButton />
+              </div>
+            </Popover.Trigger>
+            <Popover.Portal>
+              <Popover.Content>
+                <SelectInput />
+              </Popover.Content>
+            </Popover.Portal>
+          </Popover.Root>
+          <ClearAllFiltersButton onClearAllRequest={props.onClearAllRequest} />
+        </Spacings.Inline>
+      )}
+    </>
   );
 }
+
+Filters.displayName = 'Filters';
 export default Filters;
