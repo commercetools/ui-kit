e48596b1d4b660348664cb72084c16953c32597d
Valorie <47182700+valoriecarli@users.noreply.github.com>
2024-10-18 11:16:31 -0500

    FCT-1182 - Filters - Implement initial `FiltersList` API and styling (#2966)
    * chore(filters): init api and styling for filter component
    
    * chore(changeset): add changeset
    
    * chore(filters): eliminated separate components & style adjustments
    
    * chore(filters): define styling
    
    * chore(filters): remove comments
    
    * fix(filters): fix import oopsie
    
    * chore(filters): add  collapsible motion component

--- packages/components/filters/src/filters.tsx
+++ packages/components/filters/src/filters.tsx
@@ -1,22 +1,203 @@
+import * as Popover from '@radix-ui/react-popover';
+import FlatButton from '@commercetools-uikit/flat-button';
+import SelectInput from '@commercetools-uikit/select-input';
+import {
+  CloseBoldIcon,
+  FilterIcon,
+  PlusBoldIcon,
+} from '@commercetools-uikit/icons';
+import { ReactElement, ReactNode, useState } from 'react';
+import { type TAppliedFilterValue } from './filter-menu/filter-menu';
+import { type TPrimaryButtonProps } from '@commercetools-uikit/primary-button';
+import { type TSelectInputProps } from '@commercetools-uikit/select-input';
+import Spacings from '@commercetools-uikit/spacings';
 import { designTokens } from '@commercetools-uikit/design-system';
+import { css } from '@emotion/react';
+import messages from './messages';
+import { useIntl } from 'react-intl';
+import { menuStyles } from './filter-menu/filter-menu';
+import CollapsibleMotion from '@commercetools-uikit/collapsible-motion';
+
+type TAppliedFilter = {
+  /**
+   * unique identifier for the filter
+   */
+  filterKey: string;
+  /**
+   * the values applied to this filter by the user
+   */
+  values: TAppliedFilterValue[];
+};
+
+type TFilterConfiguration = {
+  /**
+   * configuration object for the filter menu.
+   */
+  filterMenuConfiguration: {
+    /**
+     * the input in which the user selects values for the filter
+     */
+    renderMenuBody: () => ReactNode;
+    /**
+     * the input in which the user can select which operator should be used for this filter
+     */
+    renderOperatorsInput?: () => ReactElement<TSelectInputProps>;
+    /**
+     * optional button that allows the user to apply selected filter values
+     */
+    renderApplyButton?: () => ReactElement<TPrimaryButtonProps>;
+    /**
+     * controls whether `clear` button in Menu Body Footer is displayed
+     */
+    onClearRequest: Function;
+    /**
+     * controls whether `sort` button in Menu Body Header is displayed
+     */
+    onSortRequest?: Function;
+  };
+  /**
+   * optional key to group filters together.
+   */
+  groupKey?: string;
+  /**
+   * indicates whether filter menu can be removed from filters
+   */
+  isPersistent?: boolean;
+  /**
+   * whether or not the filter is disabled
+   */
+  isDisabled?: boolean;
+  /**
+   * unique identifier for the filter
+   */
+  key: string;
+  /**
+   * formatted message to display the filter name
+   */
+  label: ReactNode;
+};
+
+type TFilterGroupConfiguration = {
+  /**
+   * unique identifier for the filter group
+   */
+  key: string;
+  /**
+   * formatted message to display the filter group name
+   */
+  label: ReactNode;
+};
 
 export type TFiltersProps = {
   /**
-   * Label for the filter
+   * array of applied filters, each containing a unique key and an array of values.
    */
-  label?: string;
+  appliedFilters: TAppliedFilter[];
+  /**
+   * configuration for the available filters.
+   */
+  filters: TFilterConfiguration[];
+  /**
+   * optional configuration for filter groups.
+   */
+  filterGroups?: TFilterGroupConfiguration[];
+  /**
+   * controls the `clear all` (added filters) button from the menu list
+   */
+  onClearAllRequest: () => void;
+  /**
+   * optional callback trigger to add a new filter.
+   */
+  onAddFilterRequest?: Function;
+  /**
+   * function to render a search input, selectable from applicable UI Kit components.
+   */
+  renderSearchComponent: () => ReactNode;
 };
 
+const horizontalDividerStyles = css`
+  width: 100%;
+  height: 1px;
+  border: 1px solid ${designTokens.colorNeutral90};
+  margin-top: ${designTokens.spacing25};
+  margin-bottom: ${designTokens.spacing30};
+`;
+
+const verticalDividerStyles = css`
+  width: 1px;
+  height: ${designTokens.spacing30};
+  background-color: ${designTokens.colorNeutral90};
+  margin: 0 ${designTokens.spacing20} 0 ${designTokens.spacing20};
+`;
+
+const menuListStyles = css`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: ${designTokens.spacing20};
+`;
+
 function Filters(props: TFiltersProps) {
+  const intl = useIntl();
+  const [showAddFilters, setShowAddFilters] = useState(false);
+
+  const handleFiltersClick = () => {
+    setShowAddFilters((currState) => !currState);
+  };
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        gap: designTokens.spacing40,
-      }}
-    >
-      {props.label && <span>{props.label}</span>}
-    </div>
+    <>
+      <Spacings.Inline scale="m" alignItems="center">
+        <div css={{ maxWidth: `${designTokens.constraint16}` }}>
+          {props.renderSearchComponent()}
+        </div>
+        <FlatButton
+          data-testid="filters-button"
+          label={intl.formatMessage(messages.filtersButtonLabel)}
+          icon={<FilterIcon />}
+          onClick={handleFiltersClick}
+        />
+      </Spacings.Inline>
+      <div css={horizontalDividerStyles} />
+
+      <CollapsibleMotion
+        isClosed={!showAddFilters}
+        onToggle={() => setShowAddFilters(!showAddFilters)}
+      >
+        {({ registerContentNode, containerStyles }) => (
+          <div style={containerStyles}>
+            <div ref={registerContentNode} css={menuListStyles}>
+              <Popover.Root>
+                <Popover.Trigger asChild>
+                  <div css={{ display: 'inline-flex' }}>
+                    <FlatButton
+                      data-testid="add-filter-button"
+                      label={intl.formatMessage(messages.addFiltersButtonLabel)}
+                      icon={<PlusBoldIcon />}
+                      onClick={() => setShowAddFilters(true)}
+                    />
+                  </div>
+                </Popover.Trigger>
+                <Popover.Portal>
+                  <Popover.Content side="bottom" align="start" css={menuStyles}>
+                    <SelectInput />
+                  </Popover.Content>
+                </Popover.Portal>
+              </Popover.Root>
+              <div css={verticalDividerStyles} />
+              <FlatButton
+                icon={<CloseBoldIcon />}
+                label={intl.formatMessage(messages.clearAllFiltersButtonLabel)}
+                onClick={props.onClearAllRequest}
+                tone="secondary"
+              />
+            </div>
+          </div>
+        )}
+      </CollapsibleMotion>
+    </>
   );
 }
+
+Filters.displayName = 'Filters';
 export default Filters;
