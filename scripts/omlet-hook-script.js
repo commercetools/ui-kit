09ee5ae46506649fd26c27038bbb2de3308b5de4
Michael Salzmann <michael.salzmann@commercetools.com>
2024-12-04 13:56:06 +0100

    chore: fix link to omlet afterScan hook docs

--- scripts/omlet-hook-script.js
+++ scripts/omlet-hook-script.js
@@ -1,7 +1,7 @@
 /**
  * This hook script is executed by "omlet analyze" after the scan phase.
  * It is used to add metadata to the scanned components.
- * @see https://github.com/commercetools/ui-kit
+ * @see https://docs.omlet.dev/discover-omlet/learn-omlet-cli/custom-component-properties#scanning-with-a-hook-script
  * @type {import('@omlet/cli').CliHookModule}
  */
 module.exports = {
