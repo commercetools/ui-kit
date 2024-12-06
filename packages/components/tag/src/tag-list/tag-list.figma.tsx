d71264d1981d3f012aeaa4687d727c9c40a74466
Byron Wall <byron.wall@commercetools.com>
2024-12-06 12:50:15 -0500

    feat(figma connect): implement figma connect and try it out on a few components

---
+++ packages/components/tag/src/tag-list/tag-list.figma.tsx
@@ -0,0 +1,14 @@
+// @ts-nocheck
+import { TagList } from '@commercetools-frontend/ui-kit';
+import figma from '@figma/code-connect';
+
+figma.connect(
+  TagList,
+  'https://www.figma.com/design/UoqmtHSHwxvEZRcbsM4A3Z/CT-product-design-system?node-id=5318%3A8820',
+  {
+    props: {
+      children: figma.children('*'),
+    },
+    example: (props) => <TagList>{props.children}</TagList>,
+  }
+);
