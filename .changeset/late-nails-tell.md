7b1a6cedff51db8bbecddab241f516490ee37428
Byron Wall <87667330+ByronDWall@users.noreply.github.com>
2024-12-10 12:28:05 -0500

    fix(ui kit icons): create icon-utils file and export getIconStyles, TIconProps, and TSVGProps from design system package to enable importing these methods/types in a defined namespace in svgr template, remove unnecessary index.tsx files from all generated icon folders (#3009)

---
+++ .changeset/late-nails-tell.md
@@ -0,0 +1,8 @@
+---
+'@commercetools-uikit/rich-text-utils': minor
+'@commercetools-uikit/checkbox-input': minor
+'@commercetools-uikit/icons': minor
+'@commercetools-uikit/design-system': minor
+---
+
+Add icon-utils to design-system package so that icon styling boilerplate can be standardized and reduced, remove extraneous index.tsx in favor of index.ts files in each generated icon file"
--- packages/components/icons/src/export-types.ts
+++ packages/components/icons/src/export-types.ts
@@ -1,1 +1,17 @@
-export type { Props, SVGProps } from './templates/icon.styles';
+export type Props = {
+  color?:
+    | 'solid'
+    | 'neutral60'
+    | 'surface'
+    | 'info'
+    | 'primary'
+    | 'primary40'
+    | 'warning'
+    | 'error'
+    | 'success';
+  /**
+   * The size of the icon. 'small', 'medium', 'big' have been deprecated in favor of '10', '20', '30', '40'.
+   */
+  size?: 'small' | 'medium' | 'big' | 'scale' | '10' | '20' | '30' | '40';
+};
+export type SVGProps = Props & { className: string };
