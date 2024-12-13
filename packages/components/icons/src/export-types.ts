2a958b3c98bdf2941e69740c561e38622dc4ae9d
Byron Wall <byron.wall@commercetools.com>
2024-12-13 14:30:33 -0500

    feat(icons): don't namespace CustomIcon, LeadingIcon, and InlineSvg to @commercetools-uikit/icons for backwards compatibility reasons

--- packages/components/icons/src/export-types.ts
+++ packages/components/icons/src/export-types.ts
@@ -15,7 +15,3 @@ export type Props = {
   size?: 'small' | 'medium' | 'big' | 'scale' | '10' | '20' | '30' | '40';
 };
 export type SVGProps = Props & { className: string };
-
-export * from './custom-icon/export-types';
-export * from './inline-svg/export-types';
-export * from './leading-icon/export-types';
--- packages/components/icons/src/index.ts
+++ packages/components/icons/src/index.ts
@@ -1,5 +1,2 @@
 export * from './generated';
 export * from './export-types';
-export { default as CustomIcon } from './custom-icon';
-export { default as InlineSvg } from './inline-svg';
-export { default as LeadingIcon } from './leading-icon';
