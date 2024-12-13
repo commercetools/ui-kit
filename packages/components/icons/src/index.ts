bb0981c6b2b3f66d293a447d03a4adbbeded6c85
Byron Wall <byron.wall@commercetools.com>
2024-12-13 13:38:38 -0500

    feat(icons): add CustomIcon LeadingIcon and InlineSvg to @commercetools-ui-kit/icons namespace, remove unnecessary pure annotations from icon template in svgr.config.js, add CONTRIBUTING.md file explaining how to add/update/remove icons in ui kit

--- packages/components/icons/src/index.ts
+++ packages/components/icons/src/index.ts
@@ -1,2 +1,5 @@
 export * from './generated';
 export * from './export-types';
+export { default as CustomIcon } from './custom-icon';
+export { default as InlineSvg } from './inline-svg';
+export { default as LeadingIcon } from './leading-icon';
