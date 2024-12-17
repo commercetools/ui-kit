7b16d86f2c0aca6bd0d1babb4c51d5fb494d1f1c
Tyler Ford <tyler.ford@commercetools.com>
2024-12-17 11:27:59 -0700

    feat(code connect): cleanup and publish generated files to figma

---
+++ packages/components/inputs/money-input/src/money-input.figma.tsx
@@ -0,0 +1,17 @@
+// @ts-nocheck
+import { MoneyInput } from '@commercetools-frontend/ui-kit';
+import figma from '@figma/code-connect';
+
+// REQUIRED: supply node id for the figma component
+figma.connect(
+  MoneyInput,
+  'https://www.figma.com/design/UoqmtHSHwxvEZRcbsM4A3Z/CT-product-design-system?node-id=453%3A26481',
+  {
+    props: {
+      // *This file was generated from a script*
+      // TODO: manually map props here, see https://www.figma.com/code-connect-docs/react/#figmaconnect
+      children: figma.children('*'),
+    },
+    example: (props) => <MoneyInput>{props.children}</MoneyInput>,
+  }
+);
--- packages/components/inputs/select-input/src/select-input.figma.tsx
+++ packages/components/inputs/select-input/src/select-input.figma.tsx
@@ -1,17 +1,17 @@
 // @ts-nocheck
-import { LinkButton } from '@commercetools-frontend/ui-kit';
+import { SelectInput } from '@commercetools-frontend/ui-kit';
 import figma from '@figma/code-connect';
 
 // REQUIRED: supply node id for the figma component
 figma.connect(
-  LinkButton,
-  'https://www.figma.com/design/UoqmtHSHwxvEZRcbsM4A3Z/CT-product-design-system?node-id=<placeholder>',
+  SelectInput,
+  'https://www.figma.com/design/UoqmtHSHwxvEZRcbsM4A3Z/CT-product-design-system?node-id=120%3A17426',
   {
     props: {
       // *This file was generated from a script*
       // TODO: manually map props here, see https://www.figma.com/code-connect-docs/react/#figmaconnect
       children: figma.children('*'),
     },
-    example: (props) => <LinkButton>{props.children}</LinkButton>,
+    example: (props) => <SelectInput>{props.children}</SelectInput>,
   }
 );
