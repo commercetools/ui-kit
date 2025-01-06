0a8fa96e7884101ae6571f4e4dc457e961320e19
Byron Wall <byron.wall@commercetools.com>
2025-01-06 11:37:34 -0500

    chore(react19): remove @testing-library/react-hooks as it is no longer necessary, see 'https://github.com/testing-library/react-hooks-testing-library?tab=readme-ov-file#a-note-about-react-18-support'

--- packages/components/data-table/src/use-manual-column-resizing-reducer.spec.js
+++ packages/components/data-table/src/use-manual-column-resizing-reducer.spec.js
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react';
 import useManualColumnResizing from './use-manual-column-resizing-reducer';
 
 const createMockedTableRef = (headersWidths) => {
