e023d7f820253cb6c770d749218f5fb43c7d58c7
Carlos Cortizas <carlos.martines@commercetools.com>
2025-01-10 14:49:16 +0100

    refactor(test-utils): migrate to typescript

--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable global-require */
 
-import { act } from 'react';
-import { fireEvent as originalFireEvent, render } from '@testing-library/react';
+import { act, type ReactNode } from 'react';
+import { fireEvent, render } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 
-const getMessagesForLocale = (locale) => {
+const getMessagesForLocale = (locale: string) => {
   switch (locale) {
     case 'de':
       return require('@commercetools-uikit/i18n/compiled-data/de.json');
@@ -22,7 +22,7 @@ const getMessagesForLocale = (locale) => {
 };
 
 const customRender = (
-  node,
+  node: ReactNode,
   {
     locale = 'en',
     route = '/',
@@ -45,7 +45,6 @@ const customRender = (
 // re-export everything
 export {
   act,
-  // fireEvent,
   screen,
   waitFor,
   waitForElementToBeRemoved,
@@ -57,6 +56,11 @@ export { customRender as render };
 
 // Custom events for async focus and blur.
 // This helps abstractinv the act() call from the tests.
+type TCustomFireEventApi = typeof fireEvent & {
+  asyncFocus: (element: HTMLElement) => Promise<void>;
+  asyncBlur: (element: HTMLElement) => Promise<void>;
+};
+const originalFireEvent = fireEvent as TCustomFireEventApi;
 originalFireEvent.asyncFocus = (element) => {
   return act(async () => element.focus());
 };
