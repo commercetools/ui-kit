7639115197389185d1066f66d96afa36440a9995
jaikamat <jaipkamat@gmail.com>
2025-02-10 16:11:30 -0500

    chore(uikit): unfill icons

--- packages/components/icons/src/generated/WindowEyeReact.tsx
+++ packages/components/icons/src/generated/WindowEyeReact.tsx
@@ -12,7 +12,6 @@ const SvgWindowEye = (props: TSVGProps) => (
     xmlns="http://www.w3.org/2000/svg"
     width={12}
     height={12}
-    fill="none"
     viewBox="0 0 12 12"
     aria-hidden="true"
     {...props}
