20cfb03aeb1a583ad293f9d5d6a0edda7ba4e9b2
jaikamat <jaipkamat@gmail.com>
2025-07-28 14:54:47 -0400

    chore(ui-kit): change required label indicator color

--- packages/components/label/src/required-indicator.tsx
+++ packages/components/label/src/required-indicator.tsx
@@ -4,7 +4,7 @@ import { designTokens } from '@commercetools-uikit/design-system';
 const RequiredIndicator = () => (
   <em
     css={css`
-      color: ${designTokens.colorWarning};
+      color: ${designTokens.colorWarning40};
       font-style: normal;
       margin-left: 2px;
     `}
