ca60a83b3b0bf6073c2518187ee2b112a9f833d0
Valorie <valorie.carli@commercetools.com>
2026-03-13 10:36:11 -0500

    chore(deps):  update  to eslint v9

--- jest-runner-eslint.config.js
+++ jest-runner-eslint.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   cliOptions: {
-    format: 'stylish',
-    rules: {
-      'import/no-unresolved': 2,
-      'prettier/prettier': [
-        'error',
-        { trailingComma: 'es5', singleQuote: true },
-      ],
-    },
+    format: 'eslint-formatter-pretty',
   },
 };
