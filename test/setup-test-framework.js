f207e5c8313dafef48807584974cbb8677be1dae
Valorie <valorie.carli@commercetools.com>
2026-04-22 15:17:27 -0500

    chore(jest): more tidy up

--- test/setup-test-framework.js
+++ test/setup-test-framework.js
@@ -1,10 +1,3 @@
-// This file is more-or-less also published as the
-// @commercetools-frontend/jest-preset-mc-app preset.
-// However the updated version of it was not yet pushed to NPM,
-// so we copy it to here for now. This file can be removed once the new version
-// of the preset is on NPM.
-
-// react-testing-library setup
 import '@testing-library/jest-dom';
 import 'intl-pluralrules';
 import '@formatjs/intl-relativetimeformat/polyfill';
